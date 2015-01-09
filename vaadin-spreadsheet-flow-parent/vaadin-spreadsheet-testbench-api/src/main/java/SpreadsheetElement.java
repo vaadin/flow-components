c86691ce4615de0e7f4fffcd610b37ba453aa734
Sauli Tähkäpää <sauli@vaadin.com>
2015-01-09 11:04:18 +0200

    Add vaadin-spreadsheet-testbench-api module.
    Change-Id: Id16183d25b5dd884365009e7656845be2a86b829

---
+++ vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-testbench-api/src/main/java/SpreadsheetElement.java
@@ -0,0 +1,6 @@
+import com.vaadin.testbench.TestBenchElement;
+import com.vaadin.testbench.elementsbase.ServerClass;
+
+@ServerClass("com.vaadin.addon.spreadsheet.Spreadsheet")
+public class SpreadsheetElement extends TestBenchElement {
+}
