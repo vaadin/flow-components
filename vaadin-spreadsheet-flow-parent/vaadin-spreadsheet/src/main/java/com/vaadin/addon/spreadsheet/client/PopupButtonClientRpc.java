83248cbb01e4bb0371ba41f758b7a2a756f11d24
Teppo Kurki <teppo.kurki@vaadin.com>
2015-01-22 22:39:12 +0200

    Fixed license data and added license header to source files.
    Change-Id: I2913cf265f252a5e2fdf28ba04cb20798d65f60e

--- vaadin-spreadsheet-flow-parent/vaadin-spreadsheet/src/main/java/com/vaadin/addon/spreadsheet/client/PopupButtonClientRpc.java
+++ vaadin-spreadsheet-flow-parent/vaadin-spreadsheet/src/main/java/com/vaadin/addon/spreadsheet/client/PopupButtonClientRpc.java
@@ -1,5 +1,22 @@
 package com.vaadin.addon.spreadsheet.client;
 
+/*
+ * #%L
+ * Vaadin Spreadsheet
+ * %%
+ * Copyright (C) 2013 - 2015 Vaadin Ltd
+ * %%
+ * This program is available under Commercial Vaadin Add-On License 3.0
+ * (CVALv3).
+ * 
+ * See the file license.html distributed with this software for more
+ * information about licensing.
+ * 
+ * You should have received a copy of the CVALv3 along with this program.
+ * If not, see <http://vaadin.com/license/cval-3>.
+ * #L%
+ */
+
 import com.vaadin.shared.communication.ClientRpc;
 
 public interface PopupButtonClientRpc extends ClientRpc {
--- vaadin-spreadsheet-flow-parent/vaadin-spreadsheet/src/main/java/com/vaadin/addon/spreadsheet/command/SpreadsheetCommand.java
+++ vaadin-spreadsheet-flow-parent/vaadin-spreadsheet/src/main/java/com/vaadin/addon/spreadsheet/command/SpreadsheetCommand.java
@@ -1,5 +1,22 @@
 package com.vaadin.addon.spreadsheet.command;
 
+/*
+ * #%L
+ * Vaadin Spreadsheet
+ * %%
+ * Copyright (C) 2013 - 2015 Vaadin Ltd
+ * %%
+ * This program is available under Commercial Vaadin Add-On License 3.0
+ * (CVALv3).
+ * 
+ * See the file license.html distributed with this software for more
+ * information about licensing.
+ * 
+ * You should have received a copy of the CVALv3 along with this program.
+ * If not, see <http://vaadin.com/license/cval-3>.
+ * #L%
+ */
+
 import org.apache.poi.ss.usermodel.Sheet;
 
 import com.vaadin.addon.spreadsheet.Spreadsheet;
