db838e7edd32df4a17c8aadc80f055b05a3b1315
Guille <alvarezguille@users.noreply.github.com>
2017-03-23 13:41:26 +0200

    Update to use Firefox 45 and update references vaadin/spreadsheet#505
    * Update to FF 45
    
    * Update references for FF45

--- vaadin-spreadsheet-flow-parent/vaadin-spreadsheet/src/test/java/com/vaadin/addon/spreadsheet/test/tb3/VaadinBrowserFactory.java
+++ vaadin-spreadsheet-flow-parent/vaadin-spreadsheet/src/test/java/com/vaadin/addon/spreadsheet/test/tb3/VaadinBrowserFactory.java
@@ -1,6 +1,7 @@
 package com.vaadin.addon.spreadsheet.test.tb3;
 
 import org.openqa.selenium.Platform;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -8,6 +9,9 @@ import com.vaadin.testbench.parallel.Browser;
 import com.vaadin.testbench.parallel.DefaultBrowserFactory;
 
 public class VaadinBrowserFactory extends DefaultBrowserFactory {
+    private static final String FIREFOX_VERSION = "45";
+    private static final String CHROME_VERSION = "40";
+    private static final String PHANTOM_VERSION = "2";
 
     @Override
     public DesiredCapabilities create(Browser browser) {
@@ -21,16 +25,17 @@ public class VaadinBrowserFactory extends DefaultBrowserFactory {
         case IE11:
             return createIE(browser, "11");
         case PHANTOMJS:
-            DesiredCapabilities phantom2 = create(browser, "2", Platform.LINUX);
+            DesiredCapabilities phantom2 = create(browser, PHANTOM_VERSION,
+                    Platform.LINUX);
             // Hack for the test cluster
             phantom2.setCapability("phantomjs.binary.path",
                     "/usr/bin/phantomjs2");
             return phantom2;
         case CHROME:
-            return create(browser, "40", Platform.VISTA);
+            return create(browser, CHROME_VERSION, Platform.VISTA);
         case FIREFOX:
         default:
-            return create(browser, "24", Platform.XP);
+            return createFirefox();
         }
     }
 
@@ -39,6 +44,13 @@ public class VaadinBrowserFactory extends DefaultBrowserFactory {
                 Platform.WINDOWS);
         capabilities.setCapability(
                 InternetExplorerDriver.IE_ENSURE_CLEAN_SESSION, true);
+        return capabilities;
+    }
+
+    private DesiredCapabilities createFirefox() {
+        DesiredCapabilities capabilities = create(Browser.FIREFOX,
+                FIREFOX_VERSION, Platform.WINDOWS);
+        capabilities.setCapability(FirefoxDriver.MARIONETTE, false);
         return capabilities;
     }
 
