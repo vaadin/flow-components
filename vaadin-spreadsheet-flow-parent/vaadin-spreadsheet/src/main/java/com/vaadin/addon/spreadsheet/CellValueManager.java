b794952ff18e387f961d0628e6afd067bfa8916b
Greg Woolsey <greg.woolsey@gmail.com>
2017-04-24 09:49:10 -0700

    Treat string valued formula cells the same as static string for display measurement purposes vaadin/spreadsheet#495
    Fixes: vaadin/spreadsheet#456

--- vaadin-spreadsheet-flow-parent/vaadin-spreadsheet/src/main/java/com/vaadin/addon/spreadsheet/CellValueManager.java
+++ vaadin-spreadsheet-flow-parent/vaadin-spreadsheet/src/main/java/com/vaadin/addon/spreadsheet/CellValueManager.java
@@ -293,7 +293,9 @@ public class CellValueManager implements Serializable {
                                                             .getIndex()),
                                             spreadsheet.getState(false).colW[cell
                                                     .getColumnIndex()] - 10);
-                        } else if (cell.getCellType() != Cell.CELL_TYPE_STRING) {
+                        } else if (cell.getCellType() != Cell.CELL_TYPE_STRING
+                                && (cell.getCellType() == Cell.CELL_TYPE_FORMULA
+                                        && cell.getCachedFormulaResultType() != Cell.CELL_TYPE_STRING)) {
                             cellData.needsMeasure = true;
                         }
                     }
--- vaadin-spreadsheet-flow-parent/vaadin-spreadsheet/src/test/java/com/vaadin/addon/spreadsheet/test/CellOverflowTest.java
+++ vaadin-spreadsheet-flow-parent/vaadin-spreadsheet/src/test/java/com/vaadin/addon/spreadsheet/test/CellOverflowTest.java
@@ -1,10 +1,13 @@
 package com.vaadin.addon.spreadsheet.test;
 
-import com.vaadin.addon.spreadsheet.elements.SheetCellElement;
-import com.vaadin.addon.spreadsheet.elements.SpreadsheetElement;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
+
+import org.junit.Test;
+
+import com.vaadin.addon.spreadsheet.elements.SheetCellElement;
+import com.vaadin.addon.spreadsheet.elements.SpreadsheetElement;
 
 public class CellOverflowTest extends AbstractSpreadsheetTestCase {
 
@@ -37,5 +40,22 @@ public class CellOverflowTest extends AbstractSpreadsheetTestCase {
         a1.setValue("<span>Foo</span>");
 
         compareScreen("htmlText");
+    }
+
+    @Test
+    public void cellOverflow_stringFormula_overflowsAsText()
+            throws IOException {
+        headerPage.createNewSpreadsheet();
+
+        final String valueToTest = "aaaaabbbbccccddddeeee";
+        final SpreadsheetElement spreadsheetElement = $(
+                SpreadsheetElement.class).first();
+
+        spreadsheetElement.getCellAt("B1").setValue(valueToTest);
+
+        final SheetCellElement a1 = spreadsheetElement.getCellAt("A1");
+
+        a1.setValue("=B1");
+        assertEquals(valueToTest, a1.getValue());
     }
 }
