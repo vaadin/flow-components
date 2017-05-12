515ef6a86bcd6ec9f7b6cb3abcbdf516aa3d3a0c
rogozinds <rogozinds@gmail.com>
2017-05-12 14:00:23 +0300

    Update vaadin version. Use viewport annotation in demo. vaadin/vaadin-board-flow#48
    Web-component: vaadin-board

--- integration-tests/src/main/java/com/vaadin/addon/board/testUI/AbstractTestUI.java
+++ integration-tests/src/main/java/com/vaadin/addon/board/testUI/AbstractTestUI.java
@@ -1,8 +1,10 @@
 package com.vaadin.addon.board.testUI;
 
 import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Viewport;
 import com.vaadin.ui.UI;
 
 @Theme("mytheme")
+@Viewport("width=device-width")
 public abstract class AbstractTestUI extends UI {
 }
