879f35a01f67d9a92a287ade01b238e7ed027edd
Sun Zhe <31067185+ZheSun88@users.noreply.github.com>
2017-10-26 11:31:32 +0300

    add password field and demos for both component vaadin/vaadin-text-field-flow#10
    * Inital commit with component class and license
    
    * add blank line at the end
    
    * Merge branch 'master' of https://github.com/vaadin/vaadin-text-field-flow
    
    * Add demo to components
    
    * file formatting
    
    Web-component: vaadin-text-field

---
+++ src/test/java/com/vaadin/ui/textfield/PasswordFieldView.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.ui.textfield;
+
+import com.vaadin.flow.demo.ComponentDemoServer;
+import com.vaadin.flow.demo.DemoView;
+import com.vaadin.router.Route;
+import com.vaadin.ui.button.Button;
+import com.vaadin.ui.common.HtmlImport;
+import com.vaadin.ui.html.Div;
+import com.vaadin.ui.passwordfield.PasswordField;
+
+/**
+ * View for {@link GeneratedVaadinPasswordField} demo.
+ * 
+ * @author Vaadin Ltd
+ */
+@Route("password-field")
+@HtmlImport("bower_components/vaadin-valo-theme/vaadin-text-field.html")
+@HtmlImport("bower_components/vaadin-valo-theme/vaadin-button.html")
+public class PasswordFieldView extends DemoView {
+    
+    public static void main(String... args) throws Exception {
+        new ComponentDemoServer().startServer();
+    }
+	
+	@Override
+    protected void initView() {
+        Div message = new Div();
+
+        // begin-source-example
+        // source-example-heading: Basic password field
+        PasswordField passwordField = new PasswordField();
+        passwordField.setLabel("Password field label");
+        passwordField.setPlaceholder("placeholder text");
+        passwordField.addValueChangeListener(event -> message.setText(
+                String.format("Password field value changed from '%s' to '%s'",
+                        event.getOldValue(), event.getValue())));
+        Button button = new Button("Toggle eye icon if password is hidden",
+                event -> {
+                    if (!passwordField.isPasswordVisible()) {
+                        passwordField.setRevealButtonHidden(
+                                !passwordField.isRevealButtonHidden());
+                    }
+                });
+        // end-source-example
+
+        passwordField.setId("password-field-with-value-change-listener");
+        message.setId("password-field-value");
+        button.setId("toggle-button");
+
+        addCard("Basic password field", button, passwordField, message);
+    }
+}
---
+++ src/test/java/com/vaadin/ui/textfield/TextFieldView.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.ui.textfield;
+
+import com.vaadin.flow.demo.ComponentDemoServer;
+import com.vaadin.flow.demo.DemoView;
+import com.vaadin.router.Route;
+import com.vaadin.ui.common.HtmlImport;
+import com.vaadin.ui.html.Div;
+
+/**
+ * View for {@link GeneratedVaadinTextField} demo.
+ * 
+ * @author Vaadin Ltd
+ */
+@Route("text-field")
+@HtmlImport("bower_components/vaadin-valo-theme/vaadin-text-field.html")
+public class TextFieldView extends DemoView {
+	
+    public static void main(String... args) throws Exception {
+        new ComponentDemoServer().startServer();
+    }
+	
+    @Override
+    protected void initView() {
+        Div message = new Div();
+
+        // begin-source-example
+        // source-example-heading: Basic text field
+        TextField textField = new TextField();
+        textField.setLabel("Text field label");
+        textField.setPlaceholder("placeholder text");
+        textField.addValueChangeListener(event -> message.setText(
+                String.format("Text field value changed from '%s' to '%s'",
+                        event.getOldValue(), event.getValue())));
+        // end-source-example
+
+        textField.setId("text-field-with-value-change-listener");
+        message.setId("text-field-value");
+
+        addCard("Basic text field", textField, message);
+    }
+}
