85c1c1923b63a09ad960c231dc67590d11a6a3cc
Sun Zhe <31067185+ZheSun88@users.noreply.github.com>
2017-10-26 15:06:21 +0300

    Add icons into its own repo vaadin/vaadin-icons-flow#1
    * Add icons into its own repo
    
    + gitignore and License
    + icon classes: Icon.java is specific for vaadin icons, IronIcon can be
    used with other similar icon sets, VaadinIcons is the enumeration of all
    Vaadin icons 
    + demo class
    + styles file for formatting the demo result
    
    * add copyright to icon
    
    Web-component: vaadin-icons

---
+++ src/main/java/com/vaadin/ui/icon/Icon.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.ui.icon;
+
+/**
+ * Component for displaying an icon from the
+ * <a href="https://vaadin.com/icons">Vaadin Icons</a> collection.
+ * 
+ * @author Vaadin Ltd
+ * @see VaadinIcons
+ */
+public class Icon extends IronIcon {
+
+    private static final String ICON_COLLECTION_NAME = "vaadin";
+
+    /**
+     * Creates an Icon component that displays the given icon from
+     * {@link VaadinIcons}.
+     *
+     * @param icon
+     *            the icon to display
+     */
+    public Icon(VaadinIcons icon) {
+        Icon(ICON_COLLECTION_NAME, icon.name().toLowerCase().replace('_', '-'));
+    }
+}
