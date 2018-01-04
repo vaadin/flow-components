872662e62e812d3cb9344541384e57bfcb36f2ab
Sun Zhe <31067185+ZheSun88@users.noreply.github.com>
2018-01-04 15:40:12 +0200

    Correct import in FormLayoutIT vaadin/vaadin-form-layout-flow#22
    Web-component: vaadin-form-layout

--- src/test/java/com/vaadin/flow/component/formlayout/tests/FormLayoutIT.java
+++ src/test/java/com/vaadin/flow/component/formlayout/tests/FormLayoutIT.java
@@ -20,12 +20,12 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 
 import com.vaadin.flow.component.formlayout.demo.FormLayoutView;
 import com.vaadin.flow.demo.ComponentDemoTest;
-import com.vaadin.testbench.By;
 
 /**
  * Integration tests for the {@link FormLayoutView}.
