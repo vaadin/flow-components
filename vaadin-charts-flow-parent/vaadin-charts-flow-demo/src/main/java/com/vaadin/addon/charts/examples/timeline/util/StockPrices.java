e632a4a0c64b59efcb8a4375a95636d2adf7b68a
Sayo Oladeji <oladejioluwasayo@gmail.com>
2018-01-15 18:15:43 +0200

    CHARTS-721: Timeline documentation vaadin/vaadin-charts-flow#80
    * Timeline documentation
    
    * Implemented the CompareMultipleSeries demo because it was referenced in the doc.
    
    * Removed candlestick demo range selector CSS style
    
    Web-component: vaadin-charts

--- vaadin-charts-flow-parent/vaadin-charts-flow-demo/src/main/java/com/vaadin/addon/charts/examples/timeline/util/StockPrices.java
+++ vaadin-charts-flow-parent/vaadin-charts-flow-demo/src/main/java/com/vaadin/addon/charts/examples/timeline/util/StockPrices.java
@@ -111,7 +111,7 @@ public class StockPrices {
     private static List<PriceData> readValueData(String filename) {
         JsonData jsonData = readJsonDataFrom(filename);
 
-        List<PriceData> data = new ArrayList<PriceData>();
+        List<PriceData> data = new ArrayList<>();
         for(int i = 0; i < jsonData.data.length; ++i) {
             Number[] row = jsonData.data[i];
             data.add(new PriceData(row[0].longValue(), row[1].doubleValue()));
@@ -123,7 +123,7 @@ public class StockPrices {
     private static List<OhlcData> readOhlcData(String filename) {
         JsonData jsonData = readJsonDataFrom(filename);
 
-        List<OhlcData> data = new ArrayList<OhlcData>();
+        List<OhlcData> data = new ArrayList<>();
         for(int i = 0; i < jsonData.data.length; ++i) {
             Number[] row = jsonData.data[i];
             data.add(new OhlcData(row[0].longValue(), row[1].doubleValue(),
@@ -134,10 +134,9 @@ public class StockPrices {
     }
 
     private static JsonData readJsonDataFrom(String filename) {
-        JsonData jsonData = null;
         try {
             ObjectMapper mapper = new ObjectMapper();
-            jsonData = mapper.readValue(
+            return mapper.readValue(
                     new InputStreamReader(
                             StockPrices.class.getResourceAsStream(
                                     filename)), JsonData.class);
@@ -145,7 +144,6 @@ public class StockPrices {
         } catch (IOException e) {
             throw new RuntimeException("Cannot read data from "+filename, e);
         }
-        return jsonData;
     }
 
 }
