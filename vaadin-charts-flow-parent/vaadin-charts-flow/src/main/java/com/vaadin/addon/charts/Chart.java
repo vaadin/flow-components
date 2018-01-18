7453b22c2a291f53222196c479322bc21b14cfe1
Sayo Oladeji <oladejioluwasayo@gmail.com>
2018-01-18 13:32:37 +0200

    Timeline chart validation vaadin/vaadin-charts-flow#92
    * Timeline chart validation
    
    Web-component: vaadin-charts

--- vaadin-charts-flow-parent/vaadin-charts-flow/src/main/java/com/vaadin/addon/charts/Chart.java
+++ vaadin-charts-flow-parent/vaadin-charts-flow/src/main/java/com/vaadin/addon/charts/Chart.java
@@ -31,6 +31,9 @@ import com.vaadin.flow.shared.Registration;
 import elemental.json.JsonObject;
 import elemental.json.impl.JreJsonFactory;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Tag("vaadin-chart")
 @HtmlImport("frontend://bower_components/vaadin-charts/vaadin-chart.html")
 public class Chart extends Component {
@@ -39,6 +42,10 @@ public class Chart extends Component {
     private final JreJsonFactory jsonFactory = new JreJsonFactory();
     private final ConfigurationChangeListener changeListener = new ProxyChangeForwarder(
             this, jsonFactory);
+
+    private final static List<ChartType> TIMELINE_NOT_SUPPORTED = Arrays.asList(
+            ChartType.PIE, ChartType.GAUGE, ChartType.SOLIDGAUGE, ChartType.PYRAMID,
+            ChartType.FUNNEL);
 
     /**
      * Creates a new chart with default configuration
@@ -93,6 +100,8 @@ public class Chart extends Component {
      *            defines whether the chart should be redrawn or not
      */
     public void drawChart(boolean resetConfiguration) {
+        validateTimelineAndConfiguration();
+
         final JsonObject configurationNode = jsonFactory
                 .parse(ChartSerialization.toJSON(configuration));
 
@@ -102,12 +111,32 @@ public class Chart extends Component {
 
     /**
      * Determines if the chart is a timeline chart or a normal chart.
+     * The following chart types do not support timeline:
+     * <ul>
+     *     <li>ChartType.PIE</li>
+     *     <li>ChartType.GAUGE</li>
+     *     <li>ChartType.SOLIDGAUGE</li>
+     *     <li>ChartType.PYRAMID</li>
+     *     <li>ChartType.FUNNEL</li>
+     * </ul>
+     * Enabling timeline in these unsupported chart types
+     * results in an <code>IllegalArgumentException</code>
      *
      * @param timeline
      *            true for timeline chart
      */
     public void setTimeline(Boolean timeline) {
         getElement().setProperty("timeline", timeline);
+    }
+
+    private void validateTimelineAndConfiguration() {
+        if (getElement().getProperty("timeline", false)) {
+            final ChartType type = getConfiguration().getChart().getType();
+            if (TIMELINE_NOT_SUPPORTED.contains(type)) {
+                throw new IllegalArgumentException(
+                        "Timeline is not supported for chart type '" + type + "'");
+            }
+        }
     }
 
     /**
