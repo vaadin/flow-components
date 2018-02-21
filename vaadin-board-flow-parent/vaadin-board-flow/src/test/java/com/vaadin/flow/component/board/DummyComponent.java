f6002b100a1c6568e972f8c77328b6e1e7c6e2d7
Artur <artur@vaadin.com>
2018-02-21 11:46:19 +0200

    Add license headers vaadin/vaadin-board-flow#7
    Fixes: 
    
    Web-component: vaadin-board

--- vaadin-board-flow-parent/vaadin-board-flow/src/test/java/com/vaadin/flow/component/board/DummyComponent.java
+++ vaadin-board-flow-parent/vaadin-board-flow/src/test/java/com/vaadin/flow/component/board/DummyComponent.java
@@ -1,5 +1,22 @@
 package com.vaadin.flow.component.board;
 
+/*-
+ * #%L
+ * Vaadin Board for Vaadin 10
+ * %%
+ * Copyright (C) 2017 - 2018 Vaadin Ltd
+ * %%
+ * This program is available under Commercial Vaadin Add-On License 3.0
+ * (CVALv3).
+ * 
+ * See the file license.html distributed with this software for more
+ * information about licensing.
+ * 
+ * You should have received a copy of the CVALv3 along with this program.
+ * If not, see <http://vaadin.com/license/cval-3>.
+ * #L%
+ */
+
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Tag;
 
