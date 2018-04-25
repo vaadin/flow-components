4de9d63d884a3548d80e46b005af0f4377f22d05
Denis <denis@vaadin.com>
2018-04-25 14:19:56 +0300

    Remove HasStyle from implemented interfaces vaadin/vaadin-dialog-flow#64
    * Remove HasStyle from implemented interfaces
    
    Fixes: 
    
    Web-component: vaadin-dialog

--- src/main/java/com/vaadin/flow/component/dialog/GeneratedVaadinDialog.java
+++ src/main/java/com/vaadin/flow/component/dialog/GeneratedVaadinDialog.java
@@ -54,7 +54,7 @@ import com.vaadin.flow.shared.Registration;
 @Tag("vaadin-dialog")
 @HtmlImport("frontend://bower_components/vaadin-dialog/src/vaadin-dialog.html")
 public abstract class GeneratedVaadinDialog<R extends GeneratedVaadinDialog<R>>
-        extends Component implements HasStyle {
+        extends Component {
 
     /**
      * <p>
