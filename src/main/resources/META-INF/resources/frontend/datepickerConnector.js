0a5b7b17a989561f0ec7cd1547247188ba9deee0
caalador <mikael.grankvist@gmail.com>
2018-05-25 09:23:13 +0300

    Fix MS browser parsing problem with localDateToString. vaadin/vaadin-date-picker-flow#81
    * Fix MS browser parsing problem with localDateToString.
    
    Also update regex and clean code
    
    Web-component: vaadin-date-picker

--- src/main/resources/META-INF/resources/frontend/datepickerConnector.js
+++ src/main/resources/META-INF/resources/frontend/datepickerConnector.js
@@ -1,12 +1,12 @@
 window.Vaadin.Flow.datepickerConnector = {
-    initLazy: function(datepicker) {    
+    initLazy: function (datepicker) {
         // Check whether the connector was already initialized for the datepicker
-        if (datepicker.$connector){
+        if (datepicker.$connector) {
             return;
         }
 
         datepicker.$connector = {};
-        datepicker.$connector.oldLocale;
+        let oldLocale;
 
         datepicker.addEventListener('blur', e => {
             if (!e.target.value && e.target.invalid) {
@@ -14,53 +14,92 @@ window.Vaadin.Flow.datepickerConnector = {
             }
         });
 
-        datepicker.$connector.setLocale = function(locale){
-            try{
+        const cleanString = function (string) {
+            // Clear any non ascii characters from the date string,
+            // mainly the LEFT-TO-RIGHT MARK.
+            // This is a problem for many Microsoft browsers where `toLocaleDateString`
+            // adds the LEFT-TO-RIGHT MARK see https://en.wikipedia.org/wiki/Left-to-right_mark
+            return string.replace(/[^\x00-\x7F]/g, "");
+        };
+
+        // Create a Date from our dateObject that doesn't contain setters/getters
+        const generateDate = function(dateObject) {
+            let dateString = `${dateObject.year}-${dateObject.month + 1}-${dateObject.day}`;
+            var parts = /^([-+]\d{1}|\d{2,4}|[-+]\d{6})-(\d{1,2})-(\d{1,2})$/.exec(dateString);
+            if (!parts) {
+                console.warn("Couldn't parse generated date string.");
+                return;
+            }
+
+            // Wrong date (1900-01-01), but with midnight in local time
+            var date = new Date(0, 0);
+            date.setFullYear(parseInt(parts[1], 10));
+            date.setMonth(parseInt(parts[2], 10) - 1);
+            date.setDate(parseInt(parts[3], 10));
+
+            return date;
+        };
+
+        const updateFormat = function() {
+            let inputValue = datepicker._inputValue || '';
+            if (inputValue !== "" && datepicker.i18n.parseDate) {
+                let selectedDate = datepicker.i18n.parseDate(inputValue);
+                if (!selectedDate) {
+                    return;
+                }
+
+                datepicker._selectedDate = selectedDate && generateDate(selectedDate);
+            }
+        };
+
+        datepicker.$connector.setLocale = function (locale) {
+            try {
                 // Check weather the locale is supported or not
                 new Date().toLocaleDateString(locale);
-                if(typeof oldLocale === "undefined") {
+                if (typeof oldLocale === "undefined") {
                     oldLocale = locale;
-                } 
-            } catch (e){
+                }
+            } catch (e) {
                 locale = "en-US";
                 console.warn("The locale is not supported, use default locale setting(en-US).");
             }
 
-            datepicker.i18n.formatDate = function(date){
-                let rawDate = new Date(date.year,date.month,date.day);
-                return rawDate.toLocaleDateString(locale);
-            }
+            datepicker.i18n.formatDate = function (date) {
+                let rawDate = new Date(date.year, date.month, date.day);
+                return cleanString(rawDate.toLocaleDateString(locale));
+            };
 
-            datepicker.i18n.parseDate = function(dateString){
-                if (dateString.length == 0){
+            datepicker.i18n.parseDate = function (dateString) {
+                if (dateString.length == 0) {
                     return;
                 }
 
                 //checking separator which is used in the date
-                let strings = dateString.split(/[\d]/);
-                let separators = strings.filter(string => isNaN(string));
+                let regexMatcher = /[0-9]+(.)[0-9]+\1[0-9]+/;
+                let match = regexMatcher.exec(dateString);
 
-                if (separators.length != 2 || separators[0] != separators[1]){
+                if (match === null || match.length != 2) {
+                    console.error("There was an error when getting the separator for given date string.");
                     return null;
                 } else {
-                    var separator = separators[0];
+                    var separator = match[1];
                 }
 
-                const sample = ["2009","12","31"].join(separator);
+                const sample = ["2009", "12", "31"].join(separator);
                 const sample_parts = sample.split(separator);
-                let date;
-                const targetLocaleDate = new Date(sample).toLocaleDateString(oldLocale).toString();
+                let targetLocaleDate = cleanString(new Date(sample).toLocaleDateString(oldLocale).toString());
 
-                if (targetLocaleDate == sample) {
+                let date;
+                if (targetLocaleDate === sample) {
                     //Date format "YYYY/MM/DD"
                     date = new Date(dateString);
-                } else if (targetLocaleDate == sample.split(separator).reverse().join(separator)){
+                } else if (targetLocaleDate === sample.split(separator).reverse().join(separator)) {
                     //Date format "DD/MM/YYYY"
                     date = new Date(dateString.split(separator).reverse().join(separator));
-                } else if (targetLocaleDate == [sample_parts[1], sample_parts[2], sample_parts[0]].join(separator)){
+                } else if (targetLocaleDate === [sample_parts[1], sample_parts[2], sample_parts[0]].join(separator)) {
                     //Date format "MM/DD/YYYY"
-                    const parts = dateString.split(separator);    
-                    date = new Date([parts[2],parts[0],parts[1]].join(separator));  
+                    const parts = dateString.split(separator);
+                    date = new Date([parts[2], parts[0], parts[1]].join(separator));
                 } else {
                     console.warn("Selected locale is using unsupported date format, which might affect the parsing date.");
                     date = new Date(dateString);
@@ -69,28 +108,13 @@ window.Vaadin.Flow.datepickerConnector = {
                 oldLocale = locale;
 
                 return {
-                    day:date.getDate(),
-                    month:date.getMonth(),
-                    year:date.getFullYear()
+                    day: date.getDate(),
+                    month: date.getMonth(),
+                    year: date.getFullYear()
                 };
-            }
+            };
 
-            let inputValue = datepicker._inputValue || '';
-            if (inputValue !== "" && datepicker.i18n.parseDate) {
-                let selectedDate = datepicker.i18n.parseDate(inputValue);
-                // This is ugly, but the parser is a private method.
-                var parts = /^([-+]\d{1}|\d{2,4}|[-+]\d{6})-(\d{1,2})-(\d{1,2})$/.exec(`${selectedDate.year}-${selectedDate.month + 1}-${selectedDate.day}`);
-                if (!parts) {
-                    return;
-                }
-
-                var date = new Date(0, 0); // Wrong date (1900-01-01), but with midnight in local time
-                date.setFullYear(parseInt(parts[1], 10));
-                date.setMonth(parseInt(parts[2], 10) - 1);
-                date.setDate(parseInt(parts[3], 10));
-
-                datepicker._selectedDate = selectedDate && date;
-            }
+            updateFormat();
         }
     }
 }
