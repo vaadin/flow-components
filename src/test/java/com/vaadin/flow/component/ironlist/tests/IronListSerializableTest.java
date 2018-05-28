839e2598ac47a2550b48a33ad7aec37502aecdb7
Ilia Motornyi <elmot@vaadin.com>
2018-05-28 10:32:25 +0300

    Add serialization test vaadin/vaadin-iron-list-flow#37
    Web-component: vaadin-iron-list

---
+++ src/test/java/com/vaadin/flow/component/ironlist/tests/IronListSerializableTest.java
@@ -0,0 +1,6 @@
+package com.vaadin.flow.component.ironlist.tests;
+
+import com.vaadin.flow.testutil.ClassesSerializableTest;
+
+public class IronListSerializableTest extends ClassesSerializableTest {
+}
