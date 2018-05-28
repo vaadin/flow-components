38424e27207f7ac26019d51a0c96eb205fb0ec1e
Ilia Motornyi <elmot@vaadin.com>
2018-05-28 10:34:12 +0300

    Add serialization test vaadin/vaadin-list-box-flow#26
    Web-component: vaadin-list-box

---
+++ src/test/java/com/vaadin/flow/component/listbox/test/ListBoxSerializableTest.java
@@ -0,0 +1,6 @@
+package com.vaadin.flow.component.listbox.test;
+
+import com.vaadin.flow.testutil.ClassesSerializableTest;
+
+public class ListBoxSerializableTest extends ClassesSerializableTest {
+}
