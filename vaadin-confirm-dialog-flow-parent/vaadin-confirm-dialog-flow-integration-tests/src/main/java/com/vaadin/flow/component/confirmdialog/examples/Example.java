9bb6f9d70cb3c18ef225d61d9556339ad4f10fe9
Johannes Häyry <johannes.hayry@vaadin.com>
2018-06-08 15:20:02 +0300

    Stub project
    Web-component: vaadin-confirm-dialog

---
+++ vaadin-confirm-dialog-flow-parent/vaadin-confirm-dialog-flow-integration-tests/src/main/java/com/vaadin/flow/component/confirmdialog/examples/Example.java
@@ -0,0 +1,12 @@
+package com.vaadin.flow.component.confirmdialog.examples;
+
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.page.BodySize;
+import com.vaadin.flow.router.Route;
+
+@Route("Example")
+@BodySize
+public class Example extends Div {
+    public Example() {
+    }
+}
---
+++ vaadin-confirm-dialog-flow-parent/vaadin-confirm-dialog-flow-integration-tests/src/test/java/com/vaadin/flow/component/confirmdialog/test/AbstractDemoIT.java
@@ -0,0 +1,6 @@
+package com.vaadin.flow.component.confirmdialog.test;
+
+public abstract class AbstractDemoIT extends AbstractParallelTest {
+
+    protected abstract Class<?> getView();
+}
