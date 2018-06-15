b8467a7f4c0207c04f4dd29bb079e141bc454fce
Sun Zhe <31067185+ZheSun88@users.noreply.github.com>
2018-06-15 09:40:56 +0300

    Add Serialization test vaadin/vaadin-notification-flow#55
    * Add Serialization test
    
    * New line
    
    Web-component: vaadin-notification

---
+++ src/test/java/com/vaadin/flow/component/notification/tests/NotificationSerializableTest.java
@@ -0,0 +1,7 @@
+
+package com.vaadin.flow.component.notification.tests;
+
+import com.vaadin.flow.testutil.ClassesSerializableTest;
+
+public class NotificationSerializableTest extends ClassesSerializableTest {
+}
