294e1847e4f5825dbbfd62a3cb151b0808fe1d2f
tulioag <tulio@vaadin.com>
2018-07-11 17:03:47 +0300

    Updated demo. vaadin/vaadin-cookie-consent-flow#18
    Web-component: vaadin-cookie-consent

--- vaadin-cookie-consent-flow-vaadincom-demo/src/main/java/com/vaadin/flow/component/cookieconsent/vaadincom/CookieConsentView.java
+++ vaadin-cookie-consent-flow-vaadincom-demo/src/main/java/com/vaadin/flow/component/cookieconsent/vaadincom/CookieConsentView.java
@@ -1,67 +1,63 @@
 package com.vaadin.flow.component.cookieconsent.vaadincom;
 
-import java.util.Random;
-
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.cookieconsent.CookieConsent;
 import com.vaadin.flow.component.cookieconsent.CookieConsent.Position;
-import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.demo.DemoView;
 import com.vaadin.flow.router.Route;
 
 @SuppressWarnings("serial")
 @Route("vaadin-cookie-consent")
 public class CookieConsentView extends DemoView {
-    private static final Random random = new Random();
+
+    private CookieConsent cookieConsent;
 
     @Override
     protected void initView() {
-        confirmDeleteDialog();
-        meetingStartingAlert();
+        defaultValues();
+        customValues();
     }
 
-    private void meetingStartingAlert() {
+    private void defaultValues() {
         // @formatter:off
 	// begin-source-example
-	// source-example-heading: Default Values Example
+	// source-example-heading: Basic usage
 	CookieConsent dialog = new CookieConsent();
 	// end-source-example
 	// @formatter:on
 
-        createCard("Default Values Example", "Open dialog", dialog);
+        createCard("Basic usage", dialog);
     }
 
-    private void confirmDeleteDialog() {
+    private void customValues() {
         // @formatter:off
 	// begin-source-example
-	// source-example-heading: Customization Example
-	CookieConsent dialog = new CookieConsent("We are using cookies to make your visit here awesome!", "Cool!",
-			"Why?", "https://vaadin.com/terms-of-service", Position.BOTTOM_LEFT);
+	// source-example-heading: Customize texts
+        CookieConsent dialog = new CookieConsent(
+                "We are using cookies to make your visit here awesome!",
+                "Cool!", "Why?", "https://vaadin.com/terms-of-service",
+                Position.BOTTOM_LEFT);
 	// end-source-example
 	// @formatter:on
 
-        dialog.setCookieName(createCookieName());
-        createCard("Customization Example", "Open dialog", dialog);
+        createCard("Customize texts", dialog);
     }
 
-    private void createCard(String heading, String buttonText,
-            CookieConsent element) {
-        final Div messageDiv = createMessageDiv();
-        final Button button = new Button(buttonText);
-        addCard(heading, button, messageDiv);
+    private void createCard(String heading, CookieConsent cookieConsent) {
+        final Button button = new Button("Show consent popup");
+        button.addClickListener(e -> {
+            clearCookieConsent();
+            this.cookieConsent = cookieConsent;
+            add(cookieConsent);
+            getUI().get().getPage().executeJavaScript("$0._show()",
+                    cookieConsent.getElement());
+        });
+        addCard(heading, button);
     }
 
-    private Div createMessageDiv() {
-        final Div message = new Div();
-        message.getStyle().set("whiteSpace", "pre");
-        return message;
-    }
-
-    private static String createCookieName() {
-        final String values = "ABDEFGHIJKLMNQRSTUVXYZ0123456789";
-        return "consentdemo-" + random.ints(0, values.length()).limit(50)
-                .mapToObj(values::charAt).collect(StringBuilder::new,
-                        StringBuilder::append, (a, b) -> a.append(b))
-                .toString();
+    private void clearCookieConsent() {
+        if (cookieConsent != null) {
+            remove(cookieConsent);
+        }
     }
 }
