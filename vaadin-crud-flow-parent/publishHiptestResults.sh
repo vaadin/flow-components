2cb1f3ae18bc74de7ee67af127b598f1a0eb595e
Sayo Oladeji <sayo@vaadin.com>
2018-08-27 16:07:30 +0300

    Remove Hiptest
    Web-component: vaadin-crud

--- vaadin-crud-flow-parent/publishHiptestResults.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-hiptest-publisher -c vaadin-crud-integration-tests/hiptest/hiptest-publisher.config -p "vaadin-crud-integration-tests/target/failsafe-reports/TEST-*hiptest.*.xml" --token=$1 --test-run-id=$2
