2b9b903b27a5be42315e961f98423800c18d79e3
Kirill Bulatov <mail4score@gmail.com>
2018-09-28 13:53:59 +0300

    Update java wrapper to stable version vaadin/vaadin-dialog-flow#99
    Web-component: vaadin-dialog

--- src/main/java/com/vaadin/flow/component/dialog/GeneratedVaadinDialog.java
+++ src/main/java/com/vaadin/flow/component/dialog/GeneratedVaadinDialog.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2000-2017 Vaadin Ltd.
- * 
+ * Copyright 2000-2018 Vaadin Ltd.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -16,20 +16,21 @@
 package com.vaadin.flow.component.dialog;
 
 import javax.annotation.Generated;
-import com.vaadin.flow.component.Tag;
-import com.vaadin.flow.component.dependency.HtmlImport;
-import com.vaadin.flow.component.Synchronize;
+
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.ComponentEvent;
 import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.Synchronize;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.shared.Registration;
-import com.vaadin.flow.component.Component;
 
 /**
  * <p>
  * Description copied from corresponding location in WebComponent:
  * </p>
  * <p>
- * {@code <vaadin-dialog>} is a Polymer 2 element for customized modal dialogs.
+ * {@code <vaadin-dialog>} is a Web Component for customized modal dialogs.
  * </p>
  * <p>
  * &lt;vaadin-dialog opened&gt; &lt;template&gt; Sample dialog &lt;/template&gt;
@@ -48,8 +49,8 @@ import com.vaadin.flow.component.Component;
  * how to apply styles for shadow parts</a>
  * </p>
  */
-@Generated({ "Generator: com.vaadin.generator.ComponentGenerator#1.0-SNAPSHOT",
-        "WebComponent: Vaadin.DialogElement#null", "Flow#1.0-SNAPSHOT" })
+@Generated({ "Generator: com.vaadin.generator.ComponentGenerator#1.1-SNAPSHOT",
+        "WebComponent: Vaadin.DialogElement#null", "Flow#1.1-SNAPSHOT" })
 @Tag("vaadin-dialog")
 @HtmlImport("frontend://bower_components/vaadin-dialog/src/vaadin-dialog.html")
 public abstract class GeneratedVaadinDialog<R extends GeneratedVaadinDialog<R>>
@@ -65,7 +66,7 @@ public abstract class GeneratedVaadinDialog<R extends GeneratedVaadinDialog<R>>
      * This property is synchronized automatically from client side when a
      * 'opened-changed' event happens.
      * </p>
-     * 
+     *
      * @return the {@code opened} property from the webcomponent
      */
     @Synchronize(property = "opened", value = "opened-changed")
@@ -80,12 +81,51 @@ public abstract class GeneratedVaadinDialog<R extends GeneratedVaadinDialog<R>>
      * <p>
      * True if the overlay is currently displayed.
      * </p>
-     * 
+     *
      * @param opened
      *            the boolean value to set
      */
     protected void setOpened(boolean opened) {
         getElement().setProperty("opened", opened);
+    }
+
+    /**
+     * <p>
+     * Description copied from corresponding location in WebComponent:
+     * </p>
+     * <p>
+     * Set the {@code aria-label} attribute for assistive technologies like
+     * screen readers. An {@code undefined} value for this property (the
+     * default) means that the {@code aria-label} attribute is not present at
+     * all.
+     * <p>
+     * This property is not synchronized automatically from the client side, so
+     * the returned value may not be the same as in client side.
+     * </p>
+     *
+     * @return the {@code ariaLabel} property from the webcomponent
+     */
+    protected String getAriaLabelString() {
+        return getElement().getProperty("ariaLabel");
+    }
+
+    /**
+     * <p>
+     * Description copied from corresponding location in WebComponent:
+     * </p>
+     * <p>
+     * Set the {@code aria-label} attribute for assistive technologies like
+     * screen readers. An {@code undefined} value for this property (the
+     * default) means that the {@code aria-label} attribute is not present at
+     * all.
+     * </p>
+     *
+     * @param ariaLabel
+     *            the String value to set
+     */
+    protected void setAriaLabel(String ariaLabel) {
+        getElement().setProperty("ariaLabel",
+                ariaLabel == null ? "" : ariaLabel);
     }
 
     public static class OpenedChangeEvent<R extends GeneratedVaadinDialog<R>>
@@ -105,7 +145,7 @@ public abstract class GeneratedVaadinDialog<R extends GeneratedVaadinDialog<R>>
     /**
      * Adds a listener for {@code opened-changed} events fired by the
      * webcomponent.
-     * 
+     *
      * @param listener
      *            the listener
      * @return a {@link Registration} for removing the event listener
