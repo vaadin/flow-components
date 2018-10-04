efaf3adc2124541ab4e26a0502b54cd369f1c46f
Guille <alvarezguille@users.noreply.github.com>
2018-10-04 05:16:17 -0300

    Use latest flow and flow component base vaadin/vaadin-crud-flow#57
    * Use latest flow and flow component base
    
    * Exclude TreeGrid from serializability test
    
    Web-component: vaadin-crud

--- vaadin-crud-flow-parent/vaadin-crud-flow/src/test/java/com/vaadin/flow/component/crud/CrudSerializableTest.java
+++ vaadin-crud-flow-parent/vaadin-crud-flow/src/test/java/com/vaadin/flow/component/crud/CrudSerializableTest.java
@@ -2,5 +2,13 @@ package com.vaadin.flow.component.crud;
 
 import com.vaadin.flow.testutil.ClassesSerializableTest;
 
+import java.util.stream.Stream;
+
 public class CrudSerializableTest extends ClassesSerializableTest {
+
+    @Override
+    protected Stream<String> getExcludedPatterns() {
+        return Stream.concat(super.getExcludedPatterns(),
+                Stream.of("com\\.vaadin\\.flow\\.component\\.treegrid\\.TreeGrid"));
+    }
 }
