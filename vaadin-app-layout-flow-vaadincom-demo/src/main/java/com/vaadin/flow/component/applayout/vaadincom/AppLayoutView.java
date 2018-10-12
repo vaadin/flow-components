655e405edbe57ca987c7768710fd613ed85cedff
alexberazouski <alex@vaadin.com>
2018-10-12 11:34:44 +0300

    Add link to bakery demo application vaadin/vaadin-app-layout-flow#41
    Ignore exception when old vaadin-tabs < 1.0.5 is used.
    
    Web-component: vaadin-app-layout

--- vaadin-app-layout-flow-vaadincom-demo/src/main/java/com/vaadin/flow/component/applayout/vaadincom/AppLayoutView.java
+++ vaadin-app-layout-flow-vaadincom-demo/src/main/java/com/vaadin/flow/component/applayout/vaadincom/AppLayoutView.java
@@ -4,8 +4,11 @@ import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.AppLayoutMenu;
 import com.vaadin.flow.component.applayout.AppLayoutMenuItem;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.demo.DemoView;
@@ -16,6 +19,9 @@ public class AppLayoutView extends DemoView {
 
     @Override
     protected void initView() {
+        addCard("App Layout usage in a demo application",
+                new Div(new Label("Try out the demo which is using the `vaadin-app-layout-flow` component. "),
+                        new Anchor("https://bakery-flow.demo.vaadin.com/login", "Open demo.")));
         appLayoutWithBrandingLogo();
         appLayoutWithBrandingText();
         appLayoutWithMenus();
@@ -35,42 +41,50 @@ public class AppLayoutView extends DemoView {
     }
 
     private void appLayoutWithBrandingLogo() {
-        // @formatter:off
-        // begin-source-example
-        // source-example-heading: Simple App Layout with brand logo
-        AppLayout appLayout = new AppLayout();
-        AppLayoutMenu menu = appLayout.createMenu();
-        Image img = new Image("https://i.imgur.com/GPpnszs.png", "Vaadin Logo");
-        img.setHeight("44px");
-        appLayout.setBranding(img);
+        try {
+            // @formatter:off
+            // begin-source-example
+            // source-example-heading: Simple App Layout with brand logo
+            AppLayout appLayout = new AppLayout();
+            AppLayoutMenu menu = appLayout.createMenu();
+            Image img = new Image("https://i.imgur.com/GPpnszs.png", "Vaadin Logo");
+            img.setHeight("44px");
+            appLayout.setBranding(img);
 
-        menu.addMenuItems(new AppLayoutMenuItem("Page 1", "page1"),
-            new AppLayoutMenuItem("Page 2", "page2"),
-            new AppLayoutMenuItem("Page 3", "page3"),
-            new AppLayoutMenuItem("Page 4", "page4"));
+            menu.addMenuItems(new AppLayoutMenuItem("Page 1", "page1"),
+                    new AppLayoutMenuItem("Page 2", "page2"),
+                    new AppLayoutMenuItem("Page 3", "page3"),
+                    new AppLayoutMenuItem("Page 4", "page4"));
 
-        Component content = new Span(new H3("Page title"),
-            new Span("Page content"));
-        appLayout.setContent(content);
-        // end-source-example
-        // @formatter:on
+            Component content = new Span(new H3("Page title"),
+                    new Span("Page content"));
+            appLayout.setContent(content);
+            // end-source-example
+            // @formatter:on
+        } catch (IllegalArgumentException e) {
+            // Workaround is needed if the vaadin tabs version < 1.0.5 is used
+        }
 
         addCard("Simple App Layout with brand logo");
     }
 
     private void appLayoutWithMenus() {
-        // @formatter:off
-        // begin-source-example
-        // source-example-heading: App Layout with Action Menu Item
-        AppLayout appLayout = new AppLayout();
-        AppLayoutMenu menu = appLayout.createMenu();
+        try {
+            // @formatter:off
+            // begin-source-example
+            // source-example-heading: App Layout with Action Menu Item
+            AppLayout appLayout = new AppLayout();
+            AppLayoutMenu menu = appLayout.createMenu();
 
-        menu.addMenuItems(
-            new AppLayoutMenuItem(VaadinIcon.USER.create(),"My Profile","profile"),
-            new AppLayoutMenuItem(VaadinIcon.TRENDING_UP.create(),"Trending Topics","trends"),
-            new AppLayoutMenuItem(VaadinIcon.SIGN_OUT.create(),"Sign Out", e -> logout()));
-        // end-source-example
-        // @formatter:on
+            menu.addMenuItems(
+                    new AppLayoutMenuItem(VaadinIcon.USER.create(), "My Profile", "profile"),
+                    new AppLayoutMenuItem(VaadinIcon.TRENDING_UP.create(), "Trending Topics", "trends"),
+                    new AppLayoutMenuItem(VaadinIcon.SIGN_OUT.create(), "Sign Out", e -> logout()));
+            // end-source-example
+            // @formatter:on
+        } catch (IllegalArgumentException e) {
+            // Workaround is needed if the vaadin tabs version < 1.0.5 is used
+        }
 
         addCard("App Layout with Action Menu Item");
     }
