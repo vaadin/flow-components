728219116e925db4540bdbcb3dc8ba6317c60c3f
Denis Anisimov <denis@vaadin.com>
2019-01-16 15:45:23 +0300

    Remove any CVALv3 mentions
    Web-component: vaadin-button

--- vaadin-button-flow-parent/vaadin-button-testbench/src/main/java/com/vaadin/flow/component/button/testbench/ButtonElement.java
+++ vaadin-button-flow-parent/vaadin-button-testbench/src/main/java/com/vaadin/flow/component/button/testbench/ButtonElement.java
@@ -1,14 +1,17 @@
-/**
+/*
  * Copyright 2000-2018 Vaadin Ltd.
  *
- * This program is available under Commercial Vaadin Add-On License 3.0
- * (CVALv3).
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
- * See the file licensing.txt distributed with this software for more
- * information about licensing.
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the license along with this program.
- * If not, see <http://vaadin.com/license/cval-3>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package com.vaadin.flow.component.button.testbench;
 
