6c22ea5801bfea9d74ed70904c6171e91d1d7c97
Bogdan Udrescu <bogdan@vaadin.com>
2019-01-25 13:35:36 +0200

    ListBox component multimodule refactor.
    Web-component: vaadin-list-box

--- vaadin-list-box-flow-parent/vaadin-list-box-flow/src/test/java/com/vaadin/flow/component/listbox/test/ListBoxSerializableTest.java
+++ vaadin-list-box-flow-parent/vaadin-list-box-flow/src/test/java/com/vaadin/flow/component/listbox/test/ListBoxSerializableTest.java
@@ -3,4 +3,5 @@ package com.vaadin.flow.component.listbox.test;
 import com.vaadin.flow.testutil.ClassesSerializableTest;
 
 public class ListBoxSerializableTest extends ClassesSerializableTest {
+
 }
