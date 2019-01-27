462a9704e3ea2e67b0d914a5b1ae272ced8839e6
alexberazouski <alex@vaadin.com>
2019-01-27 13:39:13 +0200

    Add demos vaadin/vaadin-details-flow#8
    Web-component: vaadin-details

--- vaadin-details-flow-parent/vaadin-details-flow-demo/src/main/java/com/vaadin/flow/component/details/vaadincom/DetailsView.java
+++ vaadin-details-flow-parent/vaadin-details-flow-demo/src/main/java/com/vaadin/flow/component/details/vaadincom/DetailsView.java
@@ -1,6 +1,9 @@
 package com.vaadin.flow.component.details.vaadincom;
 
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.details.Details;
+import com.vaadin.flow.component.details.DetailsVariant;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.demo.DemoView;
 import com.vaadin.flow.router.Route;
 
@@ -9,15 +12,53 @@ public class DetailsView extends DemoView {
 
     @Override
     protected void initView() {
-        basicDemo();
+        details();
+        disabledDetails();
+        smallSizeDetails();
+        reverseFilledDetails();
     }
 
-    private void basicDemo() {
+    private void details() {
         // begin-source-example
-        // source-example-heading: Basic Demo
-        Details component = new Details();
+        // source-example-heading: Details
+        Details component = new Details("Expandable Details",
+                new Text("Toggle using mouse, Enter and Space keys."));
         // end-source-example
 
-        addCard("Basic Demo", component);
+        addCard("Details", component);
+    }
+
+    private void disabledDetails() {
+        // begin-source-example
+        // source-example-heading: Disabled Details
+        Details component = new Details(new Span("Disabled heading"),
+                new Span("Always visible content."));
+        component.setEnabled(false);
+        component.setOpened(true);
+        // end-source-example
+
+        addCard("Disabled Details", component);
+    }
+
+    private void smallSizeDetails() {
+        // begin-source-example
+        // source-example-heading: Small Size Details
+        Details component = new Details();
+        component.setSummaryText("Small");
+        component.setContentText("Panel content");
+        component.addThemeVariants(DetailsVariant.SMALL);
+        // end-source-example
+
+        addCard("Small Size Details", component);
+    }
+
+    private void reverseFilledDetails() {
+        // begin-source-example
+        // source-example-heading: Reverse Filled Details
+        Details component = new Details("Reverse Filled", new Text("Panel content"));
+        component.addThemeVariants(DetailsVariant.REVERSE, DetailsVariant.FILLED);
+        // end-source-example
+
+        addCard("Reverse Filled Details", component);
     }
 }
