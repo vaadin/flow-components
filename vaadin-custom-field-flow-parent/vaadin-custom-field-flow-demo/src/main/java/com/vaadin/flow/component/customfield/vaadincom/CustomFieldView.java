7037bd0b9e9ecfd063099f1e74ce40d6b006afb5
Tulio Garcia <tulio@vaadin.com>
2019-01-30 22:28:51 +0200

    Improved tests and demos
    Web-component: vaadin-custom-field

--- vaadin-custom-field-flow-parent/vaadin-custom-field-flow-demo/src/main/java/com/vaadin/flow/component/customfield/vaadincom/CustomFieldView.java
+++ vaadin-custom-field-flow-parent/vaadin-custom-field-flow-demo/src/main/java/com/vaadin/flow/component/customfield/vaadincom/CustomFieldView.java
@@ -2,6 +2,8 @@ package com.vaadin.flow.component.customfield.vaadincom;
 
 import com.vaadin.flow.component.customfield.CustomField;
 import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.timepicker.TimePicker;
 import com.vaadin.flow.demo.DemoView;
 import com.vaadin.flow.router.Route;
@@ -19,9 +21,8 @@ public class CustomFieldView extends DemoView {
     }
 
     private void basicDemo() {
-        DateTimeField component = new DateTimeField();
-
-        addCard("Basic Demo", component);
+        addCard("Basic Demo", new DateTimeField());
+        addCard("Displaying the field value", new SumField());
     }
 
     // begin-source-example
@@ -37,7 +38,8 @@ public class CustomFieldView extends DemoView {
 
         @Override
         protected LocalDateTime generateModelValue() {
-            return LocalDateTime.of(datePicker.getValue(),timePicker.getValue());
+            return LocalDateTime
+                .of(datePicker.getValue(), timePicker.getValue());
         }
 
         @Override
@@ -47,4 +49,37 @@ public class CustomFieldView extends DemoView {
 
     }
     // end-source-example
+
+    // begin-source-example
+    // source-example-heading: Displaying the field value.
+    public static class SumField extends CustomField<Integer> {
+
+        private final TextField firstNumber = new TextField("First number");
+        private final TextField secondNumber = new TextField("Second number");
+        private final Div display = new Div();
+
+        SumField() {
+            super(0);
+            setLabel("Sum");
+            add(firstNumber, secondNumber, display);
+        }
+
+        @Override
+        protected Integer generateModelValue() {
+            try {
+                return Integer.valueOf(firstNumber.getValue()) + Integer
+                    .valueOf(secondNumber.getValue());
+            } catch (NumberFormatException e) {
+                return 0;
+            }
+        }
+
+        @Override
+        protected void setPresentationValue(Integer newPresentationValue) {
+            display.setText("" + newPresentationValue);
+        }
+
+    }
+    // end-source-example
+
 }
--- vaadin-custom-field-flow-parent/vaadin-custom-field-flow/src/test/java/com/vaadin/flow/component/customfield/CustomFieldTest.java
+++ vaadin-custom-field-flow-parent/vaadin-custom-field-flow/src/test/java/com/vaadin/flow/component/customfield/CustomFieldTest.java
@@ -1,34 +1,42 @@
 package com.vaadin.flow.component.customfield;
 
-import com.vaadin.flow.component.AttachEvent;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.function.Consumer;
 
 public class CustomFieldTest {
 
+    private final Object value = new Object();
+
     private CustomField<Object> systemUnderTest;
 
+    private Consumer<Object> consumer;
+
     @Before
+    @SuppressWarnings("unchecked")
     public void setUp() {
+        consumer = Mockito.mock(Consumer.class);
         systemUnderTest = new CustomField<Object>() {
             @Override
             protected Object generateModelValue() {
-                return null;
+                return value;
             }
 
             @Override
             protected void setPresentationValue(Object newPresentationValue) {
-
+                consumer.accept(newPresentationValue);
             }
         };
     }
 
     @Test
-    public void onAttach_init() {
-        systemUnderTest
-            .addAttachListener(e -> new AttachEvent(systemUnderTest, true));
-
-        Assert.assertTrue(true);
+    public void valuesAreUpdated() {
+        Assert.assertNull(systemUnderTest.getValue());
+        systemUnderTest.updateValue();
+        Assert.assertSame(value,systemUnderTest.getValue());
+        Mockito.verify(consumer).accept(value);
     }
 }
