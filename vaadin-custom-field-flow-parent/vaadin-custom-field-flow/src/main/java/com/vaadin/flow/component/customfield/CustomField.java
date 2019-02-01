c41a0365c954e0b3dec2ba962d391177ec9d2d98
Tulio Garcia <tulio@vaadin.com>
2019-02-01 16:54:14 +0200

    Added setPresentationValue with explanation
    Web-component: vaadin-custom-field

--- vaadin-custom-field-flow-parent/vaadin-custom-field-flow/src/main/java/com/vaadin/flow/component/customfield/CustomField.java
+++ vaadin-custom-field-flow-parent/vaadin-custom-field-flow/src/main/java/com/vaadin/flow/component/customfield/CustomField.java
@@ -9,9 +9,9 @@ package com.vaadin.flow.component.customfield;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -74,6 +74,18 @@ public abstract class CustomField<T> extends AbstractField<CustomField<T>, T>
      * @return new value of the field.
      */
     protected abstract T generateModelValue();
+
+    /**
+     * This method should be implemented to set the value of the fields contained
+     * in this custom field according to the value of the parameter.
+     * It can also be use to show the value to the user in some way, like placing it in an element contained on the field.
+     *
+     * {@inheritDoc}
+     *
+     * @param newPresentationValue The new presentation value.
+     */
+    @Override
+    protected abstract void setPresentationValue(T newPresentationValue);
 
     /**
      * Updates both the model and the presentation values.
