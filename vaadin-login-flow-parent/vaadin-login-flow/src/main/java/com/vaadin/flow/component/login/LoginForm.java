76ef377d64a0ac6f8adbecfc730bcc9132487ce9
Tulio Garcia <tulio@vaadin.com>
2019-02-14 17:38:13 +0200

    Copied javadoc to subclasses of AbstractLogin vaadin/vaadin-login-flow#52
    Web-component: vaadin-login

--- vaadin-login-flow-parent/vaadin-login-flow/src/main/java/com/vaadin/flow/component/login/LoginForm.java
+++ vaadin-login-flow-parent/vaadin-login-flow/src/main/java/com/vaadin/flow/component/login/LoginForm.java
@@ -26,6 +26,13 @@ import com.vaadin.flow.component.dependency.HtmlImport;
 /**
  * Server-side component for the {@code <vaadin-login-form>} component.
  *
+ * On {@link LoginForm.LoginEvent} component becomes disabled.
+ * Disabled component stops to process login events, however
+ * the {@link LoginForm.ForgotPasswordEvent} event is processed anyway.
+ * To enable use the {@link com.vaadin.flow.component.HasEnabled#setEnabled(boolean)} method.
+ * Setting error {@link #setError(boolean)} true makes component automatically enabled for
+ * the next login attempt.
+ *
  * @author Vaadin Ltd
  */
 @Tag("vaadin-login-form")
--- vaadin-login-flow-parent/vaadin-login-flow/src/main/java/com/vaadin/flow/component/login/LoginOverlay.java
+++ vaadin-login-flow-parent/vaadin-login-flow/src/main/java/com/vaadin/flow/component/login/LoginOverlay.java
@@ -29,6 +29,13 @@ import com.vaadin.flow.component.dependency.HtmlImport;
 /**
  * Server-side component for the {@code <vaadin-login-overlay>} component.
  *
+ * On {@link LoginForm.LoginEvent} component becomes disabled.
+ * Disabled component stops to process login events, however
+ * the {@link LoginForm.ForgotPasswordEvent} event is processed anyway.
+ * To enable use the {@link com.vaadin.flow.component.HasEnabled#setEnabled(boolean)} method.
+ * Setting error {@link #setError(boolean)} true makes component automatically enabled for
+ * the next login attempt.
+ *
  * @author Vaadin Ltd
  */
 @Tag("vaadin-login-overlay")
