547a0a7df5a9e899ce761e118a0dfb45ffef2c86
Klaudeta <31614592+Klaudeta@users.noreply.github.com>
2019-03-13 08:55:53 +0200

    IT for focusing components inside grid cells using keyboard (flow #4191) vaadin/vaadin-grid-flow#564
    Web-component: vaadin-grid

---
+++ vaadin-grid-flow-parent/vaadin-grid-flow-integration-tests/src/main/java/com/vaadin/flow/component/grid/it/GridDelegateFocusToComponentPage.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2000-2019 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.vaadin.flow.component.grid.it;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.NoTheme;
+
+@Route("delegate-focus-inside-component-renderer")
+@NoTheme
+public class GridDelegateFocusToComponentPage extends Div {
+
+    public GridDelegateFocusToComponentPage() {
+        Grid<String> grid = new Grid<>();
+        grid.setId("grid");
+
+        Div div = new Div();
+        div.setId("info");
+
+        grid.addColumn(item -> item);
+        grid.addComponentColumn(this::buildComplexComponent).setHeader("Components");
+        grid.addComponentColumn(item -> new Button(item, e -> div.setText(item))).setHeader("Components");
+
+        grid.setItems("foo", "bar");
+        add(grid, div);
+    }
+
+    private Div buildComplexComponent(String item){
+        TextField hiddenTextField = new TextField();
+        hiddenTextField.setValue("hidden");
+        hiddenTextField.getElement().setAttribute("hidden", true);
+        TextField disabledTextField = new TextField();
+        disabledTextField.setValue("disabled");
+        disabledTextField.setEnabled(false);
+        TextField focusableTextField = new TextField();
+        focusableTextField.setValue(item);
+        focusableTextField.setId(item);
+        Button nextFocusableComponent = new Button("OK");
+        VerticalLayout layout = new VerticalLayout(hiddenTextField, disabledTextField, focusableTextField, nextFocusableComponent);
+
+        Button anotherFocusableComponent = new Button("NO");
+        return new Div(layout, anotherFocusableComponent);
+    }
+
+
+}
---
+++ vaadin-grid-flow-parent/vaadin-grid-flow-integration-tests/src/test/java/com/vaadin/flow/component/grid/it/GridDelegateFocusToComponentIT.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2000-2019 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.vaadin.flow.component.grid.it;
+
+import com.vaadin.flow.component.grid.testbench.GridElement;
+import com.vaadin.flow.testutil.AbstractComponentIT;
+import com.vaadin.flow.testutil.TestPath;
+import com.vaadin.testbench.TestBenchElement;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+@TestPath("delegate-focus-inside-component-renderer")
+public class GridDelegateFocusToComponentIT extends AbstractComponentIT {
+
+    @Test
+    public void focusTextField(){
+        open();
+
+        // Move focus to the first  cell of second row of grid and press `Enter`
+        new Actions(getDriver()).sendKeys(Keys.TAB, Keys.TAB, Keys.ARROW_RIGHT, Keys.ENTER)
+                .build().perform();
+
+        GridElement grid = $(GridElement.class).id("grid");
+
+        Assert.assertTrue(grid.getCell(0, 1).$("vaadin-text-field").exists());
+
+        // Assert vaadin-text-field with id 'foo' is focused
+        TestBenchElement focusableComponent = grid.getCell(0, 1).$("vaadin-text-field").id("foo");
+        assertElementHasFocus(focusableComponent);
+
+    }
+    @Test
+    public void focusButton(){
+        open();
+
+        // Move focus to the first  cell of third row of grid and press `Enter`
+        new Actions(getDriver()).sendKeys(Keys.TAB, Keys.TAB, Keys.ARROW_RIGHT, Keys.ARROW_RIGHT, Keys.ENTER)
+                .build().perform();
+
+        GridElement grid = $(GridElement.class).id("grid");
+
+        Assert.assertTrue(grid.getCell(0, 2).$("vaadin-button").exists());
+
+        // Assert vaadin-button is focused
+        TestBenchElement button = grid.getCell(0, 2).$("vaadin-button").first();
+        assertElementHasFocus(button);
+
+        // Click on the button using `Enter`
+        new Actions(getDriver()).sendKeys(Keys.ENTER)
+                .build().perform();
+
+
+        WebElement info = findElement(By.id("info"));
+        Assert.assertEquals("foo", info.getText());
+
+
+    }
+
+    private void assertElementHasFocus(WebElement element) {
+        Assert.assertTrue("Element should have focus",
+                (Boolean) executeScript(
+                        "return document.activeElement === arguments[0]",
+                        element));
+    }
+}
