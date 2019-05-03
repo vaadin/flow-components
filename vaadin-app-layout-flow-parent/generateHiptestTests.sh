aa7474381bd46456e566fa90f9fde0db27dd547d
Diego Cardoso <diego@vaadin.com>
2019-05-03 09:11:16 +0300

    Remove hiptest vaadin/vaadin-app-layout-flow#88
    Web-component: vaadin-app-layout

--- vaadin-app-layout-flow-parent/generateHiptestTests.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-hiptest-publisher -c vaadin-app-layout-integration-tests/hiptest/hiptest-publisher.config --only=tests --overriden-templates=vaadin-app-layout-integration-tests/hiptest/templates --filename-pattern=%sIT.java --token=$1 --test-run-id=$2
