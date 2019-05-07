fb351271ae07691d02261a2f077f692bb4e2325a
Guille <alvarezguille@users.noreply.github.com>
2019-05-07 09:59:57 +0300

    Update to stable vaadin app layout vaadin/vaadin-app-layout-flow#90
    Web-component: vaadin-app-layout

--- vaadin-app-layout-flow-parent/vaadin-app-layout-flow/src/main/java/com/vaadin/flow/component/applayout/DrawerToggle.java
+++ vaadin-app-layout-flow-parent/vaadin-app-layout-flow/src/main/java/com/vaadin/flow/component/applayout/DrawerToggle.java
@@ -37,7 +37,7 @@ import com.vaadin.flow.component.dependency.NpmPackage;
  */
 @Tag("vaadin-drawer-toggle")
 @HtmlImport("frontend://bower_components/vaadin-app-layout/src/vaadin-drawer-toggle.html")
-@NpmPackage(value = "@vaadin/vaadin-app-layout", version = "2.0.0-beta1")
+@NpmPackage(value = "@vaadin/vaadin-app-layout", version = "2.0.0")
 @JsModule("@vaadin/vaadin-app-layout/src/vaadin-drawer-toggle.js")
 public class DrawerToggle extends Button {
 }
