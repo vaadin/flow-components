bf71eb0491cf1ee0bb9fa155b8457af4c5044fb7
Guille <alvarezguille@users.noreply.github.com>
2019-05-09 09:26:34 +0300

    Import raw component vaadin/vaadin-time-picker-flow#40
    Web-component: vaadin-time-picker

--- vaadin-time-picker-flow-parent/vaadin-time-picker-flow/src/main/java/com/vaadin/flow/component/timepicker/GeneratedVaadinTimePicker.java
+++ vaadin-time-picker-flow-parent/vaadin-time-picker-flow/src/main/java/com/vaadin/flow/component/timepicker/GeneratedVaadinTimePicker.java
@@ -131,7 +131,7 @@ import elemental.json.JsonObject;
         "WebComponent: Vaadin.TimePickerElement#2.0.0", "Flow#2.0-SNAPSHOT" })
 @Tag("vaadin-time-picker")
 @NpmPackage(value = "@vaadin/vaadin-time-picker", version = "2.0.0")
-@JsModule("@vaadin/vaadin-time-picker/vaadin-time-picker.js")
+@JsModule("@vaadin/vaadin-time-picker/src/vaadin-time-picker.js")
 @HtmlImport("frontend://bower_components/vaadin-time-picker/src/vaadin-time-picker.html")
 public abstract class GeneratedVaadinTimePicker<R extends GeneratedVaadinTimePicker<R, T>, T>
         extends AbstractSinglePropertyField<R, T>
