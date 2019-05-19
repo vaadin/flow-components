d5c7e55c9ef9ee708d19951360bd289fa203f62e
Guille <alvarezguille@users.noreply.github.com>
2019-05-19 20:31:15 +0300

    Remove vaadin-components-testbench vaadin/vaadin-time-picker-flow#44
    Use common interfaces from testbench-core
    Add specific dependency for other components TBElements
    
    Web-component: vaadin-time-picker

--- vaadin-time-picker-flow-parent/vaadin-time-picker-testbench/src/main/java/com/vaadin/flow/component/timepicker/testbench/TimePickerElement.java
+++ vaadin-time-picker-flow-parent/vaadin-time-picker-testbench/src/main/java/com/vaadin/flow/component/timepicker/testbench/TimePickerElement.java
@@ -17,7 +17,7 @@ package com.vaadin.flow.component.timepicker.testbench;
 
 import java.util.Objects;
 
-import com.vaadin.flow.component.common.testbench.HasSelectByText;
+import com.vaadin.testbench.HasSelectByText;
 import com.vaadin.testbench.HasStringValueProperty;
 import com.vaadin.testbench.TestBenchElement;
 import com.vaadin.testbench.elementsbase.Element;
