3990c5814f55bb37cdf3ef8ac9fe22d1c5d53001
Guille <alvarezguille@users.noreply.github.com>
2019-05-19 20:37:34 +0300

    Remove vaadin-components-testbench vaadin/vaadin-radio-button-flow#99
    Use common interfaces from testbench-core
    
    Web-component: vaadin-radio-button

--- vaadin-radio-button-flow-parent/vaadin-radio-button-testbench/src/main/java/com/vaadin/flow/component/radiobutton/testbench/RadioButtonGroupElement.java
+++ vaadin-radio-button-flow-parent/vaadin-radio-button-testbench/src/main/java/com/vaadin/flow/component/radiobutton/testbench/RadioButtonGroupElement.java
@@ -20,7 +20,7 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import com.vaadin.flow.component.common.testbench.HasSelectByText;
+import com.vaadin.testbench.HasSelectByText;
 import com.vaadin.testbench.TestBenchElement;
 import com.vaadin.testbench.elementsbase.Element;
 
