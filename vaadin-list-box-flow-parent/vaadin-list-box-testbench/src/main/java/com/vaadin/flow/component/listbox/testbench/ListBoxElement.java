bee8beef2bf2bf33ed1046734cfb14a5775eb1d7
Tulio Garcia <tulio@vaadin.com>
2019-05-19 20:42:55 +0300

    Remove vaadin-components-testbench dependency vaadin/vaadin-list-box-flow#50
    Web-component: vaadin-list-box

--- vaadin-list-box-flow-parent/vaadin-list-box-testbench/src/main/java/com/vaadin/flow/component/listbox/testbench/ListBoxElement.java
+++ vaadin-list-box-flow-parent/vaadin-list-box-testbench/src/main/java/com/vaadin/flow/component/listbox/testbench/ListBoxElement.java
@@ -15,8 +15,9 @@
  */
 package com.vaadin.flow.component.listbox.testbench;
 
-import com.vaadin.flow.component.common.testbench.HasLabel;
-import com.vaadin.flow.component.common.testbench.HasSelectByText;
+import com.vaadin.testbench.HasLabel;
+
+import com.vaadin.testbench.HasSelectByText;
 import com.vaadin.testbench.TestBenchElement;
 import com.vaadin.testbench.elementsbase.Element;
 import org.openqa.selenium.By;
