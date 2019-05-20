495980c6d7cd7fae485d4c66b54904c48328b4d0
Diego Cardoso <diego@vaadin.com>
2019-05-20 09:48:52 +0300

    Remove components testbench vaadin/vaadin-combo-box-flow#262
    Web-component: vaadin-combo-box

--- vaadin-combo-box-flow-parent/vaadin-combo-box-testbench/src/main/java/com/vaadin/flow/component/combobox/testbench/ComboBoxElement.java
+++ vaadin-combo-box-flow-parent/vaadin-combo-box-testbench/src/main/java/com/vaadin/flow/component/combobox/testbench/ComboBoxElement.java
@@ -17,8 +17,8 @@ package com.vaadin.flow.component.combobox.testbench;
 
 import java.util.List;
 
-import com.vaadin.flow.component.common.testbench.HasLabel;
-import com.vaadin.flow.component.common.testbench.HasSelectByText;
+import com.vaadin.testbench.HasLabel;
+import com.vaadin.testbench.HasSelectByText;
 import com.vaadin.testbench.TestBenchElement;
 import com.vaadin.testbench.elementsbase.Element;
 
