aaa0fb75f87a72cd4551d3b129c7f20d5ef7e6ad
Guille <alvarezguille@users.noreply.github.com>
2019-05-20 10:13:54 +0300

    Remove vaadin-components-testbench vaadin/vaadin-text-field-flow#183
    Use common interfaces from testbench-core
    
    Web-component: vaadin-text-field

--- vaadin-text-field-flow-parent/vaadin-text-field-testbench/src/main/java/com/vaadin/flow/component/textfield/testbench/PasswordFieldElement.java
+++ vaadin-text-field-flow-parent/vaadin-text-field-testbench/src/main/java/com/vaadin/flow/component/textfield/testbench/PasswordFieldElement.java
@@ -15,8 +15,8 @@
  */
 package com.vaadin.flow.component.textfield.testbench;
 
-import com.vaadin.flow.component.common.testbench.HasLabel;
-import com.vaadin.flow.component.common.testbench.HasPlaceholder;
+import com.vaadin.testbench.HasLabel;
+import com.vaadin.testbench.HasPlaceholder;
 import com.vaadin.testbench.HasStringValueProperty;
 import com.vaadin.testbench.TestBenchElement;
 import com.vaadin.testbench.elementsbase.Element;
