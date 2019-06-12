a3d695844b2f0440fce25368e80affc5184fc724
Yuriy Yevstihnyeyev <yuriy@vaadin.com>
2019-06-12 16:04:56 +0300

    Bump web component dependency to 1.0.3 vaadin/vaadin-menu-bar-flow#50
    * Bump web component dependency to 1.0.3
    
    * Bump context-menu webjar dep
    
    Web-component: vaadin-menu-bar

--- vaadin-menu-bar-flow-parent/vaadin-menu-bar-flow/src/main/java/com/vaadin/flow/component/menubar/MenuBar.java
+++ vaadin-menu-bar-flow-parent/vaadin-menu-bar-flow/src/main/java/com/vaadin/flow/component/menubar/MenuBar.java
@@ -48,7 +48,7 @@ import com.vaadin.flow.function.SerializableConsumer;
 @JsModule("./menubarConnector.js")
 @HtmlImport("frontend://bower_components/vaadin-menu-bar/src/vaadin-menu-bar.html")
 @JsModule("@vaadin/vaadin-menu-bar/src/vaadin-menu-bar.js")
-@NpmPackage(value = "@vaadin/vaadin-menu-bar", version = "1.0.1")
+@NpmPackage(value = "@vaadin/vaadin-menu-bar", version = "1.0.3")
 public class MenuBar extends Component
         implements HasMenuItems, HasSize, HasStyle, HasTheme {
 
