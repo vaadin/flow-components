628a1935b6292ccd66235bba6504cab3ba1637c6
Serhii Kulykov <serguey.kulikov@gmail.com>
2019-06-12 14:36:53 +0300

    Bump custom-field npm annotation to 1.0.4 vaadin/vaadin-custom-field-flow#30
    * Bump custom-field npm annotation to 1.0.4
    
    * Update flow and flow-component-base to 2.0.0.rc3
    
    Web-component: vaadin-custom-field

--- vaadin-custom-field-flow-parent/vaadin-custom-field-flow/src/main/java/com/vaadin/flow/component/customfield/CustomField.java
+++ vaadin-custom-field-flow-parent/vaadin-custom-field-flow/src/main/java/com/vaadin/flow/component/customfield/CustomField.java
@@ -48,7 +48,7 @@ import com.vaadin.flow.dom.Element;
  * @param <T> field value type
  */
 @Tag("vaadin-custom-field")
-@NpmPackage(value = "@vaadin/vaadin-custom-field", version="1.0.1")
+@NpmPackage(value = "@vaadin/vaadin-custom-field", version="1.0.4")
 @JsModule("@vaadin/vaadin-custom-field/src/vaadin-custom-field.js")
 @HtmlImport("frontend://bower_components/vaadin-custom-field/src/vaadin-custom-field.html")
 public abstract class CustomField<T> extends AbstractField<CustomField<T>, T>
