21afddbbffce48482df14d0b5cd217af0bc8e380
Serhii Kulykov <serguey.kulikov@gmail.com>
2019-06-18 10:16:45 +0300

    Bump versions, update WC dependency to 1.1.1 vaadin/vaadin-progress-bar-flow#62
    Web-component: vaadin-progress-bar

--- vaadin-progress-bar-flow-parent/vaadin-progress-bar-flow/src/main/java/com/vaadin/flow/component/progressbar/ProgressBar.java
+++ vaadin-progress-bar-flow-parent/vaadin-progress-bar-flow/src/main/java/com/vaadin/flow/component/progressbar/ProgressBar.java
@@ -25,7 +25,7 @@ import com.vaadin.flow.component.dependency.NpmPackage;
  *
  * @author Vaadin Ltd.
  */
-@NpmPackage(value = "@vaadin/vaadin-progress-bar", version = "1.1.0")
+@NpmPackage(value = "@vaadin/vaadin-progress-bar", version = "1.1.1")
 public class ProgressBar extends GeneratedVaadinProgressBar<ProgressBar>
         implements HasSize {
 
