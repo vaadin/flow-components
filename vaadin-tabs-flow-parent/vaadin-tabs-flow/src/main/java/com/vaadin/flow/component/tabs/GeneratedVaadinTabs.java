7d51da182d15f19e207051722e30937793f6dd64
Serhii Kulykov <serguey.kulikov@gmail.com>
2019-06-19 08:56:35 +0300

    Bump versions, update WC dependency to 3.0.2 vaadin/vaadin-tabs-flow#107
    Web-component: vaadin-tabs

--- vaadin-tabs-flow-parent/vaadin-tabs-flow/src/main/java/com/vaadin/flow/component/tabs/GeneratedVaadinTabs.java
+++ vaadin-tabs-flow-parent/vaadin-tabs-flow/src/main/java/com/vaadin/flow/component/tabs/GeneratedVaadinTabs.java
@@ -104,7 +104,7 @@ import com.vaadin.flow.component.dependency.NpmPackage;
 @Tag("vaadin-tabs")
 @HtmlImport("frontend://bower_components/vaadin-tabs/src/vaadin-tabs.html")
 @JsModule("@vaadin/vaadin-tabs/src/vaadin-tabs.js")
-@NpmPackage(value = "@vaadin/vaadin-tabs", version = "3.0.0")
+@NpmPackage(value = "@vaadin/vaadin-tabs", version = "3.0.2")
 public abstract class GeneratedVaadinTabs<R extends GeneratedVaadinTabs<R>>
         extends Component implements HasStyle, HasTheme {
 
