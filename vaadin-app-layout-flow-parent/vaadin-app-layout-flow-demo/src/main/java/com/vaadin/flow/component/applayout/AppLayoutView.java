5a59d001cf56a7e2b9c78dafd8508c701e978da5
Tulio Garcia <tulio@vaadin.com>
2019-06-26 20:12:28 +0300

    Show correct usage of App layout in demos. vaadin/vaadin-app-layout-flow#104
    Web-component: vaadin-app-layout

--- vaadin-app-layout-flow-parent/vaadin-app-layout-flow-demo/src/main/java/com/vaadin/flow/component/applayout/AppLayoutView.java
+++ vaadin-app-layout-flow-parent/vaadin-app-layout-flow-demo/src/main/java/com/vaadin/flow/component/applayout/AppLayoutView.java
@@ -23,72 +23,65 @@ public class AppLayoutView extends DemoView {
         addCard("App Layout usage in a demo application",
                 new Div(new Label("Try out the demo which is using the `vaadin-app-layout-flow` component. "),
                         new Anchor("https://bakery-flow.demo.vaadin.com/login", "Open demo.")));
-        appLayoutBasic();
-        appLayoutDrawerPrimary();
-        appLayoutInMobile();
-        appLayoutInMainView();
-    }
-
-    private void appLayoutBasic() {
-        // @formatter:off
-        // begin-source-example
-        // source-example-heading: Basic App Layout
-        AppLayout appLayout = new AppLayout();
-        Image img = new Image("https://i.imgur.com/GPpnszs.png", "Vaadin Logo");
-        img.setHeight("44px");
-        appLayout.addToNavbar(new DrawerToggle(), img);
-        Tabs tabs = new Tabs(new Tab("Home"), new Tab("About"));
-        tabs.setOrientation(Tabs.Orientation.VERTICAL);
-        appLayout.addToDrawer(tabs);
-        // end-source-example
-        // @formatter:on
-
         addCard("Basic App Layout");
-    }
-
-    private void appLayoutDrawerPrimary() {
-        // @formatter:off
-        // begin-source-example
-        // source-example-heading: App layout with drawer as primary section
-        AppLayout appLayout = new AppLayout();
-        appLayout.setPrimarySection(AppLayout.Section.DRAWER);
-        Image img = new Image("https://i.imgur.com/GPpnszs.png", "Vaadin Logo");
-        img.setHeight("44px");
-        appLayout.addToNavbar(new DrawerToggle(), img);
-        Tabs tabs = new Tabs(new Tab("Home"), new Tab("About"));
-        tabs.setOrientation(Tabs.Orientation.VERTICAL);
-        appLayout.addToDrawer(tabs);
-        // end-source-example
-        // @formatter:on
-
         addCard("App layout with drawer as primary section");
-    }
-
-    private void appLayoutInMobile() {
-        // @formatter:off
-        // begin-source-example
-        // source-example-heading: App layout in mobile
-        AppLayout appLayout = new AppLayout();
-        Image img = new Image("https://i.imgur.com/GPpnszs.png", "Vaadin Logo");
-        img.setHeight("44px");
-        final boolean touchOptimized = true;
-        appLayout.addToNavbar(touchOptimized, new DrawerToggle(), img);
-        Tabs tabs = new Tabs(new Tab("Home"), new Tab("About"));
-        tabs.setOrientation(Tabs.Orientation.VERTICAL);
-        appLayout.addToDrawer(tabs);
-        // end-source-example
-        // @formatter:on
-
         addCard("App layout in mobile");
-    }
-
-    private void appLayoutInMainView() {
-        addCard("App layout as main view");
+        addCard("App layout as main view for PWA");
     }
 
     // @formatter:off
     // begin-source-example
-    // source-example-heading: App layout as main view
+    // source-example-heading: Basic App Layout
+    public class BasicAppLayoutView extends AppLayout {
+        public BasicAppLayoutView() {
+            Image img = new Image("https://i.imgur.com/GPpnszs.png", "Vaadin Logo");
+            img.setHeight("44px");
+            addToNavbar(new DrawerToggle(), img);
+            Tabs tabs = new Tabs(new Tab("Home"), new Tab("About"));
+            tabs.setOrientation(Tabs.Orientation.VERTICAL);
+            addToDrawer(tabs);
+        }
+    }
+    // end-source-example
+    // @formatter:on
+
+    // @formatter:off
+    // begin-source-example
+    // source-example-heading: App layout with drawer as primary section
+    public class AppLayoutWithDrawerView extends AppLayout {
+        public AppLayoutWithDrawerView() {
+            setPrimarySection(AppLayout.Section.DRAWER);
+            Image img = new Image("https://i.imgur.com/GPpnszs.png", "Vaadin Logo");
+            img.setHeight("44px");
+            addToNavbar(new DrawerToggle(), img);
+            Tabs tabs = new Tabs(new Tab("Home"), new Tab("About"));
+            tabs.setOrientation(Tabs.Orientation.VERTICAL);
+            addToDrawer(tabs);
+        }
+    }
+    // end-source-example
+    // @formatter:on
+
+    // @formatter:off
+    // begin-source-example
+    // source-example-heading: App layout in mobile
+    public class AppLayoutInMobile extends AppLayout {
+        public AppLayoutInMobile() {
+            Image img = new Image("https://i.imgur.com/GPpnszs.png", "Vaadin Logo");
+            img.setHeight("44px");
+            final boolean touchOptimized = true;
+            addToNavbar(touchOptimized, new DrawerToggle(), img);
+            Tabs tabs = new Tabs(new Tab("Home"), new Tab("About"));
+            tabs.setOrientation(Tabs.Orientation.VERTICAL);
+            addToDrawer(tabs);
+        }
+    }
+     // end-source-example
+    // @formatter:on
+
+    // @formatter:off
+    // begin-source-example
+    // source-example-heading: App layout as main view for PWA
     @Viewport("width=device-width, minimum-scale=1, initial-scale=1, user-scalable=yes, viewport-fit=cover")
     @PWA(name = "My Application", shortName = "My App")
     class MainAppView extends AppLayout {
