04613e961b26af183ce06db15288deca9cfec25f
Petter Holmström <petter.holmstrom@outlook.com>
2019-07-12 11:13:58 +0300

    Manually sync the client side invalid flag vaadin/vaadin-text-field-flow#204
    * Manually sync the client side invalid flag if the server side flag has already changed before client side validation was disabled.
    
    * Rename test method
    
    * Fix typo in test method name
    
    * Try with some thread sleeping if that would fix the test failing on the build server
    
    * Add more assertions to try to figure out why the test fails on the build server
    
    Web-component: vaadin-text-field

--- vaadin-text-field-flow-parent/vaadin-text-field-flow/src/main/java/com/vaadin/flow/component/textfield/RequiredValidationUtil.java
+++ vaadin-text-field-flow-parent/vaadin-text-field-flow/src/main/java/com/vaadin/flow/component/textfield/RequiredValidationUtil.java
@@ -16,14 +16,17 @@
 package com.vaadin.flow.component.textfield;
 
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.HasValidation;
+import com.vaadin.flow.component.page.PendingJavaScriptResult;
+import com.vaadin.flow.function.SerializableConsumer;
 import com.vaadin.flow.internal.StateNode;
+import elemental.json.JsonValue;
 
 /**
  * Utility class for text field mixin web components to disable/enable client
  * side validation.
  *
  * @author Vaadin Ltd
- *
  */
 final class RequiredValidationUtil {
 
@@ -50,35 +53,52 @@ final class RequiredValidationUtil {
                 + "            if ( textComponent.$validate ){\n"
                 + "                textComponent.validate = textComponent.$validate;\n"
                 + "                delete textComponent.$validate;\n"
-                + "            }\n  }\n }");
+                + "            }\n  }\n }", null);
     }
 
     static void updateClientValidation(boolean requiredIndicatorVisible,
-            Component component) {
+                                       Component component) {
         if (requiredIndicatorVisible) {
-            disableClientValiation(component);
+            disableClientValiation(component, result -> {
+                if (component instanceof HasValidation && ((HasValidation) component).isInvalid()) {
+                    // By default, the invalid flag is always false when a component is created.
+                    // However, if the component is populated and validated in the same HTTP request,
+                    // the server side state may have changed before the JavaScript disabling client
+                    // side validation was properly executed. This can sometimes lead to a situation
+                    // where the client side thinks the value is valid (before client side validation
+                    // was disabled) and the server side thinks the value is invalid. This will lead to
+                    // strange behavior until the two states are synchronized again. To avoid this, we will
+                    // explicitly change the client side value if the server side is invalid.
+                    component.getUI().ifPresent(ui -> ui.getPage().executeJs("$0.invalid = true",
+                            component.getElement()));
+                }
+            });
         } else {
             enableClientValiation(component);
         }
     }
 
-    static void disableClientValiation(Component component) {
+    static void disableClientValiation(Component component, SerializableConsumer<JsonValue> resultHandler) {
         execJS(component,
-                "window.Vaadin.Flow.textConnector.disableClientValidation($0);");
+                "window.Vaadin.Flow.textConnector.disableClientValidation($0);", resultHandler);
     }
 
     static void enableClientValiation(Component component) {
         execJS(component,
-                "window.Vaadin.Flow.textConnector.enableClientValidation($0);");
+                "window.Vaadin.Flow.textConnector.enableClientValidation($0);", null);
     }
 
-    private static void execJS(Component component, String js) {
+    private static void execJS(Component component, String js, SerializableConsumer<JsonValue> resultHandler) {
         StateNode node = component.getElement().getNode();
 
         node.runWhenAttached(ui -> ui.getInternals().getStateTree()
-                .beforeClientResponse(node, context -> ui.getPage()
-                        .executeJavaScript(js, component.getElement())));
-
+                .beforeClientResponse(node, context -> {
+                            PendingJavaScriptResult javaScriptResult = ui.getPage().executeJs(js, component.getElement());
+                            if (resultHandler != null) {
+                                javaScriptResult.then(resultHandler);
+                            }
+                        }
+                ));
     }
 
 }
