9057984eb8dc16176ae50eef708aed53be4909d9
Tulio Garcia <tulio@vaadin.com>
2019-10-11 12:56:29 +0300

    Removed workaround for vaadin/vaadin-select#180 vaadin/vaadin-select-flow#45
    * Fixed vaadin/vaadin-select-flow#43 
    
    Web-component: vaadin-select

--- vaadin-select-flow-parent/vaadin-select-flow/src/main/java/com/vaadin/flow/component/select/Select.java
+++ vaadin-select-flow-parent/vaadin-select-flow/src/main/java/com/vaadin/flow/component/select/Select.java
@@ -692,12 +692,7 @@ public class Select<T> extends GeneratedVaadinSelect<Select<T>, T>
         }
         updateItemEnabled(vaadinItem);
 
-        // currently the updates for the selected item are not reflected to the
-        // input field automatically
-        // https://github.com/vaadin/vaadin-select/issues/180
-        if (item == getValue() && !resetPending) {
-            getElement().executeJavaScript("this._updateValueSlot()");
-        }
+        callClientSideRenderIfNotPending();
     }
 
     private void updateItemEnabled(VaadinItem<T> item) {
@@ -726,6 +721,17 @@ public class Select<T> extends GeneratedVaadinSelect<Select<T>, T>
         keyMapper.removeAll();
         listBox.removeAll();
         clear();
+        callClientSideRenderIfNotPending();
+
+        if (isEmptySelectionAllowed()) {
+            addEmptySelectionItem();
+        }
+        getDataProvider().fetch(new Query<>()).map(this::createItem)
+                .forEach(this::add);
+    }
+
+    private void callClientSideRenderIfNotPending() {
+
         // reset added at this point to avoid unnecessary selected item update
         if (!resetPending) {
             resetPending = true;
@@ -734,12 +740,6 @@ public class Select<T> extends GeneratedVaadinSelect<Select<T>, T>
                 resetPending = false;
             });
         }
-
-        if (isEmptySelectionAllowed()) {
-            addEmptySelectionItem();
-        }
-        getDataProvider().fetch(new Query<>()).map(this::createItem)
-                .forEach(this::add);
     }
 
     private void onDataChange(DataChangeEvent<T> event) {
