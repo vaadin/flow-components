e3656cc259346b1ef09636587b451858ae5b628e
Shadi Khani <43351116+shadikhani@users.noreply.github.com>
2019-11-27 10:43:45 +0200

    Fix Custom Field Flow demos vaadin/vaadin-custom-field-flow#52
    * Fix Custom-Field-Flow demos
    
    Web-component: vaadin-custom-field

--- vaadin-custom-field-flow-parent/vaadin-custom-field-flow-demo/src/main/java/com/vaadin/flow/component/customfield/vaadincom/CustomFieldView.java
+++ vaadin-custom-field-flow-parent/vaadin-custom-field-flow-demo/src/main/java/com/vaadin/flow/component/customfield/vaadincom/CustomFieldView.java
@@ -1,161 +1,175 @@
 package com.vaadin.flow.component.customfield.vaadincom;
 
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.customfield.CustomField;
-import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.Input;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.textfield.NumberField;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.component.timepicker.TimePicker;
+import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.demo.DemoView;
 import com.vaadin.flow.router.Route;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.function.Function;
 
 @Route("vaadin-custom-field")
 public class CustomFieldView extends DemoView {
 
     @Override
     protected void initView() {
-        basicDemo();
+        basicDemo(); // Basic Usage
+        validation(); // Validation
     }
 
     private void basicDemo() {
-        addCard("Single element wrapping", new SingleElementWrapping());
-        addCard("Native input", new NativeInput());
-        addCard("Displaying the field value", new DecimalNumberField());
-        addCard("Custom DateTime Picker", new CustomDateTimePicker());
+        addCard("Basic usage", new PhoneNumberField());
+        addCard("Value change event", new ValueChangeEvent());
+    }
+
+    private void validation() {
+        addCard("Validation", "Required", new MainForRequired());
     }
 
     // begin-source-example
-    // source-example-heading: Displaying the field value
-    public static class DecimalNumberField extends CustomField<BigDecimal> {
+    // source-example-heading: Basic usage
+    public static class PhoneNumberField extends CustomField<String> {
+        private final Select countryCode = new Select();
+        private final TextField areaCode = new TextField();
+        private final TextField subscriberCode = new TextField();
 
-        private final TextField integerPart = new TextField("Integer part");
-        private final TextField fractionalPart = new TextField(
-            "Fractional part");
+        PhoneNumberField() {
+            setLabel("Phone number");
+            countryCode.setItems("+358", "+46", "+34");
+            countryCode.getStyle().set("width", "6em");
+            countryCode.setPlaceholder("Code");
+            areaCode.setPattern("[0-9]*");
+            areaCode.setPreventInvalidInput(true);
+            areaCode.setMaxLength(4);
+            areaCode.setPlaceholder("Area");
+            areaCode.getStyle().set("width", "5em");
+            subscriberCode.setPattern("[0-9]*");
+            subscriberCode.setPreventInvalidInput(true);
+            subscriberCode.setMaxLength(8);
+            subscriberCode.setPlaceholder("Subscriber");
+            HorizontalLayout horizontalLayout = new HorizontalLayout();
+            horizontalLayout.add(countryCode, areaCode, subscriberCode);
+            add(horizontalLayout);
+        }
+
+        @Override
+        protected String generateModelValue() {
+            return countryCode.getValue() + " " + areaCode.getValue() + " "
+                    + subscriberCode.getValue();
+        }
+
+        @Override
+        protected void setPresentationValue(String newPresentationValue) {
+            if (newPresentationValue == null) {
+                areaCode.setValue(null);
+                subscriberCode.setValue(null);
+            }
+        }
+    }
+    // end-source-example
+
+    // begin-source-example
+    // source-example-heading: Value change event
+    public static class ValueChangeEvent extends HorizontalLayout {
         private final Div display = new Div();
 
-        DecimalNumberField() {
-            super(BigDecimal.ZERO);
-            setLabel("Decimal field");
-            // Add a value change listener to display the field value.
-            addValueChangeListener(e -> display.setText("" + e.getValue()));
-            add(integerPart, fractionalPart, display);
-        }
-
-        @Override
-        protected BigDecimal generateModelValue() {
-            try {
-                Function<TextField, Integer> toNumber = t ->
-                   !t.isEmpty() ? Integer.valueOf(t.getValue()) : 0;
-                int i = toNumber.apply(integerPart);
-                int f = toNumber.apply(fractionalPart);
-                return new BigDecimal(i + "." + f);
-            } catch (NumberFormatException e) {
-                return BigDecimal.ZERO;
-            }
-        }
-
-        @Override
-        protected void setPresentationValue(BigDecimal newPresentationValue) {
-            if (newPresentationValue == null) {
-                integerPart.setValue(null);
-                fractionalPart.setValue(null);
-            } else {
-                String[] parts = newPresentationValue.toPlainString()
-                    .split("\\.");
-                integerPart.setValue(parts[0]);
-                fractionalPart.setValue(parts.length > 1 ? parts[1] : null);
-            }
-        }
-
-    }
-    // end-source-example
-
-    // begin-source-example
-    // source-example-heading: Single element wrapping
-    public static class SingleElementWrapping extends CustomField<String> {
-        private final TextField wrappedField = new TextField();
-
-        SingleElementWrapping() {
-            setLabel("Name");
-            add(wrappedField);
-        }
-
-        @Override
-        protected String generateModelValue() {
-            return wrappedField.getValue();
-        }
-
-        @Override
-        protected void setPresentationValue(String newPresentationValue) {
-            wrappedField.setValue(newPresentationValue);
-        }
-    }
-
-    // end-source-example
-
-    // begin-source-example
-    // source-example-heading: Native input
-    public static class NativeInput extends CustomField<String> {
-        private final Input wrappedField = new Input();
-
-        NativeInput() {
-            setLabel("Price");
-            wrappedField.setType("number");
-            add(wrappedField);
-        }
-
-        @Override
-        protected String generateModelValue() {
-            return wrappedField.getValue();
-        }
-
-        @Override
-        protected void setPresentationValue(String newPresentationValue) {
-            wrappedField.setValue(newPresentationValue);
+        ValueChangeEvent() {
+            // The code is reused from the previous example.
+            PhoneNumberField phoneNumberField = new PhoneNumberField();
+            phoneNumberField.addValueChangeListener(
+                    e -> display.setText("Value: " + e.getValue()));
+            add(phoneNumberField, display);
         }
     }
     // end-source-example
 
     // begin-source-example
-    // source-example-heading: Custom DateTime Picker
-    public static class CustomDateTimePicker
-        extends CustomField<LocalDateTime> {
+    // source-example-heading: Required
+    public static class MainForRequired extends HorizontalLayout {
+        private final PriceField priceField = new PriceField();
 
-        private final DatePicker datePicker = new DatePicker();
-        private final TimePicker timePicker = new TimePicker();
+        MainForRequired() {
+            Binder<Product> binder = new Binder<>();
+            Product product = new Product();
+            binder.forField(priceField)
+                    .asRequired("Please fill the price amount")
+                    .bind(Product::getPrice, Product::setPrice);
+            priceField.setLabel("Price");
 
-        CustomDateTimePicker() {
-            setLabel("Start datetime");
-            add(datePicker, timePicker);
+            Button button = new Button("Submit", event -> {
+                if (binder.writeBeanIfValid(product)) {
+                    Notification.show("Submit successful", 2000,
+                            Notification.Position.MIDDLE);
+                }
+            });
+
+            VerticalLayout verticalLayout = new VerticalLayout();
+            verticalLayout.setPadding(false);
+            verticalLayout.add(priceField, button);
+            add(verticalLayout);
+        }
+    }
+
+    public static class PriceField extends CustomField<String> {
+        private final NumberField price = new NumberField();
+        private final Select<String> currency = new Select<>();
+
+        PriceField() {
+            currency.setItems("Euros", "Dollars", "Pounds");
+            currency.getStyle().set("width", "6em");
+
+            HorizontalLayout horizontalLayout = new HorizontalLayout();
+            horizontalLayout.add(price, currency);
+            add(horizontalLayout);
         }
 
         @Override
-        protected LocalDateTime generateModelValue() {
-            final LocalDate date = datePicker.getValue();
-            final LocalTime time = timePicker.getValue();
-            return date != null && time != null ?
-                LocalDateTime.of(date, time) :
-                null;
+        public String generateModelValue() {
+            if (price.getValue() == null || currency.getValue() == null)
+                return null;
+            return price.getValue() + " " + currency.getValue();
         }
 
         @Override
-        protected void setPresentationValue(
-            LocalDateTime newPresentationValue) {
-            datePicker.setValue(newPresentationValue != null ?
-                newPresentationValue.toLocalDate() :
-                null);
-            timePicker.setValue(newPresentationValue != null ?
-                newPresentationValue.toLocalTime() :
-                null);
+        public void setPresentationValue(String newPresentationValue) {
+            String[] part = newPresentationValue.split(" ");
+            price.setValue(Double.valueOf(part[0]));
+            currency.setValue(part.length > 1 ? part[1] : null);
+        }
+    }
 
+    private static class Product {
+        private String name;
+        private String price;
+
+        public Product() {
         }
 
+        public Product(String name, String price) {
+            this.name = name;
+            this.price = price;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getPrice() {
+            return price;
+        }
+
+        public void setPrice(String price) {
+            this.price = price;
+        }
     }
     // end-source-example
 
