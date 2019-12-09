d07e7724b33cd865681942ccc7e8ea8d94bf8e87
Yuriy Yevstihnyeyev <yuriy@vaadin.com>
2019-12-09 14:43:16 +0200

    Improve error handling in connector: wrap with try catch vaadin/vaadin-context-menu-flow#97
    * Improve error handling in connector: wrap with try catch
    
    * Return result from the tryCatchWrapper
    
    * Use Flow tryCatchWrapper
    
    Web-component: vaadin-context-menu

--- vaadin-context-menu-flow-parent/vaadin-context-menu-flow/src/main/resources/META-INF/resources/frontend/contextMenuConnector.js
+++ vaadin-context-menu-flow-parent/vaadin-context-menu-flow/src/main/resources/META-INF/resources/frontend/contextMenuConnector.js
@@ -1,108 +1,114 @@
 // Not using ES6 imports in this file yet because the connector in V14 must
 // still work in Legacy bower projects. See: `contextMenuConnector-es6.js` for
 // the Polymer3 approach.
-window.Vaadin.Flow.Legacy = window.Vaadin.Flow.Legacy || {};
+(function () {
+  const tryCatchWrapper = function (callback) {
+    return window.Vaadin.Flow.tryCatchWrapper(callback, 'Vaadin Context Menu', 'vaadin-context-menu-flow');
+  };
 
-window.Vaadin.Flow.contextMenuConnector = {
+  window.Vaadin.Flow.Legacy = window.Vaadin.Flow.Legacy || {};
 
-  // NOTE: This is for the TARGET component, not for the <vaadin-context-menu> itself
-  init: function(target) {
-    if (target.$contextMenuConnector) {
-      return;
-    }
+  window.Vaadin.Flow.contextMenuConnector = {
 
-    if (window.Polymer) {
+    // NOTE: This is for the TARGET component, not for the <vaadin-context-menu> itself
+    init: tryCatchWrapper(function(target) {
+      if (target.$contextMenuConnector) {
+        return;
+      }
+
+      if (window.Polymer) {
         // Polymer2 approach.
         window.Vaadin.Flow.Legacy.GestureEventListeners = window.Vaadin.Flow.Legacy.GestureEventListeners || Polymer.GestureEventListeners;
         window.Vaadin.Flow.Legacy.Gestures = window.Vaadin.Flow.Legacy.Gestures || Polymer.Gestures;
-    } else if (!window.Vaadin.Flow.Legacy.Gestures) {
-      console.log("ContextMenu is unable to load Polymer helpers.");
-      return;
-    }
+      } else if (!window.Vaadin.Flow.Legacy.Gestures) {
+        console.log("ContextMenu is unable to load Polymer helpers.");
+        return;
+      }
 
-    const GestureEventListeners = window.Vaadin.Flow.Legacy.GestureEventListeners;
-    const Gestures = window.Vaadin.Flow.Legacy.Gestures;
+      const GestureEventListeners = window.Vaadin.Flow.Legacy.GestureEventListeners;
+      const Gestures = window.Vaadin.Flow.Legacy.Gestures;
 
-    target.$contextMenuConnector = {
+      target.$contextMenuConnector = {
 
-      openOnHandler: function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        this.$contextMenuConnector.openEvent = e;
-        let detail = {};
-        if (target.getContextMenuBeforeOpenDetail) {
-          detail = target.getContextMenuBeforeOpenDetail(e);
-        }
-        target.dispatchEvent(new CustomEvent('vaadin-context-menu-before-open', {detail: detail}));
-      },
-
-      updateOpenOn: function(eventType) {
-        this.removeListener();
-        this.openOnEventType = eventType;
-
-        customElements.whenDefined('vaadin-context-menu').then(() => {
-          if (Gestures.gestures[eventType]) {
-            Gestures.addListener(target, eventType, this.openOnHandler);
-          } else {
-            target.addEventListener(eventType, this.openOnHandler);
+        openOnHandler: tryCatchWrapper(function(e) {
+          e.preventDefault();
+          e.stopPropagation();
+          this.$contextMenuConnector.openEvent = e;
+          let detail = {};
+          if (target.getContextMenuBeforeOpenDetail) {
+            detail = target.getContextMenuBeforeOpenDetail(e);
           }
+          target.dispatchEvent(new CustomEvent('vaadin-context-menu-before-open', {detail: detail}));
+        }),
+
+        updateOpenOn: tryCatchWrapper(function(eventType) {
+          this.removeListener();
+          this.openOnEventType = eventType;
+
+          customElements.whenDefined('vaadin-context-menu').then(tryCatchWrapper(() => {
+            if (Gestures.gestures[eventType]) {
+              Gestures.addListener(target, eventType, this.openOnHandler);
+            } else {
+              target.addEventListener(eventType, this.openOnHandler);
+            }
+          }));
+        }),
+
+        removeListener: tryCatchWrapper(function() {
+          if (this.openOnEventType) {
+            if (Gestures.gestures[this.openOnEventType]) {
+              Gestures.removeListener(target, this.openOnEventType, this.openOnHandler);
+            } else {
+              target.removeEventListener(this.openOnEventType, this.openOnHandler);
+            }
+          }
+        }),
+
+        openMenu: tryCatchWrapper(function(contextMenu) {
+          contextMenu.open(this.openEvent);
+        }),
+
+        removeConnector: tryCatchWrapper(function() {
+          this.removeListener();
+          target.$contextMenuConnector = undefined;
+        })
+
+      };
+    }),
+
+    generateItems: tryCatchWrapper(function(menu, appId, nodeId) {
+      menu._containerNodeId = nodeId;
+
+      const getContainer = function(nodeId) {
+        try {
+          return window.Vaadin.Flow.clients[appId].getByNodeId(nodeId);
+        } catch (error) {
+          console.error("Could not get node %s from app %s", nodeId, appId);
+          console.error(error);
+        }
+      };
+
+      const getChildItems = function(parent) {
+        const container = getContainer(parent._containerNodeId);
+        const items = container && Array.from(container.children).map(child => {
+          const item = {component: child, checked: child._checked};
+          if (child.tagName == "VAADIN-CONTEXT-MENU-ITEM" && child._containerNodeId) {
+            item.children = getChildItems(child);
+          }
+          child._item = item;
+          return item;
         });
-      },
+        return items;
+      };
 
-      removeListener: function() {
-        if (this.openOnEventType) {
-          if (Gestures.gestures[this.openOnEventType]) {
-            Gestures.removeListener(target, this.openOnEventType, this.openOnHandler);
-          } else {
-            target.removeEventListener(this.openOnEventType, this.openOnHandler);
-          }
-        }
-      },
+      const items = getChildItems(menu);
+      menu.items = items;
+    }),
 
-      openMenu: function(contextMenu) {
-        contextMenu.open(this.openEvent);
-      },
-
-      removeConnector: function() {
-        this.removeListener();
-        target.$contextMenuConnector = undefined;
+    setChecked: tryCatchWrapper(function(component, checked) {
+      if (component._item) {
+        component._item.checked = checked;
       }
-
-    };
-  },
-
-  generateItems: function(menu, appId, nodeId) {
-    menu._containerNodeId = nodeId;
-
-    const getContainer = function(nodeId) {
-      try {
-        return window.Vaadin.Flow.clients[appId].getByNodeId(nodeId);
-      } catch (error) {
-        console.error("Could not get node %s from app %s", nodeId, appId);
-        console.error(error);
-      }
-    };
-
-    const getChildItems = function(parent) {
-      const container = getContainer(parent._containerNodeId);
-      const items = container && Array.from(container.children).map(child => {
-        const item = {component: child, checked: child._checked};
-        if (child.tagName == "VAADIN-CONTEXT-MENU-ITEM" && child._containerNodeId) {
-          item.children = getChildItems(child);
-        }
-        child._item = item;
-        return item;
-      });
-      return items;
-    };
-
-    const items = getChildItems(menu);
-    menu.items = items;
-  },
-
-  setChecked: function(component, checked) {
-    if (component._item) {
-      component._item.checked = checked;
-    }
-  }
-};
+    })
+  };
+})();
