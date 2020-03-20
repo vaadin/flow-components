25d9e69c458c2e5f291411fa7276795ecb2163eb
Serhii Kulykov <iamkulykov@gmail.com>
2020-03-20 10:54:04 +0200

    Bump web component version to 2.0.7 vaadin/vaadin-grid-pro-flow#86
    Web-component: vaadin-grid-pro

--- vaadin-grid-pro-flow-parent/vaadin-grid-pro-flow/src/main/java/com/vaadin/flow/component/gridpro/GridPro.java
+++ vaadin-grid-pro-flow-parent/vaadin-grid-pro-flow/src/main/java/com/vaadin/flow/component/gridpro/GridPro.java
@@ -50,7 +50,7 @@ import elemental.json.JsonArray;
 import elemental.json.JsonObject;
 
 @Tag("vaadin-grid-pro")
-@NpmPackage(value = "@vaadin/vaadin-grid-pro", version = "2.0.6")
+@NpmPackage(value = "@vaadin/vaadin-grid-pro", version = "2.0.7")
 @JsModule("@vaadin/vaadin-grid-pro/src/vaadin-grid-pro.js")
 @JsModule("@vaadin/vaadin-grid-pro/src/vaadin-grid-pro-edit-column.js")
 @JsModule("./gridProConnector.js")
