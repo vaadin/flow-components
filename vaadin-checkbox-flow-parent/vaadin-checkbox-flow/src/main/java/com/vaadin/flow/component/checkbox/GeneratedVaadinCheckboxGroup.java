047585dde6117271c9ac9b47f0635f8cfc0773a6
Guille <alvarezguille@users.noreply.github.com>
2020-04-07 10:18:24 +0300

    Update flow and wc versions vaadin/vaadin-checkbox-flow#135
    * Update gitignore
    
    * Update parent version
    
    * Bump minor version
    
    * Update WC version
    
    Web-component: vaadin-checkbox

--- vaadin-checkbox-flow-parent/vaadin-checkbox-flow/src/main/java/com/vaadin/flow/component/checkbox/GeneratedVaadinCheckboxGroup.java
+++ vaadin-checkbox-flow-parent/vaadin-checkbox-flow/src/main/java/com/vaadin/flow/component/checkbox/GeneratedVaadinCheckboxGroup.java
@@ -123,7 +123,7 @@ import com.vaadin.flow.shared.Registration;
         "WebComponent: Vaadin.CheckboxGroupElement#2.2.2",
         "Flow#1.1-SNAPSHOT" })
 @Tag("vaadin-checkbox-group")
-@NpmPackage(value = "@vaadin/vaadin-checkbox", version = "2.2.13")
+@NpmPackage(value = "@vaadin/vaadin-checkbox", version = "2.3.0-alpha1")
 @JsModule("@vaadin/vaadin-checkbox/src/vaadin-checkbox-group.js")
 public abstract class GeneratedVaadinCheckboxGroup<R extends GeneratedVaadinCheckboxGroup<R, T>, T>
         extends AbstractSinglePropertyField<R, T>
