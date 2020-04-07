7ac20d0b8a9ab75138d36ac3be956e33a3219a6f
Guille <alvarezguille@users.noreply.github.com>
2020-04-07 16:27:04 +0300

    Update flow and wc versions vaadin/vaadin-tabs-flow#128
    * Update gitignore
    
    * Update parent version
    
    * Bump minor version
    
    * Update WC version
    
    Web-component: vaadin-tabs

--- vaadin-tabs-flow-parent/vaadin-tabs-flow/src/main/java/com/vaadin/flow/component/tabs/GeneratedVaadinTabs.java
+++ vaadin-tabs-flow-parent/vaadin-tabs-flow/src/main/java/com/vaadin/flow/component/tabs/GeneratedVaadinTabs.java
@@ -102,7 +102,7 @@ import com.vaadin.flow.component.dependency.NpmPackage;
         "WebComponent: Vaadin.TabsElement#2.1.1", "Flow#1.1-SNAPSHOT" })
 @Tag("vaadin-tabs")
 @JsModule("@vaadin/vaadin-tabs/src/vaadin-tabs.js")
-@NpmPackage(value = "@vaadin/vaadin-tabs", version = "3.0.5")
+@NpmPackage(value = "@vaadin/vaadin-tabs", version = "3.1.0-alpha1")
 public abstract class GeneratedVaadinTabs<R extends GeneratedVaadinTabs<R>>
         extends Component implements HasStyle, HasTheme {
 
