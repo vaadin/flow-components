cc482690f9d2aefd83037c2de386de5041f44808
Guille <alvarezguille@users.noreply.github.com>
2020-04-07 18:30:25 +0300

    Update flow and wc versions vaadin/vaadin-select-flow#61
    * Update gitignore
    
    * Update parent version
    
    * Bump minor version
    
    * Update WC version
    
    * Fix test
    
    Co-authored-by: Tulio Garcia <tulio@vaadin.com>
    
    Web-component: vaadin-select

--- vaadin-select-flow-parent/vaadin-select-flow-integration-tests/src/test/java/com/vaadin/flow/component/select/test/DetachIT.java
+++ vaadin-select-flow-parent/vaadin-select-flow-integration-tests/src/test/java/com/vaadin/flow/component/select/test/DetachIT.java
@@ -19,7 +19,7 @@ public class DetachIT extends AbstractComponentIT {
      */
     @Test
     public void testReattach_setRenderer() {
-        open(getTestURL());
+        open();
         getCommandExecutor().waitForVaadin();
         $("button").first().click();
         getCommandExecutor().waitForVaadin();
