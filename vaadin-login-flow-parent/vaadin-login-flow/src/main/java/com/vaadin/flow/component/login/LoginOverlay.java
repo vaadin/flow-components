1e61c8369cf4c2b45acbd06229f2a123160ad668
Guille <alvarezguille@users.noreply.github.com>
2020-04-08 13:11:26 +0300

    Update flow and wc versions vaadin/vaadin-login-flow#89
    * Update gitignore
    
    * Update parent version
    
    * Bump minor version
    
    * Update WC version
    
    Web-component: vaadin-login

--- vaadin-login-flow-parent/vaadin-login-flow/src/main/java/com/vaadin/flow/component/login/LoginOverlay.java
+++ vaadin-login-flow-parent/vaadin-login-flow/src/main/java/com/vaadin/flow/component/login/LoginOverlay.java
@@ -40,7 +40,7 @@ import com.vaadin.flow.component.dependency.NpmPackage;
  * @author Vaadin Ltd
  */
 @Tag("vaadin-login-overlay")
-@NpmPackage(value="@vaadin/vaadin-login", version = "1.0.1")
+@NpmPackage(value="@vaadin/vaadin-login", version = "1.1.0-alpha1")
 @JsModule("@vaadin/vaadin-login/src/vaadin-login-overlay.js")
 public class LoginOverlay extends AbstractLogin {
 
