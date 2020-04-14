b47a26a60fc732ec947a3dacbb31e309cb62640b
Guille <alvarezguille@users.noreply.github.com>
2020-04-14 15:56:20 +0300

    Update flow and wc versions vaadin/vaadin-crud-flow#191
    * Update gitignore
    
    * Update parent version
    
    * Bump minor version
    
    * Update WC version
    
    * Update to already existing snapshot dependencies
    
    Web-component: vaadin-crud

--- vaadin-crud-flow-parent/vaadin-crud-flow/src/main/java/com/vaadin/flow/component/crud/Crud.java
+++ vaadin-crud-flow-parent/vaadin-crud-flow/src/main/java/com/vaadin/flow/component/crud/Crud.java
@@ -65,7 +65,7 @@ import java.util.stream.Collectors;
  * @param <E> the bean type
  */
 @Tag("vaadin-crud")
-@NpmPackage(value = "@vaadin/vaadin-crud", version="1.1.0")
+@NpmPackage(value = "@vaadin/vaadin-crud", version="1.2.0-alpha1")
 @JsModule("@vaadin/vaadin-crud/src/vaadin-crud.js")
 @JsModule("@vaadin/vaadin-crud/src/vaadin-crud-edit-column.js")
 public class Crud<E> extends Component implements HasSize, HasTheme {
