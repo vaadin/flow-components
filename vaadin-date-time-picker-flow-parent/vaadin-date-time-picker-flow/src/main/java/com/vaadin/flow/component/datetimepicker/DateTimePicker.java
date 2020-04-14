9a3925e30457262612271dce5111f83917e4f4a4
Tomi Virkki <tomivirkki@users.noreply.github.com>
2020-04-14 11:24:25 +0300

    Update Web Component version to 1.0.0-beta1 vaadin/vaadin-date-time-picker-flow#29
    Web-component: vaadin-date-time-picker

--- vaadin-date-time-picker-flow-parent/vaadin-date-time-picker-flow/src/main/java/com/vaadin/flow/component/datetimepicker/DateTimePicker.java
+++ vaadin-date-time-picker-flow-parent/vaadin-date-time-picker-flow/src/main/java/com/vaadin/flow/component/datetimepicker/DateTimePicker.java
@@ -64,7 +64,7 @@ class DateTimePickerTimePicker
  */
 @Tag("vaadin-date-time-picker")
 @HtmlImport("frontend://bower_components/vaadin-date-time-picker/src/vaadin-date-time-picker.html")
-@NpmPackage(value = "@vaadin/vaadin-date-time-picker", version = "1.0.0-alpha6")
+@NpmPackage(value = "@vaadin/vaadin-date-time-picker", version = "1.0.0-beta1")
 @JsModule("@vaadin/vaadin-date-time-picker/src/vaadin-date-time-picker.js")
 public class DateTimePicker extends AbstractField<DateTimePicker, LocalDateTime>
         implements HasStyle, HasSize, HasTheme, HasValidation,
