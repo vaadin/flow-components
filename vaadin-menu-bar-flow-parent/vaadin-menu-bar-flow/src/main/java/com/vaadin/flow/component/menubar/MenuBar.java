0e6062e13796570a364f75e85e6386fd5d5c15d8
Tomi Virkki <tomivirkki@users.noreply.github.com>
2020-05-04 22:09:59 +0300

    Update Web Component version vaadin/vaadin-menu-bar-flow#84
    Web-component: vaadin-menu-bar

--- vaadin-menu-bar-flow-parent/vaadin-menu-bar-flow/src/main/java/com/vaadin/flow/component/menubar/MenuBar.java
+++ vaadin-menu-bar-flow-parent/vaadin-menu-bar-flow/src/main/java/com/vaadin/flow/component/menubar/MenuBar.java
@@ -44,7 +44,7 @@ import com.vaadin.flow.function.SerializableConsumer;
 @Tag("vaadin-menu-bar")
 @JsModule("./menubarConnector.js")
 @JsModule("@vaadin/vaadin-menu-bar/src/vaadin-menu-bar.js")
-@NpmPackage(value = "@vaadin/vaadin-menu-bar", version = "1.1.0-alpha2")
+@NpmPackage(value = "@vaadin/vaadin-menu-bar", version = "1.1.0")
 public class MenuBar extends Component
         implements HasMenuItems, HasSize, HasStyle, HasTheme {
 
