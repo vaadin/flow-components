a53b908c5c3a65a7dc191c3a52694179cf888c4d
Tomi Virkki <tomivirkki@users.noreply.github.com>
2020-05-06 11:26:34 +0300

    Update Web Component version vaadin/vaadin-grid-flow#962
    Web-component: vaadin-grid

--- vaadin-grid-flow-parent/vaadin-grid-flow/src/main/java/com/vaadin/flow/component/grid/Grid.java
+++ vaadin-grid-flow-parent/vaadin-grid-flow/src/main/java/com/vaadin/flow/component/grid/Grid.java
@@ -124,7 +124,7 @@ import elemental.json.JsonValue;
  *
  */
 @Tag("vaadin-grid")
-@NpmPackage(value = "@vaadin/vaadin-grid", version = "5.6.0-alpha3")
+@NpmPackage(value = "@vaadin/vaadin-grid", version = "5.6.1")
 @JsModule("@vaadin/vaadin-grid/src/vaadin-grid.js")
 @JsModule("@vaadin/vaadin-grid/src/vaadin-grid-column.js")
 @JsModule("@vaadin/vaadin-grid/src/vaadin-grid-sorter.js")
