bda77721e7dce76c5c42e188a4099bc1c582a069
Diego Cardoso <diego@vaadin.com>
2020-05-14 16:29:21 +0300

    Fix RTL styles for big decimal field (#306) vaadin/vaadin-text-field-flow#314
    Web-component: vaadin-text-field

--- vaadin-text-field-flow-parent/vaadin-text-field-flow/src/main/resources/META-INF/resources/frontend/vaadin-big-decimal-field.js
+++ vaadin-text-field-flow-parent/vaadin-text-field-flow/src/main/resources/META-INF/resources/frontend/vaadin-big-decimal-field.js
@@ -29,6 +29,41 @@
                   :host {
                     width: 8em;
                   }
+
+                  :host([dir="rtl"]) [part="input-field"] {
+                    direction: ltr;
+                  }
+
+                  :host([dir="rtl"]) [part="value"]::placeholder {
+                    direction: rtl;
+                  }
+
+                  :host([dir="rtl"]) [part="input-field"] ::slotted(input)::placeholder {
+                    direction: rtl;
+                  }
+
+                  :host([dir="rtl"]) [part="value"]:-ms-input-placeholder,
+                  :host([dir="rtl"]) [part="input-field"] ::slotted(input):-ms-input-placeholder {
+                    direction: rtl;
+                  }
+
+                  :host([dir="rtl"]:not([has-controls])) [part="value"]::placeholder {
+                    text-align: left;
+                  }
+
+                  :host([dir="rtl"]:not([has-controls])) [part="input-field"] ::slotted(input)::placeholder {
+                    text-align: left;
+                  }
+
+                  :host([dir="rtl"]:not([has-controls])) [part="value"]:-ms-input-placeholder,
+                  :host([dir="rtl"]:not([has-controls])) [part="input-field"] ::slotted(input):-ms-input-placeholder {
+                    text-align: left;
+                  }
+
+                  :host([dir="rtl"]) [part="value"],
+                  :host([dir="rtl"]) [part="input-field"] ::slotted(input) {
+                    --_lumo-text-field-overflow-mask-image: linear-gradient(to left, transparent, #000 1.25em) !important;
+                  }
             </style>`;
         }
         return memoizedTemplate;
