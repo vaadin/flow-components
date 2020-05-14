e99cfe35b567305b79ac1e5dc8e12c3b86728255
Tulio Garcia <tulio@vaadin.com>
2020-05-14 18:37:28 +0300

    Use getElementFromShadowRoot
    Web-component: vaadin-charts

--- vaadin-charts-flow-parent/vaadin-charts-flow-integration-tests/src/test/java/com/vaadin/flow/component/charts/tests/DynamicChangingChartIT.java
+++ vaadin-charts-flow-parent/vaadin-charts-flow-integration-tests/src/test/java/com/vaadin/flow/component/charts/tests/DynamicChangingChartIT.java
@@ -18,17 +18,16 @@ public class DynamicChangingChartIT extends AbstractTBTest {
     public void setConfiguration_changes_chart() {
         ChartElement chart = getChartElement();
         findElement(By.id("set_funnel_button")).click();
-        assertTitle(chart,"Sales funnel");
+        assertTitle(chart, "Sales funnel");
         findElement(By.id("set_polar_button")).click();
-        assertTitle(chart,"Polar Chart");
+        assertTitle(chart, "Polar Chart");
         findElement(By.id("set_line_button")).click();
-        assertTitle(chart,"Solar Employment Growth by Sector, 2010-2016");
+        assertTitle(chart, "Solar Employment Growth by Sector, 2010-2016");
     }
 
     private void assertTitle(ChartElement chart, String expectedTitle) {
-        WebElement shadowRoot = (WebElement) executeScript(
-            "return arguments[0].shadowRoot", chart);
         Assert.assertEquals(expectedTitle,
-            shadowRoot.findElement(By.className("highcharts-title")).getText());
+            getElementFromShadowRoot(chart, By.className("highcharts-title"))
+                .getText());
     }
 }
