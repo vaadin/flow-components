91ddb65c544af03695dd346e8d0f3f3b9da27e25
Serhii Kulykov <iamkulykov@gmail.com>
2020-06-02 15:25:20 +0300

    Update Flow and web component version vaadin/vaadin-details-flow#50
    Web-component: vaadin-details

--- vaadin-details-flow-parent/vaadin-details-flow/src/main/java/com/vaadin/flow/component/details/Details.java
+++ vaadin-details-flow-parent/vaadin-details-flow/src/main/java/com/vaadin/flow/component/details/Details.java
@@ -39,7 +39,7 @@ import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.shared.Registration;
 
 @Tag("vaadin-details")
-@NpmPackage(value = "@vaadin/vaadin-details", version = "1.1.0")
+@NpmPackage(value = "@vaadin/vaadin-details", version = "1.2.0-alpha1")
 @JsModule("@vaadin/vaadin-details/src/vaadin-details.js")
 public class Details extends Component implements HasEnabled, HasTheme {
 
