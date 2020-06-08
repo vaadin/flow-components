1fb169eb644378edc3b899e43fd7c3830c3d3ffc
Serhii Kulykov <iamkulykov@gmail.com>
2020-06-08 16:48:24 +0300

    Update Flow and web component version vaadin/vaadin-login-flow#95
    Web-component: vaadin-login

--- vaadin-login-flow-parent/vaadin-login-flow/src/main/java/com/vaadin/flow/component/login/LoginOverlay.java
+++ vaadin-login-flow-parent/vaadin-login-flow/src/main/java/com/vaadin/flow/component/login/LoginOverlay.java
@@ -40,7 +40,7 @@ import com.vaadin.flow.component.dependency.NpmPackage;
  * @author Vaadin Ltd
  */
 @Tag("vaadin-login-overlay")
-@NpmPackage(value="@vaadin/vaadin-login", version = "1.1.0")
+@NpmPackage(value="@vaadin/vaadin-login", version = "1.2.0-alpha2")
 @JsModule("@vaadin/vaadin-login/src/vaadin-login-overlay.js")
 public class LoginOverlay extends AbstractLogin {
 
