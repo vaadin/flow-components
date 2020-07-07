33fc222e4df8a436bf78213d21904cf039f53521
ZheSun88 <zhe@vaadin.com>
2020-07-07 14:44:27 +0300

    remove extra module

--- vaadin-checkbox-flow/vaadin-checkbox-flow/src/test/java/com/vaadin/flow/component/checkbox/tests/CheckboxSerializableTest.java
+++
@@ -1,6 +0,0 @@
-package com.vaadin.flow.component.checkbox.tests;
-
-import com.vaadin.flow.testutil.ClassesSerializableTest;
-
-public class CheckboxSerializableTest extends ClassesSerializableTest {
-}
