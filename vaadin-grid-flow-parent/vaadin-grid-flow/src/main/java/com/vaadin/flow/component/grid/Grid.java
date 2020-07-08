9b28ad83e3569814f47b493bc22295a626846b61
Diego Cardoso <diego@vaadin.com>
2020-07-08 16:32:34 +0300

    chore: bump Web Component version to 5.7.0-alpha5 vaadin/vaadin-grid-flow#1069
    Web-component: vaadin-grid

--- vaadin-grid-flow-parent/vaadin-grid-flow/src/main/java/com/vaadin/flow/component/grid/Grid.java
+++ vaadin-grid-flow-parent/vaadin-grid-flow/src/main/java/com/vaadin/flow/component/grid/Grid.java
@@ -135,7 +135,7 @@ import elemental.json.JsonValue;
  *
  */
 @Tag("vaadin-grid")
-@NpmPackage(value = "@vaadin/vaadin-grid", version = "5.7.0-alpha4")
+@NpmPackage(value = "@vaadin/vaadin-grid", version = "5.7.0-alpha5")
 @JsModule("@vaadin/vaadin-grid/src/vaadin-grid.js")
 @JsModule("@vaadin/vaadin-grid/src/vaadin-grid-column.js")
 @JsModule("@vaadin/vaadin-grid/src/vaadin-grid-sorter.js")
