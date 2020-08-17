f32d29480d069342a7c41de5e86c5dccb21dd88d
Tulio Garcia <tulio@vaadin.com>
2020-08-17 16:17:28 +0300

    test: allow running without reusing driver

--- scripts/templates/tests/AbstractComponentIT.java
+++ scripts/templates/tests/AbstractComponentIT.java
@@ -1,12 +1,33 @@
 package com.vaadin.tests;
 
-public abstract class AbstractComponentIT extends com.vaadin.flow.testutil.AbstractComponentIT {
+import com.vaadin.testbench.Parameters;
+import com.vaadin.testbench.parallel.setup.RemoteDriver;
+import com.vaadin.testbench.parallel.setup.SetupDriver;
+import org.junit.AfterClass;
+import org.openqa.selenium.WebDriver;
+
+import java.lang.reflect.Field;
+
+public abstract class AbstractComponentIT
+    extends com.vaadin.flow.testutil.AbstractComponentIT {
+
+    private static SharedBrowser browser = new SharedBrowser();
+
     @Override
     public void setup() throws Exception {
-        driver = SharedBrowser.instance.getDriver(() -> {
+        if(Parameters.getTestsInParallel() != 1)  {
+            super.setup();
+            return;
+        }
+        driver = browser.getDriver(() -> {
             super.setup();
             return getDriver();
         });
         screenshotOnFailure.setQuitDriverOnFinish(false);
+    }
+
+    @AfterClass
+    public static void runAfterTest() {
+        browser.clear();
     }
 }
