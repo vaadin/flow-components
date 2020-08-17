0be157ff8715f4f93ccc37803ac130a2d1b60968
Tulio Garcia <tulio@vaadin.com>
2020-08-17 16:17:28 +0300

    test: allow running without reusing driver

--- scripts/templates/tests/TabbedComponentDemoTest.java
+++ scripts/templates/tests/TabbedComponentDemoTest.java
@@ -1,12 +1,27 @@
 package com.vaadin.tests;
 
+import com.vaadin.testbench.Parameters;
+import org.junit.AfterClass;
+
 public abstract class TabbedComponentDemoTest extends com.vaadin.flow.demo.TabbedComponentDemoTest {
+
+    private static SharedBrowser browser = new SharedBrowser();
+
     @Override
     public void setup() throws Exception {
-        driver = SharedBrowser.instance.getDriver(() -> {
+        if(Parameters.getTestsInParallel() != 1)  {
+            super.setup();
+            return;
+        }
+        driver = browser.getDriver(() -> {
             super.setup();
             return getDriver();
         });
         screenshotOnFailure.setQuitDriverOnFinish(false);
+    }
+
+    @AfterClass
+    public static void runAfterTest() {
+        browser.clear();
     }
 }
