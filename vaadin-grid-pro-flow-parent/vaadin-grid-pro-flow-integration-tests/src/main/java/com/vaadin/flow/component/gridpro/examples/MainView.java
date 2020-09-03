62f78ad5618f0096d14213e9c087547cacee7e7c
Diego Cardoso <diego@vaadin.com>
2020-09-03 13:37:29 +0300

    test: add test for custom editor (#103) vaadin/vaadin-grid-pro-flow#105
    Web-component: vaadin-grid-pro

--- vaadin-grid-pro-flow-parent/vaadin-grid-pro-flow-integration-tests/src/main/java/com/vaadin/flow/component/gridpro/examples/MainView.java
+++ vaadin-grid-pro-flow-parent/vaadin-grid-pro-flow-integration-tests/src/main/java/com/vaadin/flow/component/gridpro/examples/MainView.java
@@ -8,8 +8,10 @@ import java.util.stream.IntStream;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.gridpro.GridPro;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Input;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
@@ -18,6 +20,8 @@ import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.router.Route;
 
 @Route
+// TODO remove this line after we can use latest minor (V14.4)
+@NpmPackage(value = "@vaadin/vaadin-select", version = "2.2.0")
 public class MainView extends VerticalLayout {
 
     public MainView() {
@@ -89,6 +93,13 @@ public class MainView extends VerticalLayout {
                     itemDisplayPanel.setText(item.toString());
                     subPropertyDisplayPanel.setText(newValue.toString());
         }).setHeader("City").setWidth("300px");
+
+        Input customField = new Input();
+        grid.addEditColumn(Person::getEmail)
+                .custom(customField, (item, newValue) ->
+                        item.setEmail(newValue))
+                .setHeader("Email")
+                .setWidth("300px");
 
         disableGrid.addClickListener(click -> grid.setEnabled(false));
 
