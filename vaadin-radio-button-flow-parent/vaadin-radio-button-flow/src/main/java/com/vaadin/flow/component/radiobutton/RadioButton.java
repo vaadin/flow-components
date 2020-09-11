e8877cc4039df3b0902d19d1e71109b255b98399
Manolo Carrasco <manolo@vaadin.com>
2020-09-11 11:19:04 +0200

    chore: update sources from masters

--- vaadin-radio-button-flow-parent/vaadin-radio-button-flow/src/main/java/com/vaadin/flow/component/radiobutton/RadioButton.java
+++ vaadin-radio-button-flow-parent/vaadin-radio-button-flow/src/main/java/com/vaadin/flow/component/radiobutton/RadioButton.java
@@ -24,7 +24,7 @@ import com.vaadin.flow.data.binder.HasItemComponents;
  *
  * @author Vaadin Ltd.
  */
-@NpmPackage(value = "@vaadin/vaadin-radio-button", version = "1.4.0")
+@NpmPackage(value = "@vaadin/vaadin-radio-button", version = "1.4.1")
 class RadioButton<T> extends GeneratedVaadinRadioButton<RadioButton<T>>
         implements HasItemComponents.ItemComponent<T>, HasComponents {
 
