b5b472c369a6ecda94cb4436550d49e052804377
Diego Cardoso <diego@vaadin.com>
2020-09-11 11:20:07 +0300

    Bump Web Component version to 1.5.1 vaadin/vaadin-radio-button-flow#157
    Web-component: vaadin-radio-button

--- vaadin-radio-button-flow-parent/vaadin-radio-button-flow/src/main/java/com/vaadin/flow/component/radiobutton/RadioButtonGroup.java
+++ vaadin-radio-button-flow-parent/vaadin-radio-button-flow/src/main/java/com/vaadin/flow/component/radiobutton/RadioButtonGroup.java
@@ -50,7 +50,7 @@ import com.vaadin.flow.shared.Registration;
  *
  * @author Vaadin Ltd.
  */
-@NpmPackage(value = "@vaadin/vaadin-radio-button", version = "1.5.0")
+@NpmPackage(value = "@vaadin/vaadin-radio-button", version = "1.5.1")
 public class RadioButtonGroup<T>
         extends GeneratedVaadinRadioGroup<RadioButtonGroup<T>, T> implements
         HasItemsAndComponents<T>, SingleSelect<RadioButtonGroup<T>, T>,
