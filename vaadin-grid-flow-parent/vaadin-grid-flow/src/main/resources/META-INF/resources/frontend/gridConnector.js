ac062017a78e5a1da7929b2e0efeaba7bcc5a495
Tomi Virkki <tomivirkki@users.noreply.github.com>
2020-09-25 10:54:43 +0300

    Bump Web Component version to 5.7.4 vaadin/vaadin-grid-flow#1121
    Web-component: vaadin-grid

--- vaadin-grid-flow-parent/vaadin-grid-flow/src/main/resources/META-INF/resources/frontend/gridConnector.js
+++ vaadin-grid-flow-parent/vaadin-grid-flow/src/main/resources/META-INF/resources/frontend/gridConnector.js
@@ -890,7 +890,6 @@ import { ItemCache } from '@vaadin/vaadin-grid/src/vaadin-grid-data-provider-mix
       grid.$connector.setVerticalScrollingEnabled = tryCatchWrapper(function(enabled) {
         // There are two scollable containers in grid so apply the changes for both
         setVerticalScrollingEnabled(grid.$.table, enabled);
-        setVerticalScrollingEnabled(grid.$.outerscroller, enabled);
 
         // Since the scrollbars were toggled, there might have been some changes to layout
         // size. Notify grid of the resize to ensure everything is in place.
