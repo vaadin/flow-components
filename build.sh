b878e657e7fcd1152a81ceb1da06869122de9e72
Manuel Carrasco Moñino <manolo@vaadin.com>
2020-10-03 08:44:25 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -51,12 +51,7 @@ saveFailedTests() {
 }
 
 computeFastBuild() {
-  if [ -z "$PR" ]
-  then
-    echo "Running validation against non-PR branch, running IT test set"
-    return -1
-  fi
-
+  [ -z "$PR" ] && return 1
   ghUrl="https://api.github.com/repos/vaadin/vaadin-flow-components/pulls/$PR"
   prTitle=`curl -s $ghUrl | jq -r .title`
   echo "$prTitle" | grep -v '\[skip ci\]' >/dev/null || return 0
