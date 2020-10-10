4577d2c4bb8ab75ac0de59855f563769adb02b75
Manolo Carrasco <manolo@vaadin.com>
2020-10-10 14:00:37 +0200

    chore: remove vaadin-avatar in V17

--- vaadin-avatar-flow-parent/vaadin-avatar-flow/src/test/java/com/vaadin/flow/component/avatar/tests/AvatarSerializableTest.java
+++
@@ -1,6 +0,0 @@
-package com.vaadin.flow.component.avatar.tests;
-
-import com.vaadin.flow.testutil.ClassesSerializableTest;
-
-public class AvatarSerializableTest extends ClassesSerializableTest {
-}
