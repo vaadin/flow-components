5ca156d5872ec36343dc7f2cb510b844af8e245a
Manolo Carrasco <manolo@vaadin.com>
2020-10-11 16:44:59 +0200

    ci: do not release any module if no changes

--- scripts/prepareDeploy.sh
+++ scripts/prepareDeploy.sh
@@ -76,9 +76,7 @@ done
 [ "$versionBase" = 14.4 -o "$versionBase" = 17.0 ] && lastTag=`git tag | grep "^$versionBase" | head -1`
 if [ -n "$lastTag" ]
 then
-  modified=`git diff --name-only $lastTag  HEAD | grep '.java$' | cut -d "/" -f1 | grep parent | sort -u | perl -pe 's,-flow-parent,,g'`
-  [ -n "$modified" ] && modules="$modified"
-
+  modules=`git diff --name-only $lastTag  HEAD | grep '.java$' | cut -d "/" -f1 | grep parent | sort -u | perl -pe 's,-flow-parent,,g'`
   echo "Increasing version of the modified modules since last release $lastTag"
   for i in $modules
   do
