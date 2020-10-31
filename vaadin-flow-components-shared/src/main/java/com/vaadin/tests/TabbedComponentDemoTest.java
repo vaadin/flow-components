ea5ff002e37a4ba1b84e86df704a5a9056ef98ce
Manolo Carrasco <manolo@vaadin.com>
2020-10-31 20:26:25 +0100

    test: run pro-components tests in headless & allow disabling headless

--- vaadin-flow-components-shared/src/main/java/com/vaadin/tests/TabbedComponentDemoTest.java
+++ vaadin-flow-components-shared/src/main/java/com/vaadin/tests/TabbedComponentDemoTest.java
@@ -12,8 +12,7 @@ public abstract class TabbedComponentDemoTest extends com.vaadin.flow.demo.Tabbe
 
     @Override
     public void setup() throws Exception {
-        browser.setup(super::setup, this::getDriver, screenshotOnFailure)
-            .ifPresent(this::setDriver);
+        browser.setup(super::setup, this::setDriver, this::getDriver, screenshotOnFailure);
     }
 
     protected int getDeploymentPort() {
