5f06e75b25295740488d4899630f914dff7596ea
Manolo Carrasco <manolo@vaadin.com>
2020-11-16 16:01:58 +0100

    test: fix compilation

--- vaadin-radio-button-flow-parent/vaadin-radio-button-flow-integration-tests/src/test/java/com/vaadin/flow/component/radiobutton/tests/RadioButtonGroupIT.java
+++ vaadin-radio-button-flow-parent/vaadin-radio-button-flow-integration-tests/src/test/java/com/vaadin/flow/component/radiobutton/tests/RadioButtonGroupIT.java
@@ -18,15 +18,15 @@ package com.vaadin.flow.component.radiobutton.tests;
 import java.util.List;
 import java.util.stream.IntStream;
 
+import com.vaadin.testbench.TestBenchElement;
+import com.vaadin.tests.ComponentDemoTest;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-
-import com.vaadin.tests.ComponentDemoTest;
-import com.vaadin.testbench.TestBenchElement;
 
 public class RadioButtonGroupIT extends ComponentDemoTest {
 
@@ -334,37 +334,6 @@ public class RadioButtonGroupIT extends ComponentDemoTest {
 
         executeScript("arguments[0].value=2;", group);
         verifyGroupValid(group, errorMessage);
-    }
-
-    @Test
-    public void verifyHelper() {
-        RadioButtonGroupElement groupWithHelperText = $(RadioButtonGroupElement.class)
-                .id("group-with-helper-text");
-        Assert.assertEquals("helperText", groupWithHelperText.getHelperText());
-
-        RadioButtonGroupElement groupWithHelperComponent = $(RadioButtonGroupElement.class)
-                .id("group-with-helper-component");
-        WebElement helperComponent = groupWithHelperComponent.getHelperComponent();
-        Assert.assertEquals("helperComponent", helperComponent.getText());
-        Assert.assertEquals("helper-component", helperComponent.getAttribute("id"));
-    }
-
-    @Test
-    public void clearHelper() {
-        RadioButtonGroupElement groupWithHelperText = $(RadioButtonGroupElement.class)
-                .id("group-with-helper-text");
-        Assert.assertEquals("helperText", groupWithHelperText.getHelperText());
-
-        $(TestBenchElement.class).id("clear-helper-text-button").click();
-        Assert.assertEquals("", groupWithHelperText.getHelperText());
-
-        RadioButtonGroupElement groupWithHelperComponent = $(RadioButtonGroupElement.class)
-                .id("group-with-helper-component");
-        WebElement helperComponent = groupWithHelperComponent.getHelperComponent();
-        Assert.assertEquals("helper-component", helperComponent.getAttribute("id"));
-
-        $(TestBenchElement.class).id("clear-helper-component-button").click();
-        Assert.assertNull(groupWithHelperComponent.getHelperComponent());
     }
 
     private void verifyGroupInvalid(TestBenchElement group,
