4f9d03c2e6b7b43f3b0ac3ca61ff49858725f53d
Tulio Garcia <tulio@vaadin.com>
2020-11-20 11:43:29 +0200

    Fix: FileBuffer getInputStream is closed (#405) (#412)
    Warranty: Fixes FileBuffer getInputStream is closed
    Fixes: vaadin/vaadin-upload#334

---
+++ vaadin-upload-flow-parent/vaadin-upload-flow/src/test/java/com/vaadin/flow/component/upload/tests/MultiFileBufferTest.java
@@ -0,0 +1,45 @@
+package com.vaadin.flow.component.upload.tests;
+
+import com.vaadin.flow.component.upload.receivers.MultiFileBuffer;
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+
+public class MultiFileBufferTest {
+
+    @Test
+    public void shouldBeAbleToReadFilesAfterReceiving() throws IOException {
+        MultiFileBuffer fileBuffer = new MultiFileBuffer();
+        TestData[] testData = { new TestData("upload1", "Upload data 1"),
+            new TestData("upload2", "Upload data 2"),
+            new TestData("upload3", "Upload data 3"), };
+        for (TestData data : testData) {
+            final byte[] dataBytes = data.data
+                .getBytes(Charset.defaultCharset());
+            try (OutputStream os = fileBuffer
+                .receiveUpload(data.filename, "text")) {
+                os.write(dataBytes);
+            }
+        }
+        for (TestData data : testData) {
+            final String readData = IOUtils
+                .toString(fileBuffer.getInputStream(data.filename),
+                    Charset.defaultCharset());
+            Assert.assertEquals(data.data, readData);
+        }
+    }
+
+    class TestData {
+        private final String filename;
+        private final String data;
+
+        public TestData(String filename, String data) {
+            this.filename = filename;
+            this.data = data;
+        }
+    }
+}
