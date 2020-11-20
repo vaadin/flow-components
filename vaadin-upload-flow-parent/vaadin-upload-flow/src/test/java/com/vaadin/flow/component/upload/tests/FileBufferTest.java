6ef61ab4f075e2b10d8fa06cb19a9bda90f215fd
Tulio Garcia <tulio@vaadin.com>
2020-11-20 11:42:50 +0200

    Fix: FileBuffer getInputStream is closed (#405) (#414)
    Warranty: Fixes FileBuffer getInputStream is closed
    Fixes: vaadin/vaadin-upload#334

---
+++ vaadin-upload-flow-parent/vaadin-upload-flow/src/test/java/com/vaadin/flow/component/upload/tests/FileBufferTest.java
@@ -0,0 +1,26 @@
+package com.vaadin.flow.component.upload.tests;
+
+import com.vaadin.flow.component.upload.receivers.FileBuffer;
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+
+public class FileBufferTest {
+
+    @Test
+    public void shouldBeAbleToReadFileAfterReceiving() throws IOException {
+        FileBuffer fileBuffer = new FileBuffer();
+        final String data = "Upload data";
+        final byte[] dataBytes = data.getBytes(Charset.defaultCharset());
+        try (OutputStream os = fileBuffer.receiveUpload("uploadData", "text")) {
+            os.write(dataBytes);
+        }
+        final String readData = IOUtils
+            .toString(fileBuffer.getInputStream(), Charset.defaultCharset());
+        Assert.assertEquals(data, readData);
+    }
+}
