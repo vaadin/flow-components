735158765d9adea9ef3cc406f1e55b604bfa95b8
Tulio Garcia <tulio@vaadin.com>
2020-12-07 20:29:06 +0200

    feat: updated licenses in java files

--- vaadin-grid-pro-flow-parent/vaadin-grid-pro-testbench/src/main/java/com/vaadin/flow/component/gridpro/testbench/GridTHTDElement.java
+++ vaadin-grid-pro-flow-parent/vaadin-grid-pro-testbench/src/main/java/com/vaadin/flow/component/gridpro/testbench/GridTHTDElement.java
@@ -6,14 +6,13 @@ package com.vaadin.flow.component.gridpro.testbench;
  * %%
  * Copyright (C) 2018 Vaadin Ltd
  * %%
- * This program is available under Commercial Vaadin Add-On License 3.0
- * (CVALv3).
+ * This program is available under Commercial Vaadin Developer License
+ * 4.0 (CVDLv4).
  * 
  * See the file license.html distributed with this software for more
  * information about licensing.
  * 
- * You should have received a copy of the CVALv3 along with this program.
- * If not, see <http://vaadin.com/license/cval-3>.
+ * For the full License, see <https://vaadin.com/license/cvdl-4.0>.
  * #L%
  */
 
