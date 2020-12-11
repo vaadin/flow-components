7e12031e255d78e9b28cb156edde85d2bbf587b1
Manuel Carrasco Moñino <manolo@vaadin.com>
2020-12-11 08:05:30 +0100

    fix: update versions for non vaadin components (#501)
    * chore: add more logs to shell script for platform update
    
    * refactor: extract get-version code to a shared module
    
    * refactor: convert updatePlatfrm to JS. Fix versions for non vaadin components
    
    * chore: use bower info for jsVersion
    
    Co-authored-by: Zhe Sun <31067185+ZheSun88@users.noreply.github.com>

--- scripts/updateNpmVer.js
+++ scripts/updateNpmVer.js
@@ -12,27 +12,9 @@ const fs = require('fs');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const replace = require('replace-in-file');
+const {getAnnotations, computeVertionToUpdate} = require('./lib/versions.js');
 
 let exclude=[];
-
-async function computeModules(){
-  const cmd = 'grep -r @NpmPackage ./vaadin*parent/*/src/*/java';
-  const output = await run(cmd);
-  const lines = output.split('\n').filter(Boolean);
-  return lines.map(line => {
-    const r = /(.*(vaadin-.*)-parent.*):(.*value *= *"([^"]+).*version *= *"((\d+)\.(\d+)[^"]*).*)/.exec(line);
-    return {
-      path: r[1],
-      name: r[2],
-      annotation: r[3],
-      package: r[4],
-      version: r[5],
-      major: r[6],
-      minor: r[7],
-      updatedVersion: ''
-    };
-  });
-}
 
 async function updateFiles(moduleData){
   if(moduleData.annotation.length>0){
@@ -63,13 +45,6 @@ async function run(cmd) {
   return stdout;
 }
 
-async function calculateVersions(data) {
-  cmd = "npm view " + data.package + " versions --json";
-  const versions = JSON.parse(await run(cmd)).filter(version => version.startsWith(`${data.major}.${data.minor}`));
-  data['updatedVersion'] = versions.pop();
-  return data;
-}
-
 /**
  * Allow exclude certain component package update, use ',' as separator
  */
@@ -81,32 +56,28 @@ async function excludeComponents() {
         break;
       }
   }
-  
   exclude=components.split(',');
-  
   packageBase='@vaadin/vaadin-';
   for(j = 0; j < exclude.length; j++){
 	  exclude[j] = packageBase.concat(exclude[j]);
   }
-  
   return exclude;
 }
 
 async function main() {
   console.log("Updating the NpmPackage annotation.")
-  const modules = await computeModules();
-  if (process.argv.length > 2){
+  const annotations = await getAnnotations();
+  if (process.argv.length > 2) {
     exclude = await excludeComponents();
-  } 
-  
-  for (i = 0; i < modules.length; i++){
-	if (exclude.includes(modules[i].package)){
-		console.log('\x1b[33m',"skip updating "+ modules[i].package +" package");
-	} else {
-		modules[i] = await calculateVersions(modules[i]);
-        await updateFiles(modules[i]);
-	}
-    
+  }
+
+  for (i = 0; i < annotations.length; i++) {
+    if (exclude.includes(annotations[i].package)) {
+      console.log('\x1b[33m', "skip updating " + annotations[i].package + " package");
+    } else {
+      await computeVertionToUpdate(annotations[i]);
+      await updateFiles(annotations[i]);
+    }
   }
 }
 
