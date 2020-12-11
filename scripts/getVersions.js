d950a754bf436d3328c3634fa68e88030fb7ff27
Manuel Carrasco Moñino <manolo@vaadin.com>
2020-12-11 08:05:30 +0100

    fix: update versions for non vaadin components (#501)
    * chore: add more logs to shell script for platform update
    
    * refactor: extract get-version code to a shared module
    
    * refactor: convert updatePlatfrm to JS. Fix versions for non vaadin components
    
    * chore: use bower info for jsVersion
    
    Co-authored-by: Zhe Sun <31067185+ZheSun88@users.noreply.github.com>

--- scripts/getVersions.js
+++ scripts/getVersions.js
@@ -4,67 +4,14 @@
  * Example
  *   ./scripts/getVersions.js
  */
-
-const https = require("https");
-const xml2js = require('xml2js');
-const fs = require('fs');
-
-async function checkBranch(branch) {
-  return new Promise(resolve => {
-    https.request({
-      method: 'HEAD',
-      hostname: 'raw.githubusercontent.com',
-      path: `vaadin/platform/${branch}/versions.json`
-    }, r => {
-      resolve(r.statusCode === 200);
-    }).end();
-  });
-}
-
-async function getVersions(branch) {
-  return new Promise(resolve => {
-    https.get(`https://raw.githubusercontent.com/vaadin/platform/${branch}/versions.json`, res => {
-      res.setEncoding("utf8");
-      let body = "";
-      res.on("data", data => {
-        body += data;
-      });
-      res.on("end", () => {
-        resolve(body);
-      });
-    });
-  }).then(body => {
-    return JSON.parse(body);
-  });
-}
+const {getVersionsCsv, getVersionsJson} = require('./lib/versions.js');
 
 async function main() {
-  const parentJs = await xml2js.parseStringPromise(fs.readFileSync('pom.xml', 'utf8'));
-  const version = parentJs.project.version[0];
-  const branch = version.replace(/^(\d+\.\d+).*$/, '$1');
-  const isMaster = !await checkBranch(branch);
-
-  return getVersions(isMaster ? 'master' : branch).then(json => {
-    return ['core', 'vaadin'].reduce((prev, k) => {
-      return prev.concat(Object.keys(json[k]).filter(pkg => /\-/.test(pkg) && json[k][pkg].javaVersion).map(pkg => {
-        const version = json[k][pkg].javaVersion;
-        const branch = version.replace('{{version}}', 'master').replace(/^(\d+\.\d+).*$/, '$1');
-        return `${pkg === 'iron-list' ? 'vaadin-' + pkg : pkg}-flow-parent:${branch}:${version}`
-      }));
-    }, []);
-  });
-
+  if (process.argv[2] == '--json') {
+    console.log(JSON.stringify(await getVersionsJson()));
+  } else {
+    console.log((await getVersionsCsv()).join('\n'));
+  }
 }
 
-main().then(arr => {
-  if (process.argv[2] == '--json') {
-    const o = {};
-    arr.forEach(line => {
-      const e = line.split(':');
-      o[e[0]] = e[1];
-    });
-    console.log(JSON.stringify(o));
-  } else {
-    console.log(arr.join('\n'))
-  }
-});
+main();
