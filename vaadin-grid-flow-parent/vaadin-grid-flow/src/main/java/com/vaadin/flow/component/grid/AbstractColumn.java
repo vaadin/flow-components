d05e793aa67db2e5102b6c9bf08c0ed417f09971
Yuriy Yevstihnyeyev <yuriy@vaadin.com>
2020-12-20 11:40:03 +0200

    fix: remove redundant sorter renderer (#537)
    Fixes: vaadin/vaadin-grid#1841.
    Cherry-pick: #515

--- vaadin-grid-flow-parent/vaadin-grid-flow/src/main/java/com/vaadin/flow/component/grid/AbstractColumn.java
+++ vaadin-grid-flow-parent/vaadin-grid-flow/src/main/java/com/vaadin/flow/component/grid/AbstractColumn.java
@@ -175,10 +175,7 @@ abstract class AbstractColumn<T extends AbstractColumn<T>> extends Component
     }
 
     protected void setHeaderComponent(Component component) {
-        /*
-         * Uses the special renderer to take care of the vaadin-grid-sorter.
-         */
-        setHeaderRenderer(new GridSorterComponentRenderer<>(this, component));
+        setHeaderRenderer(new ComponentRenderer<>(() -> component));
     }
 
     protected void setFooterComponent(Component component) {
