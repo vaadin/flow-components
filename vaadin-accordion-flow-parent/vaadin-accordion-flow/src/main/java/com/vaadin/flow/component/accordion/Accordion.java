78d981e13a189732dd036017a5c31627834809b1
David Sosa <76832183+sosa-vaadin@users.noreply.github.com>
2021-01-18 10:16:46 +0200

    fix: added HasStyle interface to the Accordion, AccordionPanel and Details components. (#569)
    fix: add HasStyle interface to the Accordion, AccordionPanel and Details components.
    
    * fix: Add a missing dependency for the Accordion integration tests.
    
    Web-Component: Accordion, AccordionPanel, Details
    Fixes #564

--- vaadin-accordion-flow-parent/vaadin-accordion-flow/src/main/java/com/vaadin/flow/component/accordion/Accordion.java
+++ vaadin-accordion-flow-parent/vaadin-accordion-flow/src/main/java/com/vaadin/flow/component/accordion/Accordion.java
@@ -27,6 +27,7 @@ import com.vaadin.flow.component.ComponentUtil;
 import com.vaadin.flow.component.DomEvent;
 import com.vaadin.flow.component.EventData;
 import com.vaadin.flow.component.HasSize;
+import com.vaadin.flow.component.HasStyle;
 import com.vaadin.flow.component.Synchronize;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
@@ -40,7 +41,7 @@ import com.vaadin.flow.shared.Registration;
 @Tag("vaadin-accordion")
 @NpmPackage(value = "@vaadin/vaadin-accordion", version = "1.2.0")
 @JsModule("@vaadin/vaadin-accordion/src/vaadin-accordion.js")
-public class Accordion extends Component implements HasSize {
+public class Accordion extends Component implements HasSize, HasStyle {
 
     private static final String OPENED_PROPERTY = "opened";
     private static final String OPENED_CHANGED_DOM_EVENT = "opened-changed";
