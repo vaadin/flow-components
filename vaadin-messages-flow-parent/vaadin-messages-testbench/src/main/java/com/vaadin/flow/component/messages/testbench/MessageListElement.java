f0fa2e4f9fe0c6a77bd2ea94074e6eb4fc697f11
Pekka Maanpää <pekkamaa@vaadin.com>
2021-02-10 16:59:38 +0200

    feat: add MessageList component (#640)
    MessageList is a Java wrapper for the <vaadin-message-list> component. It displays a list of chat messages or comments, which can be configured with the setItems method. Besides the message content, each message may display a user avatar and the time of the message.
    
    Resolves vaadin/vaadin-messages#10

--- vaadin-messages-flow-parent/vaadin-messages-testbench/src/main/java/com/vaadin/flow/component/messages/testbench/MessageListElement.java
+++ vaadin-messages-flow-parent/vaadin-messages-testbench/src/main/java/com/vaadin/flow/component/messages/testbench/MessageListElement.java
@@ -15,14 +15,28 @@
  */
 package com.vaadin.flow.component.messages.testbench;
 
+import java.util.List;
+
 import com.vaadin.testbench.TestBenchElement;
 import com.vaadin.testbench.elementsbase.Element;
 
 /**
  * A TestBench element representing a <code>&lt;vaadin-message-list&gt;</code>
  * element.
+ *
+ * @author Vaadin Ltd.
  */
 @Element("vaadin-message-list")
 public class MessageListElement extends TestBenchElement {
+
+    /**
+     * Gets the <code>&lt;vaadin-message&gt;</code> elements rendered in this
+     * message list.
+     *
+     * @return
+     */
+    public List<MessageElement> getMessageElements() {
+        return $(MessageElement.class).all();
+    }
 
 }
