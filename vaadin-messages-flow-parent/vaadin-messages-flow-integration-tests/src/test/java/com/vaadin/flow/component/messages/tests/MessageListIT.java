d3c04143b440943b4c5b0be17845a44c7a353502
Pekka Maanpää <pekkamaa@vaadin.com>
2021-02-10 16:59:38 +0200

    feat: add MessageList component (#640)
    MessageList is a Java wrapper for the <vaadin-message-list> component. It displays a list of chat messages or comments, which can be configured with the setItems method. Besides the message content, each message may display a user avatar and the time of the message.
    
    Resolves vaadin/vaadin-messages#10

--- vaadin-messages-flow-parent/vaadin-messages-flow-integration-tests/src/test/java/com/vaadin/flow/component/messages/tests/MessageListIT.java
+++ vaadin-messages-flow-parent/vaadin-messages-flow-integration-tests/src/test/java/com/vaadin/flow/component/messages/tests/MessageListIT.java
@@ -15,10 +15,13 @@
  */
 package com.vaadin.flow.component.messages.tests;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.vaadin.flow.component.messages.testbench.MessageElement;
 import com.vaadin.flow.component.messages.testbench.MessageListElement;
 import com.vaadin.flow.testutil.TestPath;
 import com.vaadin.tests.AbstractComponentIT;
@@ -26,13 +29,82 @@ import com.vaadin.tests.AbstractComponentIT;
 @TestPath("vaadin-messages/message-list-test")
 public class MessageListIT extends AbstractComponentIT {
 
+    private MessageListElement messageList;
+
     @Before
     public void init() {
         open();
+        messageList = $(MessageListElement.class).first();
     }
 
     @Test
-    public void test() {
-        Assert.assertTrue($(MessageListElement.class).exists());
+    public void setInitialItems_messagesRendered() {
+        List<MessageElement> messages = messageList.getMessageElements();
+        Assert.assertEquals("Unexpected items count", 2, messages.size());
+
+        MessageElement msg = messages.get(0);
+        Assert.assertEquals("Unexpected text content", "foo", msg.getText());
+        Assert.assertEquals("Unexpected time prop", "2021-01-01T00:00:00Z",
+                msg.getTime());
+        Assert.assertEquals("Unexpected userName prop", "sender",
+                msg.getUserName());
+        Assert.assertEquals("Unexpected userImage prop", "/test.jpg",
+                msg.getUserImg());
+        Assert.assertEquals("Unexpected userAbbreviation prop", "AB",
+                msg.getUserAbbr());
+        Assert.assertEquals("Unexpected userColorIndex prop", 1,
+                msg.getUserColorIndex());
+
+        Assert.assertEquals("Unexpected text content", "bar",
+                messages.get(1).getText());
     }
+
+    @Test
+    public void updateItemPropertiesAfterRendering_messagesUpdated() {
+        MessageElement msg = messageList.getMessageElements().get(0);
+
+        /*
+         * Testing each setter separately to make sure that they all trigger the
+         * client-side property update.
+         */
+
+        clickElementWithJs("setText");
+        Assert.assertEquals("Unexpected text content", "foo2", msg.getText());
+
+        clickElementWithJs("setTime");
+        Assert.assertEquals("Unexpected time prop", "2000-02-02T02:02:02Z",
+                msg.getTime());
+
+        clickElementWithJs("setUserName");
+        Assert.assertEquals("Unexpected userName prop", "sender2",
+                msg.getUserName());
+
+        clickElementWithJs("setUserImage");
+        Assert.assertEquals("Unexpected userImage prop", "/test2.jpg",
+                msg.getUserImg());
+
+        clickElementWithJs("setAbbreviation");
+        Assert.assertEquals("Unexpected userAbbreviation prop", "CD",
+                msg.getUserAbbr());
+
+        clickElementWithJs("setUserColorIndex");
+        Assert.assertEquals("Unexpected userColorIndex prop", 2,
+                msg.getUserColorIndex());
+    }
+
+    @Test
+    public void changeItemsAfterRendering_messagesUpdated() {
+        clickElementWithJs("setItems");
+
+        List<MessageElement> messages = messageList.getMessageElements();
+        Assert.assertEquals("Unexpected items count", 1, messages.size());
+
+        MessageElement msg = messages.get(0);
+
+        Assert.assertEquals("Unexpected text content", "", msg.getText());
+        Assert.assertEquals("Unexpected time prop", null, msg.getTime());
+        Assert.assertEquals("Unexpected userName prop", "sender3",
+                msg.getUserName());
+    }
+
 }
