5cdb2cf47670281a2da383eda92057062d05c148
Tatu Lund <tatu@vaadin.com>
2021-02-18 19:32:19 +0200

    Add test UI

---
+++ vaadin-grid-flow-parent/vaadin-grid-flow-integration-tests/src/main/java/com/vaadin/flow/component/grid/SelectDuringDataProviderChangePage.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2000-2020 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.grid.it;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("select-during-data-provider-change")
+public class SelectDuringDataProviderChangePage extends VerticalLayout {
+    private final Grid<Item> grid = new Grid<>();
+    private final Button btn = new Button("Set items");
+
+    public SelectDuringDataProviderChangePage() {
+        grid.setItems(new Item("TEST A"), new Item("TEST B"));
+
+        grid.addColumn(Item::getName);
+
+        btn.addClickListener(ev -> setItems());
+
+        grid.addSelectionListener(event -> {
+            Span span = new Span("selected: "+grid.getSelectedItems().size());
+            span.setId("ready");
+            add(span);            
+        });
+
+        add(grid, btn);
+    }
+
+    private void setItems() {
+        try {
+            // Simulate heavy work
+            Thread.sleep(3000);
+        } catch (final InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        grid.setItems(new Item("TEST1"), new Item("TEST2"));
+    }
+
+    public class Item {
+        private final String name;
+
+        public Item(final String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+}
