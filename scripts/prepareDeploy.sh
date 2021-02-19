3abd89eead508f9b05f84df3f7a680468dbc7bd4
Zhe Sun <31067185+ZheSun88@users.noreply.github.com>
2021-02-19 12:09:13 +0200

    chore:update script for V20.0 (#661)

--- scripts/prepareDeploy.sh
+++ scripts/prepareDeploy.sh
@@ -50,6 +50,7 @@ pomBase=`getBaseVersion $pomVersion`
 
 ### Load versions file for this platform release
 branch=$versionBase
+[ $branch = "20.0" ] && branch=master
 versions=`curl -s "https://raw.githubusercontent.com/vaadin/platform/$branch/versions.json"`
 [ $? != 0 ] && branch=master && versions=`curl -s "https://raw.githubusercontent.com/vaadin/platform/$branch/versions.json"`
 
