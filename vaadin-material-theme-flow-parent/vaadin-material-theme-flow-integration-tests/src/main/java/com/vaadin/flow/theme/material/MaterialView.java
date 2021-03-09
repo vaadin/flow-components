5f31b75455212d5f2c2712b7350b87053c90f6f9
Manolo Carrasco <manolo@vaadin.com>
2021-03-09 10:38:35 +0100

    test: move implicit and explicit material tests from flow

--- vaadin-material-theme-flow-parent/vaadin-material-theme-flow-integration-tests/src/main/java/com/vaadin/flow/theme/material/MaterialView.java
+++ vaadin-material-theme-flow-parent/vaadin-material-theme-flow-integration-tests/src/main/java/com/vaadin/flow/theme/material/MaterialView.java
@@ -1,4 +1,8 @@
 package com.vaadin.flow.theme.material;
 
-public class MaterialView {
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+
+@Theme(themeClass = Material.class)
+public class MaterialView implements AppShellConfigurator {
 }
