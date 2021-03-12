cca1d8cce6004174ae64d06960e2b84c9d0b2c2c
web-padawan <iamkulykov@gmail.com>
2021-03-12 11:23:39 +0200

    feat: use unified web components versions

--- vaadin-text-field-flow-parent/vaadin-text-field-flow/src/main/java/com/vaadin/flow/component/textfield/GeneratedVaadinNumberField.java
+++ vaadin-text-field-flow-parent/vaadin-text-field-flow/src/main/java/com/vaadin/flow/component/textfield/GeneratedVaadinNumberField.java
@@ -41,7 +41,7 @@ import com.vaadin.flow.function.SerializableFunction;
         "WebComponent: Vaadin.NumberFieldElement#2.3.0-alpha8",
         "Flow#1.3-SNAPSHOT" })
 @Tag("vaadin-number-field")
-@NpmPackage(value = "@vaadin/vaadin-text-field", version = "3.0.2")
+@NpmPackage(value = "@vaadin/vaadin-text-field", version = "20.0.0-alpha1")
 @JsModule("@vaadin/vaadin-text-field/src/vaadin-number-field.js")
 public abstract class GeneratedVaadinNumberField<R extends GeneratedVaadinNumberField<R, T>, T>
         extends GeneratedVaadinTextField<R, T> implements HasStyle {
