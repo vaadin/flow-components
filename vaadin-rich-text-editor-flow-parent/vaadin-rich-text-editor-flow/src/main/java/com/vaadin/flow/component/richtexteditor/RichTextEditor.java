4bebb264482045531b5428bf9f24bb6a21645524
Tulio Garcia <28783969+tulioag@users.noreply.github.com>
2021-03-22 09:58:27 +0200

    Undo space change

--- vaadin-rich-text-editor-flow-parent/vaadin-rich-text-editor-flow/src/main/java/com/vaadin/flow/component/richtexteditor/RichTextEditor.java
+++ vaadin-rich-text-editor-flow-parent/vaadin-rich-text-editor-flow/src/main/java/com/vaadin/flow/component/richtexteditor/RichTextEditor.java
@@ -11,10 +11,10 @@ import java.util.Objects;
  * %%
  * This program is available under Commercial Vaadin Developer License
  * 4.0 (CVDLv4).
- *
+ * 
  * See the file license.html distributed with this software for more
  * information about licensing.
- *
+ * 
  * For the full License, see <https://vaadin.com/license/cvdl-4.0>.
  * #L%
  */
