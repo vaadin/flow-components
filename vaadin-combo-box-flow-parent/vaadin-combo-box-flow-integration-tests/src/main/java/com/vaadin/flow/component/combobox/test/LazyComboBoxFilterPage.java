a43ee8f17d0e582619b113f5fc03d425e7a76969
Tulio Garcia <28783969+tulioag@users.noreply.github.com>
2021-04-20 17:18:19 +0300

    Sonar fixes

--- vaadin-combo-box-flow-parent/vaadin-combo-box-flow-integration-tests/src/main/java/com/vaadin/flow/component/combobox/test/LazyComboBoxFilterPage.java
+++ vaadin-combo-box-flow-parent/vaadin-combo-box-flow-integration-tests/src/main/java/com/vaadin/flow/component/combobox/test/LazyComboBoxFilterPage.java
@@ -17,7 +17,6 @@ package com.vaadin.flow.component.combobox.test;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
