64f1f14e3a86010d88ed2e0114861c2a3c01d504
Stefan Uebe <stefan@vaadin.com>
2021-04-21 10:05:59 +0200

    Added ITs for dom event "grid-cell-focus" (#1536)

---
+++ vaadin-grid-flow-parent/vaadin-grid-flow-integration-tests/src/main/java/com/vaadin/flow/component/grid/it/GridCellFocusPage.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2000-2021 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.grid.it;
+
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.Grid.Column;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.router.Route;
+
+import java.util.Optional;
+
+/**
+ * Page created for testing purposes. Not suitable for demos.
+ *
+ * @author Vaadin Ltd.
+ *
+ */
+@Route("vaadin-grid/grid-cell-focus-page")
+public class GridCellFocusPage extends Div {
+
+    public static final String ID_GRID = "cell-focus-grid";
+    public static final String ID_ITEM_RESULT = "cell-focus-item-result";
+    public static final String ID_COLUMN_RESULT = "cell-focus-column-result";
+    public static final String ID_SECTION_RESULT = "cell-focus-section-result";
+
+    public static final String KEY_FIRST_COLUMN = "first-column";
+    public static final String KEY_SECOND_COLUMN = "second-column";
+    public static final String KEY_THIRD_COLUMN = "third-column";
+
+    public static final String NO_ITEM = "-- no item --";
+    public static final String NO_COLUMN = "-- no column --";
+    public static final String NO_SECTION = "-- no section --";
+
+    public static final String SECTION_HEADER = "header";
+    public static final String SECTION_DETAILS = "details";
+    public static final String SECTION_FOOTER = "footer";
+
+    public GridCellFocusPage() {
+        setSizeFull();
+
+        Grid<String> grid = new Grid<>();
+        grid.setId(ID_GRID);
+
+        grid.addColumn(s -> s + "1").setKey(KEY_FIRST_COLUMN).setHeader("First column header").setFooter("First column footer");
+        grid.addColumn(s -> s + "2").setKey(KEY_SECOND_COLUMN).setHeader("Second column header").setFooter("Second column footer");
+        grid.addColumn(s -> s + "3").setKey(KEY_THIRD_COLUMN).setHeader("Third column header").setFooter("Third column footer");
+
+        grid.setItems("A", "B", "C", "D");
+
+        Span itemResult = new Span();
+        itemResult.setId(ID_ITEM_RESULT);
+
+        Span colResult = new Span();
+        colResult.setId(ID_COLUMN_RESULT);
+
+        Span sectionResult = new Span();
+        sectionResult.setId(ID_SECTION_RESULT);
+
+        add(itemResult, colResult, sectionResult, grid);
+
+        grid.addCellFocusListener(event -> {
+            String item = event.getItem().orElse(NO_ITEM);
+            String column = event.getColumn().map(Column::getKey).orElse(NO_COLUMN);
+
+            String section = NO_SECTION;
+            if (event.isHeaderCell()) {
+                section = SECTION_HEADER;
+            } else if (event.isDetailsCell()) {
+                section = SECTION_DETAILS;
+            } else if (event.isFooterCell()) {
+                section = SECTION_FOOTER;
+            }
+
+            itemResult.setText(item);
+            colResult.setText(column);
+            sectionResult.setText(section);
+        });
+    }
+}
---
+++ vaadin-grid-flow-parent/vaadin-grid-flow-integration-tests/src/test/java/com/vaadin/flow/component/grid/it/GridCellFocusPageIT.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2000-2021 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.grid.it;
+
+import com.vaadin.flow.component.grid.testbench.GridElement;
+import com.vaadin.flow.component.grid.testbench.GridTHTDElement;
+import com.vaadin.flow.testutil.TestPath;
+import com.vaadin.testbench.TestBenchElement;
+import com.vaadin.tests.AbstractComponentIT;
+import elemental.html.SpanElement;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * IT for grid's Flow based cell focus event.
+ * @author Vaadin Ltd
+ */
+@TestPath("vaadin-grid/grid-cell-focus-page")
+public class GridCellFocusPageIT extends AbstractComponentIT {
+
+    @Test
+    public void test_focusFirstCell() {
+        open();
+
+        getGrid().getCell(0, 0).focus();
+        assertItemResult(GridCellFocusPage.ID_ITEM_RESULT, "A");
+        assertItemResult(GridCellFocusPage.ID_COLUMN_RESULT, GridCellFocusPage.KEY_FIRST_COLUMN);
+        assertItemResult(GridCellFocusPage.ID_SECTION_RESULT, GridCellFocusPage.SECTION_DETAILS);
+
+
+        getGrid().getCell(1, 0).focus();
+        assertItemResult(GridCellFocusPage.ID_ITEM_RESULT, "B");
+        assertItemResult(GridCellFocusPage.ID_COLUMN_RESULT, GridCellFocusPage.KEY_FIRST_COLUMN);
+        assertItemResult(GridCellFocusPage.ID_SECTION_RESULT, GridCellFocusPage.SECTION_DETAILS);
+
+        getGrid().getCell(2, 1).focus();
+        assertItemResult(GridCellFocusPage.ID_ITEM_RESULT, "C");
+        assertItemResult(GridCellFocusPage.ID_COLUMN_RESULT, GridCellFocusPage.KEY_SECOND_COLUMN);
+        assertItemResult(GridCellFocusPage.ID_SECTION_RESULT, GridCellFocusPage.SECTION_DETAILS);
+    }
+
+    @Test
+    public void test_focusHeaderCell() {
+        open();
+
+        getGrid().getHeaderCell(0).focus();
+        assertItemResult(GridCellFocusPage.ID_ITEM_RESULT, GridCellFocusPage.NO_ITEM);
+        assertItemResult(GridCellFocusPage.ID_COLUMN_RESULT, GridCellFocusPage.KEY_FIRST_COLUMN);
+        assertItemResult(GridCellFocusPage.ID_SECTION_RESULT, GridCellFocusPage.SECTION_HEADER);
+
+        getGrid().getHeaderCell(1).focus();
+        assertItemResult(GridCellFocusPage.ID_ITEM_RESULT, GridCellFocusPage.NO_ITEM);
+        assertItemResult(GridCellFocusPage.ID_COLUMN_RESULT, GridCellFocusPage.KEY_SECOND_COLUMN);
+        assertItemResult(GridCellFocusPage.ID_SECTION_RESULT, GridCellFocusPage.SECTION_HEADER);
+
+    }
+
+    @Test
+    public void test_focusFooterCell() {
+        open();
+
+        getGrid().getFooterCell(0).focus();
+        assertItemResult(GridCellFocusPage.ID_ITEM_RESULT, GridCellFocusPage.NO_ITEM);
+        assertItemResult(GridCellFocusPage.ID_COLUMN_RESULT, GridCellFocusPage.KEY_FIRST_COLUMN);
+        assertItemResult(GridCellFocusPage.ID_SECTION_RESULT, GridCellFocusPage.SECTION_FOOTER);
+
+        getGrid().getFooterCell(1).focus();
+        assertItemResult(GridCellFocusPage.ID_ITEM_RESULT, GridCellFocusPage.NO_ITEM);
+        assertItemResult(GridCellFocusPage.ID_COLUMN_RESULT, GridCellFocusPage.KEY_SECOND_COLUMN);
+        assertItemResult(GridCellFocusPage.ID_SECTION_RESULT, GridCellFocusPage.SECTION_FOOTER);
+    }
+
+    private GridElement getGrid() {
+        return $(GridElement.class).id(GridCellFocusPage.ID_GRID);
+    }
+
+    private void assertItemResult(String resultFieldId, String expectedResult) {
+        String text = $(TestBenchElement.class)
+                .id(resultFieldId)
+                .getText();
+
+        Assert.assertEquals(expectedResult, text);
+    }
+}
