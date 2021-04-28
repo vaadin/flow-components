c9f164a44618a08483de25f66c9486c6c463b1d0
Manolo Carrasco <manolo@vaadin.com>
2021-04-28 21:05:24 +0200

    chore: run formatter:format

--- vaadin-grid-flow-parent/vaadin-grid-flow-integration-tests/src/main/java/com/vaadin/flow/component/grid/it/RecalculateColumnWidthsPage.java
+++ vaadin-grid-flow-parent/vaadin-grid-flow-integration-tests/src/main/java/com/vaadin/flow/component/grid/it/RecalculateColumnWidthsPage.java
@@ -46,9 +46,12 @@ public class RecalculateColumnWidthsPage extends VerticalLayout {
 
         add(grid1);
 
-        // Ensure _recalculateColumnWidthOnceLoadingFinished flag is cleared, otherwise the flag
-        // would trigger the column recalculation automatically when refreshing the data
-        // The web component has some flaky behaviour where the flag is not always cleared
+        // Ensure _recalculateColumnWidthOnceLoadingFinished flag is cleared,
+        // otherwise the flag
+        // would trigger the column recalculation automatically when refreshing
+        // the data
+        // The web component has some flaky behaviour where the flag is not
+        // always cleared
         // after the initial data load
         // See https://github.com/vaadin/web-components/issues/268
         grid1.getElement().executeJs(
