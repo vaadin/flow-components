96b60640abb0b89bbe410047438e34c801f740f1
diegocardoso <diego@vaadin.com>
2021-04-29 16:54:22 +0300

    fix: use NativeButton instead of Button on test page

--- vaadin-list-box-flow-parent/vaadin-list-box-flow-integration-tests/src/main/java/com/vaadin/flow/component/listbox/test/ListBoxRetainValuePage.java
+++ vaadin-list-box-flow-parent/vaadin-list-box-flow-integration-tests/src/main/java/com/vaadin/flow/component/listbox/test/ListBoxRetainValuePage.java
@@ -19,8 +19,8 @@ package com.vaadin.flow.component.listbox.test;
 import java.util.Arrays;
 import java.util.List;
 
-import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.NativeButton;
 import com.vaadin.flow.component.listbox.ListBox;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -33,7 +33,7 @@ public class ListBoxRetainValuePage extends VerticalLayout {
         ListBox<String> listBox = new ListBox<>();
         listBox.setItems(listBoxItems);
         listBox.setValue("2");
-        Button addButton = new Button("add");
+        NativeButton addButton = new NativeButton("add");
         addButton.setId("button");
         Div value = new Div();
         value.setId("value");
