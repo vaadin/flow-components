14078cf38a97b4f0567a10922ea5105a1c685d5c
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2021-04-30 06:49:59 +0200

    Fix code formatting

--- vaadin-grid-flow-parent/vaadin-grid-flow-integration-tests/src/main/java/com/vaadin/flow/component/grid/it/RecalculateColumnWidthsPage.java
+++ vaadin-grid-flow-parent/vaadin-grid-flow-integration-tests/src/main/java/com/vaadin/flow/component/grid/it/RecalculateColumnWidthsPage.java
@@ -53,16 +53,16 @@ public class RecalculateColumnWidthsPage extends VerticalLayout {
         button.addClickListener(event -> {
 
             ts2.b = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr,"
-                + " sed diam nonumy eirmod tempor invidunt ut labore et dolore"
-                + " magna aliquyam erat, sed diam voluptua. At vero eos et"
-                + " accusam et justo duo dolores et ea rebum. Stet clita kasd"
-                + " gubergren, no sea takimata sanctus est Lorem ipsum dolor"
-                + " sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing"
-                + " elitr, sed diam nonumy eirmod tempor invidunt ut labore et"
-                + " dolore magna aliquyam erat, sed diam voluptua. At vero eos"
-                + " et accusam et justo duo dolores et ea rebum. Stet clita"
-                + " kasd gubergren, no sea takimata sanctus est Lorem ipsum"
-                + " dolor sit amet.";
+                    + " sed diam nonumy eirmod tempor invidunt ut labore et dolore"
+                    + " magna aliquyam erat, sed diam voluptua. At vero eos et"
+                    + " accusam et justo duo dolores et ea rebum. Stet clita kasd"
+                    + " gubergren, no sea takimata sanctus est Lorem ipsum dolor"
+                    + " sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing"
+                    + " elitr, sed diam nonumy eirmod tempor invidunt ut labore et"
+                    + " dolore magna aliquyam erat, sed diam voluptua. At vero eos"
+                    + " et accusam et justo duo dolores et ea rebum. Stet clita"
+                    + " kasd gubergren, no sea takimata sanctus est Lorem ipsum"
+                    + " dolor sit amet.";
 
             grid1.getDataProvider().refreshAll();
             grid1.recalculateColumnWidths();
