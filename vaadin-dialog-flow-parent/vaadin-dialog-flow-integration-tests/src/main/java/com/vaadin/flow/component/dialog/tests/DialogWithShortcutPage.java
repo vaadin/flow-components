642d0f9290000f88875e97cb24a3e01f3a907d15
Pekka Hyvönen <pekka@vaadin.com>
2021-05-18 10:30:28 +0300

    chore: add IT for dialog shortcut and writing (#761)
    When a shortcut is added with listenOn(dialog), writing to an input
    (like TextField) inside the dialog should not be blocked.
    
    Related to vaadin/flow#10362
    
    Co-authored-by: Manolo Carrasco <manolo@vaadin.com>
    Co-authored-by: Tulio Garcia <28783969+tulioag@users.noreply.github.com>

--- vaadin-dialog-flow-parent/vaadin-dialog-flow-integration-tests/src/main/java/com/vaadin/flow/component/dialog/tests/DialogWithShortcutPage.java
+++ vaadin-dialog-flow-parent/vaadin-dialog-flow-integration-tests/src/main/java/com/vaadin/flow/component/dialog/tests/DialogWithShortcutPage.java
@@ -7,6 +7,7 @@ import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.ShortcutRegistration;
 import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Input;
@@ -18,13 +19,13 @@ import com.vaadin.flow.router.Route;
 public class DialogWithShortcutPage extends VerticalLayout {
 
     public static final Key SHORTCUT_KEY = Key.KEY_X;
-    public static final String SHORTCUT = String.join("",
-            SHORTCUT_KEY.getKeys());
+    public static final String SHORTCUT = "x";
     public static final String EVENT_LOG = "event-log";
     public static final String UI_BUTTON = "ui-button";
     public static final String MODELESS_SHORTCUT_ON_UI = "modeless-shortcut-on-ui";
     public static final String MODELESS_SHORTCUT_LISTEN_ON_DIALOG = "modeless-shortcur-listen-on-dialog";
     public static final String LISTEN_ON_DIALOG = "listen-on-dialog";
+    public static final String LISTEN_ON_DIALOG_ALLOW_DEFAULT = "listen-on-dialog-allow-default";
     public static final String SHORTCUT_ON_UI = "shortcut-on-ui";
     public static final String DIALOG_ID = "dialog";
     public static final String REUSABLE_DIALOG = "reusable-dialog";
@@ -41,24 +42,30 @@ public class DialogWithShortcutPage extends VerticalLayout {
         eventLog.setId(EVENT_LOG);
         final NativeButton modelessWithShortcutOnUi = new NativeButton(
                 "Modeless with shortcut on UI",
-                e -> createAndOpenDialog(false).setModal(false));
+                e -> createAndOpenDialog(false, true).setModal(false));
         modelessWithShortcutOnUi.setId(MODELESS_SHORTCUT_ON_UI);
         final NativeButton modelessWithShortcutListenOnDialog = new NativeButton(
                 "Modeless with shortcut listenOn(dialog)",
-                e -> createAndOpenDialog(true).setModal(false));
+                e -> createAndOpenDialog(true, true).setModal(false));
         modelessWithShortcutListenOnDialog
                 .setId(MODELESS_SHORTCUT_LISTEN_ON_DIALOG);
         final NativeButton dialogWithShortcutListenOnDialog = new NativeButton(
                 "Dialog with shortcut listenOn(dialog)",
-                e -> createAndOpenDialog(true));
+                e -> createAndOpenDialog(true, true));
         dialogWithShortcutListenOnDialog.setId(LISTEN_ON_DIALOG);
+        final NativeButton dialogWithShortcutListenOnDialogAllowBrowserDefault = new NativeButton(
+                "Dialog with shortcut listenOn(dialog).allowBrowserDefault()",
+                e -> createAndOpenDialog(true, false));
+        dialogWithShortcutListenOnDialogAllowBrowserDefault
+                .setId(LISTEN_ON_DIALOG_ALLOW_DEFAULT);
         final NativeButton dialogWithShortcutOnUi = new NativeButton(
-                "Dialog with shortcut on UI", e -> createAndOpenDialog(false));
+                "Dialog with shortcut on UI",
+                e -> createAndOpenDialog(false, true));
         dialogWithShortcutOnUi.setId(SHORTCUT_ON_UI);
         final NativeButton reusableDialogButton = new NativeButton(
                 "Reusable dialog", event -> {
                     if (reusableDialog == null) {
-                        reusableDialog = createAndOpenDialog(true);
+                        reusableDialog = createAndOpenDialog(true, true);
                     } else {
                         reusableDialog.open();
                     }
@@ -66,6 +73,7 @@ public class DialogWithShortcutPage extends VerticalLayout {
         reusableDialogButton.setId(REUSABLE_DIALOG);
         add(modelessWithShortcutOnUi, modelessWithShortcutListenOnDialog,
                 dialogWithShortcutOnUi, dialogWithShortcutListenOnDialog,
+                dialogWithShortcutListenOnDialogAllowBrowserDefault,
                 reusableDialogButton);
 
         NativeButton nonDialogButton = new NativeButton(
@@ -82,13 +90,16 @@ public class DialogWithShortcutPage extends VerticalLayout {
         attachEvent.getUI().setId(UI_ID);
     }
 
-    private Dialog createAndOpenDialog(boolean listenOnDialog) {
+    private Dialog createAndOpenDialog(boolean listenOnDialog,
+            boolean preventDefault) {
         int index = dialogCounter++;
         final String dialogId = DIALOG_ID + index;
         NativeButton myDialogButton = createDialogButton();
         myDialogButton.setId(dialogId + "-button");
+        final ComboBox<String> comboBox = new ComboBox<>();
+        comboBox.setItems("foo", "bar", "xxx");
         Dialog dialog = new Dialog(new Div(new Div(new Text("" + index)),
-                myDialogButton, new Input()));
+                myDialogButton, new Input(), comboBox));
         NativeButton closeButton = new NativeButton("Close",
                 buttonClickEvent -> dialog.close());
         dialog.add(closeButton);
@@ -100,6 +111,7 @@ public class DialogWithShortcutPage extends VerticalLayout {
         if (listenOnDialog) {
             registration.listenOn(dialog);
         }
+        registration.setBrowserDefaultAllowed(!preventDefault);
         return dialog;
     }
 
