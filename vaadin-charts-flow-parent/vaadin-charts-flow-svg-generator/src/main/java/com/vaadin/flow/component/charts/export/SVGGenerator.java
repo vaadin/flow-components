49c515f0e22ad3cfa852965c539744e6c331e3b3
diegocardoso <diego@vaadin.com>
2021-06-01 13:02:33 +0300

    style: use formatter

--- vaadin-charts-flow-parent/vaadin-charts-flow-svg-generator/src/main/java/com/vaadin/flow/component/charts/export/SVGGenerator.java
+++ vaadin-charts-flow-parent/vaadin-charts-flow-svg-generator/src/main/java/com/vaadin/flow/component/charts/export/SVGGenerator.java
@@ -2,7 +2,7 @@ package com.vaadin.flow.component.charts.export;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException; 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -12,6 +12,8 @@ import java.util.List;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.LoggerFactory;
 
+import com.vaadin.flow.component.charts.model.Configuration;
+import com.vaadin.flow.component.charts.util.ChartSerialization;
 import com.vaadin.flow.server.frontend.FrontendTools;
 import com.vaadin.flow.server.frontend.FrontendUtils;
 
@@ -19,21 +21,24 @@ public class SVGGenerator {
 
     public static String generateSVG(Configuration chartConfiguration) {
         assert chartConfiguration != null;
-        String chartConfigurationString = ChartSerialization.toJSON(chartConfiguration);
-        
+        String chartConfigurationString = ChartSerialization
+                .toJSON(chartConfiguration);
+
         try {
             Path tempDirPath = Files.createTempDirectory("svg-export");
-            File bundleTempFile = Files.createFile(tempDirPath.resolve("export-svg-bundle.js")).toFile();
+            File bundleTempFile = Files
+                    .createFile(tempDirPath.resolve("export-svg-bundle.js"))
+                    .toFile();
 
             tempDirPath.toFile().deleteOnExit();
             try (FileOutputStream out = new FileOutputStream(bundleTempFile)) {
-                IOUtils.copy(SVGGenerator.class
-                        .getResourceAsStream("/META-INF/frontend/generated/jsdom-exporter-bundle.js"), out);
+                IOUtils.copy(SVGGenerator.class.getResourceAsStream(
+                        "/META-INF/frontend/generated/jsdom-exporter-bundle.js"),
+                        out);
 
-                String command = String.format("const hc = require('%s');\n"
-                                + "hc({\n"
-                                + "options: %s,\n"
-                                + "outfile: 'chart.svg',\n"
+                String command = String.format(
+                        "const hc = require('%s');\n" + "hc({\n"
+                                + "options: %s,\n" + "outfile: 'chart.svg',\n"
                                 + "})",
                         bundleTempFile.getAbsolutePath().replace("\\", "/"),
                         chartConfigurationString);
@@ -48,13 +53,16 @@ public class SVGGenerator {
             }
         } catch (Exception e) {
             e.printStackTrace();
-            LoggerFactory.getLogger(SVGGenerator.class).error("Unable to create temp file for bundle", e);
+            LoggerFactory.getLogger(SVGGenerator.class)
+                    .error("Unable to create temp file for bundle", e);
         }
         return "";
     }
- 
-    private static int runNodeScript(String script) throws InterruptedException, IOException {
-        FrontendTools tools = new FrontendTools("", () -> FrontendUtils.getVaadinHomeDirectory().getAbsolutePath());
+
+    private static int runNodeScript(String script)
+            throws InterruptedException, IOException {
+        FrontendTools tools = new FrontendTools("",
+                () -> FrontendUtils.getVaadinHomeDirectory().getAbsolutePath());
         String node = tools.getNodeExecutable();
         List<String> command = new ArrayList<>();
         command.add(node);
