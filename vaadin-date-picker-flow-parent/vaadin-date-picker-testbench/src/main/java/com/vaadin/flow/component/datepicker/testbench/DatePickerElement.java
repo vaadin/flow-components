c5d6bdc7c4849e410f44f2489f21fcdc3524f6dd
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2021-06-07 09:53:44 +0200

    fix: update date picker overlay after setting I18N settings (#1742) (CP: 14.7)
    Fixes: #1411
    Warranty: Fixes date picker overlay not refreshing after changing I18N settings
    
    * fix: update date picker overlay after setting I18N settings
    
    * Extract date picker element helpers into TestBench element
    
    * Add test for partial i18n configs
    
    * Fix SonarQube issues
    
    * Simplify shadow DOM interactions with test bench queries
    
    (cherry picked from commit ec6054541b6fe1df6d40b044dc89059a9d3edbb2)

--- vaadin-date-picker-flow-parent/vaadin-date-picker-testbench/src/main/java/com/vaadin/flow/component/datepicker/testbench/DatePickerElement.java
+++ vaadin-date-picker-flow-parent/vaadin-date-picker-testbench/src/main/java/com/vaadin/flow/component/datepicker/testbench/DatePickerElement.java
@@ -15,12 +15,15 @@
  */
 package com.vaadin.flow.component.datepicker.testbench;
 
-import java.time.LocalDate;
-
+import com.vaadin.flow.component.button.testbench.ButtonElement;
 import com.vaadin.testbench.HasHelper;
 import com.vaadin.testbench.HasLabel;
 import com.vaadin.testbench.TestBenchElement;
 import com.vaadin.testbench.elementsbase.Element;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * A TestBench element representing a <code>&lt;vaadin-date-picker&gt;</code>
@@ -29,6 +32,65 @@ import com.vaadin.testbench.elementsbase.Element;
 @Element("vaadin-date-picker")
 public class DatePickerElement extends TestBenchElement
         implements HasLabel, HasHelper {
+
+    public static class OverlayContentElement extends TestBenchElement {
+        /**
+         * Gets all visible month calendars that are currently rendered by the
+         * infinite scroller in the overlay.
+         *
+         * @return
+         */
+        public List<MonthCalendarElement> getVisibleMonthCalendars() {
+            return this.$("vaadin-month-calendar").all().stream()
+                    .map(el -> el.wrap(MonthCalendarElement.class))
+                    .collect(Collectors.toList());
+        }
+
+        /**
+         * Gets the today button from the overlays toolbar
+         *
+         * @return
+         */
+        public ButtonElement getTodayButton() {
+            return this.$(ButtonElement.class).attribute("part", "today-button")
+                    .first();
+        }
+
+        /**
+         * Gets the cancel button from the overlays toolbar
+         *
+         * @return
+         */
+        public ButtonElement getCancelButton() {
+            return this.$(ButtonElement.class)
+                    .attribute("part", "cancel-button").first();
+        }
+    }
+
+    public static class MonthCalendarElement extends TestBenchElement {
+        /**
+         * Gets the header text of the month calendar, e.g. `January 1999`
+         *
+         * @return
+         */
+        public String getHeaderText() {
+            return this.$(TestBenchElement.class)
+                    .attribute("part", "month-header").first().getText();
+        }
+
+        /**
+         * Gets the weekday headers that are rendered by the month calendar
+         *
+         * @return
+         */
+        public List<WeekdayElement> getWeekdays() {
+            return this.$(WeekdayElement.class).attribute("part", "weekday")
+                    .all();
+        }
+    }
+
+    public static class WeekdayElement extends TestBenchElement {
+    }
 
     /**
      * Clears the value of the date picker.
@@ -90,6 +152,27 @@ public class DatePickerElement extends TestBenchElement
     }
 
     /**
+     * Opens the overlay, sets the value to the inner input element as a string
+     * and closes the overlay. This simulates the user typing into the input and
+     * triggering an update of the value property.
+     */
+    public void setInputValue(String value) {
+        this.open();
+        setProperty("_inputValue", value);
+        this.close();
+    }
+
+    /**
+     * Gets the visible presentation value from the inner input element as a
+     * string. This value depends on the used Locale.
+     *
+     * @return
+     */
+    public String getInputValue() {
+        return getPropertyString("_inputValue");
+    }
+
+    /**
      * When auto open is enabled, the dropdown will open when the field is
      * clicked.
      *
@@ -98,5 +181,32 @@ public class DatePickerElement extends TestBenchElement
      */
     public boolean isAutoOpen() {
         return !getPropertyBoolean("autoOpenDisabled");
+    }
+
+    /**
+     * Opens the date picker overlay
+     */
+    public void open() {
+        executeScript("arguments[0].open();", this);
+    }
+
+    /**
+     * Closes the date picker overlay
+     */
+    public void close() {
+        executeScript("arguments[0].close();", this);
+    }
+
+    /**
+     * Gets the content of the first date picker overlay on the page Should only
+     * be used with a single date picker at a time, there is no check that the
+     * overlay belongs to this specific date picker
+     *
+     * @return
+     */
+    public OverlayContentElement getOverlayContent() {
+        return this.$("vaadin-date-picker-overlay").onPage().waitForFirst()
+                .$(TestBenchElement.class).id("content")
+                .$(OverlayContentElement.class).id("overlay-content");
     }
 }
