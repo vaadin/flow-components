44285f31291193bc4e06dd3187e9ab5ca3c514bd
ZheSun88 <zhe@vaadin.com>
2021-06-09 11:58:41 +0300

    chore:use driver manager to component test (14.7)

--- vaadin-flow-components-shared/src/main/java/com/vaadin/tests/AbstractValidationTest.java
+++ vaadin-flow-components-shared/src/main/java/com/vaadin/tests/AbstractValidationTest.java
@@ -1,6 +1,9 @@
 package com.vaadin.tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.List;
@@ -9,6 +12,11 @@ public abstract class AbstractValidationTest
         extends com.vaadin.flow.testutil.AbstractValidationTest {
 
     private static SharedBrowser browser = new SharedBrowser();
+
+    @BeforeClass
+    public static void setupClass() {
+        ParallelTest.setupClass();
+    }
 
     @Override
     public void setup() throws Exception {
