6209ec358ffb63abf64fd0347cebc75482266d10
Tomi Virkki <virkki@vaadin.com>
2021-06-14 10:36:25 +0300

    test: fix a test case

--- vaadin-icons-flow-parent/vaadin-icons-flow-integration-tests/src/test/java/com/vaadin/flow/component/icon/tests/IconIT.java
+++ vaadin-icons-flow-parent/vaadin-icons-flow-integration-tests/src/test/java/com/vaadin/flow/component/icon/tests/IconIT.java
@@ -88,9 +88,33 @@ public class IconIT extends AbstractComponentIT {
             WebElement label = labels.get(i);
             WebElement icon = icons.get(i);
             String enumName = VaadinIcon.values()[i].name();
+            String normalizedIconName = normalize(VaadinIcon.values()[i])
+                    .name();
             Assert.assertEquals(enumName, label.getText());
             assertIconProperty(icon, "vaadin",
-                    enumName.toLowerCase().replace('_', '-'));
+                    normalizedIconName.toLowerCase().replace('_', '-'));
+        }
+    }
+
+    /**
+     * In case a the given icon is one of the deprecated values, the
+     * corresponding updated VaadinIcon is returned. Otherwise the given
+     * VaadinIcon is returned as such.
+     */
+    private static VaadinIcon normalize(VaadinIcon icon) {
+        switch (icon) {
+        case BUSS:
+            return VaadinIcon.BUS;
+        case FUNCION:
+            return VaadinIcon.FUNCTION;
+        case MEGAFONE:
+            return VaadinIcon.MEGAPHONE;
+        case PALETE:
+            return VaadinIcon.PALETTE;
+        case TRENDIND_DOWN:
+            return VaadinIcon.TRENDING_DOWN;
+        default:
+            return icon;
         }
     }
 
