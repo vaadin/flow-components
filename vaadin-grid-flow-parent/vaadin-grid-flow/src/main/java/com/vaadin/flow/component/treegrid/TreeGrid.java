ed484dd75b924233a753a0390ead708ff8b56032
David Sosa <sosa@vaadin.com>
2021-07-08 16:06:14 +0300

    Run formatter

--- vaadin-grid-flow-parent/vaadin-grid-flow/src/main/java/com/vaadin/flow/component/treegrid/TreeGrid.java
+++ vaadin-grid-flow-parent/vaadin-grid-flow/src/main/java/com/vaadin/flow/component/treegrid/TreeGrid.java
@@ -942,10 +942,12 @@ public class TreeGrid<T> extends Grid<T>
     }
 
     /**
-     * The effective index of an item depends on the complete hierarchy of the tree.
-     * {@link TreeGrid} uses lazy loading for performance reasons and does not know about the complete hierarchy.
-     * Without the knowledge of the complete hierarchy, {@link TreeGrid} can’t reliably calculate an exact scroll position.
-     * <b>This uncertainty makes this method unreliable and so should be avoided.</b>
+     * The effective index of an item depends on the complete hierarchy of the
+     * tree. {@link TreeGrid} uses lazy loading for performance reasons and does
+     * not know about the complete hierarchy. Without the knowledge of the
+     * complete hierarchy, {@link TreeGrid} can’t reliably calculate an exact
+     * scroll position. <b>This uncertainty makes this method unreliable and so
+     * should be avoided.</b>
      *
      * @param rowIndex
      *            zero based index of the item to scroll to in the current view.
