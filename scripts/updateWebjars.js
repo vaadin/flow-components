87d6399a237a5286e876a7db89d3b5289b61fe41
Manuel Carrasco Moñino <manolo@vaadin.com>
2021-07-14 09:28:03 +0200

    chore: fix pom formatting in scripts, align with master [skip ci] (#1896)
    * chore: remove unneeded tag in templates
    
    * chore: fix pom formatting in scripts. Align with master
    
    * chore: re-generate pom files
    
    * chore: fix bower pom generation

--- scripts/updateWebjars.js
+++ scripts/updateWebjars.js
@@ -30,8 +30,8 @@ async function updateWebjars(pom){
 
     //collect vaadin webjars
     //if there is dependencyManagement section, collect both places
-    const deps = pomJs.project.dependencyManagement ? 
-          [].concat(pomJs.project.dependencyManagement[0].dependencies[0].dependency, pomJs.project.dependencies[0].dependency): 
+    const deps = pomJs.project.dependencyManagement ?
+          [].concat(pomJs.project.dependencyManagement[0].dependencies[0].dependency, pomJs.project.dependencies[0].dependency):
           pomJs.project.dependencies[0].dependency;
 
     const webjars = deps.filter(dep => dep.groupId == vaadinWebjarGroupId);
@@ -44,16 +44,16 @@ async function updateWebjars(pom){
 
       if (updatedVersion != webjarVersion){
         updateFile = true;
-        webjars[j].version = updatedVersion; 
+        webjars[j].version = updatedVersion;
 
-			  if (pomJs.project.dependencyManagement){
-				  pomJs.project.dependencyManagement[0].dependencies[0].dependency.filter(dep => {
-					  if (dep.artifactId == webjarName){
-						  dep.version = updatedVersion;
-						  console.log('\x1b[33m', "In "+name+' dependencyManagement,' +dep.artifactId+ " has been updated from "+ webjarVersion +" to " + updatedVersion);
-  					}
-	  			})
-		  	}
+              if (pomJs.project.dependencyManagement){
+                  pomJs.project.dependencyManagement[0].dependencies[0].dependency.filter(dep => {
+                      if (dep.artifactId == webjarName){
+                          dep.version = updatedVersion;
+                          console.log('\x1b[33m', "In "+name+' dependencyManagement,' +dep.artifactId+ " has been updated from "+ webjarVersion +" to " + updatedVersion);
+                      }
+                  })
+              }
 
         pomJs.project.dependencies[0].dependency.filter(dep => {
           if (dep.artifactId == webjarName){
@@ -63,24 +63,24 @@ async function updateWebjars(pom){
         })
       }
     }
-    
+
     for (k=0; k<proComponentsWebjars.length; k++){
       const proWebjarName = proComponentsWebjars[k].artifactId +'';
       const proWebjarVersion = proComponentsWebjars[k].version +'';
       const proWebjarUpdatedVersion = await findProComponentUpdatedVersion(proWebjarName, proWebjarVersion);
-      
+
       if (proWebjarUpdatedVersion != proWebjarVersion){
         updateFile = true;
-        proComponentsWebjars[k].version = proWebjarUpdatedVersion; 
+        proComponentsWebjars[k].version = proWebjarUpdatedVersion;
 
         if (pomJs.project.dependencyManagement){
-				  pomJs.project.dependencyManagement[0].dependencies[0].dependency.filter(dep => {
-					  if (dep.artifactId == proWebjarName){
-						  dep.version = proWebjarUpdatedVersion;
-						  console.log('\x1b[33m', "[PRO Component]In "+name+' dependencyManagement,' +dep.artifactId+ " has been updated from "+ proWebjarVersion +" to " + proWebjarUpdatedVersion);
-					  }
-				  })
-			  }
+                  pomJs.project.dependencyManagement[0].dependencies[0].dependency.filter(dep => {
+                      if (dep.artifactId == proWebjarName){
+                          dep.version = proWebjarUpdatedVersion;
+                          console.log('\x1b[33m', "[PRO Component]In "+name+' dependencyManagement,' +dep.artifactId+ " has been updated from "+ proWebjarVersion +" to " + proWebjarUpdatedVersion);
+                      }
+                  })
+              }
 
         pomJs.project.dependencies[0].dependency.filter(dep => {
           if (dep.artifactId == proWebjarName){
@@ -92,9 +92,9 @@ async function updateWebjars(pom){
     }
 
     if(updateFile){
-        const updatedPom = new xml2js.Builder().buildObject(pomJs);
+        const updatedPom = new xml2js.Builder({renderOpts: {pretty: true, indent: '    '}}).buildObject(pomJs);
         console.log('\x1b[32m', `Writing ${pom}`);
-        fs.writeFileSync(pom, updatedPom + '\n', 'utf8'); 
+        fs.writeFileSync(pom, updatedPom + '\n', 'utf8');
     } else {
         console.log("\x1b[0m",'No need to updated '+ name);
     }
@@ -104,28 +104,28 @@ async function findProComponentUpdatedVersion(name, version) {
     let verArray = version.split('.');
     let prereleaseUrl = "https://maven.vaadin.com/vaadin-prereleases/com/vaadin/webjar/"+name+"/maven-metadata.xml";
     let releaseUrl = "https://repo1.maven.org/maven2/com/vaadin/webjar/"+name+"/maven-metadata.xml";
-    prereleaseXml = await getMetadata(prereleaseUrl, name);    
+    prereleaseXml = await getMetadata(prereleaseUrl, name);
     prereleaseXml = await xml2js.parseStringPromise(prereleaseXml, 'utf8');
-    
+
     releaseXml = await getMetadata(releaseUrl, name);
     releaseXml = await xml2js.parseStringPromise(releaseXml, 'utf8');
-    
-    let aviVersions = releaseXml.metadata.versioning[0].versions[0].version; 
+
+    let aviVersions = releaseXml.metadata.versioning[0].versions[0].version;
     let minorVersions = aviVersions.filter(version => version.startsWith([verArray[0],verArray[1]].join('.')));
-    
+
     if (minorVersions.length > 0){
       return minorVersions[minorVersions.length-1];
     } else {
-      let aviPreVersions = prereleaseXml.metadata.versioning[0].versions[0].version; 
+      let aviPreVersions = prereleaseXml.metadata.versioning[0].versions[0].version;
       let minorPreVersions = aviPreVersions.filter(version => version.startsWith([verArray[0],verArray[1]].join('.')));
-      
+
       return minorPreVersions[minorPreVersions.length-1];
     }
 }
 
 /**
  * Return the latest patch version from maven central
- * 
+ *
  * @param {*} name webjar artifactId
  * @param {*} version current webjar version
  */
@@ -134,8 +134,8 @@ async function findUpdatedVersion(name, version){
     let url = "https://repo1.maven.org/maven2/org/webjars/bowergithub/vaadin/"+name+"/maven-metadata.xml"
     xml = await getMetadata(url, name);
     xml = await xml2js.parseStringPromise(xml, 'utf8');
-  
-    let aviVersions = xml.metadata.versioning[0].versions[0].version; 
+
+    let aviVersions = xml.metadata.versioning[0].versions[0].version;
     let minorVersions = aviVersions.filter(version => version.startsWith([verArray[0],verArray[1]].join('.')));
 
     return minorVersions[minorVersions.length-1]
