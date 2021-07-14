0537ba3e95ac0d8c7210c465a43ec8a037ec5873
Manuel Carrasco Moñino <manolo@vaadin.com>
2021-07-14 09:28:03 +0200

    chore: fix pom formatting in scripts, align with master [skip ci] (#1896)
    * chore: remove unneeded tag in templates
    
    * chore: fix pom formatting in scripts. Align with master
    
    * chore: re-generate pom files
    
    * chore: fix bower pom generation

--- scripts/updateJavaPOMs.js
+++ scripts/updateJavaPOMs.js
@@ -8,15 +8,11 @@
 const xml2js = require('xml2js');
 const fs = require('fs');
 const path = require('path');
-const { version } = require('os');
-const exec = require('util').promisify(require('child_process').exec);
 
 const templateDir = path.dirname(process.argv[1]) + '/templates';
 const mod = process.argv[2] || process.exit(1);
 const name = mod.replace('-flow-parent', '');
-const propertyName = artifactId2versionName(name);
 const componentName = name.replace('vaadin-', '');
-const versionName = `version.${name.replace(/-/g, '.')}`;
 const desc = name.split('-').map(w => w.replace(/./, m => m.toUpperCase())).join(' ');
 const proComponents = ['board',
                        'charts',
@@ -25,44 +21,6 @@ const proComponents = ['board',
                        'crud',
                        'grid-pro',
                        'rich-text-editor'];
-let rootJs;
-let rootVersion;
-let componentVersion;
-let originalVersion;
-let oldVersionSchema;
-let versions;
-
-function artifactId2versionName(artifactId) {
-  return `${artifactId.replace(/-/g, '.')}.version`;
-}
-
-async function readVersions() {
-  const { stdout, stderr } = await exec('node ./scripts/getVersions.js --json');
-  versions = JSON.parse(stdout);
-}
-
-async function readRootPoms() {
-  rootJs = await xml2js.parseStringPromise(fs.readFileSync('pom.xml', 'utf8'));
-  rootVersion = rootJs.project.version[0];
-  const projectVersion = (await xml2js.parseStringPromise(fs.readFileSync(`${mod}/pom.xml`, 'utf8'))).project.version;
-  if (projectVersion) {
-    originalVersion = projectVersion[0];
-  } else if (rootJs.project.properties[0][propertyName]) {
-    originalVersion = rootJs.project.properties[0][propertyName][0];
-  }
-  oldVersionSchema = (/^(14\.[3-4]|17\.0)/.test(rootVersion));
-  if (oldVersionSchema) {
-    componentVersion = `\$\{${artifactId2versionName(name)}\}`;
-  } else {
-    componentVersion = '${project.version}';
-  }
-}
-
-function mergeDependencies(prj1, prj2) {
-  const arr1 = prj1 && prj1.dependencies && prj1.dependencies[0] && prj1.dependencies[0].dependency || [];
-  const arr2 = prj2 && prj2.dependencies && prj2.dependencies[0] && prj2.dependencies[0].dependency || [];
-  return [...arr1, ...arr2.filter(a => !arr1.find(b => b.artifactId[0] === a.artifactId[0]))];
-}
 
 function renameComponent(array, name) {
   for(let i = 0; array && i < array.length; i++) {
@@ -75,31 +33,30 @@ async function renameBase(js) {
   renameComponent(js.project.artifactId, name);
   renameComponent(js.project.name, desc);
   renameComponent(js.project.description, desc);
-  js.project.parent[0].version = [originalVersion];
+
+  const parentJs = await xml2js.parseStringPromise(fs.readFileSync('pom.xml', 'utf8'));
+  js.project.parent[0].version = [parentJs.project.version[0]];
 }
 
 function setDependenciesVersion(dependencies) {
   dependencies && dependencies[0] && dependencies[0].dependency.forEach(dep => {
-    if (dep.groupId[0] === 'com.vaadin' 
-      && /^vaadin-.*(flow|testbench|demo)$/.test(dep.artifactId[0])
-      && !/shared/.test(dep.artifactId[0])
-      ) {
-      const version = oldVersionSchema ? artifactId2versionName(dep.artifactId[0].replace(/-(flow.*|testbench)$/, '')) : 'project.version';
-      dep.version = [`\$\{${version}\}`];
-    }
-    if (dep.artifactId[0] == 'vaadin-flow-components-shared') {
-      const version = oldVersionSchema ? 'vaadin.flow.components.shared.version' : 'project.version';
-      dep.version = [`\$\{${version}\}`];
+    if (dep.groupId[0] === 'com.vaadin' && /^vaadin-.*(flow|testbench)$/.test(dep.artifactId[0])) {
+      dep.version = [ '${project.version}' ];
     }
   });
   return dependencies;
 }
 
 function mergePlugins(build1, build2) {
-  return [
-    ... (build1 && build1[0] && build1[0].plugins && build1[0].plugins[0] && build1[0].plugins[0].plugin || []),
-    ... (build2 && build2[0] && build2[0].plugins && build2[0].plugins[0] && build2[0].plugins[0].plugin || [])
-  ]
+  const arr1 = build1 && build1[0] && build1[0].plugins && build1[0].plugins[0] && build1[0].plugins[0].plugin || [];
+  const arr2 = build2 && build2[0] && build2[0].plugins && build2[0].plugins[0] && build2[0].plugins[0].plugin || [];
+  return [...arr1, ...arr2.filter(a => !arr1.find(b => b.artifactId[0] === a.artifactId[0]))];
+}
+
+function mergeDependencies(prj1, prj2) {
+  const arr1 = prj1 && prj1.dependencies && prj1.dependencies[0] && prj1.dependencies[0].dependency || [];
+  const arr2 = prj2 && prj2.dependencies && prj2.dependencies[0] && prj2.dependencies[0].dependency || [];
+  return [...arr1, ...arr2.filter(a => !arr1.find(b => b.artifactId[0] === a.artifactId[0]))];
 }
 
 async function consolidate(template, pom, cb) {
@@ -108,49 +65,48 @@ async function consolidate(template, pom, cb) {
 
   await renameBase(tplJs);
 
+  tplJs.project.artifactId[0] = pomJs.project.artifactId[0] || tplJs.project.artifactId[0];
+
   pomJs.project.dependencies[0] = {dependency: mergeDependencies(tplJs.project, pomJs.project)};
 
   tplJs.project.dependencies = setDependenciesVersion(pomJs.project.dependencies);
-  tplJs.project.parent[0].version = [rootVersion];
-  if (oldVersionSchema) {
-    tplJs.project.version = [componentVersion];
-  } else {
-    delete tplJs.project.version;
-  }
+
   cb && cb(tplJs, pomJs);
 
-  const xml = new xml2js.Builder().buildObject(tplJs);
+  const xml = new xml2js.Builder({renderOpts: {pretty: true, indent: '    '}}).buildObject(tplJs);
   console.log(`writing ${pom}`);
-  fs.writeFileSync(pom, xml
-    // ident using 4 spaces to make sonar happy
-    .replace(/\n( +)</g, '\n$1$1<') + '\n', 'utf8');
+  fs.writeFileSync(pom, xml + '\n', 'utf8');
 }
 
 async function consolidatePomParent() {
   const template = proComponents.includes(componentName) ? 'pom-parent-pro.xml' : 'pom-parent.xml';
-  await consolidate(template, `${mod}/pom.xml`, js => {
-    renameComponent(js.project.modules[0].module, name);
-  
-    renameComponent(js.project.profiles[0].profile[0].modules[0].module, name);
-    js.project.parent[0].version = [rootVersion];
-    delete js.project.version;
+  consolidate(template, `${mod}/pom.xml`, (js, org)  => {
+
+    // Replace `component` key with actual component name
+    const modules = js.project.modules[0].module;
+    renameComponent(modules, name);
+    const itModules = js.project.profiles[0].profile[0].modules[0].module
+    renameComponent(itModules, name);
+
+    // add testbench if module exists
+    if (fs.existsSync(`${mod}/${name}-testbench/pom.xml`)) {
+      modules.push(`${name}-testbench`);
+    }
+    // add it's if module exists
+    if (fs.existsSync(`${mod}/${name}-flow-demo/pom.xml`)) {
+      modules.push(`${name}-flow-demo`);
+    }
+    // add bower it's if module exists
+    const bowerItModule = `${name}-flow-integration-tests/pom-bower-mode.xml`;
+    if (fs.existsSync(`${mod}/${bowerItModule}`)) {
+      itModules.indexOf(bowerItModule) < 0 && itModules.push(bowerItModule);
+    }
+
+    // add other modules present in original pom
+    org.project.modules[0].module.forEach(
+      mod => !/(flow|flow-demo|testbench|flow-integration-test)$/.test(mod) && modules.push(mod));
+
   });
-}
-async function consolidatePomFlow() {
-  const template = proComponents.includes(componentName) ? 'pom-flow-pro.xml' : 'pom-flow.xml';
-  consolidate(template, `${mod}/${name}-flow/pom.xml`, (tplJs, pomJs) => {
-      tplJs.project.build && (tplJs.project.build[0].plugins[0] = {plugin: mergePlugins(tplJs.project.build, pomJs.project.build)});
-    });
-}
-async function consolidatePomTB() {
-  await consolidate('pom-testbench.xml', `${mod}/${name}-testbench/pom.xml`)
-}
-async function consolidatePomDemo() {
-  const demoPom = `${mod}/${name}-flow-demo/pom.xml`;
-  fs.existsSync(demoPom) && consolidate('pom-demo.xml', demoPom);
-}
-async function consolidatePomIT() {
-  await consolidate('pom-integration-tests.xml', `${mod}/${name}-flow-integration-tests/pom.xml`);
 }
 
 async function consolidatePomBowerIT() {
@@ -158,31 +114,28 @@ async function consolidatePomBowerIT() {
  fs.existsSync(bowerITPom) && consolidate('pom-bower-mode.xml', bowerITPom);
 }
 
-async function saveRootPom() {
-  if (oldVersionSchema) {
-    console.log(`updating ${propertyName} = ${originalVersion} in root pom.xml`);
-    rootJs.project.properties[0]['vaadin.flow.components.shared.version'] = [rootVersion];
-    rootJs.project.properties[0][propertyName] = [versions[mod] + '-SNAPSHOT'];
-
-    const xml = new xml2js.Builder().buildObject(rootJs);
-    fs.writeFileSync('pom.xml', xml + '\n', 'utf8');
-  }
+async function consolidatePomFlow() {
+  const template = proComponents.includes(componentName) ? 'pom-flow-pro.xml' : 'pom-flow.xml';
+  consolidate(template, `${mod}/${name}-flow/pom.xml`, (tplJs, pomJs) => {
+    tplJs.project.build && (tplJs.project.build[0].plugins[0] = {plugin: mergePlugins(tplJs.project.build, pomJs.project.build)});
+  });
+}
+async function consolidatePomTB() {
+  const tbPom = `${mod}/${name}-testbench/pom.xml`;
+  fs.existsSync(tbPom) && await consolidate('pom-testbench.xml', tbPom)
+}
+async function consolidatePomDemo() {
+  const demoPom = `${mod}/${name}-flow-demo/pom.xml`;
+  fs.existsSync(demoPom) && await consolidate('pom-demo.xml', demoPom);
+}
+async function consolidatePomIT() {
+  const itPom = `${mod}/${name}-flow-integration-tests/pom.xml`;
+  consolidate('pom-integration-tests.xml', itPom);
 }
 
-function saveVersionVariable(name, version) {
-  return name;
-}
-
-async function main() {
-  await readVersions()
-  await readRootPoms();
-  await consolidatePomParent();
-  await consolidatePomFlow();
-  await consolidatePomTB();
-  await consolidatePomDemo();
-  await consolidatePomIT();
-  await consolidatePomBowerIT();
-  await saveRootPom();
-}
-
-main()
+consolidatePomParent();
+consolidatePomFlow();
+consolidatePomTB();
+consolidatePomDemo();
+consolidatePomIT();
+consolidatePomBowerIT();
