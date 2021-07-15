c47630146ac3a9a68e14ca7a22b3b98361a4f4db
David Sosa <76832183+sosa-vaadin@users.noreply.github.com>
2021-07-15 10:45:03 +0300

    docs: add warning about unreliable behaviour of scrollToIndex(int) in TreeGrid (#1882)
    Co-authored-by: Manuel Carrasco Moñino <manolo@vaadin.com>

--- vaadin-grid-flow-parent/vaadin-grid-flow/src/main/java/com/vaadin/flow/component/treegrid/TreeGrid.java
+++ vaadin-grid-flow-parent/vaadin-grid-flow/src/main/java/com/vaadin/flow/component/treegrid/TreeGrid.java
@@ -940,4 +940,20 @@ public class TreeGrid<T> extends Grid<T>
         }
         return (HierarchicalDataProvider<T, SerializablePredicate<T>>) super.getDataProvider();
     }
+
+    /**
+     * The effective index of an item depends on the complete hierarchy of the
+     * tree. {@link TreeGrid} uses lazy loading for performance reasons and does
+     * not know about the complete hierarchy. Without the knowledge of the
+     * complete hierarchy, {@link TreeGrid} can’t reliably calculate an exact
+     * scroll position. <b>This uncertainty makes this method unreliable and so
+     * should be avoided.</b>
+     *
+     * @param rowIndex
+     *            zero based index of the item to scroll to in the current view.
+     */
+    @Override
+    public void scrollToIndex(int rowIndex) {
+        super.scrollToIndex(rowIndex);
+    }
 }
