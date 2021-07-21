1033600bd8514d46803bfad9425b77aabb5c0ce3
Sascha Ißbrücker <sissbruecker@vaadin.com>
2021-07-21 15:11:54 +0200

    fix: allow partial I18N instances for Upload (#1906)
    * fix: allow partial I18N instances for Upload
    
    * fix code style issues

---
+++ vaadin-upload-flow-parent/vaadin-upload-flow-integration-tests/src/main/java/com/vaadin/flow/component/upload/tests/UploadView.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2000-2021 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ */
+package com.vaadin.flow.component.upload.tests;
+
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.upload.Upload;
+import com.vaadin.flow.component.upload.UploadI18N;
+import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
+import com.vaadin.flow.component.upload.receivers.MultiFileMemoryBuffer;
+import com.vaadin.flow.router.Route;
+
+/**
+ * View for {@link Upload} tests.
+ *
+ * @author Vaadin Ltd
+ */
+@Route("vaadin-upload")
+public class UploadView extends Div {
+
+    public UploadView() {
+        createSimpleUpload();
+    }
+
+    private void createSimpleUpload() {
+        Div output = new Div();
+        Div eventsOutput = new Div();
+
+        MultiFileMemoryBuffer buffer = new MultiFileMemoryBuffer();
+        Upload upload = new Upload(buffer);
+
+        upload.addSucceededListener(event -> {
+            try {
+                output.add(event.getFileName());
+                output.add(IOUtils.toString(
+                        buffer.getInputStream(event.getFileName()), "UTF-8"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            eventsOutput.add("-succeeded");
+        });
+        upload.addAllFinishedListener(event -> eventsOutput.add("-finished"));
+
+        upload.setMaxFileSize(500 * 1024);
+        upload.setId("test-upload");
+        output.setId("test-output");
+        eventsOutput.setId("test-events-output");
+
+        addCard("Simple in memory receiver", upload, output, eventsOutput);
+    }
+
+    private void addCard(String title, Component... components) {
+        Div container = new Div(components);
+        container.addComponentAsFirst(new H2(title));
+        add(container);
+    }
+
+}
