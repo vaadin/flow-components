88f7da7ba7d835a076139c09c430ed95de47edb3
shivansh29 <ashivanshbajpai@gmail.com>
2021-07-30 01:55:03 +0530

    first commit

--- vaadin-charts-flow-parent/vaadin-charts-flow/src/main/java/com/vaadin/flow/component/charts/util/ChartSerialization.java
+++ vaadin-charts-flow-parent/vaadin-charts-flow/src/main/java/com/vaadin/flow/component/charts/util/ChartSerialization.java
@@ -43,7 +43,7 @@ public class ChartSerialization implements Serializable {
 
     private static ObjectWriter jsonWriter;
 
-    final static ObjectWriter jsonWriterChartOptions;
+    static final ObjectWriter jsonWriterChartOptions;
 
     static {
         ObjectMapper defaultMapper = ChartSerialization.createObjectMapper();
@@ -55,6 +55,9 @@ public class ChartSerialization implements Serializable {
 
         // writer is thread safe so we can use a shared instance
         jsonWriter = createObjectMapper().writer();
+    }
+	
+	private ChartSerialization() {
     }
 
     /**
