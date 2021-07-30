1aaf2ea8805b03665a53ba2a09d05ba8f6551897
Guille <alvarezguille@users.noreply.github.com>
2021-07-30 16:58:00 +0300

    Fix: make axisRescaled null safe (#1938)
    Axis#setExtremes can be called with null params to reset to previous configuration.
    
    Fixes #1915

--- vaadin-charts-flow-parent/vaadin-charts-flow-integration-tests/src/test/java/com/vaadin/flow/component/charts/tests/DynamicExtremesIT.java
+++ vaadin-charts-flow-parent/vaadin-charts-flow-integration-tests/src/test/java/com/vaadin/flow/component/charts/tests/DynamicExtremesIT.java
@@ -21,8 +21,10 @@ import com.vaadin.flow.component.charts.examples.dynamic.DynamicExtremes;
 import com.vaadin.flow.component.charts.testbench.ChartElement;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertEquals;
 
 public class DynamicExtremesIT extends AbstractTBTest {
 
@@ -34,9 +36,14 @@ public class DynamicExtremesIT extends AbstractTBTest {
     @Test
     public void axisFunction_toggleExtremesPoint_pointHidden() {
         ChartElement chart = getChartElement();
+        WebElement toggleExtremesButton = findElement(
+                By.id("toggleExtremesButton"));
         int initialVisiblePointsCount = chart.getVisiblePoints().size();
-        findElement(By.id("toggleExtremesButton")).click();
+        toggleExtremesButton.click();
         assertNotEquals(initialVisiblePointsCount,
+                chart.getVisiblePoints().size());
+        toggleExtremesButton.click();
+        assertEquals(initialVisiblePointsCount,
                 chart.getVisiblePoints().size());
     }
 }
