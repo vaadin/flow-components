35e19d0bb8f150589b4c8ebe5289985853bccbba
Tomi Virkki <virkki@vaadin.com>
2021-08-17 16:21:29 +0300

    chore: run formatter on flow-component-renderer.js

--- vaadin-renderer-flow-parent/vaadin-renderer-flow/src/main/resources/META-INF/resources/frontend/flow-component-renderer.js
+++ vaadin-renderer-flow-parent/vaadin-renderer-flow/src/main/resources/META-INF/resources/frontend/flow-component-renderer.js
@@ -1,36 +1,39 @@
 import '@polymer/polymer/polymer-legacy.js';
 import { html } from '@polymer/polymer/lib/utils/html-tag.js';
 import { Debouncer } from '@polymer/polymer/lib/utils/debounce.js';
-import { animationFrame, idlePeriod } from '@polymer/polymer/lib/utils/async.js';
+import {
+  animationFrame,
+  idlePeriod,
+} from '@polymer/polymer/lib/utils/async.js';
 import { PolymerElement } from '@polymer/polymer/polymer-element.js';
 class FlowComponentRenderer extends PolymerElement {
   static get template() {
     return html`
-    <style>
-      @keyframes flow-component-renderer-appear {
-        to {
-          opacity: 1;
+      <style>
+        @keyframes flow-component-renderer-appear {
+          to {
+            opacity: 1;
+          }
         }
-      }
-      :host {
-        animation: 1ms flow-component-renderer-appear;
-      }
-    </style>
-    <slot></slot>
-`;
+        :host {
+          animation: 1ms flow-component-renderer-appear;
+        }
+      </style>
+      <slot></slot>
+    `;
   }
 
-  static get is() { return 'flow-component-renderer'; }
+  static get is() {
+    return 'flow-component-renderer';
+  }
   static get properties() {
     return {
       nodeid: Number,
-      appid: String
+      appid: String,
     };
   }
   static get observers() {
-    return [
-      '_attachRenderedComponentIfAble(appid, nodeid)'
-    ]
+    return ['_attachRenderedComponentIfAble(appid, nodeid)'];
   }
 
   connectedCallback() {
@@ -41,9 +44,9 @@ class FlowComponentRenderer extends PolymerElement {
   /* workaround for issue vaadin/flow#5025 */
   _runChrome72ShadowDomBugWorkaround() {
     const userAgent = navigator.userAgent;
-    if (userAgent && userAgent.match('Chrome\/')) {
+    if (userAgent && userAgent.match('Chrome/')) {
       // example: ... Chrome/72.0.3626.96 ...
-      const majorVersionString = userAgent.split('Chrome\/')[1].split('.')[0];
+      const majorVersionString = userAgent.split('Chrome/')[1].split('.')[0];
       if (majorVersionString && parseInt(majorVersionString) > 71) {
         const debouncedNotifyResize = this._getDebouncedNotifyResizeFunction();
 
@@ -77,34 +80,34 @@ class FlowComponentRenderer extends PolymerElement {
     // 2. assign a debounced proxy to notifyResize, if not yet there
     if (!component._debouncedNotifyResize) {
       component._debouncedNotifyResize = function () {
-        component.__debouncedNotifyResize =
-            Debouncer.debounce(
-                component.__debouncedNotifyResize, // initially undefined
-                animationFrame,
-                component.notifyResize);
-      }
+        component.__debouncedNotifyResize = Debouncer.debounce(
+          component.__debouncedNotifyResize, // initially undefined
+          animationFrame,
+          component.notifyResize
+        );
+      };
     }
     return component._debouncedNotifyResize;
   }
 
-  ready(){
-      super.ready();
-      this.addEventListener("click", function(event){
-          if (this.firstChild &&
-                  typeof this.firstChild.click === "function" &&
-                      event.target === this ){
-              event.stopPropagation();
-              this.firstChild.click();
-          }
-      });
-      this.addEventListener('animationend', this._onAnimationEnd);
+  ready() {
+    super.ready();
+    this.addEventListener('click', function (event) {
+      if (
+        this.firstChild &&
+        typeof this.firstChild.click === 'function' &&
+        event.target === this
+      ) {
+        event.stopPropagation();
+        this.firstChild.click();
+      }
+    });
+    this.addEventListener('animationend', this._onAnimationEnd);
   }
 
   _asyncAttachRenderedComponentIfAble() {
-    this._debouncer = Debouncer.debounce(
-      this._debouncer,
-      idlePeriod,
-      () => this._attachRenderedComponentIfAble()
+    this._debouncer = Debouncer.debounce(this._debouncer, idlePeriod, () =>
+      this._attachRenderedComponentIfAble()
     );
   }
 
@@ -117,11 +120,10 @@ class FlowComponentRenderer extends PolymerElement {
       if (!renderedComponent) {
         this._clear();
         this._asyncAttachRenderedComponentIfAble();
-      } else if (this.firstChild !== renderedComponent){
+      } else if (this.firstChild !== renderedComponent) {
         this.replaceChild(renderedComponent, this.firstChild);
         this._defineFocusTarget();
         this.onComponentRendered();
-
       } else {
         this._defineFocusTarget();
         this.onComponentRendered();
@@ -141,7 +143,11 @@ class FlowComponentRenderer extends PolymerElement {
     try {
       return window.Vaadin.Flow.clients[this.appid].getByNodeId(this.nodeid);
     } catch (error) {
-      console.error("Could not get node %s from app %s", this.nodeid, this.appid);
+      console.error(
+        'Could not get node %s from app %s',
+        this.nodeid,
+        this.appid
+      );
       console.error(error);
     }
     return null;
@@ -153,39 +159,42 @@ class FlowComponentRenderer extends PolymerElement {
     }
   }
 
-  onComponentRendered(){
+  onComponentRendered() {
     // subclasses can override this method to execute custom logic on resize
   }
 
   /* Setting the `focus-target` attribute to the first focusable descendant
   starting from the firstChild necessary for the focus to be delegated
   within the flow-component-renderer when used inside a vaadin-grid cell  */
-  _defineFocusTarget(){
+  _defineFocusTarget() {
     var focusable = this._getFirstFocusableDescendant(this.firstChild);
-    if(focusable !== null) {
+    if (focusable !== null) {
       focusable.setAttribute('focus-target', 'true');
     }
   }
 
-  _getFirstFocusableDescendant(node){
-    if(this._isFocusable(node)) {
+  _getFirstFocusableDescendant(node) {
+    if (this._isFocusable(node)) {
       return node;
     }
-    if ( !node.children ){
-        return null;
+    if (!node.children) {
+      return null;
     }
     for (var i = 0; i < node.children.length; i++) {
       var focusable = this._getFirstFocusableDescendant(node.children[i]);
-      if(focusable !== null) {
+      if (focusable !== null) {
         return focusable;
       }
     }
     return null;
   }
 
-  _isFocusable(node){
-    if (node.hasAttribute && typeof node.hasAttribute === 'function' &&
-            (node.hasAttribute("disabled") || node.hasAttribute("hidden"))) {
+  _isFocusable(node) {
+    if (
+      node.hasAttribute &&
+      typeof node.hasAttribute === 'function' &&
+      (node.hasAttribute('disabled') || node.hasAttribute('hidden'))
+    ) {
       return false;
     }
 
