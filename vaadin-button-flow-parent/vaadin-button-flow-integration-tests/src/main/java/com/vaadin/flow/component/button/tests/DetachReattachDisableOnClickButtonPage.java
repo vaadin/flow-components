4737bd3b3dec88b291f8572c2b1794d12ab7f806
Paola De Bartolo <paola.debartolo@gmail.com>
2021-08-19 11:51:05 -0300

    style: apply code formatting

--- vaadin-button-flow-parent/vaadin-button-flow-integration-tests/src/main/java/com/vaadin/flow/component/button/tests/DetachReattachDisableOnClickButtonPage.java
+++ vaadin-button-flow-parent/vaadin-button-flow-integration-tests/src/main/java/com/vaadin/flow/component/button/tests/DetachReattachDisableOnClickButtonPage.java
@@ -22,27 +22,27 @@ import com.vaadin.flow.router.Route;
 @Route("vaadin-button/detach-reattach-disable-on-click-button")
 public class DetachReattachDisableOnClickButtonPage extends Div {
 
-  public DetachReattachDisableOnClickButtonPage() {
-    Button disableOnClickButton = new Button("Disable on click");
-    disableOnClickButton.setId("disable-on-click");
-    disableOnClickButton.setDisableOnClick(true);
-    disableOnClickButton.addClickListener(
-        event -> {
-          try {
-            Thread.sleep(1500);
-          } catch (InterruptedException e) {
-            e.printStackTrace();
-          } finally {
-            event.getSource().setEnabled(true);
-          }
+    public DetachReattachDisableOnClickButtonPage() {
+        Button disableOnClickButton = new Button("Disable on click");
+        disableOnClickButton.setId("disable-on-click");
+        disableOnClickButton.setDisableOnClick(true);
+        disableOnClickButton.addClickListener(event -> {
+            try {
+                Thread.sleep(1500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                event.getSource().setEnabled(true);
+            }
         });
 
-    Button removeFromViewButton =
-        new Button("Remove from view", event -> remove(disableOnClickButton));
-    removeFromViewButton.setId("remove-from-view");
-    Button addToViewButton = new Button("Add to view", event -> add(disableOnClickButton));
-    addToViewButton.setId("add-to-view");
+        Button removeFromViewButton = new Button("Remove from view",
+                event -> remove(disableOnClickButton));
+        removeFromViewButton.setId("remove-from-view");
+        Button addToViewButton = new Button("Add to view",
+                event -> add(disableOnClickButton));
+        addToViewButton.setId("add-to-view");
 
-    add(removeFromViewButton, addToViewButton, disableOnClickButton);
-  }
+        add(removeFromViewButton, addToViewButton, disableOnClickButton);
+    }
 }
--- vaadin-button-flow-parent/vaadin-button-flow-integration-tests/src/test/java/com/vaadin/flow/component/button/tests/DetachReattachDisableOnClickButtonIT.java
+++ vaadin-button-flow-parent/vaadin-button-flow-integration-tests/src/test/java/com/vaadin/flow/component/button/tests/DetachReattachDisableOnClickButtonIT.java
@@ -26,70 +26,71 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 @TestPath("vaadin-button/detach-reattach-disable-on-click-button")
 public class DetachReattachDisableOnClickButtonIT extends AbstractComponentIT {
 
-  private ButtonElement getDisableOnClickButton() {
-    return $(ButtonElement.class).id("disable-on-click");
-  }
+    private ButtonElement getDisableOnClickButton() {
+        return $(ButtonElement.class).id("disable-on-click");
+    }
 
-  private void assertDisableOnClickButtonEnabled(ButtonElement disableOnClickButton) {
-    Assert.assertTrue(
-        "'Disable on click' button should be enabled", disableOnClickButton.isEnabled());
-  }
+    private void assertDisableOnClickButtonEnabled(
+            ButtonElement disableOnClickButton) {
+        Assert.assertTrue("'Disable on click' button should be enabled",
+                disableOnClickButton.isEnabled());
+    }
 
-  private void assertDisableOnClickButtonDisabled(ButtonElement disableOnClickButton) {
-    Assert.assertFalse(
-        "'Disable on click' button should be disabled", disableOnClickButton.isEnabled());
-  }
+    private void assertDisableOnClickButtonDisabled(
+            ButtonElement disableOnClickButton) {
+        Assert.assertFalse("'Disable on click' button should be disabled",
+                disableOnClickButton.isEnabled());
+    }
 
-  private void clickDisableOnClickButton(ButtonElement disableOnClickButton) {
-    getCommandExecutor().disableWaitForVaadin();
+    private void clickDisableOnClickButton(ButtonElement disableOnClickButton) {
+        getCommandExecutor().disableWaitForVaadin();
 
-    // Click 'Disable on click' button
-    disableOnClickButton.click();
+        // Click 'Disable on click' button
+        disableOnClickButton.click();
 
-    // Check 'Disable on click' button is disabled
-    assertDisableOnClickButtonDisabled(disableOnClickButton);
+        // Check 'Disable on click' button is disabled
+        assertDisableOnClickButtonDisabled(disableOnClickButton);
 
-    waitUntil(
-        ExpectedConditions.elementToBeClickable($(ButtonElement.class).id("disable-on-click")),
-        2000);
+        waitUntil(ExpectedConditions.elementToBeClickable(
+                $(ButtonElement.class).id("disable-on-click")), 2000);
 
-    // Check 'Disable on click' button is enabled again
-    assertDisableOnClickButtonEnabled(disableOnClickButton);
+        // Check 'Disable on click' button is enabled again
+        assertDisableOnClickButtonEnabled(disableOnClickButton);
 
-    getCommandExecutor().enableWaitForVaadin();
-  }
+        getCommandExecutor().enableWaitForVaadin();
+    }
 
-  @Test
-  public void detachReattachDisableOnClickButtonTest() {
-    open();
+    @Test
+    public void detachReattachDisableOnClickButtonTest() {
+        open();
 
-    ButtonElement disableOnClickButton = getDisableOnClickButton();
+        ButtonElement disableOnClickButton = getDisableOnClickButton();
 
-    // Check 'Disable on click' button should be enabled
-    assertDisableOnClickButtonEnabled(disableOnClickButton);
+        // Check 'Disable on click' button should be enabled
+        assertDisableOnClickButtonEnabled(disableOnClickButton);
 
-    // Remove 'Disable on click' button
-    ButtonElement removeFromViewButton = $(ButtonElement.class).id("remove-from-view");
-    removeFromViewButton.click();
+        // Remove 'Disable on click' button
+        ButtonElement removeFromViewButton = $(ButtonElement.class)
+                .id("remove-from-view");
+        removeFromViewButton.click();
 
-    waitUntil(
-        ExpectedConditions.numberOfElementsToBe(By.id("disable-on-click"), 0),
-        2000);
+        waitUntil(ExpectedConditions
+                .numberOfElementsToBe(By.id("disable-on-click"), 0), 2000);
 
-    // Re-attach 'Disable on click" button
-    ButtonElement addToViewButton = $(ButtonElement.class).id("add-to-view");
-    addToViewButton.click();
+        // Re-attach 'Disable on click" button
+        ButtonElement addToViewButton = $(ButtonElement.class)
+                .id("add-to-view");
+        addToViewButton.click();
 
-    waitUntil(
-        ExpectedConditions.numberOfElementsToBe(By.id("disable-on-click"), 1),
-        2000);
+        waitUntil(ExpectedConditions
+                .numberOfElementsToBe(By.id("disable-on-click"), 1), 2000);
 
-    disableOnClickButton = getDisableOnClickButton();
+        disableOnClickButton = getDisableOnClickButton();
 
-    // Check 'Disable on click' button is enabled
-    assertDisableOnClickButtonEnabled(disableOnClickButton);
+        // Check 'Disable on click' button is enabled
+        assertDisableOnClickButtonEnabled(disableOnClickButton);
 
-    // Click 'Disable on click' button
-    clickDisableOnClickButton(disableOnClickButton);
-  }
+        // Click 'Disable on click' button
+        clickDisableOnClickButton(disableOnClickButton);
+    }
 }
