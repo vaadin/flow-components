551a686b225c86a33a5ff4cf9089d8d29abb7464
Tatu Lund <tatu@vaadin.com>
2021-09-03 16:17:24 +0300

    Code formatting

--- vaadin-combo-box-flow-parent/vaadin-combo-box-flow/src/main/java/com/vaadin/flow/component/combobox/ComboBoxVariant.java
+++ vaadin-combo-box-flow-parent/vaadin-combo-box-flow/src/main/java/com/vaadin/flow/component/combobox/ComboBoxVariant.java
@@ -20,8 +20,7 @@ package com.vaadin.flow.component.combobox;
  */
 public enum ComboBoxVariant {
     LUMO_SMALL("small"), LUMO_ALIGN_CENTER("align-center"), LUMO_ALIGN_RIGHT(
-            "align-right"), LUMO_HELPER_ABOVE_FIELD(
-                    "helper-above-field");
+            "align-right"), LUMO_HELPER_ABOVE_FIELD("helper-above-field");
 
     private final String variant;
 
