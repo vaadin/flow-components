b1a09112baa3d38ac5aa7e06ebfe98fbd22be5dc
Tatu Lund <tatu@vaadin.com>
2021-09-27 08:32:04 +0300

    Add support for theme variants

--- vaadin-date-picker-flow-parent/vaadin-date-picker-flow/src/main/java/com/vaadin/flow/component/datepicker/DatePicker.java
+++ vaadin-date-picker-flow-parent/vaadin-date-picker-flow/src/main/java/com/vaadin/flow/component/datepicker/DatePicker.java
@@ -28,6 +28,7 @@ import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.HasHelper;
 import com.vaadin.flow.component.HasSize;
+import com.vaadin.flow.component.HasTheme;
 import com.vaadin.flow.component.HasValidation;
 import com.vaadin.flow.component.HasValue;
 import com.vaadin.flow.component.UI;
@@ -54,7 +55,7 @@ import elemental.json.JsonType;
 @JsModule("./datepickerConnector.js")
 @NpmPackage(value = "date-fns", version = "2.23.0")
 public class DatePicker extends GeneratedVaadinDatePicker<DatePicker, LocalDate>
-        implements HasSize, HasValidation, HasHelper {
+        implements HasSize, HasValidation, HasHelper, HasTheme {
 
     private static final String PROP_AUTO_OPEN_DISABLED = "autoOpenDisabled";
 
@@ -706,6 +707,30 @@ public class DatePicker extends GeneratedVaadinDatePicker<DatePicker, LocalDate>
     public Registration addInvalidChangeListener(
             ComponentEventListener<InvalidChangeEvent<DatePicker>> listener) {
         return super.addInvalidChangeListener(listener);
+    }
+
+    /**
+     * Adds theme variants to the component.
+     *
+     * @param variants
+     *            theme variants to add
+     */
+    public void addThemeVariants(DatePickerVariant... variants) {
+        getThemeNames()
+                .addAll(Stream.of(variants).map(DatePickerVariant::getVariantName)
+                        .collect(Collectors.toList()));
+    }
+
+    /**
+     * Removes theme variants from the component.
+     *
+     * @param variants
+     *            theme variants to remove
+     */
+    public void removeThemeVariants(DatePickerVariant... variants) {
+        getThemeNames().removeAll(
+                Stream.of(variants).map(DatePickerVariant::getVariantName)
+                        .collect(Collectors.toList()));
     }
 
     /**
