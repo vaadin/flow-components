5ebedfae1c64a4c83e74cca95159ef483f96d2d6
Tatu Lund <tatu@vaadin.com>
2021-10-01 16:26:34 +0300

    Update unit test

--- vaadin-combo-box-flow-parent/vaadin-combo-box-flow/src/test/java/com/vaadin/flow/component/combobox/ComboBoxVariantTest.java
+++ vaadin-combo-box-flow-parent/vaadin-combo-box-flow/src/test/java/com/vaadin/flow/component/combobox/ComboBoxVariantTest.java
@@ -18,10 +18,18 @@ package com.vaadin.flow.component.combobox;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
 
 public class ComboBoxVariantTest {
 
-    private final ComboBox<String> comboBox = new ComboBox<>();
+    private ComboBox<String> comboBox;
+
+    @Before
+    public void initTest() {
+        comboBox = new ComboBox<>();
+    }
 
     @Test
     public void addAndRemoveLumoAlignCenterVariant_themeAttributeUpdated() {
@@ -33,34 +41,77 @@ public class ComboBoxVariantTest {
     }
 
     @Test
-    public void addAndRemoveLumoAlignRightVariant_themeAttributeUpdated() {
+    public void addLumoAlignRightVariant_themeAttributeUpdated() {
         assertThemeAttribute(null);
         comboBox.addThemeVariants(ComboBoxVariant.LUMO_ALIGN_RIGHT);
         assertThemeAttribute("align-right");
-        comboBox.removeThemeVariants(ComboBoxVariant.LUMO_ALIGN_RIGHT);
-        assertThemeAttribute(null);
     }
 
     @Test
-    public void addAndRemoveLumoSmallVariant_themeAttributeUpdated() {
+    public void addLumoSmallVariant_themeAttributeUpdated() {
         assertThemeAttribute(null);
         comboBox.addThemeVariants(ComboBoxVariant.LUMO_SMALL);
         assertThemeAttribute("small");
-        comboBox.removeThemeVariants(ComboBoxVariant.LUMO_SMALL);
+    }
+
+    @Test
+    public void addLumoAlignLeftVariant_themeAttributeUpdated() {
+        assertThemeAttribute(null);
+        comboBox.addThemeVariants(ComboBoxVariant.LUMO_ALIGN_LEFT);
+        assertThemeAttribute("align-left");
+    }
+
+    @Test
+    public void addLumoHelperAboveField_themeAttributeUpdated() {
+        assertThemeAttribute(null);
+        comboBox.addThemeVariants(ComboBoxVariant.LUMO_HELPER_ABOVE_FIELD);
+        assertThemeAttribute("helper-above-field");
+    }
+
+    @Test
+    public void addMaterialAlwaysFloatLabel_themeAttributeUpdated() {
+        assertThemeAttribute(null);
+        comboBox.addThemeVariants(ComboBoxVariant.MATERIAL_ALWAYS_FLOAT_LABEL);
+        assertThemeAttribute("always-float-label");
+    }
+
+    @Test
+    public void addAndRemoveMultipleVariants_themeAttributeUpdated() {
+        assertThemeAttribute(null);
+        comboBox.addThemeVariants(ComboBoxVariant.LUMO_SMALL);
+        comboBox.addThemeVariants(ComboBoxVariant.LUMO_HELPER_ABOVE_FIELD);
+        assertThemeAttributeContains("helper-above-field");
+        assertThemeAttributeContains("small");
+        comboBox.removeThemeVariants(ComboBoxVariant.LUMO_HELPER_ABOVE_FIELD);
+        assertThemeAttribute("small");
+    }
+
+    @Test
+    public void addAndRemoveAllMultipleVariants_themeAttributeUpdated() {
+        assertThemeAttribute(null);
+        comboBox.addThemeVariants(ComboBoxVariant.LUMO_SMALL);
+        comboBox.addThemeVariants(ComboBoxVariant.LUMO_HELPER_ABOVE_FIELD);
+        comboBox.getThemeNames().clear();
         assertThemeAttribute(null);
     }
 
     @Test
-    public void addAndRemoveLumoHelperAboveField_themeAttributeUpdated() {
+    public void addTwiceAndSeeIbce_themeAttributeUpdated() {
         assertThemeAttribute(null);
-        comboBox.addThemeVariants(ComboBoxVariant.LUMO_HELPER_ABOVE_FIELD);
-        assertThemeAttribute("helper-above-field");
-        comboBox.removeThemeVariants(ComboBoxVariant.LUMO_HELPER_ABOVE_FIELD);
-        assertThemeAttribute(null);
+        comboBox.addThemeVariants(ComboBoxVariant.LUMO_SMALL);
+        comboBox.addThemeVariants(ComboBoxVariant.LUMO_SMALL);
+        assertThemeAttribute("small");
     }
 
     private void assertThemeAttribute(String expected) {
         String actual = comboBox.getThemeName();
-        assertEquals("Unexpected theme attribute on dialog", expected, actual);
+        assertEquals("Unexpected theme attribute on combobox", expected,
+                actual);
+    }
+
+    private void assertThemeAttributeContains(String expected) {
+        String actual = comboBox.getThemeName();
+        assertTrue("Theme attribute not present on combobox",
+                actual.contains(expected));
     }
 }
