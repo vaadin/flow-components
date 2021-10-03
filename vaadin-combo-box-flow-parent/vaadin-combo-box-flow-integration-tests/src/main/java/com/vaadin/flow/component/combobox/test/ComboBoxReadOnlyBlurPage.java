367f123744fde674baedbde4c27a3198164e3228
Tatu Lund <tatu@vaadin.com>
2021-10-03 17:34:13 +0300

    Update ComboBoxReadOnlyBlurPage.java

--- vaadin-combo-box-flow-parent/vaadin-combo-box-flow-integration-tests/src/main/java/com/vaadin/flow/component/combobox/test/ComboBoxReadOnlyBlurPage.java
+++ vaadin-combo-box-flow-parent/vaadin-combo-box-flow-integration-tests/src/main/java/com/vaadin/flow/component/combobox/test/ComboBoxReadOnlyBlurPage.java
@@ -42,6 +42,7 @@ public class ComboBoxReadOnlyBlurPage extends VerticalLayout {
         NativeButton button = new NativeButton("Click");
         button.setId("blur-combo");
         button.addClickListener(ev -> {
+            button.focus();
             Span span = new Span("Clicked");
             span.setId("button-clicked");
             add(span);
