7021b7d065ad970af3dda15eccbe742beb0b2b47
Hadi Amiri <84700249+hdamr@users.noreply.github.com>
2021-10-18 13:02:45 +0300

    BFP: issue with ComboBox initial value when used in LitTemplate (#2139) (CP 14.8) (#2221)
    BFP #2059, skip the test for bower mode.

---
+++ vaadin-combo-box-flow-parent/vaadin-combo-box-flow-integration-tests/src/test/java/com/vaadin/flow/component/combobox/test/ComboBoxInLitTemplateIT.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2000-2018 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.combobox.test;
+
+import com.vaadin.flow.component.combobox.testbench.ComboBoxElement;
+import com.vaadin.flow.testutil.TestPath;
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Test;
+
+@TestPath("vaadin-combo-box/combo-box-in-lit-template-page")
+public class ComboBoxInLitTemplateIT extends AbstractComboBoxIT {
+
+    private ComboBoxElement comboBox;
+
+    @Before
+    public void init() {
+        open();
+        Assume.assumeFalse(isBower());
+    }
+
+    /**
+     * See: https://github.com/vaadin/flow-components/issues/2059
+     */
+    @Test
+    public void comboBoxInitialValue_ShouldBeSetCorrectly_WhenSetValueUsed() {
+        comboBox = $("combo-box-lit-wrapper").first().$(ComboBoxElement.class)
+                .id("combo");
+        String labelValue = "1";
+        Assert.assertEquals(labelValue, comboBox.getSelectedText());
+    }
+
+    @Test
+    public void comboBox_retainValue_WhenOpenClosed() {
+        comboBox = $("combo-box-lit-wrapper").first().$(ComboBoxElement.class)
+                .id("combo");
+        String labelValue = "1";
+        comboBox.openPopup();
+        comboBox.closePopup();
+
+        Assert.assertEquals(labelValue, comboBox.getSelectedText());
+    }
+
+    private boolean isBower() {
+        return (Boolean) $("html").first().getCommandExecutor()
+                .executeScript("return !!window.Vaadin.Lumo");
+    }
+}
