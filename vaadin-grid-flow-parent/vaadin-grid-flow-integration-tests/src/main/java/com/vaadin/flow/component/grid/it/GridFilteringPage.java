bb990f2bca196aee9508cbbf9821d6419e99109b
Hadi Amiri <hadi@vaadin.com>
2021-11-12 12:09:53 +0200

    chore: don't use import.* for reducing cherry-pick conflict.

--- vaadin-grid-flow-parent/vaadin-grid-flow-integration-tests/src/main/java/com/vaadin/flow/component/grid/it/GridFilteringPage.java
+++ vaadin-grid-flow-parent/vaadin-grid-flow-integration-tests/src/main/java/com/vaadin/flow/component/grid/it/GridFilteringPage.java
@@ -1,6 +1,11 @@
 package com.vaadin.flow.component.grid.it;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
