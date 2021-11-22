471aba5de99433905cc875ea6b43ce715ad80217
Yuriy Yevstihnyeyev <yuriy@vaadin.com>
2021-11-22 09:34:19 +0200

    chore: bump master to V23

--- vaadin-select-flow-parent/vaadin-select-flow/src/main/java/com/vaadin/flow/component/select/Select.java
+++ vaadin-select-flow-parent/vaadin-select-flow/src/main/java/com/vaadin/flow/component/select/Select.java
@@ -185,7 +185,7 @@ public class Select<T> extends GeneratedVaadinSelect<Select<T>, T>
      * even though that is not visible from the component level.
      */
     @Tag("vaadin-list-box")
-    @NpmPackage(value = "@vaadin/polymer-legacy-adapter", version = "22.0.0-rc1")
+    @NpmPackage(value = "@vaadin/polymer-legacy-adapter", version = "23.0.0-alpha1")
     @JsModule("@vaadin/polymer-legacy-adapter/style-modules.js")
     private class InternalListBox<T> extends Component
             implements HasItemComponents<T> {
