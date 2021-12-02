ed7a8576fbd42f103a09d6c48791e84def03480e
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2021-12-02 13:19:09 +0100

    fix: prevent HasItem.getItem throwing ClassCastException (#2390)
    (cherry picked from commit 5b6ba0608c1e728843c53f6331d0dee8b85508bd)

--- vaadin-charts-flow-parent/vaadin-charts-flow/src/main/java/com/vaadin/flow/component/charts/events/HasItem.java
+++ vaadin-charts-flow-parent/vaadin-charts-flow/src/main/java/com/vaadin/flow/component/charts/events/HasItem.java
@@ -8,18 +8,23 @@ package com.vaadin.flow.component.charts.events;
  * %%
  * This program is available under Commercial Vaadin Add-On License 3.0
  * (CVALv3).
- * 
+ *
  * See the file licensing.txt distributed with this software for more
  * information about licensing.
- * 
+ *
  * You should have received a copy of the CVALv3 along with this program.
  * If not, see <https://vaadin.com/license/cval-3>.
  * #L%
  */
 
 import com.vaadin.flow.component.charts.Chart;
+import com.vaadin.flow.component.charts.model.AbstractSeriesItem;
 import com.vaadin.flow.component.charts.model.DataSeries;
 import com.vaadin.flow.component.charts.model.DataSeriesItem;
+import com.vaadin.flow.component.charts.model.ListSeries;
+import com.vaadin.flow.component.charts.model.Node;
+import com.vaadin.flow.component.charts.model.NodeSeries;
+import com.vaadin.flow.component.charts.model.Series;
 
 /**
  * Indicates that an event has an associated item
@@ -31,18 +36,75 @@ public interface HasItem extends HasSeries {
     String getCategory();
 
     /**
-     * Returns the index of {@link #getItem()} in {@link #getSeries()}.
+     * Returns the index of the series item, that is associated with this event,
+     * in {@link #getSeries()}. Can be used to identify the item within the
+     * series.
+     * <p>
+     * Example for {@link ListSeries}:
      *
-     * @return
+     * <pre>
+     * int itemIndex = event.getItemIndex();
+     * ListSeries series = (ListSeries) event.getSeries();
+     * Number datum = series.getData()[itemIndex];
+     * </pre>
+     *
+     * @return the index of the item in the series
+     * @see #getItem()
+     * @see #getItemId()
      */
     int getItemIndex();
 
     /**
-     * Returns the item that was clicked
+     * The ID of the series item that is associated with the event. Can be used
+     * to identify the item within the series.
+     * <p>
+     * Example for {@link NodeSeries}:
      *
-     * @return
+     * <pre>
+     * String id = this.getItemId();
+     * NodeSeries series = (NodeSeries) this.getSeries();
+     * Optional&lt;Node&gt; nodeForId = series.getNodes().stream()
+     *   .filter(node -> node.getId().equals(id))
+     *   .findFirst();
+     * </pre>
+     * <p>
+     * Only {@link AbstractSeriesItem} and {@link Node} support setting an ID.
+     * For other types of series items this property will always return null.
+     * For {@link AbstractSeriesItem} the ID is optional. Unless the developer
+     * has explicitly set an ID for the item associated with the event, this
+     * property will be null. See {@link #getItem()} or {@link #getItemIndex()}
+     * for alternatives.
+     *
+     * @return the ID of the series item associated with the event, or null if
+     *         the series item has no ID
+     *
+     * @see #getItem()
+     * @see #getItemIndex()
+     */
+    String getItemId();
+
+    /**
+     * Returns the data series item that this event is associated with.
+     * <p>
+     * <b>NOTE:</b> This method only works with series of type
+     * {@link DataSeries}. For other series an
+     * {@link UnsupportedOperationException} will be thrown. See
+     * {@link #getItemIndex()} or {@link #getItemId()} for alternatives.
+     *
+     * @return the {@link DataSeriesItem} that is associated with this event
+     * @throws UnsupportedOperationException
+     *             when using this method with a series that is not a DataSeries
+     * @see #getItemIndex()
+     * @see #getItemId()
      */
     default DataSeriesItem getItem() {
-        return ((DataSeries) getSeries()).get(getItemIndex());
+        Series series = getSeries();
+        if (!(series instanceof DataSeries)) {
+            String seriesClassName = series.getClass().getSimpleName();
+            throw new UnsupportedOperationException(String.format(
+                    "HasItem.getItem does not support series of type: %s. Only series of type com.vaadin.flow.component.charts.model.DataSeries are supported. Please check the API docs for further information.",
+                    seriesClassName));
+        }
+        return ((DataSeries) series).get(getItemIndex());
     }
 }
