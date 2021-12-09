614d2e321e8943aab68ea993937105233686c541
Tatu Lund <tatu@vaadin.com>
2021-12-09 20:44:33 +0200

    Fix package name

--- vaadin-grid-pro-flow-parent/vaadin-grid-pro-flow-integration-tests/src/main/java/com/vaadin/flow/component/gridpro/it/GridProDetachAttachPage.java
+++ vaadin-grid-pro-flow-parent/vaadin-grid-pro-flow-integration-tests/src/main/java/com/vaadin/flow/component/gridpro/it/GridProDetachAttachPage.java
@@ -1,4 +1,3 @@
-
 /*
  * Copyright 2000-2019 Vaadin Ltd.
  *
@@ -14,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.vaadin.devday.demo.views;
+package com.vaadin.flow.component.gridpro.it;
 
 import java.util.ArrayList;
 import java.util.List;
