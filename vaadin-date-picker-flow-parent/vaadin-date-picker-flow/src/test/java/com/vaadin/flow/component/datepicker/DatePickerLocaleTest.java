d3f3ac83f7aacdc0b656aa467780f4fbb6a77d30
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2021-12-09 13:12:09 +0100

    simplify date picker locale tests

--- vaadin-date-picker-flow-parent/vaadin-date-picker-flow/src/test/java/com/vaadin/flow/component/datepicker/DatePickerLocaleTest.java
+++ vaadin-date-picker-flow-parent/vaadin-date-picker-flow/src/test/java/com/vaadin/flow/component/datepicker/DatePickerLocaleTest.java
@@ -1,114 +1,50 @@
 package com.vaadin.flow.component.datepicker;
 
-import java.util.Objects;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.time.LocalDate;
+import java.util.Locale;
 
+import net.jcip.annotations.NotThreadSafe;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.vaadin.flow.component.UI;
-import com.vaadin.flow.server.VaadinRequest;
-import com.vaadin.flow.server.VaadinService;
-import com.vaadin.flow.server.VaadinSession;
 
+@NotThreadSafe
 public class DatePickerLocaleTest {
 
-    private DatePicker datePicker;
-    private MockUI ui;
+    private UI ui;
 
     @Before
-    public void initTest() {
-        ui = new MockUI();
+    public void setUp() {
+        ui = new UI();
         UI.setCurrent(ui);
-
-        datePicker = new DatePicker();
     }
 
     @Test
-    public void newDatePicker_getLocaleIsNotNull() {
-        Objects.requireNonNull(datePicker.getLocale());
+    public void newDatePicker_returnsUiLocale() {
+        Locale finnishLocale = new Locale("fi-FI");
+        ui.setLocale(finnishLocale);
+        DatePicker datePicker = new DatePicker();
+        Assert.assertEquals(finnishLocale, datePicker.getLocale());
     }
 
-    public static class MockUI extends UI {
-
-        public MockUI() {
-            this(findOrcreateSession());
-        }
-
-        public MockUI(VaadinSession session) {
-            getInternals().setSession(session);
-            setCurrent(this);
-        }
-
-        @Override
-        protected void init(VaadinRequest request) {
-            // Do nothing
-        }
-
-        private static VaadinSession findOrcreateSession() {
-            VaadinSession session = VaadinSession.getCurrent();
-            if (session == null) {
-                session = new AlwaysLockedVaadinSession(null);
-                VaadinSession.setCurrent(session);
-            }
-            return session;
-        }
+    @Test
+    public void newDatePickerWithCustomLocale_returnsCustomLocale() {
+        Locale finnishLocale = new Locale("fi-FI");
+        Locale usLocale = new Locale("en-US");
+        ui.setLocale(finnishLocale);
+        DatePicker datePicker = new DatePicker(LocalDate.now(), usLocale);
+        Assert.assertEquals(usLocale, datePicker.getLocale());
     }
 
-    public static class AlwaysLockedVaadinSession extends MockVaadinSession {
-
-        public AlwaysLockedVaadinSession(VaadinService service) {
-            super(service);
-            lock();
-        }
-
-    }
-
-    public static class MockVaadinSession extends VaadinSession {
-        /*
-         * Used to make sure there's at least one reference to the mock session
-         * while it's locked. This is used to prevent the session from being
-         * eaten by GC in tests where @Before creates a session and sets it as
-         * the current instance without keeping any direct reference to it. This
-         * pattern has a chance of leaking memory if the session is not unlocked
-         * in the right way, but it should be acceptable for testing use.
-         */
-        private static final ThreadLocal<MockVaadinSession> referenceKeeper = new ThreadLocal<>();
-
-        public MockVaadinSession(VaadinService service) {
-            super(service);
-        }
-
-        @Override
-        public void close() {
-            super.close();
-            closeCount++;
-        }
-
-        public int getCloseCount() {
-            return closeCount;
-        }
-
-        @Override
-        public Lock getLockInstance() {
-            return lock;
-        }
-
-        @Override
-        public void lock() {
-            super.lock();
-            referenceKeeper.set(this);
-        }
-
-        @Override
-        public void unlock() {
-            super.unlock();
-            referenceKeeper.remove();
-        }
-
-        private int closeCount;
-
-        private ReentrantLock lock = new ReentrantLock();
+    @Test
+    public void setCustomLocale_returnsCustomLocale() {
+        Locale finnishLocale = new Locale("fi-FI");
+        Locale usLocale = new Locale("en-US");
+        ui.setLocale(finnishLocale);
+        DatePicker datePicker = new DatePicker();
+        datePicker.setLocale(usLocale);
+        Assert.assertEquals(usLocale, datePicker.getLocale());
     }
 }
