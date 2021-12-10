32a6a6df9c8e8ebbc7748e0de2f24bee860d0439
ZheSun88 <zhe@vaadin.com>
2021-12-10 12:31:30 +0200

    fix compilation

--- vaadin-charts-flow-parent/vaadin-charts-flow/src/main/java/com/vaadin/flow/component/charts/model/serializers/BeanSerializerDelegator.java
+++ vaadin-charts-flow-parent/vaadin-charts-flow/src/main/java/com/vaadin/flow/component/charts/model/serializers/BeanSerializerDelegator.java
@@ -16,6 +16,7 @@ package com.vaadin.flow.component.charts.model.serializers;
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.BeanPropertyWriter;
 import com.fasterxml.jackson.databind.ser.impl.BeanAsArraySerializer;
 import com.fasterxml.jackson.databind.ser.impl.ObjectIdWriter;
 import com.fasterxml.jackson.databind.ser.std.BeanSerializerBase;
@@ -71,8 +72,18 @@ public class BeanSerializerDelegator<T> extends BeanSerializerBase {
     }
 
     @Override
+    protected BeanSerializerBase withByNameInclusion(Set<String> set, Set<String> set1) {
+        return null;
+    }
+
+    @Override
     public BeanSerializerBase withFilterId(Object filterId) {
         return new BeanSerializerDelegator(this, filterId);
+    }
+
+    @Override
+    protected BeanSerializerBase withProperties(BeanPropertyWriter[] beanPropertyWriters, BeanPropertyWriter[] beanPropertyWriters1) {
+        return null;
     }
 
     @Override
