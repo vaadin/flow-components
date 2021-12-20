9d8f1b894b1f9699a2ef0159dcc01fe369483ad0
web-padawan <iamkulykov@gmail.com>
2021-12-20 15:13:57 +0200

    test: fix getting confirm-dialog buttons

--- vaadin-confirm-dialog-flow-parent/vaadin-confirm-dialog-testbench/src/main/java/com/vaadin/flow/component/confirmdialog/testbench/ConfirmDialogElement.java
+++ vaadin-confirm-dialog-flow-parent/vaadin-confirm-dialog-testbench/src/main/java/com/vaadin/flow/component/confirmdialog/testbench/ConfirmDialogElement.java
@@ -18,8 +18,6 @@ package com.vaadin.flow.component.confirmdialog.testbench;
 
 import org.openqa.selenium.SearchContext;
 
-import com.vaadin.flow.component.button.testbench.ButtonElement;
-import com.vaadin.testbench.ElementQuery;
 import com.vaadin.testbench.TestBenchElement;
 import com.vaadin.testbench.elementsbase.Element;
 
@@ -32,26 +30,16 @@ public class ConfirmDialogElement extends TestBenchElement {
                 "content");
     }
 
-    private TestBenchElement getButton(String buttonId, String slotName) {
-        ElementQuery<TestBenchElement> query = $(TestBenchElement.class)
-                .attribute("slot", slotName);
-        if (query.exists()) {
-            return query.first();
-        }
-
-        return $(ButtonElement.class).id(buttonId);
-    }
-
     public TestBenchElement getConfirmButton() {
-        return getButton("confirm", "confirm-button");
+        return getPropertyElement("_confirmButton");
     }
 
     public TestBenchElement getRejectButton() {
-        return getButton("reject", "reject-button");
+        return getPropertyElement("_rejectButton");
     }
 
     public TestBenchElement getCancelButton() {
-        return getButton("cancel", "cancel-button");
+        return getPropertyElement("_cancelButton");
     }
 
     public String getMessageText() {
