954350d424dc03f180e9e680a439c4a06992f640
Tatu Lund <tatu@vaadin.com>
2022-01-11 15:00:21 +0200

    Add unit test

---
+++ vaadin-upload-flow-parent/vaadin-upload-flow/src/test/java/com/vaadin/flow/component/upload/tests/UploadClearFilesTest.java
@@ -0,0 +1,158 @@
+/*
+ * Copyright 2000-2021 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.upload;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.upload.receivers.MultiFileMemoryBuffer;
+import com.vaadin.flow.data.provider.DataCommunictorAsyncTest.AlwaysLockedVaadinSession;
+import com.vaadin.flow.data.provider.DataCommunictorAsyncTest.MockVaadinSession;
+import com.vaadin.flow.internal.StateNode;
+import com.vaadin.flow.server.VaadinRequest;
+import com.vaadin.flow.server.VaadinService;
+import com.vaadin.flow.server.VaadinServlet;
+import com.vaadin.flow.server.VaadinServletService;
+import com.vaadin.flow.server.VaadinSession;
+
+import elemental.json.Json;
+
+import java.io.IOException;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class UploadClearFilesTest {
+
+    String mockJson = "[{\"loaded\": 11037,\"held\": false,\"status\": \"\",\"xhr\": {},\"uploadTarget\": \"VAADIN/dynamic/resource/5/53877f5e-28d9-47a2-b061-80670615fb6e/upload\",\"formDataName\": \"file\",\"indeterminate\": false,\"uploading\": false,\"error\": false,\"abort\": false,\"complete\": true,\"progress\": 100}]";
+
+    private Upload upload;
+
+    @Before
+    public void initTest() throws IOException {
+        UI ui = new MockUI();
+        UI.setCurrent(ui);
+
+        upload = new Upload();
+        upload.getElement().setProperty("files", mockJson);
+    }
+
+    @Test
+    public void addAndRemoveLumoAlignCenterVariant_themeAttributeUpdated() {
+        String files = upload.getElement().getProperty("files");
+        assertNotNull(files);
+        assertFalse(files.isEmpty());
+        upload.getElement().setPropertyJson("files", Json.createArray());
+        files = upload.getElement().getProperty("files");
+        assertTrue(files.equals("[]"));
+    }
+
+    class TestData {
+        private final String filename;
+        private final String data;
+
+        public TestData(String filename, String data) {
+            this.filename = filename;
+            this.data = data;
+        }
+    }
+
+    public static class MockUI extends UI {
+
+        public MockUI() {
+            this(findOrcreateSession());
+        }
+
+        public MockUI(VaadinSession session) {
+            getInternals().setSession(session);
+            setCurrent(this);
+        }
+
+        @Override
+        protected void init(VaadinRequest request) {
+            // Do nothing
+        }
+
+        private static VaadinSession findOrcreateSession() {
+            VaadinSession session = VaadinSession.getCurrent();
+            if (session == null) {
+                session = new AlwaysLockedVaadinSession(
+                        new VaadinServletService(new VaadinServlet(), null));
+                VaadinSession.setCurrent(session);
+            }
+            return session;
+        }
+    }
+
+    public static class AlwaysLockedVaadinSession extends MockVaadinSession {
+
+        public AlwaysLockedVaadinSession(VaadinService service) {
+            super(service);
+            lock();
+        }
+
+    }
+
+    public static class MockVaadinSession extends VaadinSession {
+        /*
+         * Used to make sure there's at least one reference to the mock session
+         * while it's locked. This is used to prevent the session from being
+         * eaten by GC in tests where @Before creates a session and sets it as
+         * the current instance without keeping any direct reference to it. This
+         * pattern has a chance of leaking memory if the session is not unlocked
+         * in the right way, but it should be acceptable for testing use.
+         */
+        private static final ThreadLocal<MockVaadinSession> referenceKeeper = new ThreadLocal<>();
+
+        public MockVaadinSession(VaadinService service) {
+            super(service);
+        }
+
+        @Override
+        public void close() {
+            super.close();
+            closeCount++;
+        }
+
+        public int getCloseCount() {
+            return closeCount;
+        }
+
+        @Override
+        public Lock getLockInstance() {
+            return lock;
+        }
+
+        @Override
+        public void lock() {
+            super.lock();
+            referenceKeeper.set(this);
+        }
+
+        @Override
+        public void unlock() {
+            super.unlock();
+            referenceKeeper.remove();
+        }
+
+        private int closeCount;
+
+        private ReentrantLock lock = new ReentrantLock();
+    }
+}
