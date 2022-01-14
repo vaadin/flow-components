bd2fbc6d0c25174571cb88929b9b40976e170061
Sascha Ißbrücker <sissbruecker@vaadin.com>
2022-01-14 10:17:59 +0100

    chore: add pattern for options

--- vaadin-map-flow-parent/vaadin-map-flow/src/main/java/com/vaadin/flow/component/map/configuration/source/OSMSource.java
+++ vaadin-map-flow-parent/vaadin-map-flow/src/main/java/com/vaadin/flow/component/map/configuration/source/OSMSource.java
@@ -3,8 +3,23 @@ package com.vaadin.flow.component.map.configuration.source;
 import com.vaadin.flow.component.map.configuration.Constants;
 
 public class OSMSource extends UrlTileSource {
+
+    public OSMSource() {
+        this(new Options());
+    }
+
+    public OSMSource(Options options) {
+        super(options);
+    }
+
     @Override
     public String getType() {
         return Constants.OL_SOURCE_OSM;
+    }
+
+    public static class Options extends UrlTileSource.BaseOptions<Options> {
+        public Options() {
+            setUrl("https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png");
+        }
     }
 }
--- vaadin-map-flow-parent/vaadin-map-flow/src/main/java/com/vaadin/flow/component/map/configuration/source/UrlTileSource.java
+++ vaadin-map-flow-parent/vaadin-map-flow/src/main/java/com/vaadin/flow/component/map/configuration/source/UrlTileSource.java
@@ -2,8 +2,20 @@ package com.vaadin.flow.component.map.configuration.source;
 
 import com.vaadin.flow.component.map.configuration.Constants;
 
+import java.util.Objects;
+
 public abstract class UrlTileSource extends Source {
+
     private String url;
+
+    private final boolean opaque;
+
+    protected UrlTileSource(BaseOptions options) {
+        Objects.requireNonNull(options);
+
+        this.url = options.url;
+        this.opaque = options.opaque;
+    }
 
     @Override
     public String getType() {
@@ -17,5 +29,27 @@ public abstract class UrlTileSource extends Source {
     public void setUrl(String url) {
         this.url = url;
         notifyChange();
+    }
+
+    public boolean isOpaque() {
+        return opaque;
+    }
+
+    protected static class BaseOptions<T extends BaseOptions<T>> {
+        private String url;
+        private boolean opaque = true;
+
+        public T setUrl(String url) {
+            this.url = url;
+            return (T) this;
+        }
+
+        public T setOpaque(boolean opaque) {
+            this.opaque = opaque;
+            return (T) this;
+        }
+    }
+
+    public static class Options extends BaseOptions<Options> {
     }
 }
