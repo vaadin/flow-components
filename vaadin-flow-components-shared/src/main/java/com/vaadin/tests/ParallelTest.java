ea7b13c4e8f780e0097ac60a3ef33ceaa290c81a
Manolo Carrasco <manolo@vaadin.com>
2022-01-26 13:32:19 +0100

    test: remove SharedBrowser

--- vaadin-flow-components-shared/src/main/java/com/vaadin/tests/ParallelTest.java
+++ vaadin-flow-components-shared/src/main/java/com/vaadin/tests/ParallelTest.java
@@ -2,15 +2,12 @@ package com.vaadin.tests;
 
 import java.io.File;
 
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public abstract class ParallelTest
         extends com.vaadin.testbench.parallel.ParallelTest {
-
-    private static SharedBrowser browser = new SharedBrowser();
 
     @BeforeClass
     public static void setupClass() {
@@ -26,19 +23,8 @@ public abstract class ParallelTest
         }
     }
 
-    @Override
-    public void setup() throws Exception {
-        browser.setup(super::setup, this::setDriver, this::getDriver,
-                screenshotOnFailure);
-    }
-
     protected int getDeploymentPort() {
         return 8080;
-    }
-
-    @AfterClass
-    public static void runAfterTest() {
-        browser.clear();
     }
 
 }
