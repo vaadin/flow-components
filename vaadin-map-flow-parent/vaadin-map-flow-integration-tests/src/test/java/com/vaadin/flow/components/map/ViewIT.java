0069e3d2bd99c9bbf70328bf438f69ad781c94b3
Sascha Ißbrücker <sissbruecker@vaadin.com>
2022-01-27 16:42:29 +0100

    fix flaky test

--- vaadin-map-flow-parent/vaadin-map-flow-integration-tests/src/test/java/com/vaadin/flow/components/map/ViewIT.java
+++ vaadin-map-flow-parent/vaadin-map-flow-integration-tests/src/test/java/com/vaadin/flow/components/map/ViewIT.java
@@ -26,25 +26,25 @@ public class ViewIT extends AbstractComponentIT {
         TestBenchElement setCenterButton = $("button").id("set-center-button");
         setCenterButton.click();
 
-        List<Double> center = (List<Double>) map
+        List<Number> center = (List<Number>) map
                 .evaluateOLExpression("map.getView().getCenter()");
-        Assert.assertEquals(center.get(0), 2482424.644689998, 0.0001);
-        Assert.assertEquals(center.get(1), 8500614.173537256, 0.0001);
+        Assert.assertEquals(center.get(0).doubleValue(), 2482424.644689998, 0.0001);
+        Assert.assertEquals(center.get(1).doubleValue(), 8500614.173537256, 0.0001);
 
         // Testing setZoom
         TestBenchElement setZoomButton = $("button").id("set-zoom-button");
         setZoomButton.click();
-        long zoomLevel = (long) map
+        Number zoomLevel = (Number) map
                 .evaluateOLExpression("map.getView().getZoom()");
-        Assert.assertEquals(zoomLevel, 14);
+        Assert.assertEquals(zoomLevel.doubleValue(), 14, 0.1);
 
         // Testing setRotation
         TestBenchElement setRotationButton = $("button")
                 .id("set-rotation-button");
         setRotationButton.click();
 
-        double rotation = (double) map
+        Number rotation = (Number) map
                 .evaluateOLExpression("map.getView().getRotation()");
-        Assert.assertEquals(rotation, 0.785398, 0.1);
+        Assert.assertEquals(rotation.doubleValue(), 0.785398, 0.1);
     }
 }
