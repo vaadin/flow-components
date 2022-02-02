16b3089cc95a63b8c5e7718b27f9370c78ce4426
Sascha Ißbrücker <sissbruecker@vaadin.com>
2022-02-02 10:56:53 +0100

    feat: finalize support for XYZ source (#2603)
    * feat: finalize xyz source
    
    * add basic IT

--- vaadin-map-flow-parent/vaadin-map-flow/src/main/java/com/vaadin/flow/component/map/configuration/source/XYZSource.java
+++ vaadin-map-flow-parent/vaadin-map-flow/src/main/java/com/vaadin/flow/component/map/configuration/source/XYZSource.java
@@ -19,8 +19,12 @@ package com.vaadin.flow.component.map.configuration.source;
 import com.vaadin.flow.component.map.configuration.Constants;
 
 /**
- * Map source for loading tiled images from a map service using the XYZ URL
- * format
+ * Source for loading tiled images from a map service using the
+ * <a href="https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames">Slippy
+ * Map</a> tile numbering scheme, also known as XYZ format.
+ * <p>
+ * This is commonly used by OpenStreetMap, as well as other services who have
+ * adopted the OSM tile numbering scheme.
  */
 public class XYZSource extends TileImageSource {
 
@@ -37,8 +41,43 @@ public class XYZSource extends TileImageSource {
         return Constants.OL_SOURCE_XYZ;
     }
 
+    /**
+     * The URL template in XYZ format (see also
+     * <a href="https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames">Slippy
+     * map tilenames</a>) used to load individual image tiles. The URL must
+     * include the {@code x}, {@code y} or {@code -y}, and {@code z}
+     * placeholders, where {@code x} and {@code y} identify the tile in the tile
+     * grid for the zoom level {@code z}.
+     * <p>
+     * Example: {@code https://a.tile.openstreetmap.org/{z}/{x}/{y}.png}
+     *
+     * @return the URL template
+     */
+    @Override
+    public String getUrl() {
+        return super.getUrl();
+    }
+
+    /**
+     * Sets the URL template in XYZ format.
+     * 
+     * @param url
+     *            the new URL template
+     */
+    @Override
+    public void setUrl(String url) {
+        super.setUrl(url);
+    }
+
     public static abstract class BaseOptions<T extends BaseOptions<T>>
             extends TileImageSource.BaseOptions<T> {
+        /**
+         * @see XYZSource#getUrl()
+         */
+        @Override
+        public T setUrl(String url) {
+            return super.setUrl(url);
+        }
     }
 
     public static class Options extends BaseOptions<Options> {
