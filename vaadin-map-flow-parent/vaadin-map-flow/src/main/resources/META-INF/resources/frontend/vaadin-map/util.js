1d53515841efe5a2e097269777594e37d841880a
Sascha Ißbrücker <sissbruecker@vaadin.com>
2022-02-03 17:06:52 +0100

    fix jsdoc

--- vaadin-map-flow-parent/vaadin-map-flow/src/main/resources/META-INF/resources/frontend/vaadin-map/util.js
+++ vaadin-map-flow-parent/vaadin-map-flow/src/main/resources/META-INF/resources/frontend/vaadin-map/util.js
@@ -4,7 +4,7 @@ import VectorSource from "ol/source/Vector";
  * Searches an OpenLayers map instance for the layer whose source contains a specific feature
  * @param map an OpenLayers map instance
  * @param feature the feature that should be contained in the layers source
- * @returns {*} the layer that contains the feature, or null
+ * @returns {*} the layer that contains the feature, or undefined
  */
 export function getLayerForFeature(map, feature) {
   const layers = map.getLayers().getArray();
