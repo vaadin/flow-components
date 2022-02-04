4f45aa7f3edae83ff6e098ff4e4bb895818e79b8
Sascha Ißbrücker <sissbruecker@vaadin.com>
2022-02-04 16:22:44 +0100

    feat: map feature click events (#2604)
    * feat: feature click events
    
    * run formatter
    
    * switch to using singleclick event
    
    * fix jsdoc
    
    Co-authored-by: Hadi Amiri <84700249+hdamr@users.noreply.github.com>

--- vaadin-map-flow-parent/vaadin-map-flow-integration-tests/src/main/java/com/vaadin/flow/component/map/MapEventsPage.java
+++ vaadin-map-flow-parent/vaadin-map-flow-integration-tests/src/main/java/com/vaadin/flow/component/map/MapEventsPage.java
@@ -1,6 +1,7 @@
 package com.vaadin.flow.component.map;
 
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.NativeButton;
 import com.vaadin.flow.component.map.configuration.View;
 import com.vaadin.flow.router.Route;
 
@@ -18,29 +19,38 @@ public class MapEventsPage extends Div {
         map.setWidth("400px");
         map.getView().setZoom(3);
 
-        map.addViewMoveEndEventListener(event -> {
-            View mapView = map.getView();
-            String stateText = mapView.getCenter().getX() + ";"
-                    + mapView.getCenter().getY() + ";";
-            stateText += mapView.getRotation() + ";";
-            stateText += mapView.getZoom();
+        NativeButton addMoveEndListener = new NativeButton(
+                "Add move end listener", e -> {
+                    map.addViewMoveEndEventListener(event -> {
+                        View mapView = map.getView();
+                        String stateText = mapView.getCenter().getX() + ";"
+                                + mapView.getCenter().getY() + ";";
+                        stateText += mapView.getRotation() + ";";
+                        stateText += mapView.getZoom();
 
-            viewState.setText(stateText);
+                        viewState.setText(stateText);
 
-            String eventDataText = event.getCenter().getX() + ";"
-                    + event.getCenter().getY() + ";" + event.getRotation() + ";"
-                    + event.getZoom();
+                        String eventDataText = event.getCenter().getX() + ";"
+                                + event.getCenter().getY() + ";"
+                                + event.getRotation() + ";" + event.getZoom();
 
-            eventData.setText(eventDataText);
-        });
+                        eventData.setText(eventDataText);
+                    });
+                });
+        addMoveEndListener.setId("add-move-end-listener");
 
-        map.addClickEventListener(event -> {
-            eventData.setText(event.getCoordinate().getX() + ";"
-                    + event.getCoordinate().getY() + ";"
-                    + event.getMouseDetails().getAbsoluteX() + ";"
-                    + event.getMouseDetails().getAbsoluteY());
-        });
+        NativeButton addClickListener = new NativeButton("Add click listener",
+                e -> {
+                    map.addClickEventListener(event -> {
+                        eventData.setText(event.getCoordinate().getX() + ";"
+                                + event.getCoordinate().getY() + ";"
+                                + event.getMouseDetails().getAbsoluteX() + ";"
+                                + event.getMouseDetails().getAbsoluteY());
+                    });
+                });
+        addClickListener.setId("add-click-listener");
 
-        add(map, viewState, eventData);
+        add(map, viewState, eventData,
+                new Div(addMoveEndListener, addClickListener));
     }
 }
