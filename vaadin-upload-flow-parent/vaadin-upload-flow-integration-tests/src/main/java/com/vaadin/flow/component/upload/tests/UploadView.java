333c50a647e821df78ebce432df06153ec7dcf3d
Tatu Lund <tatu@vaadin.com>
2022-02-07 15:16:33 +0200

    feat: Add clearFilelist method (#2223)
    * Add clearFilelist method
    
    * Add missing import
    
    * Update vaadin-upload-flow-parent/vaadin-upload-flow/src/main/java/com/vaadin/flow/component/upload/Upload.java
    
    Co-authored-by: David Sosa <76832183+sosa-vaadin@users.noreply.github.com>
    
    * Update vaadin-upload-flow-parent/vaadin-upload-flow/src/main/java/com/vaadin/flow/component/upload/Upload.java
    
    Co-authored-by: David Sosa <76832183+sosa-vaadin@users.noreply.github.com>
    
    * Add unit test
    
    * Fix
    
    * Delete UploadClearFilesTest.java
    
    * Move file
    
    * Rename method
    
    * add integration test
    
    * Use existing setFiles
    
    * Update vaadin-upload-flow-parent/vaadin-upload-flow/src/main/java/com/vaadin/flow/component/upload/Upload.java
    
    Co-authored-by: Serhii Kulykov <iamkulykov@gmail.com>
    
    Co-authored-by: David Sosa <76832183+sosa-vaadin@users.noreply.github.com>
    Co-authored-by: Yuriy Yevstihnyeyev <yuriy@vaadin.com>
    Co-authored-by: Serhii Kulykov <iamkulykov@gmail.com>
    Co-authored-by: Tomi Virkki <tomivirkki@users.noreply.github.com>

--- vaadin-upload-flow-parent/vaadin-upload-flow-integration-tests/src/main/java/com/vaadin/flow/component/upload/tests/UploadView.java
+++ vaadin-upload-flow-parent/vaadin-upload-flow-integration-tests/src/main/java/com/vaadin/flow/component/upload/tests/UploadView.java
@@ -18,14 +18,13 @@ package com.vaadin.flow.component.upload.tests;
 
 import java.io.IOException;
 
+import com.vaadin.flow.component.html.NativeButton;
 import org.apache.commons.io.IOUtils;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.upload.Upload;
-import com.vaadin.flow.component.upload.UploadI18N;
-import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
 import com.vaadin.flow.component.upload.receivers.MultiFileMemoryBuffer;
 import com.vaadin.flow.router.Route;
 
@@ -44,6 +43,7 @@ public class UploadView extends Div {
     private void createSimpleUpload() {
         Div output = new Div();
         Div eventsOutput = new Div();
+        Div fileList = new Div();
 
         MultiFileMemoryBuffer buffer = new MultiFileMemoryBuffer();
         Upload upload = new Upload(buffer);
@@ -60,12 +60,23 @@ public class UploadView extends Div {
         });
         upload.addAllFinishedListener(event -> eventsOutput.add("-finished"));
 
+        NativeButton clearFileListBtn = new NativeButton("Clear file list",
+                e -> upload.clearFileList());
+
+        NativeButton printFileListBtn = new NativeButton("Print file list",
+                e -> fileList
+                        .setText(upload.getElement().getProperty("files")));
+
         upload.setMaxFileSize(500 * 1024);
         upload.setId("test-upload");
+        clearFileListBtn.setId("clear-file-list");
+        printFileListBtn.setId("print-file-list");
+        fileList.setId("file-list");
         output.setId("test-output");
         eventsOutput.setId("test-events-output");
 
-        addCard("Simple in memory receiver", upload, output, eventsOutput);
+        addCard("Simple in memory receiver", upload, output, eventsOutput,
+                fileList, clearFileListBtn, printFileListBtn);
     }
 
     private void addCard(String title, Component... components) {
