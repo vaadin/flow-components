cb5cb6fe4fec585c04e8771946c9df96aa901c19
Sascha Ißbrücker <sissbruecker@vaadin.com>
2022-02-12 18:35:20 +0100

    fix collection sync

--- vaadin-map-flow-parent/vaadin-map-flow/src/main/resources/META-INF/resources/frontend/vaadin-map/synchronization/util.js
+++ vaadin-map-flow-parent/vaadin-map-flow/src/main/resources/META-INF/resources/frontend/vaadin-map/synchronization/util.js
@@ -22,19 +22,25 @@ export function convertToSizeArray(size) {
  * Synchronizes an OpenLayers collection with data from a Javascript array
  */
 export function synchronizeCollection(collection, updatedIds, options) {
-  const updatedItems = updatedIds.map((id) => options.lookup.get(id));
-
-  // Iterate updated items and update collection at indexes where reference doesn't match
-  updatedItems.forEach((updatedItem, i) => {
-    const existingItem = collection.item(i);
-    const isUnchanged = existingItem && existingItem.id === updatedItem.id;
-    if (isUnchanged) return;
-    collection.setAt(i, updatedItem);
-  });
-  // Remove leftover collection elements
-  while (collection.getLength() > updatedItems.length) {
-    collection.pop();
-  }
+  // Check if we have changes
+  const hasChanges =
+    updatedIds.length !== collection.getLength() ||
+    collection
+      .getArray()
+      .some((existingItem, index) => existingItem.id !== updatedIds[index]);
+  // Skip if there aren't any changes
+  if (!hasChanges) return;
+  // Get instance references from ids, these must have been synchronized earlier
+  const updatedItems = updatedIds
+    .map((id) => options.lookup.get(id))
+    // This shouldn't be necessary, but having this safe-guard allows us to
+    // at least continue the sync in case a reference is missing
+    .filter((item) => !!item);
+  // Rebuild the collection
+  // It shouldn't matter whether we just add/move/remove specific items, or rebuild
+  // the whole thing, this will result in several change events anyway
+  collection.clear();
+  updatedItems.forEach((item) => collection.push(item));
 }
 
 /**
