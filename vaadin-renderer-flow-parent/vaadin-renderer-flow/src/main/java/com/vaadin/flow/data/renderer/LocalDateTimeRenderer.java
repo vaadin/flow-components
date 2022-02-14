0d30e144da9dd60faa3a32b71c4470a3f82cc238
Tatu Lund <tatu@vaadin.com>
2022-02-14 15:29:15 +0200

    Formatting

--- vaadin-renderer-flow-parent/vaadin-renderer-flow/src/main/java/com/vaadin/flow/data/renderer/LocalDateTimeRenderer.java
+++ vaadin-renderer-flow-parent/vaadin-renderer-flow/src/main/java/com/vaadin/flow/data/renderer/LocalDateTimeRenderer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2021 Vaadin Ltd.
+ * Copyright 2000-2022 Vaadin Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -198,6 +198,6 @@ public class LocalDateTimeRenderer<SOURCE>
     @Override
     protected String getFormattedValue(LocalDateTime dateTime) {
         return dateTime == null ? nullRepresentation
-                 : formatter.get().format(dateTime);
+                : formatter.get().format(dateTime);
     }
 }
