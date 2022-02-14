d14375883cf18ef1e7d4aba6b809ed520ef36a96
Tatu Lund <tatu@vaadin.com>
2022-02-14 19:01:57 +0200

    Update unit test

---
+++ vaadin-renderer-flow-parent/vaadin-renderer-flow/src/test/java/com/vaadin/flow/data/renderer/FormattedRenderersSerializableTest.java
@@ -0,0 +1,34 @@
+package com.vaadin.flow.data.renderer;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+import org.junit.Test;
+
+public class FormattedRenderersSerializableTest  {
+
+    @Test
+    public void numberRendererIsSerializable() throws IOException {
+        final NumberRenderer renderer = new NumberRenderer(
+                value -> value.toString(),"");
+        new ObjectOutputStream(new ByteArrayOutputStream())
+                .writeObject(renderer);
+    }
+
+    @Test
+    public void localDateTimeRendererIsSerializable() throws IOException {
+        final LocalDateTimeRenderer renderer = new LocalDateTimeRenderer(
+                value -> value.toString(),"");
+        new ObjectOutputStream(new ByteArrayOutputStream())
+                .writeObject(renderer);
+    }
+
+    @Test
+    public void localDateRendererIsSerializable() throws IOException {
+        final LocalDateRenderer renderer = new LocalDateRenderer(
+                value -> value.toString(), "");
+        new ObjectOutputStream(new ByteArrayOutputStream())
+                .writeObject(renderer);
+    }
+}
