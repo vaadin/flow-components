4c7737200d9802596c4af0281ca66bee09e5e14e
Tatu Lund <tatu@vaadin.com>
2022-02-14 15:28:23 +0200

    Formatting

--- vaadin-renderer-flow-parent/vaadin-renderer-flow/src/main/java/com/vaadin/flow/data/renderer/LocalDateRenderer.java
+++ vaadin-renderer-flow-parent/vaadin-renderer-flow/src/main/java/com/vaadin/flow/data/renderer/LocalDateRenderer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2021 Vaadin Ltd.
+ * Copyright 2000-2022 Vaadin Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -189,7 +189,8 @@ public class LocalDateRenderer<SOURCE>
     @Override
     protected String getFormattedValue(LocalDate date) {
         try {
-            return date == null ? nullRepresentation : formatter.get().format(date);
+            return date == null ? nullRepresentation
+                    : formatter.get().format(date);
         } catch (Exception e) {
             throw new IllegalStateException("Could not format input date '"
                     + date + "' using formatter '" + formatter + "'", e);
