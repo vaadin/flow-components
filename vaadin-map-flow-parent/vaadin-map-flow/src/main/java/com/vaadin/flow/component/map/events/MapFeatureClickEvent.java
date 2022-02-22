d5d639956299114d43db24ef18463dc7cd9cbf2b
Sascha Ißbrücker <sissbruecker@vaadin.com>
2022-02-22 09:53:19 +0100

    chore: map javadoc improvements (#2702)
    * chore: improve javadoc for map component
    
    * add documentation for AbstractConfigurationObject
    
    * improve javadoc for view extent
    
    * improve wording

--- vaadin-map-flow-parent/vaadin-map-flow/src/main/java/com/vaadin/flow/component/map/events/MapFeatureClickEvent.java
+++ vaadin-map-flow-parent/vaadin-map-flow/src/main/java/com/vaadin/flow/component/map/events/MapFeatureClickEvent.java
@@ -1,5 +1,21 @@
 package com.vaadin.flow.component.map.events;
 
+/*
+ * #%L
+ * Vaadin Map
+ * %%
+ * Copyright (C) 2022 - 2022 Vaadin Ltd
+ * %%
+ * This program is available under Commercial Vaadin Developer License
+ * 4.0 (CVDLv4).
+ *
+ * See the file license.html distributed with this software for more
+ * information about licensing.
+ *
+ * For the full License, see <https://vaadin.com/license/cvdl-4.0>.
+ * #L%
+ */
+
 import com.vaadin.flow.component.ComponentEvent;
 import com.vaadin.flow.component.DomEvent;
 import com.vaadin.flow.component.EventData;
