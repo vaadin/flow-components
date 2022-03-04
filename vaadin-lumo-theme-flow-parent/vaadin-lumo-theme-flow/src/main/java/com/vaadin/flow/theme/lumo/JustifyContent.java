37e0fc3ea5ce3847bcc50596db078f15c9e55eb3
Joacim Päivärinne <joacim@vaadin.com>
2022-03-04 15:59:34 +0200

    Update JustifyContent.java

--- vaadin-lumo-theme-flow-parent/vaadin-lumo-theme-flow/src/main/java/com/vaadin/flow/theme/lumo/JustifyContent.java
+++ vaadin-lumo-theme-flow-parent/vaadin-lumo-theme-flow/src/main/java/com/vaadin/flow/theme/lumo/JustifyContent.java
@@ -2,12 +2,11 @@ package com.vaadin.flow.theme.lumo;
 
 public class JustifyContent {
 
+	public static String AROUND = "justify-around";
+	public static String BETWEEN = "justify-between";
 	public static String CENTER = "justify-center";
 	public static String END = "justify-end";
+	public static String EVENLY = "justify-evenly";
 	public static String START = "justify-start";
-
-	public static String SPACE_AROUND = "justify-around";
-	public static String SPACE_BETWEEN = "justify-between";
-	public static String SPACE_EVENLY = "justify-evenly";
 
 }
