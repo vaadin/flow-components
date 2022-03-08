ea3ae5a9abf361ad8e2438d0a55837e93fbdf45f
Diego Cardoso <diego@vaadin.com>
2022-03-08 10:14:06 +0200

    refactor: remove progress-bar demo module (#2819)

--- vaadin-progress-bar-flow-parent/vaadin-progress-bar-flow-integration-tests/src/test/java/com/vaadin/flow/component/progressbar/tests/ProgressBarIT.java
+++ vaadin-progress-bar-flow-parent/vaadin-progress-bar-flow-integration-tests/src/test/java/com/vaadin/flow/component/progressbar/tests/ProgressBarIT.java
@@ -17,29 +17,36 @@
 
 package com.vaadin.flow.component.progressbar.tests;
 
+import com.vaadin.flow.component.progressbar.ProgressBarVariant;
+import com.vaadin.flow.testutil.TestPath;
+import com.vaadin.tests.AbstractComponentIT;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-import com.vaadin.flow.component.progressbar.demo.ProgressBarView;
-import com.vaadin.tests.ComponentDemoTest;
-
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
-import static org.junit.Assert.assertThat;
 
 /**
  * Integration tests for the {@link ProgressBarView}.
  *
  * @author Vaadin Ltd.
  */
-public class ProgressBarIT extends ComponentDemoTest {
+@TestPath("vaadin-progress-bar")
+public class ProgressBarIT extends AbstractComponentIT {
+
+    @Before
+    public void init() {
+        open();
+    }
 
     @Test
     public void clickOnProgressButtonIncrementsProgressBarValue() {
-        WebElement progressBar = layout
-                .findElement(By.id("custom-progress-bar"));
-        WebElement button = layout.findElement(By.id("progress-button"));
+        WebElement progressBar = findElement(By.id("custom-progress-bar"));
+        WebElement button = findElement(By.id("progress-button"));
         assertThat(valueOf(progressBar), is("20"));
 
         scrollIntoViewAndClick(button);
@@ -49,15 +56,16 @@ public class ProgressBarIT extends ComponentDemoTest {
 
     @Test
     public void assertVariants() {
-        verifyThemeVariantsBeingToggled();
+        WebElement progressBar = findElement(By.id("progress-bar-theme-variant"));
+        scrollToElement(progressBar);
+
+        Assert.assertEquals(ProgressBarVariant.LUMO_ERROR.getVariantName(), progressBar.getAttribute("theme"));
+
+        findElement(By.id("remove-theme-variant-button")).click();
+        Assert.assertNull(progressBar.getAttribute("theme"));
     }
 
     private String valueOf(WebElement progressBar) {
         return progressBar.getAttribute("value");
-    }
-
-    @Override
-    protected String getTestPath() {
-        return "/vaadin-progress-bar";
     }
 }
