aaf990450f1e6dcc2108ae7abdc16a8438c53a8b
Manuel Carrasco Moñino <manolo@vaadin.com>
2022-04-26 18:09:12 +0200

    chore: change CSS selectors vaadin/spreadsheet#832
    * chore: change CSS selectors
    
    * feat!: disable popupButtons until table-support is implemented
    
    * chore: remove comment from css
    
    * Remove window, checkbox and remaining spreadsheetport styles
    
    Co-authored-by: Guillermo Alvarez <guillermo@vaadin.com>

--- vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow/src/main/resources/META-INF/resources/frontend/vaadin-spreadsheet/vaadin-spreadsheet.js
+++ vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow/src/main/resources/META-INF/resources/frontend/vaadin-spreadsheet/vaadin-spreadsheet.js
@@ -212,14 +212,11 @@ export class VaadinSpreadsheet extends LitElement {
     if (!this.api) {
       this.injectStyle('css_valo', css_valo);
 
-      this.classList.add('spreadsheetport');
-
       let overlays = document.getElementById('spreadsheet-overlays');
       if (!overlays) {
         overlays = document.createElement('div');
         overlays.id = 'spreadsheet-overlays';
-        overlays.classList.add('spreadsheetport');
-        document.body.appendChild(overlays);
+        document.body.appendChild(overlays);        
       }
 
       this.api = new Spreadsheet(this);
