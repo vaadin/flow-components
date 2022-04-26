af47ecc7cd82bd0f4da5dc77a57a9879a0bda3d4
Sergey Vinogradov <mr.vursen@gmail.com>
2022-04-26 12:16:56 +0300

    fix: postpone tryCatchWrapper until Flow app is initialized (#3055) (#3063)

--- vaadin-menu-bar-flow-parent/vaadin-menu-bar-flow/src/main/resources/META-INF/resources/frontend/menubarConnector.js
+++ vaadin-menu-bar-flow-parent/vaadin-menu-bar-flow/src/main/resources/META-INF/resources/frontend/menubarConnector.js
@@ -19,88 +19,92 @@
     return window.Vaadin.Flow.tryCatchWrapper(callback, 'Vaadin Menu Bar');
   };
 
-  window.Vaadin.Flow.menubarConnector = {
-    /**
-     * Initializes the connector for a menu bar element.
-     *
-     * @param {HTMLElement} menubar
-     * @param {string} appId
-     */
-    initLazy: tryCatchWrapper(function (menubar, appId) {
-      if (menubar.$connector) {
-        return;
-      }
+  /**
+   * Initializes the connector for a menu bar element.
+   *
+   * @param {HTMLElement} menubar
+   * @param {string} appId
+   */
+  function initLazy(menubar, appId) {
+    if (menubar.$connector) {
+      return;
+    }
 
-      const observer = new MutationObserver((records) => {
-        const hasChangedAttributes = records.some((entry) => {
-          const oldValue = entry.oldValue;
-          const newValue = entry.target.getAttribute(entry.attributeName);
-          return oldValue !== newValue;
-        });
-
-        if (hasChangedAttributes) {
-          menubar.$connector.generateItems();
-        }
+    const observer = new MutationObserver((records) => {
+      const hasChangedAttributes = records.some((entry) => {
+        const oldValue = entry.oldValue;
+        const newValue = entry.target.getAttribute(entry.attributeName);
+        return oldValue !== newValue;
       });
 
-      menubar.$connector = {
-        /**
-         * Generates and assigns the items to the menu bar.
-         *
-         * When the method is called without providing a node id,
-         * the previously generated items tree will be used.
-         * That can be useful if you only want to sync the disabled and hidden properties of root items.
-         *
-         * @param {number | undefined} nodeId
-         */
-        generateItems: tryCatchWrapper((nodeId) => {
-          if (!menubar.shadowRoot) {
-            // workaround for https://github.com/vaadin/flow/issues/5722
-            setTimeout(() => menubar.$connector.generateItems(nodeId));
-            return;
-          }
+      if (hasChangedAttributes) {
+        menubar.$connector.generateItems();
+      }
+    });
 
-          if (nodeId) {
-            menubar.__generatedItems = window.Vaadin.Flow.contextMenuConnector.generateItemsTree(appId, nodeId);
-          }
+    menubar.$connector = {
+      /**
+       * Generates and assigns the items to the menu bar.
+       *
+       * When the method is called without providing a node id,
+       * the previously generated items tree will be used.
+       * That can be useful if you only want to sync the disabled and hidden properties of root items.
+       *
+       * @param {number | undefined} nodeId
+       */
+      generateItems: tryCatchWrapper((nodeId) => {
+        if (!menubar.shadowRoot) {
+          // workaround for https://github.com/vaadin/flow/issues/5722
+          setTimeout(() => menubar.$connector.generateItems(nodeId));
+          return;
+        }
 
-          let items = menubar.__generatedItems || [];
+        if (nodeId) {
+          menubar.__generatedItems = window.Vaadin.Flow.contextMenuConnector.generateItemsTree(appId, nodeId);
+        }
 
-          // Propagate disabled state from items to parent buttons
-          items.forEach((item) => (item.disabled = item.component.disabled));
+        let items = menubar.__generatedItems || [];
 
-          // Remove hidden items entirely from the array. Just hiding them
-          // could cause the overflow button to be rendered without items.
-          //
-          // The items-prop needs to be set even when all items are visible
-          // to update the disabled state and re-render buttons.
-          items = items.filter((item) => !item.component.hidden);
+        // Propagate disabled state from items to parent buttons
+        items.forEach((item) => (item.disabled = item.component.disabled));
 
-          // Observe for hidden and disabled attributes in case they are changed by Flow.
-          // When a change occurs, the observer will re-generate items on top of the existing tree
-          // to sync the new attribute values with the corresponding properties in the items array.
-          items.forEach((item) => {
-            observer.observe(item.component, {
-              attributeFilter: ['hidden', 'disabled'],
-              attributeOldValue: true
+        // Remove hidden items entirely from the array. Just hiding them
+        // could cause the overflow button to be rendered without items.
+        //
+        // The items-prop needs to be set even when all items are visible
+        // to update the disabled state and re-render buttons.
+        items = items.filter((item) => !item.component.hidden);
+
+        // Observe for hidden and disabled attributes in case they are changed by Flow.
+        // When a change occurs, the observer will re-generate items on top of the existing tree
+        // to sync the new attribute values with the corresponding properties in the items array.
+        items.forEach((item) => {
+          observer.observe(item.component, {
+            attributeFilter: ['hidden', 'disabled'],
+            attributeOldValue: true
+          });
+        });
+
+        menubar.items = items;
+
+        // Propagate click events from the menu buttons to the item components
+        menubar._buttons.forEach((button) => {
+          if (button.item && button.item.component) {
+            button.addEventListener('click', (e) => {
+              if (e.composedPath().indexOf(button.item.component) === -1) {
+                button.item.component.click();
+                e.stopPropagation();
+              }
             });
-          });
+          }
+        });
+      })
+    };
+  }
 
-          menubar.items = items;
-
-          // Propagate click events from the menu buttons to the item components
-          menubar._buttons.forEach((button) => {
-            if (button.item && button.item.component) {
-              button.addEventListener('click', (e) => {
-                if (e.composedPath().indexOf(button.item.component) === -1) {
-                  button.item.component.click();
-                  e.stopPropagation();
-                }
-              });
-            }
-          });
-        })
-      };
-    })
+  window.Vaadin.Flow.menubarConnector = {
+    initLazy(...args) {
+      return tryCatchWrapper(initLazy)(...args);
+    }
   };
 })();
