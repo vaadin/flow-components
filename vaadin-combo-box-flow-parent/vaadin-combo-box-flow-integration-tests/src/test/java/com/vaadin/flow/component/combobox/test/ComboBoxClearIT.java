a6600951f75d8e165e78bb6a77d6e591d12a6c2c
Tatu Lund <tatu@vaadin.com>
2022-04-26 15:09:29 +0300

    Create IT

---
+++ vaadin-combo-box-flow-parent/vaadin-combo-box-flow-integration-tests/src/test/java/com/vaadin/flow/component/combobox/test/ComboBoxClearIT.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2000-2012 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.combobox.test;
+
+import java.util.List;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import com.vaadin.flow.component.combobox.testbench.ComboBoxElement;
+import com.vaadin.flow.testutil.TestPath;
+
+@TestPath("vaadin-combo-box/clear")
+public class ComboBoxClearIT extends AbstractComboBoxIT {
+
+    private ComboBoxElement box;
+
+    @Test
+    public void comboBoxClear() {
+        open();
+        waitUntil(driver -> findElements(By.tagName("vaadin-combo-box"))
+                .size() > 0);
+        box = $(ComboBoxElement.class).first();
+        Assert.assertEquals(box.getSelectedText(),"Eight");
+        box.clear();
+        Assert.assertTrue(box.getSelectedText().isEmpty());
+    }
+}
