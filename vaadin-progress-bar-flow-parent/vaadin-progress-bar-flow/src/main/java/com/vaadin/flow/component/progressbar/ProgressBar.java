d90d9724c06dbf7ea69f7e3077e57361deb1bcc3
Serhii Kulykov <iamkulykov@gmail.com>
2022-05-16 14:52:06 +0300

    chore: update web components to 23.1.0-beta4, fix ITs (23.1)  (#3190)
    * chore: Update NpmPackages Webjars versions (23.1)
    
    * chore: update web components to 23.1.0-beta3
    
    * test: ignore iron-icon deprecation warning
    
    * test: ignore iron-icon deprecation warning in DatePicker IT
    
    * chore: update web components to 23.1.0-beta4
    
    Co-authored-by: Vaadin Bot <vaadin-bot@users.noreply.github.com>
    Co-authored-by: Tomi Virkki <virkki@vaadin.com>

--- vaadin-progress-bar-flow-parent/vaadin-progress-bar-flow/src/main/java/com/vaadin/flow/component/progressbar/ProgressBar.java
+++ vaadin-progress-bar-flow-parent/vaadin-progress-bar-flow/src/main/java/com/vaadin/flow/component/progressbar/ProgressBar.java
@@ -27,8 +27,8 @@ import com.vaadin.flow.component.dependency.NpmPackage;
  *
  * @author Vaadin Ltd.
  */
-@NpmPackage(value = "@vaadin/progress-bar", version = "23.1.0-beta1")
-@NpmPackage(value = "@vaadin/vaadin-progress-bar", version = "23.1.0-beta1")
+@NpmPackage(value = "@vaadin/progress-bar", version = "23.1.0-beta4")
+@NpmPackage(value = "@vaadin/vaadin-progress-bar", version = "23.1.0-beta4")
 public class ProgressBar extends GeneratedVaadinProgressBar<ProgressBar>
         implements HasSize {
 
