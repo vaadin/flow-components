03f95c6b81165de0a017e2506d9ee47f89b780ca
web-padawan <iamkulykov@gmail.com>
2022-05-16 13:10:30 +0300

    chore: update web components to 23.1.0-beta4

--- vaadin-avatar-flow-parent/vaadin-avatar-flow/src/main/java/com/vaadin/flow/component/avatar/Avatar.java
+++ vaadin-avatar-flow-parent/vaadin-avatar-flow/src/main/java/com/vaadin/flow/component/avatar/Avatar.java
@@ -55,11 +55,11 @@ import java.util.stream.Stream;
  * @author Vaadin Ltd
  */
 @Tag("vaadin-avatar")
-@NpmPackage(value = "@vaadin/polymer-legacy-adapter", version = "23.1.0-beta3")
+@NpmPackage(value = "@vaadin/polymer-legacy-adapter", version = "23.1.0-beta4")
 @JsModule("@vaadin/polymer-legacy-adapter/style-modules.js")
 @JsModule("@vaadin/avatar/src/vaadin-avatar.js")
-@NpmPackage(value = "@vaadin/avatar", version = "23.1.0-beta3")
-@NpmPackage(value = "@vaadin/vaadin-avatar", version = "23.1.0-beta3")
+@NpmPackage(value = "@vaadin/avatar", version = "23.1.0-beta4")
+@NpmPackage(value = "@vaadin/vaadin-avatar", version = "23.1.0-beta4")
 public class Avatar extends Component implements HasStyle, HasSize, HasTheme {
 
     /**
--- vaadin-progress-bar-flow-parent/vaadin-progress-bar-flow/src/main/java/com/vaadin/flow/component/progressbar/GeneratedVaadinProgressBar.java
+++ vaadin-progress-bar-flow-parent/vaadin-progress-bar-flow/src/main/java/com/vaadin/flow/component/progressbar/GeneratedVaadinProgressBar.java
@@ -100,7 +100,7 @@ import com.vaadin.flow.component.dependency.NpmPackage;
  * </table>
  */
 @Tag("vaadin-progress-bar")
-@NpmPackage(value = "@vaadin/polymer-legacy-adapter", version = "23.1.0-beta3")
+@NpmPackage(value = "@vaadin/polymer-legacy-adapter", version = "23.1.0-beta4")
 @JsModule("@vaadin/polymer-legacy-adapter/style-modules.js")
 @JsModule("@vaadin/progress-bar/src/vaadin-progress-bar.js")
 public abstract class GeneratedVaadinProgressBar<R extends GeneratedVaadinProgressBar<R>>
