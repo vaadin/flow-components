7879dd7acee8e224e2837cfdf7fbfe725f3ff6e5
Tatu Lund <tatu@vaadin.com>
2022-05-24 19:41:12 +0300

    Add unit test

--- vaadin-grid-pro-flow-parent/vaadin-grid-pro-flow/src/test/java/com/vaadin/flow/component/gridpro/GridProTest.java
+++ vaadin-grid-pro-flow-parent/vaadin-grid-pro-flow/src/test/java/com/vaadin/flow/component/gridpro/GridProTest.java
@@ -1,8 +1,14 @@
 package com.vaadin.flow.component.gridpro;
 
 import com.vaadin.flow.component.ComponentUtil;
+import com.vaadin.flow.component.HasValueAndElement;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.AbstractField.ComponentValueChangeEvent;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.data.provider.DataCommunicator;
 import com.vaadin.flow.data.provider.KeyMapper;
+import com.vaadin.flow.shared.Registration;
 import com.vaadin.flow.data.provider.DataProvider;
 import elemental.json.JsonObject;
 import elemental.json.impl.JreJsonFactory;
@@ -58,6 +64,13 @@ public class GridProTest {
     }
 
     @Test
+    public void addCustomColumn_hasValueAndElement() {
+        FakeField field = new FakeField();
+        grid.addEditColumn(Person::getName).custom(field,
+                (item, newValue) -> item.setName(newValue));
+    }
+
+    @Test
     public void setEnterNextRow_getEnterNextRow() {
         grid.setEnterNextRow(true);
         Assert.assertEquals(grid.getEnterNextRow(), true);
@@ -91,5 +104,34 @@ public class GridProTest {
 
         Assert.assertEquals(2, items.size());
         items.forEach(item -> Assert.assertEquals(testItem, item));
+    }
+
+    @Tag(Tag.DIV)
+    public class FakeField extends Component implements
+            HasValueAndElement<ComponentValueChangeEvent<FakeField, String>, String> {
+        private String value;
+
+        @Override
+        public void setValue(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public String getValue() {
+            return value;
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public Registration addValueChangeListener(
+                ValueChangeListener<? super ComponentValueChangeEvent<FakeField, String>> listener) {
+            @SuppressWarnings("rawtypes")
+            ComponentEventListener componentListener = event -> {
+                ComponentValueChangeEvent<FakeField, String> valueChangeEvent = (ComponentValueChangeEvent<FakeField, String>) event;
+                listener.valueChanged(valueChangeEvent);
+            };
+            return ComponentUtil.addListener(this,
+                    ComponentValueChangeEvent.class, componentListener);
+        }
     }
 }
