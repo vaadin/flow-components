2cce8e0aff21d9c879e6e44fc46b3b3d8daae5fe
Diego Cardoso <diego@vaadin.com>
2022-05-24 11:17:09 +0300

    test: migrate some ITs from FW8 (#3195)
    * test: migrate some ITs from FW8
    
    * test: simplify assertion
    
    * test: use capital L for long value
    
    * test: trying to use timezoned date
    
    * Revert "test: trying to use timezoned date"
    
    This reverts commit c30f31112bd1c2115b105992d13a0acac1bc7edb.
    
    * test: ignore failing test
    
    * refactor: move creation of new ss to init
    
    * test: add workaround for failing test

---
+++ vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow-integration-tests/src/test/java/com/vaadin/flow/component/spreadsheet/test/FormulaFieldFormatIT.java
@@ -0,0 +1,70 @@
+package com.vaadin.flow.component.spreadsheet.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Locale;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test for formula field formatting.
+ *
+ */
+public class FormulaFieldFormatIT extends AbstractSpreadsheetIT {
+
+    @Before
+    public void init() {
+        getDriver().get(getBaseURL());
+        setDefaultLocale();
+    }
+
+    @Test
+    public void numberFormat_sheetWithNumberFormatRuleForNumericCells_formulaFieldContentsUnformattedExceptForLocale() {
+        loadFile("number_format.xlsx");
+        assertFormat("F3", "3,333.333", "3333.333");
+        assertFormat("H3", "3,333.33 €", "3333.333");
+    }
+
+    @Test
+    public void rounding_sheetWithNumberFormatRuleForNumericCells_formulaFieldContentsUnformatted() {
+        loadFile("rounding.xlsx");
+        assertFormat("B2", "5", "4.99999");
+        assertFormat("B3", "5", "5.00005");
+    }
+
+    @Test
+    public void rounding_sheetWithGeneralFormatRuleForNumericCells_formulaFieldContentsUnformattedExceptForLocale() {
+        loadFile("general_round.xlsx");
+        assertFormat("E3", "1E+12", "999999999999");
+        assertFormat("E14", "10", "9.99999999999");
+    }
+
+    @Test
+    public void dateFormat_sheetWithDateFormatRuleForDateCells_formulaFieldContentsSimpleDateFormat() {
+        loadFile("date_format.xlsx");
+
+        // Cell values and formula field values equals with dates
+        assertFormat("A5", "14-Mar-14", "14-Mar-14");
+        assertFormat("A10", "3/14/14 12:00 AM", "3/14/14 12:00 AM");
+        assertFormat("A1", "3/14", "3/14");
+    }
+
+    @Test
+    public void formulaTrimming_invalidPOIFormula_formulaIsNotTrimmed() {
+        loadFile("table-subtotals-ranges.xlsx");
+        assertFormat("C4", "300", "=SUBTOTAL(109,Sheet1!$C$2:$C$3)");
+    }
+
+    private void assertFormat(String cell, String cellValue,
+            String formulaFieldValue) {
+        assertEquals("Unexpected cell content,", cellValue, getCellValue(cell));
+        clickCell(cell);
+        assertEquals("Unexpected formula bar value,", formulaFieldValue,
+                getFormulaFieldValue());
+    }
+
+    private void setDefaultLocale() {
+        setLocale(Locale.US);
+    }
+
+}
