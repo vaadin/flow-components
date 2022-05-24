e8ca750665af30085f74ae13164c4b26c9dc2c8f
Tomi Virkki <virkki@vaadin.com>
2022-05-24 15:37:49 +0300

    test: migrate some ITs from FW8

---
+++ vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow-integration-tests/src/test/java/com/vaadin/flow/component/spreadsheet/test/RemoveInsertRowIT.java
@@ -0,0 +1,289 @@
+package com.vaadin.flow.component.spreadsheet.test;
+
+import com.vaadin.flow.component.spreadsheet.testbench.SpreadsheetElement;
+import com.vaadin.flow.component.spreadsheet.tests.fixtures.TestFixtures;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+
+public class RemoveInsertRowIT extends AbstractSpreadsheetIT {
+
+    @Before
+    public void init() {
+        getDriver().get(getBaseURL());
+        createNewSpreadsheet();
+    }
+
+    @Test
+    public void removeRow_theFirstCellHasInvalidFormula_formulaIndicatorIsRemoved() {
+        final SpreadsheetElement spreadsheet = getSpreadsheet();
+        spreadsheet.getCellAt("A1").setValue("=a");
+
+        deleteFirstRow(spreadsheet);
+
+        waitUntil(new ExpectedCondition<Object>() {
+            @Override
+            public Object apply(WebDriver webDriver) {
+                return !spreadsheet.getCellAt("A1")
+                        .hasInvalidFormulaIndicator();
+            }
+        });
+    }
+
+    @Test
+    public void removeRow_theSecondRowCellHasInvalidFormula_formulaIndicatorIsRemoved() {
+        final SpreadsheetElement spreadsheet = getSpreadsheet();
+        spreadsheet.getCellAt("A2").setValue("=a");
+
+        deleteRow(spreadsheet, 2);
+
+        waitUntil(new ExpectedCondition<Object>() {
+            @Override
+            public Object apply(WebDriver webDriver) {
+                return spreadsheet
+                        .findElements(
+                                By.className("cell-invalidformula-triangle"))
+                        .isEmpty();
+            }
+        });
+    }
+
+    @Test
+    public void insertRow_theFirstCellHasInvalidFormula_theInvalidFormulaIsMovedToNextRow() {
+        final SpreadsheetElement spreadsheet = getSpreadsheet();
+        spreadsheet.getCellAt("A1").setValue("=a");
+
+        insertNewFirstRow(spreadsheet);
+
+        waitUntil(new ExpectedCondition<Object>() {
+            @Override
+            public Object apply(WebDriver webDriver) {
+                return spreadsheet.getCellAt("A2").hasInvalidFormulaIndicator();
+            }
+        });
+    }
+
+    @Test
+    public void removeRow_theSecondRowHasInvalidFormulaCell_formulaIndicatorIsMovedUp() {
+        final SpreadsheetElement spreadsheet = getSpreadsheet();
+        spreadsheet.getCellAt("A2").setValue("=a");
+
+        deleteFirstRow(spreadsheet);
+
+        waitUntil(new ExpectedCondition<Object>() {
+            @Override
+            public Object apply(WebDriver webDriver) {
+                return spreadsheet.getCellAt("A1").hasInvalidFormulaIndicator();
+            }
+        });
+    }
+
+    @Test
+    public void removeRow_theSecondAndThirdRowHasInvalidFormulaCell_formulaIndicatorIsMovedUp() {
+        final SpreadsheetElement spreadsheet = getSpreadsheet();
+        spreadsheet.getCellAt("A2").setValue("=a");
+        spreadsheet.getCellAt("A3").setValue("=a");
+
+        deleteFirstRow(spreadsheet);
+
+        waitUntil(new ExpectedCondition<Object>() {
+            @Override
+            public Object apply(WebDriver webDriver) {
+                return spreadsheet.getCellAt("A1").hasInvalidFormulaIndicator()
+                        && spreadsheet.getCellAt("A2")
+                                .hasInvalidFormulaIndicator();
+            }
+        });
+    }
+
+    @Test
+    public void insertRow_theFirstAndSecondRowHasInvalidFormulaCellAndTheRowIsAddedBetween_theFirstAndThirdRowHasErrorIndicator() {
+        final SpreadsheetElement spreadsheet = getSpreadsheet();
+        spreadsheet.getCellAt("A1").setValue("=a");
+        spreadsheet.getCellAt("A2").setValue("=a");
+
+        insertRow(spreadsheet, 2);
+
+        waitUntil(new ExpectedCondition<Object>() {
+            @Override
+            public Object apply(WebDriver webDriver) {
+                return spreadsheet.getCellAt("A1").hasInvalidFormulaIndicator()
+                        && !spreadsheet.getCellAt("A2")
+                                .hasInvalidFormulaIndicator()
+                        && spreadsheet.getCellAt("A3")
+                                .hasInvalidFormulaIndicator();
+            }
+        });
+    }
+
+    @Test
+    public void removeRow_theFirstRowHasInvalidFormulaCellAndTheSecondRowIsRemoved_theFirstRowHasErrorIndicator() {
+        final SpreadsheetElement spreadsheet = getSpreadsheet();
+        spreadsheet.getCellAt("A1").setValue("=a");
+
+        deleteRow(spreadsheet, 2);
+
+        waitUntil(new ExpectedCondition<Object>() {
+            @Override
+            public Object apply(WebDriver webDriver) {
+                return spreadsheet.getCellAt("A1").hasInvalidFormulaIndicator();
+            }
+        });
+    }
+
+    @Test
+    public void removeRow_theFirstCellHasMergedCell_thereIsNoMergedCells() {
+        final SpreadsheetElement spreadsheet = getSpreadsheet();
+        new Actions(driver).clickAndHold(spreadsheet.getCellAt("A1"))
+                .release(spreadsheet.getCellAt("B1")).perform();
+        spreadsheet.getCellAt("A1").contextClick();
+        spreadsheet.getContextMenu().getItem("Merge cells").click();
+
+        deleteFirstRow(spreadsheet);
+
+        waitUntil(new ExpectedCondition<Object>() {
+            @Override
+            public Object apply(WebDriver webDriver) {
+                return spreadsheet.findElements(By.cssSelector(".merged-cell"))
+                        .isEmpty();
+            }
+        });
+    }
+
+    @Test
+    public void insertRow_theFirstCellHasMergedCell_theMergedCellIsMovedToNextRow() {
+        final SpreadsheetElement spreadsheet = getSpreadsheet();
+        new Actions(driver).clickAndHold(spreadsheet.getCellAt("A1"))
+                .release(spreadsheet.getCellAt("B1")).perform();
+        spreadsheet.getCellAt("A1").contextClick();
+        spreadsheet.getContextMenu().getItem("Merge cells").click();
+
+        insertNewFirstRow(spreadsheet);
+
+        waitUntil(new ExpectedCondition<Object>() {
+            @Override
+            public Object apply(WebDriver webDriver) {
+                return spreadsheet
+                        .findElement(By.cssSelector(".col1.row2.merged-cell"))
+                        .isDisplayed();
+            }
+        });
+    }
+
+    @Ignore("Ignore until https://github.com/vaadin/flow-components/issues/3223 is fixed")
+    @Test
+    public void removeRow_theFirstCellHasPopupButton_thereIsNoPopupButtons() {
+        final SpreadsheetElement spreadsheet = getSpreadsheet();
+        clickCell("B1");
+        loadTestFixture(TestFixtures.PopupButton);
+        clickCell("A1");
+
+        deleteFirstRow(spreadsheet);
+
+        waitUntil(new ExpectedCondition<Object>() {
+            @Override
+            public Object apply(WebDriver webDriver) {
+                return !spreadsheet.getCellAt("A1").hasPopupButton();
+            }
+        });
+    }
+
+    @Ignore("Ignore until https://github.com/vaadin/flow-components/issues/3223 is fixed")
+    @Test
+    public void removeRow_theSecondRowCellHasPopupButton_thereIsNoPopupButtons() {
+        final SpreadsheetElement spreadsheet = getSpreadsheet();
+        clickCell("B1");
+        loadTestFixture(TestFixtures.PopupButton);
+        clickCell("A2");
+
+        deleteRow(spreadsheet, 2);
+
+        waitUntil(new ExpectedCondition<Object>() {
+            @Override
+            public Object apply(WebDriver webDriver) {
+                return spreadsheet.findElements(By.className("popupbutton"))
+                        .isEmpty();
+            }
+        });
+    }
+
+    @Ignore("Ignore until https://github.com/vaadin/flow-components/issues/3223 is fixed")
+    @Test
+    public void removeRow_theSecondRowCellHasPopupButton_popupButtonIsMovedUp() {
+        final SpreadsheetElement spreadsheet = getSpreadsheet();
+        clickCell("B1");
+        loadTestFixture(TestFixtures.PopupButton);
+        clickCell("A2");
+
+        deleteFirstRow(spreadsheet);
+
+        waitUntil(new ExpectedCondition<Object>() {
+            @Override
+            public Object apply(WebDriver webDriver) {
+                return spreadsheet.getCellAt("A1").hasPopupButton();
+            }
+        });
+    }
+
+    @Ignore("Ignore until https://github.com/vaadin/flow-components/issues/3223 is fixed")
+    @Test
+    public void insertRow_theFirstCellHasPopupButton_thePopupButtonIsMovedToNextRow() {
+        final SpreadsheetElement spreadsheet = getSpreadsheet();
+        clickCell("B1");
+        loadTestFixture(TestFixtures.PopupButton);
+        clickCell("A1");
+
+        insertNewFirstRow(spreadsheet);
+
+        waitUntil(new ExpectedCondition<Object>() {
+            @Override
+            public Object apply(WebDriver webDriver) {
+                return spreadsheet.getCellAt("A2").hasPopupButton();
+            }
+        });
+    }
+
+    @Ignore("This is a known issue which should be fixed. The problem is most likely on client side.")
+    @Test
+    public void insertRow_theFirstAndSecondRowHasPopupbuttonsNewRowIsAddedToFirstRow_theSecondAndThirdRowHasPopupButton() {
+        final SpreadsheetElement spreadsheet = getSpreadsheet();
+        clickCell("B1");
+        loadTestFixture(TestFixtures.PopupButton);
+        clickCell("A1");
+        clickCell("A2");
+
+        insertNewFirstRow(spreadsheet);
+
+        waitUntil(new ExpectedCondition<Object>() {
+            @Override
+            public Object apply(WebDriver webDriver) {
+                return !spreadsheet.getCellAt("A1").hasPopupButton()
+                        && spreadsheet.getCellAt("A2").hasPopupButton()
+                        && spreadsheet.getCellAt("A3").hasPopupButton();
+            }
+        });
+    }
+
+    private void insertNewFirstRow(SpreadsheetElement spreadsheet) {
+        insertRow(spreadsheet, 1);
+    }
+
+    private void insertRow(SpreadsheetElement spreadsheet, int row) {
+        contextClickOnRowHeader(row);
+        clickItem("Insert new row");
+    }
+
+    private void deleteFirstRow(SpreadsheetElement spreadsheet) {
+        deleteRow(spreadsheet, 1);
+    }
+
+    private void deleteRow(SpreadsheetElement spreadsheet, int row) {
+        contextClickOnRowHeader(row);
+        clickItem("Delete row " + row);
+    }
+}
---
+++ vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow-integration-tests/src/test/java/com/vaadin/flow/component/spreadsheet/test/RowShiftIT.java
@@ -0,0 +1,76 @@
+package com.vaadin.flow.component.spreadsheet.test;
+
+import com.vaadin.flow.component.spreadsheet.tests.fixtures.TestFixtures;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RowShiftIT extends AbstractSpreadsheetIT {
+
+    @Before
+    public void init() {
+        getDriver().get(getBaseURL());
+        createNewSpreadsheet();
+    }
+
+    private void createRow(int n, int column) {
+        for (int i = 0; i < n; i++) {
+            getSpreadsheet().getCellAt(i + 1, column)
+                    .setValue(Integer.toString(i + 1));
+        }
+    }
+
+    @Test
+    public void testBasic() {
+        createRow(10, 1);
+        clickCell("A5");
+
+        loadTestFixture(TestFixtures.InsertRow);
+        Assert.assertEquals("4", getSpreadsheet().getCellAt("A4").getValue());
+        Assert.assertEquals("", getSpreadsheet().getCellAt("A5").getValue());
+        Assert.assertEquals("5", getSpreadsheet().getCellAt("A6").getValue());
+        clickCell("A7");
+        loadTestFixture(TestFixtures.DeleteRow);
+        Assert.assertEquals("8", getSpreadsheet().getCellAt("A8").getValue());
+    }
+
+    @Test
+    public void testFormula() {
+        createRow(10, 1);
+        getSpreadsheet().getCellAt("B1").setValue("=$A$6");
+        getSpreadsheet().getCellAt("C1").setValue("=A6");
+        getSpreadsheet().getCellAt("B8").setValue("=$A$6");
+        getSpreadsheet().getCellAt("C8").setValue("=A6");
+
+        clickCell("A3");
+        loadTestFixture(TestFixtures.InsertRow);
+
+        Assert.assertEquals("6", getSpreadsheet().getCellAt("B1").getValue());
+        Assert.assertEquals("6", getSpreadsheet().getCellAt("C1").getValue());
+        Assert.assertEquals("6", getSpreadsheet().getCellAt("B9").getValue());
+        Assert.assertEquals("6", getSpreadsheet().getCellAt("C9").getValue());
+    }
+
+    @Test
+    public void testDeleteFormulaReference() {
+        getSpreadsheet().getCellAt("A3").setValue("42");
+        getSpreadsheet().getCellAt("C1").setValue("=A3");
+        clickCell("A4");
+        loadTestFixture(TestFixtures.DeleteRow);
+
+        Assert.assertEquals("42", getSpreadsheet().getCellAt("A3").getValue());
+        Assert.assertEquals("42", getSpreadsheet().getCellAt("C1").getValue());
+
+        clickCell("A2");
+        loadTestFixture(TestFixtures.DeleteRow);
+        clickCell("A3");
+        Assert.assertEquals("42", getSpreadsheet().getCellAt("A2").getValue());
+        Assert.assertEquals("42", getSpreadsheet().getCellAt("C1").getValue());
+
+        clickCell("A2");
+        loadTestFixture(TestFixtures.DeleteRow);
+        Assert.assertEquals("#REF!",
+                getSpreadsheet().getCellAt("C1").getValue());
+    }
+}
