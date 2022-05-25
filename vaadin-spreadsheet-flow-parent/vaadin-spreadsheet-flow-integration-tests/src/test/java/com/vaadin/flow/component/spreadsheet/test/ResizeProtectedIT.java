44643eeab827d2ab070ea75e6170c65994fd537c
Tomi Virkki <virkki@vaadin.com>
2022-05-25 15:02:21 +0300

    test: migrate some ITs from FW8

---
+++ vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow-integration-tests/src/test/java/com/vaadin/flow/component/spreadsheet/test/ResizeProtectedIT.java
@@ -0,0 +1,63 @@
+package com.vaadin.flow.component.spreadsheet.test;
+
+import com.vaadin.flow.component.spreadsheet.testbench.SheetHeaderElement;
+import com.vaadin.flow.component.spreadsheet.testbench.SpreadsheetElement;
+import com.vaadin.testbench.TestBenchElement;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.interactions.Actions;
+
+public class ResizeProtectedIT extends AbstractSpreadsheetIT {
+
+    @Before
+    public void init() {
+        getDriver().get(getBaseURL());
+        createNewSpreadsheet();
+    }
+
+    @Test
+    public void resizing_protectedSheet_columnResizeFails() {
+        loadFile("multiple_sheets_protected.xlsx");
+
+        double originalWidth = getCellAt(2, 2).getSize().getWidth();
+
+        TestBenchElement resizeHandle = $(SpreadsheetElement.class).first()
+                .getColumnHeader(2).getResizeHandle();
+        SheetHeaderElement target = $(SpreadsheetElement.class).first()
+                .getColumnHeader(4);
+
+        new Actions(driver).dragAndDrop(resizeHandle, target).perform();
+
+        double newWidth = getCellAt(2, 2).getSize().getWidth();
+
+        Assert.assertTrue(String.format(
+                "Width changed when it shouldn't have. Was: %s, now: %s.",
+                originalWidth, newWidth), originalWidth == newWidth);
+    }
+
+    @Test
+    public void resizing_protectedSheetWithFormatColumnsEnabled_columnResizeSuccessful() {
+        loadFile("protected_format_columns.xlsx");
+
+        final double originalWidth = getCellAt(2, 2).getSize().getWidth();
+
+        TestBenchElement resizeHandle = $(SpreadsheetElement.class).first()
+                .getColumnHeader(2).getResizeHandle();
+        SheetHeaderElement target = $(SpreadsheetElement.class).first()
+                .getColumnHeader(4);
+
+        new Actions(driver).dragAndDrop(resizeHandle, target).perform();
+
+        double newWidth = getCellAt(2, 2).getSize().getWidth();
+
+        assertInRange(2.5 * originalWidth, newWidth, 3.5 * originalWidth);
+    }
+
+    protected void assertInRange(double from, double value, double to) {
+        Assert.assertTrue("Value [" + value + "] is not in range: [" + from
+                + " - " + to + "]", value >= from && value <= to);
+    }
+
+}
