9dce7947c2318410e9c20e5e0d8698e3f6fc1833
Sergey Vinogradov <mr.vursen@gmail.com>
2022-06-03 12:49:39 +0300

    fix: disable client-side validation for CustomField (#3277)

--- vaadin-custom-field-flow-parent/vaadin-custom-field-flow/src/main/java/com/vaadin/flow/component/customfield/CustomField.java
+++ vaadin-custom-field-flow-parent/vaadin-custom-field-flow/src/main/java/com/vaadin/flow/component/customfield/CustomField.java
@@ -32,6 +32,7 @@ import com.vaadin.flow.component.dependency.NpmPackage;
 import org.slf4j.LoggerFactory;
 
 import com.vaadin.flow.component.AbstractField;
+import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Focusable;
 import com.vaadin.flow.component.HasSize;
@@ -83,6 +84,13 @@ public abstract class CustomField<T> extends AbstractField<CustomField<T>, T>
         super(defaultValue);
         // Force a value update when the change event generated
         getElement().addEventListener("change", e -> this.updateValue());
+    }
+
+    @Override
+    protected void onAttach(AttachEvent attachEvent) {
+        super.onAttach(attachEvent);
+
+        FieldValidationUtil.disableClientValidation(this);
     }
 
     /**
@@ -176,14 +184,10 @@ public abstract class CustomField<T> extends AbstractField<CustomField<T>, T>
     /**
      * <p>
      * This property is set to true when the control value is invalid.
-     * <p>
-     * This property is synchronized automatically from client side when a
-     * 'invalid-changed' event happens.
      * </p>
      *
      * @return the {@code invalid} property from the webcomponent
      */
-    @Synchronize(property = "invalid", value = "invalid-changed")
     @Override
     public boolean isInvalid() {
         return getElement().getProperty("invalid", false);
