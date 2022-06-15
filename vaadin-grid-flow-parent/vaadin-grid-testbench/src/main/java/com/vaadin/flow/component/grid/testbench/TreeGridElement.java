56969e94c701431bd5e18a6f5b356d42a0b5f90e
ugur-vaadin <ugur@vaadin.com>
2022-06-15 13:11:08 +0300

    Update TreeGridElement.java
    Fixed the script. The component may not have a connector attached to it. If so, we now use the client side check.

--- vaadin-grid-flow-parent/vaadin-grid-testbench/src/main/java/com/vaadin/flow/component/grid/testbench/TreeGridElement.java
+++ vaadin-grid-flow-parent/vaadin-grid-testbench/src/main/java/com/vaadin/flow/component/grid/testbench/TreeGridElement.java
@@ -309,7 +309,7 @@ public class TreeGridElement extends GridElement {
      */
     public boolean isLoadingExpandedRows() {
         return (Boolean) executeScript(
-                "return arguments[0].$connector && (arguments[0].$connector.hasEnsureSubCacheQueue() || arguments[0].$connector.hasParentRequestQueue())",
+                "return !!arguments[0].$connector ? (arguments[0].$connector.hasEnsureSubCacheQueue() || arguments[0].$connector.hasParentRequestQueue()) : arguments[0]._cache.isLoading()",
                 this);
     }
 
