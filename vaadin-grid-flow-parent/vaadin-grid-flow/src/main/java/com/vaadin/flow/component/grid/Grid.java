49c2d805be322d60a23558e064ba867e18b697b1
Sascha Ißbrücker <sissbruecker@vaadin.com>
2022-06-24 11:06:23 +0200

    refactor: Use KeyMapper by default to generate unique keys (#3251) (#3300)
    Co-authored-by: Sascha Ißbrücker <sissbruecker@vaadin.com>
    Co-authored-by: Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>

--- vaadin-grid-flow-parent/vaadin-grid-flow/src/main/java/com/vaadin/flow/component/grid/Grid.java
+++ vaadin-grid-flow-parent/vaadin-grid-flow/src/main/java/com/vaadin/flow/component/grid/Grid.java
@@ -1383,8 +1383,7 @@ public class Grid<T> extends Component implements HasStyle, HasSize,
                 .getUniqueKeyProperty();
         if (uniqueKeyPropertyName != null
                 && !jsonObject.hasKey(uniqueKeyPropertyName)) {
-            jsonObject.put(uniqueKeyPropertyName,
-                    getUniqueKeyProvider().apply(item));
+            jsonObject.put(uniqueKeyPropertyName, getUniqueKey(item));
         }
     }
 
@@ -4363,5 +4362,11 @@ public class Grid<T> extends Component implements HasStyle, HasSize,
             }
             return result;
         };
+    }
+
+    private String getUniqueKey(T item) {
+        return Optional.ofNullable(getUniqueKeyProvider())
+                .map(provider -> provider.apply(item))
+                .orElse(getDataCommunicator().getKeyMapper().key(item));
     }
 }
