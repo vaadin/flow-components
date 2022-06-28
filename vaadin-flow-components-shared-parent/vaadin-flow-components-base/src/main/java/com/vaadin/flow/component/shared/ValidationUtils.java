a67e9028089625f991f8f8ac9fc4862b3e808105
Diego Cardoso <diego@vaadin.com>
2022-06-28 14:26:38 +0300

    style: apply formatting

--- vaadin-flow-components-shared-parent/vaadin-flow-components-base/src/main/java/com/vaadin/flow/component/shared/ValidationUtils.java
+++ vaadin-flow-components-shared-parent/vaadin-flow-components-base/src/main/java/com/vaadin/flow/component/shared/ValidationUtils.java
@@ -5,30 +5,33 @@ import com.vaadin.flow.data.binder.ValidationResult;
 import java.util.Objects;
 
 public class ValidationUtils {
-    public static <T> ValidationResult checkRequired(boolean required, T value, T emptyValue) {
+    public static <T> ValidationResult checkRequired(boolean required, T value,
+            T emptyValue) {
         final boolean isRequiredButEmpty = required
-            && Objects.equals(emptyValue, value);
+                && Objects.equals(emptyValue, value);
         if (isRequiredButEmpty) {
             return ValidationResult.error(ValidationError.REQUIRED);
         }
         return ValidationResult.ok();
     }
 
-    public static <T extends Comparable<T>> ValidationResult checkGreaterThanMax(T value, T maxValue) {
+    public static <T extends Comparable<T>> ValidationResult checkGreaterThanMax(
+            T value, T maxValue) {
         final boolean isGreaterThanMax = value != null && maxValue != null
-            && value.compareTo(maxValue) > 0;
+                && value.compareTo(maxValue) > 0;
         if (isGreaterThanMax) {
             return ValidationResult.error(ValidationError.GREATER_THAN_MAX);
         }
-        return  ValidationResult.ok();
+        return ValidationResult.ok();
     }
 
-    public static <T extends Comparable<T>> ValidationResult checkSmallerThanMin(T value, T minValue) {
+    public static <T extends Comparable<T>> ValidationResult checkSmallerThanMin(
+            T value, T minValue) {
         final boolean isSmallerThanMin = value != null && minValue != null
-            && value.compareTo(minValue) < 0;
+                && value.compareTo(minValue) < 0;
         if (isSmallerThanMin) {
             return ValidationResult.error(ValidationError.SMALLER_THAN_MIN);
         }
-        return  ValidationResult.ok();
+        return ValidationResult.ok();
     }
 }
