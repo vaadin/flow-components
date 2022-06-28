e456ccdd81a7ef113244da9b61eb0e6984a2009e
Diego Cardoso <diego@vaadin.com>
2022-06-28 14:21:40 +0300

    feat: create common validation logic to util class

---
+++ vaadin-flow-components-shared-parent/vaadin-flow-components-base/src/main/java/com/vaadin/flow/component/shared/ValidationError.java
@@ -0,0 +1,15 @@
+package com.vaadin.flow.component.shared;
+
+import java.io.Serializable;
+
+public final class ValidationError implements Serializable {
+    public static final String REQUIRED = "VALIDATION_REQUIRED_ERROR";
+
+    public static final String GREATER_THAN_MAX = "VALIDATION_GREATER_THAN_MAX_ERROR";
+    public static final String SMALLER_THAN_MIN = "VALIDATION_SMALLER_THAN_MIN_ERROR";
+
+    public static final String MAX_LENGTH_EXCEEDED = "VALIDATION_MAX_LENGTH_EXCEEDED_ERROR";
+    public static final String MIN_LENGTH_NOT_REACHED = "VALIDATION_MIN_LENGTH_NOT_REACHED_ERROR";
+
+    public static final String PATTERN_VIOLATED = "VALIDATION_PATTERN_VIOLATED_ERROR";
+}
