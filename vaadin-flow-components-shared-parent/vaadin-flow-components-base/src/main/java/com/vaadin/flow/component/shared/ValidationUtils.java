d143cc455a27c74130ab4569219796e64fd5b185
Diego Cardoso <diego@vaadin.com>
2022-07-05 17:56:05 +0300

    docs: add javadoc to ValidationUtils class and methods

--- vaadin-flow-components-shared-parent/vaadin-flow-components-base/src/main/java/com/vaadin/flow/component/shared/ValidationUtils.java
+++ vaadin-flow-components-shared-parent/vaadin-flow-components-base/src/main/java/com/vaadin/flow/component/shared/ValidationUtils.java
@@ -4,7 +4,25 @@ import com.vaadin.flow.data.binder.ValidationResult;
 
 import java.util.Objects;
 
+/**
+ * Util methods for component validation
+ */
 public class ValidationUtils {
+
+    /**
+     * Checks the required validation constraint
+     *
+     * @param required
+     *            the required state of the component
+     * @param value
+     *            the current value set on the component
+     * @param emptyValue
+     *            the empty value for the component
+     * @return <code>Validation.ok()</code> if the validation pass,
+     *         <code>Validation.error()</code> otherwise
+     * @param <T>
+     *            the type of the component value
+     */
     public static <T> ValidationResult checkRequired(boolean required, T value,
             T emptyValue) {
         final boolean isRequiredButEmpty = required
@@ -15,6 +33,19 @@ public class ValidationUtils {
         return ValidationResult.ok();
     }
 
+    /**
+     * Checks if the value being set to the component is greater than the max
+     * value defined
+     *
+     * @param value
+     *            the current value set on the component
+     * @param maxValue
+     *            the max value set on the component
+     * @return <code>Validation.ok()</code> if the validation pass,
+     *         <code>Validation.error()</code> otherwise
+     * @param <T>
+     *            the type of the component value
+     */
     public static <T extends Comparable<T>> ValidationResult checkGreaterThanMax(
             T value, T maxValue) {
         final boolean isGreaterThanMax = value != null && maxValue != null
@@ -25,6 +56,19 @@ public class ValidationUtils {
         return ValidationResult.ok();
     }
 
+    /**
+     * Checks if the value being set to the component is smaller than the max
+     * value defined
+     *
+     * @param value
+     *            the current value set on the component
+     * @param minValue
+     *            the min value set on the component
+     * @return <code>Validation.ok()</code> if the validation pass,
+     *         <code>Validation.error()</code> otherwise
+     * @param <T>
+     *            the type of the component value
+     */
     public static <T extends Comparable<T>> ValidationResult checkSmallerThanMin(
             T value, T minValue) {
         final boolean isSmallerThanMin = value != null && minValue != null
