e7c0c1dfb381728a0ce75ef9e51ae42b59221861
Tatu Lund <tatu@vaadin.com>
2022-08-04 08:22:45 +0300

    Do not inject styles if they are already included

--- vaadin-lumo-theme-flow-parent/vaadin-lumo-theme-flow/src/main/resources/META-INF/resources/frontend/lumo-utility.ts
+++ vaadin-lumo-theme-flow-parent/vaadin-lumo-theme-flow/src/main/resources/META-INF/resources/frontend/lumo-utility.ts
@@ -1,5 +1,14 @@
 import { utility } from '@vaadin/vaadin-lumo-styles/utility.js';
 
-const tpl = document.createElement('template');
-tpl.innerHTML = `<style>${utility.cssText}</style>`;
-document.head.appendChild(tpl.content);
+var hasStyles = false;
+const styleTags = document.head.getElementsByTagName("style");
+for (var i=0;i<styleTags.length;i++) {
+  if (styleTags[i].sheet === utility.styleSheet) {
+    hasStyles = true;
+  }
+}
+if (!hasStyles) {
+  const tpl = document.createElement('template');
+  tpl.innerHTML = `<style>${utility.cssText}</style>`;
+  document.head.appendChild(tpl.content);
+}
