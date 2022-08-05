940e12c37b8a2a416417425f463ae66d533a5878
web-padawan <iamkulykov@gmail.com>
2022-08-05 11:09:40 +0300

    refactor: format new TS include file

--- vaadin-lumo-theme-flow-parent/vaadin-lumo-theme-flow/src/main/resources/META-INF/resources/frontend/lumo-utility.ts
+++ vaadin-lumo-theme-flow-parent/vaadin-lumo-theme-flow/src/main/resources/META-INF/resources/frontend/lumo-utility.ts
@@ -1,10 +1,11 @@
 import { utility } from '@vaadin/vaadin-lumo-styles/utility.js';
 
 var hasStyles = false;
-const styleTags = document.head.getElementsByTagName("style");
-for (var i=0;i<styleTags.length;i++) {
+const styleTags = document.head.getElementsByTagName('style');
+for (var i = 0; i < styleTags.length; i++) {
   if (styleTags[i].sheet === utility.styleSheet) {
     hasStyles = true;
+    break;
   }
 }
 if (!hasStyles) {
