684a9e9e86d216f5133f38b7acaa067ac9d9236e
Sergey Vinogradov <mr.vursen@gmail.com>
2022-08-10 17:26:51 +0300

    add abstract classes

---
+++ vaadin-date-time-picker-flow-parent/vaadin-date-time-picker-flow-integration-tests/src/test/java/com/vaadin/flow/component/datetimepicker/validation/AbstractValidationIT.java
@@ -0,0 +1,35 @@
+package com.vaadin.flow.component.datetimepicker.validation;
+
+import org.junit.Assert;
+import org.junit.Before;
+
+import com.vaadin.flow.component.datetimepicker.testbench.DateTimePickerElement;
+import com.vaadin.tests.AbstractComponentIT;
+
+import static com.vaadin.flow.component.datetimepicker.validation.AbstractValidationPage.SERVER_VALIDITY_STATE;
+import static com.vaadin.flow.component.datetimepicker.validation.AbstractValidationPage.SERVER_VALIDITY_STATE_BUTTON;
+
+public class AbstractValidationIT extends AbstractComponentIT {
+    protected DateTimePickerElement field;
+
+    @Before
+    public void init() {
+        open();
+        field = $(DateTimePickerElement.class).first();
+    }
+
+    protected void assertErrorMessage(String expected) {
+        Assert.assertEquals(expected, field.getPropertyString("errorMessage"));
+    }
+
+    protected void assertClientValid(boolean expected) {
+        Assert.assertEquals(expected, !field.getPropertyBoolean("invalid"));
+    }
+
+    protected void assertServerValid(boolean expected) {
+        $("button").id(SERVER_VALIDITY_STATE_BUTTON).click();
+
+        var actual = $("div").id(SERVER_VALIDITY_STATE).getText();
+        Assert.assertEquals(String.valueOf(expected), actual);
+    }
+}
