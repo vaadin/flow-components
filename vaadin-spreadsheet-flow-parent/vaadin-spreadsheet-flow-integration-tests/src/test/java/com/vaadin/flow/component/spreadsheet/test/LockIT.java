2a0125faf594514a28f5a4d6e78bd9296227264b
Sascha Ißbrücker <sissbruecker@vaadin.com>
2022-08-11 11:32:10 +0200

    run formatter

--- vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow-integration-tests/src/test/java/com/vaadin/flow/component/spreadsheet/test/LockIT.java
+++ vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow-integration-tests/src/test/java/com/vaadin/flow/component/spreadsheet/test/LockIT.java
@@ -33,7 +33,8 @@ public class LockIT extends AbstractSpreadsheetIT {
         // Assert that a locked cell cannot be edited
         Assert.assertEquals("locked", spreadsheet.getCellAt("B2").getValue());
         SheetCellElement lockedCell = spreadsheet.getCellAt("B2");
-        selectCell("B2"); // work around Selenium issue with double-click targeting wrong cell
+        selectCell("B2"); // work around Selenium issue with double-click
+                          // targeting wrong cell
         lockedCell.doubleClick();
         Assert.assertFalse(spreadsheet.getCellValueInput().isDisplayed());
 
