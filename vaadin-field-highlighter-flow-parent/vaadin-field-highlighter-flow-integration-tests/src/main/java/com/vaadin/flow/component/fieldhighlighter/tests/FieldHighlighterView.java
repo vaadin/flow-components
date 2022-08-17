426d9b67c836b8da91e8d90b1cecd2a48aab7c21
Zhe Sun <zhe@vaadin.com>
2022-08-17 09:38:37 +0300

    chore: add IT test view

---
+++ vaadin-field-highlighter-flow-parent/vaadin-field-highlighter-flow-integration-tests/src/main/java/com/vaadin/flow/component/fieldhighlighter/tests/FieldHighlighterView.java
@@ -0,0 +1,29 @@
+package com.vaadin.flow.component.fieldhighlighter.tests;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.fieldhighlighter.FieldHighlighterInitializer;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.dom.Element;
+import com.vaadin.flow.router.Route;
+
+@Route(value = "vaadin-field-highlighter")
+public class fieldHighlighterView extends Div {
+
+    static class FieldHighlighter extends FieldHighlighterInitializer {
+        public void initialize(Element field) {
+            super.init(field);
+        }
+    }
+    public fieldHighlighterView() {
+        TextField tf = new TextField("TF with field highlighter");
+        Button button = new Button("call init");
+        button.addClickListener(event ->
+                new FieldHighlighter().initialize(tf.getElement()));
+
+        tf.setId("tf-with-highlighter");
+        button.setId("call-init");
+
+        add(tf, button);
+    }
+}
