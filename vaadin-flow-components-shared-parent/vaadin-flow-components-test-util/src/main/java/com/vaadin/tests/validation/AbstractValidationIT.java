38b6c5a4411c587a5de4a7ebf2fee89a0550f109
Sergey Vinogradov <mr.vursen@gmail.com>
2022-08-30 09:32:58 +0300

    feat: add abstract validation test classes

---
+++ vaadin-flow-components-shared-parent/vaadin-flow-components-test-util/src/main/java/com/vaadin/tests/validation/AbstractValidationIT.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2000-2022 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.tests.validation;
+
+import com.vaadin.testbench.TestBenchElement;
+import com.vaadin.tests.AbstractComponentIT;
+
+import org.junit.Assert;
+import org.junit.Before;
+
+import static com.vaadin.tests.validation.AbstractValidationPage.SERVER_VALIDITY_STATE;
+import static com.vaadin.tests.validation.AbstractValidationPage.SERVER_VALIDITY_STATE_BUTTON;
+
+public abstract class AbstractValidationIT<F extends TestBenchElement>
+        extends AbstractComponentIT {
+    protected F field;
+
+    @Before
+    public void init() {
+        open();
+        field = getField();
+    }
+
+    protected abstract F getField();
+
+    protected void assertErrorMessage(String expected) {
+        Assert.assertEquals(expected, field.getPropertyString("errorMessage"));
+    }
+
+    protected void assertClientValid() {
+        Assert.assertTrue("The field should be valid on the client-side", isClientValid());
+    }
+
+    protected void assertClientInvalid() {
+        Assert.assertFalse("The field should be invalid on the client-side", isClientValid());
+    }
+
+    protected void assertServerValid() {
+        Assert.assertTrue("The field should be valid on the server-side", isServerValid());
+    }
+
+    protected void assertServerInvalid() {
+        Assert.assertFalse("The field should be invalid on the server-side", isServerValid());
+    }
+
+    private boolean isServerValid() {
+        $("button").id(SERVER_VALIDITY_STATE_BUTTON).click();
+
+        String actual = $("div").id(SERVER_VALIDITY_STATE).getText();
+        return actual.equals("true");
+    }
+
+    private boolean isClientValid() {
+        return !field.getPropertyBoolean("invalid");
+    }
+}
