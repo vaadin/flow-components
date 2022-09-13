cf28d7e30b4e712521bb8d354ef83452732d7806
Tomi Virkki <virkki@vaadin.com>
2022-09-13 16:23:24 +0300

    refactor: add dependencies, remove temporary client code

--- vaadin-tabs-flow-parent/vaadin-tabs-flow/src/main/resources/META-INF/resources/frontend/tabsheet/theme/material/vaadin-tabsheet.js
+++
@@ -1,4 +0,0 @@
-import '@vaadin/tabs/theme/material/vaadin-tabs.js';
-import '@vaadin/tabs/theme/material/vaadin-tab.js';
-import './vaadin-tabsheet-styles.js';
-import '../../src/vaadin-tabsheet.js';
