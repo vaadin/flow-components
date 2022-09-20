c0f27fc1c67e328d755149c2a8c6946aed41a47a
ugur-vaadin <ugur@vaadin.com>
2022-09-20 22:12:45 +0300

    refactor: remove unnecessary use of page number
    Remove unnecessary use of page number. Change the location of the new code block in which the presence of a debouncer is checked. Change the name of the debouncer variable.

--- vaadin-combo-box-flow-parent/vaadin-combo-box-flow/src/main/resources/META-INF/resources/frontend/comboBoxConnector.js
+++ vaadin-combo-box-flow-parent/vaadin-combo-box-flow/src/main/resources/META-INF/resources/frontend/comboBoxConnector.js
@@ -21,7 +21,6 @@ import { ComboBoxPlaceholder } from '@vaadin/combo-box/src/vaadin-combo-box-plac
         const pageCallbacks = {};
         let cache = {};
         let lastFilter = '';
-        let pageInDebouncer;
         const placeHolder = new window.Vaadin.ComboBoxPlaceholder();
         const MAX_RANGE_COUNT = Math.max(comboBox.pageSize * 2, 500); // Max item count in active range
 
@@ -71,13 +70,6 @@ import { ComboBoxPlaceholder } from '@vaadin/combo-box/src/vaadin-combo-box-plac
         };
 
         comboBox.dataProvider = function (params, callback) {
-          const filterUnchanged = params.filter === lastFilter;
-          if (filterUnchanged && this._debouncer) {
-            if (pageInDebouncer !== params.page) {
-              pageCallbacks[params.page] = callback;
-            }
-            return;
-          }
           if (params.pageSize != comboBox.pageSize) {
             throw 'Invalid pageSize';
           }
@@ -100,11 +92,11 @@ import { ComboBoxPlaceholder } from '@vaadin/combo-box/src/vaadin-combo-box-plac
             }
           }
 
-          if (!filterUnchanged) {
+          const filterChanged = params.filter !== lastFilter;
+          if (filterChanged) {
             cache = {};
             lastFilter = params.filter;
-            this._debouncer = Debouncer.debounce(this._debouncer, timeOut.after(500), () => {
-              pageInDebouncer = params.page;
+            this._filterDebouncer = Debouncer.debounce(this._filterDebouncer, timeOut.after(500), () => {
               if (serverFacade.getLastFilterSentToServer() === params.filter) {
                 // Fixes the case when the filter changes
                 // to something else and back to the original value
@@ -115,11 +107,16 @@ import { ComboBoxPlaceholder } from '@vaadin/combo-box/src/vaadin-combo-box-plac
               if (params.filter !== lastFilter) {
                 throw new Error("Expected params.filter to be '" + lastFilter + "' but was '" + params.filter + "'");
               }
-              this._debouncer = undefined;
+              this._filterDebouncer = undefined;
               // Call the method again after debounce.
               clearPageCallbacks();
               comboBox.dataProvider(params, callback);
             });
+            return;
+          }
+
+          if (this._filterDebouncer) {
+            pageCallbacks[params.page] = callback;
             return;
           }
 
