db9624b77f778f8cc85957c3a92b9d0c9d46e278
Sergey Vinogradov <mr.vursen@gmail.com>
2022-10-05 10:41:46 +0300

    run formatter

--- vaadin-combo-box-flow-parent/vaadin-combo-box-flow/src/test/java/com/vaadin/flow/component/combobox/ComboboxSerializableTest.java
+++ vaadin-combo-box-flow-parent/vaadin-combo-box-flow/src/test/java/com/vaadin/flow/component/combobox/ComboboxSerializableTest.java
@@ -9,17 +9,20 @@ import com.vaadin.flow.testutil.ClassesSerializableTest;
 
 public class ComboboxSerializableTest extends ClassesSerializableTest {
     @Test
-    public void setItems_callSetRequestedRange_comboBoxSerializable() throws Throwable {
+    public void setItems_callSetRequestedRange_comboBoxSerializable()
+            throws Throwable {
         final ComboBox<String> comboBox = new ComboBox<>();
         comboBox.setItems(List.of("Item 1", "Item 2"));
         callSetRequestedRange(comboBox, 0, 2, "");
         serializeAndDeserialize(comboBox);
     }
 
-    private void callSetRequestedRange(ComboBox<String> comboBox, int start, int length, String filter)
-            throws NoSuchMethodException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-        Method method = ComboBoxBase.class.getDeclaredMethod("setRequestedRange",
-                int.class, int.class, String.class);
+    private void callSetRequestedRange(ComboBox<String> comboBox, int start,
+            int length, String filter)
+            throws NoSuchMethodException, IllegalAccessException,
+            IllegalArgumentException, InvocationTargetException {
+        Method method = ComboBoxBase.class.getDeclaredMethod(
+                "setRequestedRange", int.class, int.class, String.class);
         method.setAccessible(true);
         method.invoke(comboBox, start, length, filter);
     }
