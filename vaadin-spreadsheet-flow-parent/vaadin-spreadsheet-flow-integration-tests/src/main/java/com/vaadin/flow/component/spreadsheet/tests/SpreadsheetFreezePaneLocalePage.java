f875994e0141f3a2d5d66a34d98262d41fc14a91
Tomi Virkki <virkki@vaadin.com>
2022-10-10 11:34:02 +0300

    test: migrate freeze pane locale IT from v8 spreadsheet

---
+++ vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow-integration-tests/src/main/java/com/vaadin/flow/component/spreadsheet/tests/SpreadsheetFreezePaneLocalePage.java
@@ -0,0 +1,40 @@
+package com.vaadin.flow.component.spreadsheet.tests;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Locale;
+
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.spreadsheet.Spreadsheet;
+import com.vaadin.flow.router.Route;
+
+@Route("vaadin-spreadsheet/freeze-pane-locale")
+public class SpreadsheetFreezePaneLocalePage extends Div {
+
+    public SpreadsheetFreezePaneLocalePage() {
+        super();
+        setSizeFull();
+
+        ClassLoader classLoader = SpreadsheetPage.class.getClassLoader();
+        URL resource = classLoader.getResource(
+                "test_sheets" + File.separator + "freezepanels.xlsx");
+
+        try {
+            File file = new File(resource.toURI());
+
+            Spreadsheet sheet = new Spreadsheet(file);
+
+            sheet.setLocale(Locale.FRANCE);
+
+            add(sheet);
+
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
