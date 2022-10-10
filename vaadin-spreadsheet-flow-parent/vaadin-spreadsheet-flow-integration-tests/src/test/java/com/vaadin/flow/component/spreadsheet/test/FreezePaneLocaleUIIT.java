8072f8a48a7178af619d2c0863e351a6e74d4437
Tomi Virkki <virkki@vaadin.com>
2022-10-10 12:10:25 +0300

    chore: fix it file name

--- vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow-integration-tests/src/test/java/com/vaadin/flow/component/spreadsheet/test/FreezePaneLocaleUIIT.java
+++ vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow-integration-tests/src/test/java/com/vaadin/flow/component/spreadsheet/test/FreezePaneLocaleUIIT.java
@@ -5,7 +5,7 @@ import com.vaadin.flow.component.spreadsheet.testbench.SpreadsheetElement;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class FreezePaneLocaleUITest extends AbstractSpreadsheetIT {
+public class FreezePaneLocaleUIIT extends AbstractSpreadsheetIT {
 
     @Test
     public void loadSpreadsheetWithFrozenColumns_setLocaleAfterLoad_SpreadsheetLoadedCorrectly() {
