0e4962cd5c284604440acc61e8b51ad8e080337d
Tomi Virkki <virkki@vaadin.com>
2022-10-14 10:35:11 +0300

    chore: format

--- vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow/src/test/java/com/vaadin/flow/component/spreadsheet/tests/FormulasTest.java
+++ vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow/src/test/java/com/vaadin/flow/component/spreadsheet/tests/FormulasTest.java
@@ -69,7 +69,8 @@ public class FormulasTest {
     public void setInvalidFormula_invalidFormulaCellsSet() throws Exception {
         // onSheetScroll must be invoked once, otherwise cell comments are not
         // loaded
-        TestHelper.fireClientEvent(spreadsheet, "onSheetScroll", "[1, 1, 1, 1]");
+        TestHelper.fireClientEvent(spreadsheet, "onSheetScroll",
+                "[1, 1, 1, 1]");
 
         // Create a formula cell with an invalid formula
         var A1 = spreadsheet.createFormulaCell(0, 0, "Sheet2!A1");
