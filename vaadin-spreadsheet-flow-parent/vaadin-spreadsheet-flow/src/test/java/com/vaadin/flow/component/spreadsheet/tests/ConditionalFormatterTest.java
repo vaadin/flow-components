9c083f8d030ba05d25a4d8f1f71c0d78fae12ec2
Diego Cardoso <diego@vaadin.com>
2022-10-18 17:13:22 +0300

    test: add tests for conditionall formatter

--- vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow/src/test/java/com/vaadin/flow/component/spreadsheet/tests/ConditionalFormatterTest.java
+++ vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow/src/test/java/com/vaadin/flow/component/spreadsheet/tests/ConditionalFormatterTest.java
@@ -1,10 +1,19 @@
 package com.vaadin.flow.component.spreadsheet.tests;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.core.JsonParser;
+import com.nimbusds.jose.shaded.json.parser.JSONParser;
 import com.vaadin.flow.component.spreadsheet.ConditionalFormatter;
 import com.vaadin.flow.component.spreadsheet.SheetImageWrapper;
 import com.vaadin.flow.component.spreadsheet.Spreadsheet;
+import elemental.json.JsonValue;
+import elemental.json.impl.JreJsonObject;
+import elemental.json.impl.JsonUtil;
 import org.apache.poi.ss.usermodel.ClientAnchor;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Set;
 
 /**
  * Tests for conditional formatting
@@ -29,8 +38,71 @@ public class ConditionalFormatterTest {
     @Test
     public void matchesFormula_rulesWithoutFormula_formulasEvaluatedWithoutExceptions() {
         // ensure sheet with rules without formulas is active
-        createConditionalFormatterRulesForSheet(
+        Spreadsheet sheet = createConditionalFormatterRulesForSheet(
                 "ConditionalFormatterSamples.xlsx", 3);
+    }
+
+    @Test
+    public void cellValueMatchesFormula_cellHasFormatting() {
+        var sheet = createConditionalFormatterRulesForSheet(
+                "ConditionalFormatterSamples.xlsx", 1);
+        var cell = sheet.getCell("D7");
+        Assert.assertNotNull(
+                sheet.getConditionalFormatter().getCellFormattingIndex(cell));
+    }
+
+    @Test
+    public void cellValueMatchedFormula_valueIsChangeToNotMatch_cellHasNoFormatting() {
+        var sheet = createConditionalFormatterRulesForSheet(
+                "ConditionalFormatterSamples.xlsx", 1);
+        var cell = sheet.getCell("D7");
+        cell.setCellValue(550);
+        sheet.refreshCells(cell);
+        Assert.assertNull(
+                sheet.getConditionalFormatter().getCellFormattingIndex(cell));
+    }
+
+    @Test
+    public void cellDoesntMatchFormula_cellHasNoFormatting() {
+        var sheet = createConditionalFormatterRulesForSheet(
+                "ConditionalFormatterSamples.xlsx", 1);
+        var cell = sheet.getCell("D9");
+        Assert.assertNull(
+                sheet.getConditionalFormatter().getCellFormattingIndex(cell));
+    }
+
+    @Test
+    public void cellValuesMatchedFormula_styleIsPresent() {
+        var sheet = createConditionalFormatterRulesForSheet(
+                "ConditionalFormatterSamples.xlsx", 1);
+        var cell = sheet.getCell("D7");
+
+        var styles = (JreJsonObject) JsonUtil.parse(
+                sheet.getElement().getProperty("conditionalFormattingStyles"));
+        var cellFormattingIndex = sheet.getConditionalFormatter()
+                .getCellFormattingIndex(cell);
+
+        Assert.assertEquals(1, cellFormattingIndex.size());
+        var formattingIndex = cellFormattingIndex.stream().findFirst()
+                .orElse(-1).toString();
+        Assert.assertNotNull(styles.get(formattingIndex));
+    }
+
+    @Test
+    public void sheetHas() {
+        var sheet = createConditionalFormatterRulesForSheet(
+                "ConditionalFormatterSamples.xlsx", 1);
+        var cell = sheet.getCell("D7");
+
+        var styles = (JreJsonObject) JsonUtil.parse(
+                sheet.getElement().getProperty("conditionalFormattingStyles"));
+        var cellFormattingIndex = sheet.getConditionalFormatter()
+                .getCellFormattingIndex(cell);
+
+        Assert.assertEquals(1, cellFormattingIndex.size());
+        var formattingIndex = cellFormattingIndex.stream().findFirst()
+                .orElse(-1).toString();
+        Assert.assertNotNull(styles.get(formattingIndex));
     }
 
     @Test
@@ -43,13 +115,15 @@ public class ConditionalFormatterTest {
         createConditionalFormatterRulesForSheet(fileName, null);
     }
 
-    private void createConditionalFormatterRulesForSheet(String fileName,
+    private Spreadsheet createConditionalFormatterRulesForSheet(String fileName,
             Integer sheetIndex) {
         Spreadsheet sheet = TestHelper.createSpreadsheet(fileName);
 
         if (sheetIndex != null && sheet.getActiveSheetIndex() != sheetIndex) {
             sheet.setActiveSheetIndex(sheetIndex);
         }
-        new ConditionalFormatter(sheet).createConditionalFormatterRules();
+        // new ConditionalFormatter(sheet).createConditionalFormatterRules();
+
+        return sheet;
     }
 }
