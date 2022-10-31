114275675cd206f9f34a34c7d3475f22fa598ff8
Diego Cardoso <diego@vaadin.com>
2022-10-31 16:33:45 +0200

    refactor!: make SpreadsheetComponentFactory package private

--- vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow/src/main/java/com/vaadin/flow/component/spreadsheet/SpreadsheetComponentFactory.java
+++ vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow/src/main/java/com/vaadin/flow/component/spreadsheet/SpreadsheetComponentFactory.java
@@ -50,7 +50,7 @@ import com.vaadin.flow.component.Component;
  *
  * @author Vaadin Ltd.
  */
-public interface SpreadsheetComponentFactory extends Serializable {
+interface SpreadsheetComponentFactory extends Serializable {
 
     /**
      * Should return a unique component that is displayed inside the cell
@@ -84,7 +84,7 @@ public interface SpreadsheetComponentFactory extends Serializable {
      *         becomes visible or <code>null</code> if no component should be
      *         displayed when the cell is not selected.
      */
-    public Component getCustomComponentForCell(Cell cell, int rowIndex,
+    Component getCustomComponentForCell(Cell cell, int rowIndex,
             int columnIndex, Spreadsheet spreadsheet, Sheet sheet);
 
     /**
@@ -123,7 +123,7 @@ public interface SpreadsheetComponentFactory extends Serializable {
      *         <code>null</code> if the default editor (input field) should be
      *         used.
      */
-    public Component getCustomEditorForCell(Cell cell, int rowIndex,
+    Component getCustomEditorForCell(Cell cell, int rowIndex,
             int columnIndex, Spreadsheet spreadsheet, Sheet sheet);
 
     /**
@@ -149,7 +149,7 @@ public interface SpreadsheetComponentFactory extends Serializable {
      * @param customEditor
      *            The component that is displayed inside the cell
      */
-    public void onCustomEditorDisplayed(Cell cell, int rowIndex,
+    void onCustomEditorDisplayed(Cell cell, int rowIndex,
             int columnIndex, Spreadsheet spreadsheet, Sheet sheet,
             Component customEditor);
 }
