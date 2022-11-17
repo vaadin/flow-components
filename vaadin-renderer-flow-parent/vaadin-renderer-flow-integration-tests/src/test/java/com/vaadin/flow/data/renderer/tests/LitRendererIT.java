8689090e0ef0c88b5f24bf28c1bef92552f25e8c
Tomi Virkki <virkki@vaadin.com>
2022-11-17 11:45:28 +0200

    chore: clean up log filtering

--- vaadin-renderer-flow-parent/vaadin-renderer-flow-integration-tests/src/test/java/com/vaadin/flow/data/renderer/tests/LitRendererIT.java
+++ vaadin-renderer-flow-parent/vaadin-renderer-flow-integration-tests/src/test/java/com/vaadin/flow/data/renderer/tests/LitRendererIT.java
@@ -121,10 +121,8 @@ public class LitRendererIT extends AbstractComponentIT {
 
     private String getClientCallableLogArray() {
         String message = getLogEntries(Level.WARNING).stream()
-                // Discard lit-element warning lines
-                .filter(m -> !m.getMessage().contains(
-                        "The main 'lit-element' module entrypoint is deprecated."))
-                .filter(m -> !m.getMessage().contains("Lit is in dev mode"))
+                // Discard all but event messages
+                .filter(m -> m.getMessage().contains("event:"))
                 // Return first warning message in console
                 .findFirst().get().getMessage();
         return message.split("\"")[1];
