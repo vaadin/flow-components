362e3868433de4c5c171babf961ca7de8a84084c
Sascha Ißbrücker <sissbruecker@vaadin.com>
2022-11-24 14:17:40 +0100

    refactor!: remove RadioButtonGroup implementation of HasItemComponents
    Removes the `RadioButtonGroup` implementation of `HasItemComponents` and `HasComponents`, as the component is not intended to be used as a generic component container.

--- vaadin-radio-button-flow-parent/vaadin-radio-button-flow-integration-tests/src/test/java/com/vaadin/flow/component/radiobutton/tests/RadioButtonGroupIT.java
+++ vaadin-radio-button-flow-parent/vaadin-radio-button-flow-integration-tests/src/test/java/com/vaadin/flow/component/radiobutton/tests/RadioButtonGroupIT.java
@@ -16,7 +16,6 @@
 package com.vaadin.flow.component.radiobutton.tests;
 
 import java.util.List;
-import java.util.stream.IntStream;
 
 import com.vaadin.flow.component.radiobutton.RadioGroupVariant;
 import com.vaadin.flow.testutil.TestPath;
@@ -225,111 +224,6 @@ public class RadioButtonGroupIT extends AbstractComponentIT {
 
         Assert.assertEquals("Group should have the 'foo' item selected", "foo",
                 group.getSelectedText());
-    }
-
-    @Test
-    public void addedComponentsAfterItems() {
-        WebElement group = findElement(
-                By.id("button-group-with-appended-text"));
-
-        List<WebElement> elements = group.findElements(By.xpath("./*"));
-
-        Assert.assertEquals(
-                "Unexpected amount of elements in radio-button-group", 6,
-                elements.size());
-        Assert.assertEquals("First element should be a <vaadin-radio-button>",
-                "vaadin-radio-button", elements.get(0).getTagName());
-        Assert.assertEquals("Second element should be a <vaadin-radio-button>",
-                "vaadin-radio-button", elements.get(1).getTagName());
-        Assert.assertEquals("Thirs element should be a <vaadin-radio-button>",
-                "vaadin-radio-button", elements.get(2).getTagName());
-        Assert.assertEquals("Fourth element should be a <label>", "label",
-                elements.get(3).getTagName());
-    }
-
-    @Test
-    public void insertedComponentsBetweenItems() {
-        WebElement group = findElement(
-                By.id("button-group-with-inserted-component"));
-
-        List<WebElement> elements = group.findElements(By.xpath("./*"));
-
-        Assert.assertEquals(
-                "Unexpected amount of elements in radio-button-group", 7,
-                elements.size());
-        Assert.assertEquals("Second element should be a label", "label",
-                elements.get(1).getTagName());
-        Assert.assertEquals("Third element should be a <hr>", "hr",
-                elements.get(2).getTagName());
-        Assert.assertEquals("Fourth element should be a <vaadin-radio-button>",
-                "vaadin-radio-button", elements.get(3).getTagName());
-    }
-
-    @Test
-    public void componentsPrependedBeforeItems() {
-        WebElement group = findElement(
-                By.id("button-group-with-prepended-component"));
-
-        List<WebElement> elements = group.findElements(By.xpath("./*"));
-
-        Assert.assertEquals(
-                "Unexpected amount of elements in radio-button-group", 14,
-                elements.size());
-        // Three groups of (label, hr, vaadin-radio-button, vaadin-radio-button)
-        IntStream.range(0, 2).forEach(i -> {
-            int firstInGroup = i * 4;
-            Assert.assertEquals(
-                    "Expected first in group " + (i + 1) + " to be a label",
-                    "label", elements.get(firstInGroup).getTagName());
-            Assert.assertEquals(
-                    "Expected second in group " + (i + 1) + " to be a <hr>",
-                    "hr", elements.get(firstInGroup + 1).getTagName());
-            Assert.assertEquals(
-                    "Expected third in group " + (i + 1)
-                            + " to be a <vaadin-radio-button>",
-                    "vaadin-radio-button",
-                    elements.get(firstInGroup + 2).getTagName());
-            Assert.assertEquals(
-                    "Expected fourth in group " + (i + 1)
-                            + " to be a <vaadin-radio-button>",
-                    "vaadin-radio-button",
-                    elements.get(firstInGroup + 3).getTagName());
-        });
-    }
-
-    @Test
-    public void dynamicComponentForAfterItem() {
-        WebElement group = findElement(
-                By.id("button-group-with-dynamic-component"));
-
-        List<WebElement> elements = group.findElements(By.xpath("./*"));
-
-        Assert.assertEquals(
-                "Unexpected amount of elements in radio-button-group", 8,
-                elements.size());
-        for (int i = 0; i < 6; i++) {
-            Assert.assertEquals(
-                    "Expected only <vaadin-radio-button> to be available",
-                    "vaadin-radio-button", elements.get(i).getTagName());
-        }
-
-        executeScript("arguments[0].value=1;", group);
-        // It takes a while to update DOM in busy CI
-        waitUntil(e -> {
-            List<WebElement> els = group.findElements(By.xpath("./*"));
-            // Expected one label to have been added to radio-button-group
-            // Second element should be a label as first element was selected",
-            return els.size() == 9 && "label".equals(els.get(1).getTagName());
-        }, 200);
-
-        executeScript("arguments[0].value=5;", group);
-        // It takes a while to update DOM in busy CI
-        waitUntil(e -> {
-            List<WebElement> els = group.findElements(By.xpath("./*"));
-            // Expected label to stay, just change place in radio-button-group
-            // Fifth element should be a label as fourth element was selected
-            return els.size() == 9 && "label".equals(els.get(5).getTagName());
-        }, 200);
     }
 
     @Test
