b900392ac16a5fd0124243254ca9aab5b4efcdf3
Tatu Lund <tatu@vaadin.com>
2022-11-25 11:04:19 +0200

    Add unit test

---
+++ vaadin-upload-flow-parent/vaadin-upload-flow/src/test/java/com/vaadin/flow/component/upload/tests/UploadTest.java
@@ -0,0 +1,11 @@
+package com.vaadin.flow.component.upload.tests;
+
+import org.junit.Test;
+
+public class UploadTest {
+
+    @Test
+    public void uploadNewUpload() {
+        Upload upload = new Upload();
+    }
+}
