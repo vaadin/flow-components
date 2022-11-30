d6e7c5dc6f87017e9cf9a9f038b5f019f74ac420
Sergey Vinogradov <mr.vursen@gmail.com>
2022-11-30 14:08:13 +0200

    add tests

---
+++ vaadin-text-field-flow-parent/vaadin-text-field-flow-integration-tests/src/test/java/com/vaadin/flow/component/textfield/tests/validation/NumberFieldValidationBasicIT.java
@@ -0,0 +1,158 @@
+/*
+ * Copyright 2000-2022 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.textfield.tests.validation;
+
+import org.junit.Test;
+import org.openqa.selenium.Keys;
+
+import com.vaadin.flow.component.textfield.testbench.NumberFieldElement;
+import com.vaadin.flow.testutil.TestPath;
+import com.vaadin.tests.validation.AbstractValidationIT;
+
+import static com.vaadin.flow.component.textfield.tests.validation.NumberFieldValidationBasicPage.MIN_INPUT;
+import static com.vaadin.flow.component.textfield.tests.validation.NumberFieldValidationBasicPage.MAX_INPUT;
+import static com.vaadin.flow.component.textfield.tests.validation.NumberFieldValidationBasicPage.STEP_INPUT;
+import static com.vaadin.flow.component.textfield.tests.validation.NumberFieldValidationBasicPage.REQUIRED_BUTTON;
+import static com.vaadin.flow.component.textfield.tests.validation.NumberFieldValidationBasicPage.DETACH_FIELD_BUTTON;
+import static com.vaadin.flow.component.textfield.tests.validation.NumberFieldValidationBasicPage.ATTACH_FIELD_BUTTON;
+
+@TestPath("vaadin-number-field/validation/basic")
+public class NumberFieldValidationBasicIT
+        extends AbstractValidationIT<NumberFieldElement> {
+    @Test
+    public void fieldIsInitiallyValid() {
+        assertClientValid();
+        assertServerValid();
+    }
+
+    @Test
+    public void onlyServerCanSetFieldToValid() {
+        $("button").id(REQUIRED_BUTTON).click();
+
+        executeScript("arguments[0].validate()", testField);
+        assertClientInvalid();
+
+        testField.sendKeys("1234");
+        executeScript("arguments[0].validate()", testField);
+        assertClientInvalid();
+
+        testField.sendKeys(Keys.ENTER);
+        assertServerValid();
+        assertClientValid();
+    }
+
+    @Test
+    public void detach_attach_onlyServerCanSetFieldToValid() {
+        $("button").id(DETACH_FIELD_BUTTON).click();
+        $("button").id(ATTACH_FIELD_BUTTON).click();
+
+        testField = getTestField();
+
+        onlyServerCanSetFieldToValid();
+    }
+
+    @Test
+    public void required_triggerInputBlur_assertValidity() {
+        $("button").id(REQUIRED_BUTTON).click();
+
+        testField.sendKeys(Keys.TAB);
+        assertServerInvalid();
+        assertClientInvalid();
+    }
+
+    @Test
+    public void required_changeInputValue_assertValidity() {
+        $("button").id(REQUIRED_BUTTON).click();
+
+        testField.setValue("1234");
+        assertServerValid();
+        assertClientValid();
+
+        testField.setValue("");
+        assertServerInvalid();
+        assertClientInvalid();
+    }
+
+    @Test
+    public void min_changeInputValue_assertValidity() {
+        $("input").id(MIN_INPUT).sendKeys("2", Keys.ENTER);
+
+        testField.setValue("1.8");
+        assertClientInvalid();
+        assertServerInvalid();
+
+        testField.setValue("2");
+        assertClientValid();
+        assertServerValid();
+
+        testField.setValue("2.2");
+        assertClientValid();
+        assertServerValid();
+    }
+
+    @Test
+    public void max_changeInputValue_assertValidity() {
+        $("input").id(MAX_INPUT).sendKeys("2", Keys.ENTER);
+
+        testField.setValue("2.2");
+        assertClientInvalid();
+        assertServerInvalid();
+
+        testField.setValue("2");
+        assertClientValid();
+        assertServerValid();
+
+        testField.setValue("1.8");
+        assertClientValid();
+        assertServerValid();
+    }
+
+    @Test
+    public void step_changeInputValue_assertValidity() {
+        $("input").id(STEP_INPUT).sendKeys("2", Keys.ENTER);
+
+        testField.setValue("1");
+        assertClientInvalid();
+        assertServerInvalid();
+
+        testField.setValue("1.5");
+        assertClientInvalid();
+        assertServerInvalid();
+
+        testField.setValue("2");
+        assertClientValid();
+        assertServerValid();
+    }
+
+    @Test
+    public void badInput_changeInputValue_assertValidity() {
+        testField.setValue("INVALID");
+        assertServerInvalid();
+        assertClientInvalid();
+
+        testField.setValue("2");
+        assertServerValid();
+        assertClientValid();
+
+        testField.setValue("INVALID");
+        assertServerInvalid();
+        assertClientInvalid();
+    }
+
+    protected NumberFieldElement getTestField() {
+        return $(NumberFieldElement.class).first();
+    }
+}
