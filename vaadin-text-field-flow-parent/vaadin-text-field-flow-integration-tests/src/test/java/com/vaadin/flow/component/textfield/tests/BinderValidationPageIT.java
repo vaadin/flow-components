d72a5c7956079694c0765c1fe2a20853e79f899a
Sergey Vinogradov <mr.vursen@gmail.com>
2022-12-05 11:19:34 +0200

    feat: integrate client-side validation into PasswordField (#4249)
    * add integration tests
    
    * implement new validation behavior, fix tests
    
    * remove obsolete tests
    
    * run formatter
    
    * remove more tests
    
    * add more tests
    
    * fix tabbing logic in tests

--- vaadin-text-field-flow-parent/vaadin-text-field-flow-integration-tests/src/test/java/com/vaadin/flow/component/textfield/tests/BinderValidationPageIT.java
+++ vaadin-text-field-flow-parent/vaadin-text-field-flow-integration-tests/src/test/java/com/vaadin/flow/component/textfield/tests/BinderValidationPageIT.java
@@ -27,7 +27,6 @@ import com.vaadin.flow.component.textfield.testbench.BigDecimalFieldElement;
 import com.vaadin.flow.component.textfield.testbench.EmailFieldElement;
 import com.vaadin.flow.component.textfield.testbench.IntegerFieldElement;
 import com.vaadin.flow.component.textfield.testbench.NumberFieldElement;
-import com.vaadin.flow.component.textfield.testbench.PasswordFieldElement;
 import com.vaadin.flow.component.textfield.testbench.TextAreaElement;
 import com.vaadin.flow.component.textfield.testbench.TextFieldElement;
 import com.vaadin.tests.AbstractComponentIT;
@@ -61,9 +60,8 @@ public class BinderValidationPageIT extends AbstractComponentIT {
 
     private List<Class<? extends TestBenchElement>> fieldClasses = Arrays
             .asList(TextFieldElement.class, TextAreaElement.class,
-                    PasswordFieldElement.class, EmailFieldElement.class,
-                    BigDecimalFieldElement.class, IntegerFieldElement.class,
-                    NumberFieldElement.class);
+                    EmailFieldElement.class, BigDecimalFieldElement.class,
+                    IntegerFieldElement.class, NumberFieldElement.class);
 
     @Test
     public void fields_internalValidationPass_binderValidationFail_fieldInvalid() {
