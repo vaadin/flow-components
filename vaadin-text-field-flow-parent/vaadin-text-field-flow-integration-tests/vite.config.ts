adfa861e21fe390e7b6a1b45b9f00fc47945ad1e
Sergey Vinogradov <mr.vursen@gmail.com>
2022-12-07 17:47:29 +0200

    revert unintentional changes

--- vaadin-text-field-flow-parent/vaadin-text-field-flow-integration-tests/vite.config.ts
+++ vaadin-text-field-flow-parent/vaadin-text-field-flow-integration-tests/vite.config.ts
@@ -15,8 +15,8 @@ const customConfig: UserConfigFn = (env) => ({
   },
 
   // Use local version of web-components, disabled by default
-  // To use this un-comment the lines below and change the path to
-  // the absolute path of your web-components repo's node_modules
+  // To use this un-comment the lines below and change the path to 
+  // the absolute path of your web-components repo's node_modules 
   // folder
   // DO NOT COMMIT THESE CHANGES!
   /*
