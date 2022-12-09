6e5b7f90926bf8cdb6b846b88d7d8a98c9f22905
Sergey Vinogradov <mr.vursen@gmail.com>
2022-12-09 16:13:44 +0200

    remove feature flag usage from TextArea

--- vaadin-text-field-flow-parent/vaadin-text-field-flow/src/main/java/com/vaadin/flow/component/textfield/TextArea.java
+++ vaadin-text-field-flow-parent/vaadin-text-field-flow/src/main/java/com/vaadin/flow/component/textfield/TextArea.java
@@ -92,9 +92,7 @@ public class TextArea extends GeneratedVaadinTextArea<TextArea, String>
 
         addValueChangeListener(e -> validate());
 
-        if (isFeatureFlagEnabled(FeatureFlags.ENFORCE_FIELD_VALIDATION)) {
-            addClientValidatedEventListener(e -> validate());
-        }
+        addClientValidatedEventListener(e -> validate());
     }
 
     /**
@@ -452,25 +450,17 @@ public class TextArea extends GeneratedVaadinTextArea<TextArea, String>
 
     @Override
     public Validator<String> getDefaultValidator() {
-        if (isFeatureFlagEnabled(FeatureFlags.ENFORCE_FIELD_VALIDATION)) {
-            return (value, context) -> getValidationSupport()
-                    .checkValidity(value);
-        }
-
-        return Validator.alwaysPass();
+        return (value, context) -> getValidationSupport()
+                .checkValidity(value);
     }
 
     @Override
     public Registration addValidationStatusChangeListener(
             ValidationStatusChangeListener<String> listener) {
-        if (isFeatureFlagEnabled(FeatureFlags.ENFORCE_FIELD_VALIDATION)) {
-            return addClientValidatedEventListener(
-                    event -> listener.validationStatusChanged(
-                            new ValidationStatusChangeEvent<>(this,
-                                    !isInvalid())));
-        }
-
-        return null;
+        return addClientValidatedEventListener(
+                event -> listener.validationStatusChanged(
+                        new ValidationStatusChangeEvent<>(this,
+                                !isInvalid())));
     }
 
     /**
@@ -486,12 +476,8 @@ public class TextArea extends GeneratedVaadinTextArea<TextArea, String>
     @Override
     protected void onAttach(AttachEvent attachEvent) {
         super.onAttach(attachEvent);
-        if (isFeatureFlagEnabled(FeatureFlags.ENFORCE_FIELD_VALIDATION)) {
-            ClientValidationUtil
-                    .preventWebComponentFromModifyingInvalidState(this);
-        } else {
-            FieldValidationUtil.disableClientValidation(this);
-        }
+        ClientValidationUtil
+                .preventWebComponentFromModifyingInvalidState(this);
     }
 
     // Override is only required to keep binary compatibility with other 23.x
@@ -506,26 +492,5 @@ public class TextArea extends GeneratedVaadinTextArea<TextArea, String>
     @Override
     public void removeThemeVariants(TextAreaVariant... variants) {
         HasThemeVariant.super.removeThemeVariants(variants);
-    }
-
-    /**
-     * Returns true if the given feature flag is enabled, false otherwise.
-     * <p>
-     * Exposed with protected visibility to support mocking
-     * <p>
-     * The method requires the {@code VaadinService} instance to obtain the
-     * available feature flags, otherwise, the feature is considered disabled.
-     *
-     * @param feature
-     *            the feature flag.
-     * @return whether the feature flag is enabled.
-     */
-    protected boolean isFeatureFlagEnabled(Feature feature) {
-        VaadinService service = VaadinService.getCurrent();
-        if (service == null) {
-            return false;
-        }
-
-        return FeatureFlags.get(service.getContext()).isEnabled(feature);
     }
 }
