6d1457f9abb607d3aa1fe0d1426456c05933129e
Sergey Vinogradov <mr.vursen@gmail.com>
2022-12-12 17:58:28 +0200

    test: align the naming of validation tests (#4343)

--- vaadin-text-field-flow-parent/vaadin-text-field-flow-integration-tests/src/test/java/com/vaadin/flow/component/textfield/tests/validation/IntegerFieldBasicValidationIT.java
+++ vaadin-text-field-flow-parent/vaadin-text-field-flow-integration-tests/src/test/java/com/vaadin/flow/component/textfield/tests/validation/IntegerFieldBasicValidationIT.java
@@ -28,12 +28,148 @@ import static com.vaadin.flow.component.textfield.tests.validation.IntegerFieldV
 import static com.vaadin.flow.component.textfield.tests.validation.IntegerFieldValidationBasicPage.REQUIRED_BUTTON;
 
 @TestPath("vaadin-integer-field/validation/basic")
-public class IntegerFieldValidationBasicIT
+public class IntegerFieldBasicValidationIT
         extends AbstractValidationIT<IntegerFieldElement> {
     @Test
     public void fieldIsInitiallyValid() {
         assertClientValid();
         assertServerValid();
+    }
+
+    @Test
+    public void triggerBlur_assertValidity() {
+        testField.sendKeys(Keys.TAB);
+        assertServerValid();
+        assertClientValid();
+    }
+
+    @Test
+    public void required_triggerBlur_assertValidity() {
+        $("button").id(REQUIRED_BUTTON).click();
+
+        testField.sendKeys(Keys.TAB);
+        assertServerInvalid();
+        assertClientInvalid();
+    }
+
+    @Test
+    public void required_changeValue_assertValidity() {
+        $("button").id(REQUIRED_BUTTON).click();
+
+        testField.setValue("1234");
+        assertServerValid();
+        assertClientValid();
+
+        testField.setValue("");
+        assertServerInvalid();
+        assertClientInvalid();
+    }
+
+    @Test
+    public void min_triggerBlur_assertValidity() {
+        $("input").id(MIN_INPUT).sendKeys("2", Keys.ENTER);
+
+        testField.sendKeys(Keys.TAB);
+        assertServerValid();
+        assertClientValid();
+    }
+
+    @Test
+    public void min_changeValue_assertValidity() {
+        $("input").id(MIN_INPUT).sendKeys("2", Keys.ENTER);
+
+        testField.setValue("1");
+        assertClientInvalid();
+        assertServerInvalid();
+
+        testField.setValue("2");
+        assertClientValid();
+        assertServerValid();
+
+        testField.setValue("3");
+        assertClientValid();
+        assertServerValid();
+    }
+
+    @Test
+    public void max_triggerBlur_assertValidity() {
+        $("input").id(MAX_INPUT).sendKeys("2", Keys.ENTER);
+
+        testField.sendKeys(Keys.TAB);
+        assertServerValid();
+        assertClientValid();
+    }
+
+    @Test
+    public void max_changeValue_assertValidity() {
+        $("input").id(MAX_INPUT).sendKeys("2", Keys.ENTER);
+
+        testField.setValue("3");
+        assertClientInvalid();
+        assertServerInvalid();
+
+        testField.setValue("2");
+        assertClientValid();
+        assertServerValid();
+
+        testField.setValue("1");
+        assertClientValid();
+        assertServerValid();
+    }
+
+    @Test
+    public void step_triggerBlur_assertValidity() {
+        $("input").id(STEP_INPUT).sendKeys("2", Keys.ENTER);
+
+        testField.sendKeys(Keys.TAB);
+        assertServerValid();
+        assertClientValid();
+    }
+
+    @Test
+    public void step_changeValue_assertValidity() {
+        $("input").id(STEP_INPUT).sendKeys("2", Keys.ENTER);
+
+        testField.setValue("1");
+        assertClientInvalid();
+        assertServerInvalid();
+
+        testField.setValue("2");
+        assertClientValid();
+        assertServerValid();
+
+        testField.setValue("3");
+        assertClientInvalid();
+        assertServerInvalid();
+    }
+
+    @Test
+    public void badInput_changeValue_assertValidity() {
+        testField.sendKeys("--2", Keys.TAB);
+        assertServerInvalid();
+        assertClientInvalid();
+
+        testField.setValue("2");
+        assertServerValid();
+        assertClientValid();
+
+        testField.sendKeys("--2", Keys.TAB);
+        assertServerInvalid();
+        assertClientInvalid();
+    }
+
+    @Test
+    public void integerOverflow_setValueExceedingMaxInteger_assertValidity() {
+        testField.sendKeys("999999999999", Keys.TAB);
+        assertServerInvalid();
+        assertClientInvalid();
+    }
+
+    @Test
+    public void integerOverflow_setValueExceedingMinInteger_assertValidity() {
+        testField.sendKeys("-999999999999", Keys.TAB);
+        assertServerInvalid();
+        assertClientInvalid();
     }
 
     @Test
@@ -66,142 +202,6 @@ public class IntegerFieldValidationBasicIT
         executeScript("arguments[0].invalid = false", testField);
 
         assertServerInvalid();
-    }
-
-    @Test
-    public void triggerInputBlur_assertValidity() {
-        testField.sendKeys(Keys.TAB);
-        assertServerValid();
-        assertClientValid();
-    }
-
-    @Test
-    public void required_triggerInputBlur_assertValidity() {
-        $("button").id(REQUIRED_BUTTON).click();
-
-        testField.sendKeys(Keys.TAB);
-        assertServerInvalid();
-        assertClientInvalid();
-    }
-
-    @Test
-    public void required_changeInputValue_assertValidity() {
-        $("button").id(REQUIRED_BUTTON).click();
-
-        testField.setValue("1234");
-        assertServerValid();
-        assertClientValid();
-
-        testField.setValue("");
-        assertServerInvalid();
-        assertClientInvalid();
-    }
-
-    @Test
-    public void min_triggerInputBlur_assertValidity() {
-        $("input").id(MIN_INPUT).sendKeys("2", Keys.ENTER);
-
-        testField.sendKeys(Keys.TAB);
-        assertServerValid();
-        assertClientValid();
-    }
-
-    @Test
-    public void min_changeInputValue_assertValidity() {
-        $("input").id(MIN_INPUT).sendKeys("2", Keys.ENTER);
-
-        testField.setValue("1");
-        assertClientInvalid();
-        assertServerInvalid();
-
-        testField.setValue("2");
-        assertClientValid();
-        assertServerValid();
-
-        testField.setValue("3");
-        assertClientValid();
-        assertServerValid();
-    }
-
-    @Test
-    public void max_triggerInputBlur_assertValidity() {
-        $("input").id(MAX_INPUT).sendKeys("2", Keys.ENTER);
-
-        testField.sendKeys(Keys.TAB);
-        assertServerValid();
-        assertClientValid();
-    }
-
-    @Test
-    public void max_changeInputValue_assertValidity() {
-        $("input").id(MAX_INPUT).sendKeys("2", Keys.ENTER);
-
-        testField.setValue("3");
-        assertClientInvalid();
-        assertServerInvalid();
-
-        testField.setValue("2");
-        assertClientValid();
-        assertServerValid();
-
-        testField.setValue("1");
-        assertClientValid();
-        assertServerValid();
-    }
-
-    @Test
-    public void step_triggerInputBlur_assertValidity() {
-        $("input").id(STEP_INPUT).sendKeys("2", Keys.ENTER);
-
-        testField.sendKeys(Keys.TAB);
-        assertServerValid();
-        assertClientValid();
-    }
-
-    @Test
-    public void step_changeInputValue_assertValidity() {
-        $("input").id(STEP_INPUT).sendKeys("2", Keys.ENTER);
-
-        testField.setValue("1");
-        assertClientInvalid();
-        assertServerInvalid();
-
-        testField.setValue("2");
-        assertClientValid();
-        assertServerValid();
-
-        testField.setValue("3");
-        assertClientInvalid();
-        assertServerInvalid();
-    }
-
-    @Test
-    public void badInput_changeInputValue_assertValidity() {
-        testField.sendKeys("--2", Keys.TAB);
-        assertServerInvalid();
-        assertClientInvalid();
-
-        testField.setValue("2");
-        assertServerValid();
-        assertClientValid();
-
-        testField.sendKeys("--2", Keys.TAB);
-        assertServerInvalid();
-        assertClientInvalid();
-    }
-
-    @Test
-    public void integerOverflow_setInputValueExceedingMaxInteger_assertValidity() {
-        testField.sendKeys("999999999999", Keys.TAB);
-        assertServerInvalid();
-        assertClientInvalid();
-    }
-
-    @Test
-    public void integerOverflow_setInputValueExceedingMinInteger_assertValidity() {
-        testField.sendKeys("-999999999999", Keys.TAB);
-        assertServerInvalid();
-        assertClientInvalid();
     }
 
     protected IntegerFieldElement getTestField() {
