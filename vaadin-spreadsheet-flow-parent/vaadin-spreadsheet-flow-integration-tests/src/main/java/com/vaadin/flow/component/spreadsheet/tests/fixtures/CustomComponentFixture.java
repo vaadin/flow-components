b5e3e400709504960e4fb9a87eb2ddc56d1ab980
ugur-vaadin <ugur@vaadin.com>
2022-12-15 20:00:36 +0200

    refactor: cleanup of test fixtures

--- vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow-integration-tests/src/main/java/com/vaadin/flow/component/spreadsheet/tests/fixtures/CustomComponentFixture.java
+++ vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow-integration-tests/src/main/java/com/vaadin/flow/component/spreadsheet/tests/fixtures/CustomComponentFixture.java
@@ -8,91 +8,49 @@ import com.vaadin.flow.component.spreadsheet.SpreadsheetComponentFactory;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Sheet;
 
-import java.util.Arrays;
-
 public class CustomComponentFixture implements SpreadsheetFixture {
 
     static final int HEADER_ROW_INDEX = 0;
 
     static final int COMPONENT_ROW_INDEX = 1;
 
+    static final int COMPONENT_COLUMN_INDEX = 1;
+
     @Override
     public void loadFixture(final Spreadsheet spreadsheet) {
-        for (ComponentType componentType : ComponentType.values()) {
-            spreadsheet.createCell(HEADER_ROW_INDEX,
-                    componentType.getColumnIndex(),
-                    componentType.getHeaderText());
-        }
+        spreadsheet.createCell(HEADER_ROW_INDEX, COMPONENT_COLUMN_INDEX,
+                "Button");
         spreadsheet
-                .setSpreadsheetComponentFactory(new ComponentEditorFactory());
-    }
-}
-
-class ComponentEditorFactory implements SpreadsheetComponentFactory {
-
-    private final Button button = new Button("Click",
-            e -> Notification.show("Button clicked!"));
-
-    @Override
-    public Component getCustomComponentForCell(Cell cell, int rowIndex,
-            int columnIndex, Spreadsheet spreadsheet, Sheet sheet) {
-        ComponentType componentType = ComponentType
-                .getComponentTypeByIndex(rowIndex, columnIndex);
-        if (componentType == null) {
-            return null;
-        }
-        return getCustomComponent(componentType);
+                .setSpreadsheetComponentFactory(new CustomComponentFactory());
     }
 
-    @Override
-    public Component getCustomEditorForCell(Cell cell, final int rowIndex,
-            final int columnIndex, final Spreadsheet spreadsheet, Sheet sheet) {
-        return null;
-    }
+    private class CustomComponentFactory
+            implements SpreadsheetComponentFactory {
 
-    @Override
-    public void onCustomEditorDisplayed(Cell cell, int rowIndex,
-            int columnIndex, Spreadsheet spreadsheet, Sheet sheet,
-            Component customEditor) {
-    }
+        private final Button button = new Button("Click",
+                e -> Notification.show("Button clicked!"));
 
-    private Component getCustomComponent(ComponentType componentType) {
-        if (componentType != null) {
-            switch (componentType) {
-            case BUTTON:
-                return button;
+        @Override
+        public Component getCustomComponentForCell(Cell cell, int rowIndex,
+                int columnIndex, Spreadsheet spreadsheet, Sheet sheet) {
+            if (rowIndex != COMPONENT_ROW_INDEX
+                    || columnIndex != COMPONENT_COLUMN_INDEX) {
+                return null;
             }
+            return button;
         }
-        return null;
-    }
-}
 
-enum ComponentType {
-
-    BUTTON(1, "Button");
-
-    private int columnIndex;
-
-    private final String headerText;
-
-    ComponentType(int columnIndex, String headerText) {
-        this.columnIndex = columnIndex;
-        this.headerText = headerText;
-    }
-
-    public int getColumnIndex() {
-        return columnIndex;
-    }
-
-    public String getHeaderText() {
-        return headerText;
-    }
-
-    public static ComponentType getComponentTypeByIndex(int row, int col) {
-        if (row != CustomComponentFixture.COMPONENT_ROW_INDEX) {
+        @Override
+        public Component getCustomEditorForCell(Cell cell, final int rowIndex,
+                final int columnIndex, final Spreadsheet spreadsheet,
+                Sheet sheet) {
             return null;
         }
-        return Arrays.stream(values()).filter(type -> type.columnIndex == col)
-                .findAny().orElse(null);
+
+        @Override
+        public void onCustomEditorDisplayed(Cell cell, int rowIndex,
+                int columnIndex, Spreadsheet spreadsheet, Sheet sheet,
+                Component customEditor) {
+        }
     }
 }
--- vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow-integration-tests/src/main/java/com/vaadin/flow/component/spreadsheet/tests/fixtures/SimpleCustomEditorFixture.java
+++ vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow-integration-tests/src/main/java/com/vaadin/flow/component/spreadsheet/tests/fixtures/SimpleCustomEditorFixture.java
@@ -31,160 +31,164 @@ public class SimpleCustomEditorFixture implements SpreadsheetFixture {
         }
         spreadsheet.setSpreadsheetComponentFactory(new CustomEditorFactory());
     }
-}
 
-class CustomEditorFactory implements SpreadsheetComponentFactory {
+    private class CustomEditorFactory implements SpreadsheetComponentFactory {
 
-    private TextField textField;
+        private TextField textField;
 
-    private TextArea textArea;
+        private TextArea textArea;
 
-    private Checkbox checkbox;
+        private Checkbox checkbox;
 
-    private DatePicker datePicker;
+        private DatePicker datePicker;
 
-    private ComboBox<String> comboBox;
+        private ComboBox<String> comboBox;
 
-    @Override
-    public Component getCustomComponentForCell(Cell cell, int rowIndex,
-            int columnIndex, Spreadsheet spreadsheet, Sheet sheet) {
-        return null;
-    }
-
-    @Override
-    public Component getCustomEditorForCell(Cell cell, final int rowIndex,
-            final int columnIndex, final Spreadsheet spreadsheet, Sheet sheet) {
-        EditorType editorType = EditorType.getEditorTypeByIndex(rowIndex,
-                columnIndex);
-        if (editorType == null) {
+        @Override
+        public Component getCustomComponentForCell(Cell cell, int rowIndex,
+                int columnIndex, Spreadsheet spreadsheet, Sheet sheet) {
             return null;
         }
-        return getCustomEditor(editorType, rowIndex, columnIndex, spreadsheet);
-    }
 
-    @Override
-    public void onCustomEditorDisplayed(Cell cell, int rowIndex,
-            int columnIndex, Spreadsheet spreadsheet, Sheet sheet,
-            Component customEditor) {
-        if (cell == null) {
-            return;
-        }
-        ((HasValue) customEditor)
-                .setValue(getCellValueForEditor(rowIndex, columnIndex, cell));
-    }
-
-    private Component getCustomEditor(EditorType editorType, int rowIndex,
-            int columnIndex, Spreadsheet spreadsheet) {
-        if (editorType != null) {
-            switch (editorType) {
-            case TEXT_FIELD:
-                if (textField == null) {
-                    initTextField();
-                    textField.addValueChangeListener(e -> spreadsheet
-                            .refreshCells(spreadsheet.createCell(rowIndex,
-                                    columnIndex, e.getValue())));
-                }
-                return textField;
-            case CHECKBOX:
-                if (checkbox == null) {
-                    initCheckbox();
-                    checkbox.addValueChangeListener(e -> spreadsheet
-                            .refreshCells(spreadsheet.createCell(rowIndex,
-                                    columnIndex, e.getValue())));
-                }
-                return checkbox;
-            case DATE_PICKER:
-                if (datePicker == null) {
-                    initDatePicker();
-                    datePicker.addValueChangeListener(e -> spreadsheet
-                            .refreshCells(spreadsheet.createCell(rowIndex,
-                                    columnIndex, e.getValue().format(
-                                            DateTimeFormatter.ISO_DATE))));
-                }
-                return datePicker;
-            case TEXT_AREA:
-                if (textArea == null) {
-                    initTextArea();
-                    textArea.addValueChangeListener(e -> spreadsheet
-                            .refreshCells(spreadsheet.createCell(rowIndex,
-                                    columnIndex, e.getValue())));
-                }
-                return textArea;
-            case COMBO_BOX:
-                if (comboBox == null) {
-                    initComboBox();
-                    comboBox.addValueChangeListener(e -> spreadsheet
-                            .refreshCells(spreadsheet.createCell(rowIndex,
-                                    columnIndex, e.getValue())));
-                }
-                return comboBox;
+        @Override
+        public Component getCustomEditorForCell(Cell cell, final int rowIndex,
+                final int columnIndex, final Spreadsheet spreadsheet,
+                Sheet sheet) {
+            EditorType editorType = EditorType.getEditorTypeByIndex(rowIndex,
+                    columnIndex);
+            if (editorType == null) {
+                return null;
             }
+            return getCustomEditor(editorType, rowIndex, columnIndex,
+                    spreadsheet);
         }
-        return null;
-    }
 
-    private Object getCellValueForEditor(int rowIndex, int columnIndex,
-            Cell cell) {
-        EditorType editorType = EditorType.getEditorTypeByIndex(rowIndex,
-                columnIndex);
-        if (editorType == EditorType.DATE_PICKER) {
-            return LocalDate.parse(cell.getStringCellValue(),
-                    DateTimeFormatter.ISO_DATE);
+        @Override
+        public void onCustomEditorDisplayed(Cell cell, int rowIndex,
+                int columnIndex, Spreadsheet spreadsheet, Sheet sheet,
+                Component customEditor) {
+            if (cell == null) {
+                return;
+            }
+            ((HasValue) customEditor).setValue(
+                    getCellValueForEditor(rowIndex, columnIndex, cell));
         }
-        if (editorType == EditorType.CHECKBOX) {
-            return cell.getBooleanCellValue();
-        }
-        return cell.getStringCellValue();
-    }
 
-    private void initComboBox() {
-        comboBox = new ComboBox<>();
-        comboBox.setItems("10", "20", "30", "40", "50");
-    }
-
-    private void initTextArea() {
-        textArea = new TextArea();
-    }
-
-    private void initDatePicker() {
-        datePicker = new DatePicker();
-    }
-
-    private void initCheckbox() {
-        checkbox = new Checkbox();
-    }
-
-    private void initTextField() {
-        textField = new TextField();
-    }
-}
-
-enum EditorType {
-    TEXT_FIELD(1, "TextField"), CHECKBOX(2, "Checkbox"), DATE_PICKER(3,
-            "DatePicker"), TEXT_AREA(4, "TextArea"), COMBO_BOX(5, "ComboBox");
-
-    private int columnIndex;
-
-    private String headerText;
-
-    EditorType(int columnIndex, String headerText) {
-        this.columnIndex = columnIndex;
-        this.headerText = headerText;
-    }
-
-    public int getColumnIndex() {
-        return columnIndex;
-    }
-
-    public String getHeaderText() {
-        return headerText;
-    }
-
-    public static EditorType getEditorTypeByIndex(int row, int col) {
-        if (row != SimpleCustomEditorFixture.COMPONENT_ROW_INDEX) {
+        private Component getCustomEditor(EditorType editorType, int rowIndex,
+                int columnIndex, Spreadsheet spreadsheet) {
+            if (editorType != null) {
+                switch (editorType) {
+                case TEXT_FIELD:
+                    if (textField == null) {
+                        initTextField();
+                        textField.addValueChangeListener(e -> spreadsheet
+                                .refreshCells(spreadsheet.createCell(rowIndex,
+                                        columnIndex, e.getValue())));
+                    }
+                    return textField;
+                case CHECKBOX:
+                    if (checkbox == null) {
+                        initCheckbox();
+                        checkbox.addValueChangeListener(e -> spreadsheet
+                                .refreshCells(spreadsheet.createCell(rowIndex,
+                                        columnIndex, e.getValue())));
+                    }
+                    return checkbox;
+                case DATE_PICKER:
+                    if (datePicker == null) {
+                        initDatePicker();
+                        datePicker.addValueChangeListener(e -> spreadsheet
+                                .refreshCells(spreadsheet.createCell(rowIndex,
+                                        columnIndex, e.getValue().format(
+                                                DateTimeFormatter.ISO_DATE))));
+                    }
+                    return datePicker;
+                case TEXT_AREA:
+                    if (textArea == null) {
+                        initTextArea();
+                        textArea.addValueChangeListener(e -> spreadsheet
+                                .refreshCells(spreadsheet.createCell(rowIndex,
+                                        columnIndex, e.getValue())));
+                    }
+                    return textArea;
+                case COMBO_BOX:
+                    if (comboBox == null) {
+                        initComboBox();
+                        comboBox.addValueChangeListener(e -> spreadsheet
+                                .refreshCells(spreadsheet.createCell(rowIndex,
+                                        columnIndex, e.getValue())));
+                    }
+                    return comboBox;
+                }
+            }
             return null;
         }
-        return Arrays.stream(values()).filter(type -> type.columnIndex == col)
-                .findAny().orElse(null);
+
+        private Object getCellValueForEditor(int rowIndex, int columnIndex,
+                Cell cell) {
+            EditorType editorType = EditorType.getEditorTypeByIndex(rowIndex,
+                    columnIndex);
+            if (editorType == EditorType.DATE_PICKER) {
+                return LocalDate.parse(cell.getStringCellValue(),
+                        DateTimeFormatter.ISO_DATE);
+            }
+            if (editorType == EditorType.CHECKBOX) {
+                return cell.getBooleanCellValue();
+            }
+            return cell.getStringCellValue();
+        }
+
+        private void initComboBox() {
+            comboBox = new ComboBox<>();
+            comboBox.setItems("10", "20", "30", "40", "50");
+        }
+
+        private void initTextArea() {
+            textArea = new TextArea();
+        }
+
+        private void initDatePicker() {
+            datePicker = new DatePicker();
+        }
+
+        private void initCheckbox() {
+            checkbox = new Checkbox();
+        }
+
+        private void initTextField() {
+            textField = new TextField();
+        }
+    }
+
+    private enum EditorType {
+        TEXT_FIELD(1, "TextField"), CHECKBOX(2, "Checkbox"), DATE_PICKER(3,
+                "DatePicker"), TEXT_AREA(4,
+                        "TextArea"), COMBO_BOX(5, "ComboBox");
+
+        private int columnIndex;
+
+        private String headerText;
+
+        EditorType(int columnIndex, String headerText) {
+            this.columnIndex = columnIndex;
+            this.headerText = headerText;
+        }
+
+        public int getColumnIndex() {
+            return columnIndex;
+        }
+
+        public String getHeaderText() {
+            return headerText;
+        }
+
+        public static EditorType getEditorTypeByIndex(int row, int col) {
+            if (row != COMPONENT_ROW_INDEX) {
+                return null;
+            }
+            return Arrays.stream(values())
+                    .filter(type -> type.columnIndex == col).findAny()
+                    .orElse(null);
+        }
     }
 }
