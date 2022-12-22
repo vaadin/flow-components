344c6207d353145608963a08dcaac50d193ac27f
Sergey Vinogradov <mr.vursen@gmail.com>
2022-12-22 16:53:52 +0200

    add unit tests

---
+++ vaadin-time-picker-flow-parent/vaadin-time-picker-flow-integration-tests/src/test/java/com/vaadin/flow/component/timepicker/tests/ClearValueIT.java
@@ -0,0 +1,41 @@
+package com.vaadin.flow.component.timepicker.tests;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Keys;
+
+import com.vaadin.flow.component.timepicker.testbench.TimePickerElement;
+import com.vaadin.flow.testutil.TestPath;
+import com.vaadin.tests.AbstractComponentIT;
+
+import static com.vaadin.flow.component.timepicker.tests.ClearValuePage.CLEAR_BUTTON;
+
+@TestPath("vaadin-time-picker/clear-value")
+public class ClearValueIT extends AbstractComponentIT {
+    private TimePickerElement timePicker;
+
+    @Before
+    public void init() {
+        open();
+        timePicker = $(TimePickerElement.class).first();
+    }
+
+    @Test
+    public void setInputValue_clearValue_inputValueIsEmpty() {
+        timePicker.sendKeys("12:00", Keys.ENTER);
+        Assert.assertEquals("12:00", timePicker.getTimePickerInputValue());
+
+        $("button").id(CLEAR_BUTTON).click();
+        Assert.assertEquals("", timePicker.getTimePickerInputValue());
+    }
+
+    @Test
+    public void setBadInputValue_clearValue_inputValueIsEmpty() {
+        timePicker.sendKeys("INVALID", Keys.ENTER);
+        Assert.assertEquals("INVALID", timePicker.getTimePickerInputValue());
+
+        $("button").id(CLEAR_BUTTON).click();
+        Assert.assertEquals("", timePicker.getTimePickerInputValue());
+    }
+}
