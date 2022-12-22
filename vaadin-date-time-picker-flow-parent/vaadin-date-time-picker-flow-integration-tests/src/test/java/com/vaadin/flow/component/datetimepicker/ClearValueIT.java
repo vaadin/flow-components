2f0382d69b95d85045bc08104c157e737e4f94de
Sergey Vinogradov <mr.vursen@gmail.com>
2022-12-22 20:35:30 +0200

    add tests

---
+++ vaadin-date-time-picker-flow-parent/vaadin-date-time-picker-flow-integration-tests/src/test/java/com/vaadin/flow/component/datetimepicker/ClearValueIT.java
@@ -0,0 +1,52 @@
+package com.vaadin.flow.component.datetimepicker;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Keys;
+
+import com.vaadin.flow.component.datetimepicker.testbench.DateTimePickerElement;
+import com.vaadin.flow.testutil.TestPath;
+import com.vaadin.testbench.TestBenchElement;
+import com.vaadin.tests.AbstractComponentIT;
+
+import static com.vaadin.flow.component.datetimepicker.ClearValuePage.CLEAR_BUTTON;
+
+@TestPath("vaadin-date-time-picker/clear-value")
+public class ClearValueIT extends AbstractComponentIT {
+    private DateTimePickerElement dateTimePicker;
+    private TestBenchElement dateInput;
+    private TestBenchElement timeInput;
+
+    @Before
+    public void init() {
+        open();
+        dateTimePicker = $(DateTimePickerElement.class).first();
+        dateInput = dateTimePicker.$("input").first();
+        timeInput = dateTimePicker.$("input").last();
+    }
+
+    @Test
+    public void setInputValue_clearValue_inputValueIsEmpty() {
+        dateInput.sendKeys("1/1/2022", Keys.ENTER);
+        timeInput.sendKeys("12:00 PM", Keys.ENTER);
+        Assert.assertEquals("1/1/2022", dateInput.getPropertyString("value"));
+        Assert.assertEquals("12:00 PM", timeInput.getPropertyString("value"));
+
+        $("button").id(CLEAR_BUTTON).click();
+        Assert.assertEquals("", dateInput.getPropertyString("value"));
+        Assert.assertEquals("", timeInput.getPropertyString("value"));
+    }
+
+    @Test
+    public void setBadInputValue_clearValue_inputValueIsEmpty() {
+        dateInput.sendKeys("INVALID", Keys.ENTER);
+        timeInput.sendKeys("INVALID", Keys.ENTER);
+        Assert.assertEquals("INVALID", dateInput.getPropertyString("value"));
+        Assert.assertEquals("INVALID", timeInput.getPropertyString("value"));
+
+        $("button").id(CLEAR_BUTTON).click();
+        Assert.assertEquals("", dateInput.getPropertyString("value"));
+        Assert.assertEquals("", timeInput.getPropertyString("value"));
+    }
+}
