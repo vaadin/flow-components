86b1b1432e452449cff510ab9c220b445916a820
Marcus Hellberg <marcus@vaadin.com>
2022-12-27 10:53:50 -0800

    Fix formatting

--- vaadin-virtual-list-flow-parent/vaadin-virtual-list-flow/src/main/java/com/vaadin/flow/component/virtuallist/VirtualList.java
+++ vaadin-virtual-list-flow-parent/vaadin-virtual-list-flow/src/main/java/com/vaadin/flow/component/virtuallist/VirtualList.java
@@ -296,8 +296,8 @@ public class VirtualList<T> extends Component implements HasDataProvider<T>,
     }
 
     /**
-     * Scrolls to the given row index. Scrolls so that the element is shown at the
-     * start of the visible area whenever possible.
+     * Scrolls to the given row index. Scrolls so that the element is shown at
+     * the start of the visible area whenever possible.
      *
      * If the index parameter exceeds current item set size the grid will scroll
      * to the end.
@@ -320,8 +320,8 @@ public class VirtualList<T> extends Component implements HasDataProvider<T>,
      * Scrolls to the last element of the list.
      */
     public void scrollToEnd() {
-        getUI().ifPresent(
-                ui -> ui.beforeClientResponse(this, ctx -> getElement()
-                        .executeJs("this.scrollToIndex(this.items.length - 1)")));
+        getUI().ifPresent(ui -> ui.beforeClientResponse(this,
+                ctx -> getElement().executeJs(
+                        "this.scrollToIndex(this.items.length - 1)")));
     }
 }
