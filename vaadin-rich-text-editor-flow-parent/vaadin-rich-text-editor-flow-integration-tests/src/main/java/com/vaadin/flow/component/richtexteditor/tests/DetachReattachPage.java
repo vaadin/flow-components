56a76a8290eb464997190d9e721a9e57d0e1184c
Sascha Ißbrücker <sissbruecker@vaadin.com>
2023-01-11 17:15:18 +0100

    add attach/reattach IT

---
+++ vaadin-rich-text-editor-flow-parent/vaadin-rich-text-editor-flow-integration-tests/src/main/java/com/vaadin/flow/component/richtexteditor/tests/DetachReattachPage.java
@@ -0,0 +1,23 @@
+package com.vaadin.flow.component.richtexteditor.tests;
+
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.NativeButton;
+import com.vaadin.flow.component.richtexteditor.RichTextEditor;
+import com.vaadin.flow.router.Route;
+
+@Route("vaadin-rich-text-editor/detach-reattach")
+public class DetachReattachPage extends Div {
+    public DetachReattachPage() {
+        RichTextEditor editor = new RichTextEditor();
+        editor.setValue("<h1>foo</h1>");
+
+        NativeButton detach = new NativeButton("Detach", e -> remove(editor));
+        detach.setId("detach");
+
+        NativeButton attach = new NativeButton("Attach", e -> add(editor));
+        attach.setId("attach");
+
+        add(editor);
+        add(new Div(detach, attach));
+    }
+}
---
+++ vaadin-rich-text-editor-flow-parent/vaadin-rich-text-editor-flow-integration-tests/src/test/java/com/vaadin/flow/component/richtexteditor/tests/DetachReattachIT.java
@@ -0,0 +1,27 @@
+package com.vaadin.flow.component.richtexteditor.tests;
+
+import com.vaadin.flow.component.richtexteditor.testbench.RichTextEditorElement;
+import com.vaadin.flow.testutil.TestPath;
+import com.vaadin.tests.AbstractComponentIT;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+@TestPath("vaadin-rich-text-editor/detach-reattach")
+public class DetachReattachIT extends AbstractComponentIT {
+    @Before
+    public void init() {
+        open();
+    }
+
+    @Test
+    public void detach_reattach_htmlValueInitialized() {
+        $("button").id("detach").click();
+        $("button").id("attach").click();
+
+        RichTextEditorElement editor = $(RichTextEditorElement.class).first();
+
+        Assert.assertEquals("<h1>foo</h1>",
+                editor.getEditor().getProperty("innerHTML"));
+    }
+}
