a258183d929843716c09b476bcd741c848d89ced
Artur Signell <artur@vaadin.com>
2023-01-25 14:06:25 +0200

    fix: Depend on testbench-shared to avoid junit4 dependency
    The shared artifact contains the element classes, the core artifact also contains junit4 specific code

--- vaadin-charts-flow-parent/vaadin-charts-testbench/src/main/java/com/vaadin/flow/component/charts/testbench/ChartElement.java
+++ vaadin-charts-flow-parent/vaadin-charts-testbench/src/main/java/com/vaadin/flow/component/charts/testbench/ChartElement.java
@@ -1,13 +1,9 @@
 package com.vaadin.flow.component.charts.testbench;
 
-import com.vaadin.testbench.TestBenchElement;
-import com.vaadin.testbench.elementsbase.Element;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-
 import java.util.List;
 
-import static org.junit.Assert.assertNotNull;
+import com.vaadin.testbench.TestBenchElement;
+import com.vaadin.testbench.elementsbase.Element;
 
 @Element("vaadin-chart")
 public class ChartElement extends TestBenchElement {
@@ -20,15 +16,4 @@ public class ChartElement extends TestBenchElement {
         return $(":not([visibility=hidden]).highcharts-point").all();
     }
 
-    private List<WebElement> getElementsFromShadowRootBySelector(
-            String selector) {
-        return getElementsFromShadowRoot(By.cssSelector(selector));
-    }
-
-    private List<WebElement> getElementsFromShadowRoot(By by) {
-        WebElement shadowRoot = (WebElement) executeScript(
-                "return arguments[0].shadowRoot", this);
-        assertNotNull("Could not locate shadowRoot in the element", shadowRoot);
-        return shadowRoot.findElements(by);
-    }
 }
