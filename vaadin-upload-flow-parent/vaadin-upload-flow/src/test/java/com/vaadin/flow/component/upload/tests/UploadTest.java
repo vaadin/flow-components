fadc89941d4f290c1ec384798ce76e5b3da4cd44
Tomi Virkki <tomivirkki@users.noreply.github.com>
2023-02-09 11:05:37 +0200

    fix: make isAutoUpload return correct boolean value (#4658)

--- vaadin-upload-flow-parent/vaadin-upload-flow/src/test/java/com/vaadin/flow/component/upload/tests/UploadTest.java
+++ vaadin-upload-flow-parent/vaadin-upload-flow/src/test/java/com/vaadin/flow/component/upload/tests/UploadTest.java
@@ -2,6 +2,7 @@ package com.vaadin.flow.component.upload.tests;
 
 import com.vaadin.flow.component.upload.Upload;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class UploadTest {
@@ -10,5 +11,12 @@ public class UploadTest {
     public void uploadNewUpload() {
         // Test no NPE due missing UI when setAttribute is called.
         Upload upload = new Upload();
+    }
+
+    @Test
+    public void upload_setAutoUpload_isAutoUpload() {
+        Upload upload = new Upload();
+        upload.setAutoUpload(true);
+        Assert.assertEquals(true, upload.isAutoUpload());
     }
 }
