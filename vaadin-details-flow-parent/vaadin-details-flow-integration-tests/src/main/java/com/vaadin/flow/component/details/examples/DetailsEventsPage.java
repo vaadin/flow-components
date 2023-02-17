162bdb2e27bc15dce756a2083a0ccc324d3d3404
Sascha Ißbrücker <sissbruecker@vaadin.com>
2023-02-17 16:03:49 +0100

    fix: use property change listener for opened change event (#4665) (CP: 14:10) (#4672)
    (cherry picked from commit 7514bdfe329152066593f2496602c80621d6467c)

---
+++ vaadin-details-flow-parent/vaadin-details-flow-integration-tests/src/main/java/com/vaadin/flow/component/details/examples/DetailsEventsPage.java
@@ -0,0 +1,29 @@
+package com.vaadin.flow.component.details.examples;
+
+import com.vaadin.flow.component.details.Details;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.NativeButton;
+import com.vaadin.flow.router.Route;
+
+@Route("vaadin-details/events")
+public class DetailsEventsPage extends Div {
+    public DetailsEventsPage() {
+        Details details = new Details();
+
+        Div output = new Div();
+        output.setId("output");
+        output.getStyle().set("white-space", "pre");
+        details.addOpenedChangeListener(e -> {
+            output.setText(output.getText() + String.format(
+                    "Opened changed: opened=%s, isFromClient=%s\n",
+                    e.isOpened(), e.isFromClient()));
+        });
+
+        NativeButton toggle = new NativeButton("Toggle", e -> {
+            details.setOpened(!details.isOpened());
+        });
+        toggle.setId("toggle");
+
+        add(details, new Div(toggle), output);
+    }
+}
---
+++ vaadin-details-flow-parent/vaadin-details-flow-integration-tests/src/test/java/com/vaadin/flow/component/details/test/DetailsEventsIT.java
@@ -0,0 +1,43 @@
+package com.vaadin.flow.component.details.test;
+
+import com.vaadin.flow.component.details.testbench.DetailsElement;
+import com.vaadin.flow.testutil.TestPath;
+import com.vaadin.testbench.TestBenchElement;
+import com.vaadin.tests.AbstractComponentIT;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+@TestPath("vaadin-details/events")
+public class DetailsEventsIT extends AbstractComponentIT {
+    private DetailsElement details;
+    private TestBenchElement toggle;
+    private TestBenchElement output;
+
+    @Before
+    public void init() {
+        open();
+        details = $(DetailsElement.class).waitForFirst();
+        toggle = $(TestBenchElement.class).id("toggle");
+        output = $(TestBenchElement.class).id("output");
+    }
+
+    @Test
+    public void noInitialOpenedChangeEvent() {
+        Assert.assertEquals("", output.getText());
+    }
+
+    @Test
+    public void toggleOnClient_openedChangeEventIsFromClient() {
+        details.toggle();
+        Assert.assertEquals("Opened changed: opened=true, isFromClient=true",
+                output.getText());
+    }
+
+    @Test
+    public void toggleOnServer_openedChangeEventIsNotFromClient() {
+        toggle.click();
+        Assert.assertEquals("Opened changed: opened=true, isFromClient=false",
+                output.getText());
+    }
+}
