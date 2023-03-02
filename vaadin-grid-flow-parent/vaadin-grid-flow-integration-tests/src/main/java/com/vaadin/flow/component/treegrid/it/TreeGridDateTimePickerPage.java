b7d6d625308285138585e9b4d29c158fead9091a
Ugur Saglam <106508695+ugur-vaadin@users.noreply.github.com>
2023-03-02 21:40:26 +0200

    refactor: revert i18n workaround (#4706)
    * fix: add another deferring layer for updating i18n
    
    * fix: use custom event after ready to update date time picker i18n
    
    * fix: wrap i18n update into microtask
    
    * fix: wrap inner i18n update into microtask
    
    * fix: revert microtask related changes
    
    * fix wrap the inner i18n update
    
    * fix: add extra i18n updates for debugging purposes
    
    * fix: revert extra i18n updates and add waitUntil condition to tests
    
    * Revert "fix: revert extra i18n updates and add waitUntil condition to tests"
    
    This reverts commit d0edb419532b3fa920534e5c5cd6d64885af3b35.
    
    * fix: add even more i18n updates for debugging purposes
    
    * fix: use requestAnimationFrame for i18n update
    
    * Update vaadin-date-picker-flow-parent/vaadin-date-picker-flow/src/main/resources/META-INF/resources/frontend/datepickerConnector.js
    
    Co-authored-by: Sergey Vinogradov <mr.vursen@gmail.com>
    
    * fix: update i18n when set to a different value from the last set value
    
    * fix: set last set i18n before updating the property
    
    * fix: revert connector workarounds
    
    * Update vaadin-grid-flow-parent/vaadin-grid-flow-integration-tests/src/test/java/com/vaadin/flow/component/treegrid/it/TreeGridDateTimePickerIT.java
    
    Co-authored-by: Sergey Vinogradov <mr.vursen@gmail.com>
    
    * fix: use dash instead of spaces in picker ids
    
    * fix: use replace instead of replaceAll
    
    ---------
    
    Co-authored-by: Sergey Vinogradov <mr.vursen@gmail.com>

---
+++ vaadin-grid-flow-parent/vaadin-grid-flow-integration-tests/src/main/java/com/vaadin/flow/component/treegrid/it/TreeGridDateTimePickerPage.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2000-2023 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.treegrid.it;
+
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.datetimepicker.DateTimePicker;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.treegrid.TreeGrid;
+import com.vaadin.flow.data.provider.hierarchy.TreeData;
+import com.vaadin.flow.router.Route;
+
+import java.time.LocalDateTime;
+
+@Route("vaadin-grid/tree-grid-date-time-picker")
+public class TreeGridDateTimePickerPage extends Div {
+
+    private TreeGrid<String> grid;
+
+    public TreeGridDateTimePickerPage() {
+        grid = new TreeGrid<>();
+        grid.addHierarchyColumn(row -> row);
+        grid.addComponentColumn(this::getDateTimePicker);
+
+        TreeData<String> data = new TreeData<>();
+        data.addRootItems("Row 1", "Row 2");
+        data.addItem("Row 1", "Child 1");
+        data.addItem("Row 1", "Child 2");
+        data.addItem("Row 2", "Child 3");
+        data.addItem("Row 2", "Child 4");
+        grid.setTreeData(data);
+        grid.expand("Row 1", "Row 2");
+
+        add(grid);
+    }
+
+    private DateTimePicker getDateTimePicker(String item) {
+        DateTimePicker dateTimePicker = new DateTimePicker("Date and time");
+        dateTimePicker.setId("id-" + item.replace(" ", "-"));
+        DatePicker.DatePickerI18n i18n = new DatePicker.DatePickerI18n();
+        i18n.setDateFormat("dd.MM.yyyy");
+        dateTimePicker.setDatePickerI18n(i18n);
+        dateTimePicker.setValue(LocalDateTime.of(2000, 6, 13, 12, 0));
+        return dateTimePicker;
+    }
+}
---
+++ vaadin-grid-flow-parent/vaadin-grid-flow-integration-tests/src/test/java/com/vaadin/flow/component/treegrid/it/TreeGridDateTimePickerIT.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2000-2023 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.treegrid.it;
+
+import com.vaadin.flow.component.datetimepicker.testbench.DateTimePickerElement;
+import com.vaadin.flow.testutil.TestPath;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+@TestPath("vaadin-grid/tree-grid-date-time-picker")
+public class TreeGridDateTimePickerIT extends AbstractTreeGridIT {
+
+    @Before
+    public void before() {
+        open();
+        setupTreeGrid();
+    }
+
+    @Test
+    public void shouldHaveI18nAppliedToRoot() {
+        Assert.assertEquals("13.06.2000", $(DateTimePickerElement.class)
+                .id("id-Row-1").getDatePresentation());
+    }
+
+    @Test
+    public void shouldHaveI18nAppliedToChild() {
+        Assert.assertEquals("13.06.2000", $(DateTimePickerElement.class)
+                .id("id-Child-1").getDatePresentation());
+    }
+}
