389441b3bafb14f268a9a6f4f17f080ddc1a8ee9
Tomi Virkki <virkki@vaadin.com>
2023-03-03 13:35:54 +0200

    refactor: revert timing change

--- vaadin-grid-flow-parent/vaadin-grid-flow/src/main/resources/META-INF/resources/frontend/gridConnector.js
+++ vaadin-grid-flow-parent/vaadin-grid-flow/src/main/resources/META-INF/resources/frontend/gridConnector.js
@@ -1,5 +1,5 @@
 import { Debouncer } from '@polymer/polymer/lib/utils/debounce.js';
-import { timeOut, microTask } from '@polymer/polymer/lib/utils/async.js';
+import { timeOut, animationFrame, microTask } from '@polymer/polymer/lib/utils/async.js';
 import { Grid } from '@vaadin/grid/src/vaadin-grid.js';
 import { ItemCache } from '@vaadin/grid/src/vaadin-grid-data-provider-mixin.js';
 import { isFocusable } from '@vaadin/grid/src/vaadin-grid-active-item-mixin.js';
@@ -145,7 +145,7 @@ import { isFocusable } from '@vaadin/grid/src/vaadin-grid-active-item-mixin.js';
             level: targetCache.getLevel()
           });
 
-          ensureSubCacheDebouncer = Debouncer.debounce(ensureSubCacheDebouncer, microTask, () => {
+          ensureSubCacheDebouncer = Debouncer.debounce(ensureSubCacheDebouncer, animationFrame, () => {
             while (ensureSubCacheQueue.length) {
               grid.$connector.flushEnsureSubCache();
             }
