055063a5dee017f342f3bb8532f546719df82d3c
Tomi Virkki <tomivirkki@users.noreply.github.com>
2023-03-06 10:54:31 +0200

    chore: update web components version to 24.1.0-alpha1 (#4765)

--- vaadin-tabs-flow-parent/vaadin-tabs-flow/src/main/java/com/vaadin/flow/component/tabs/Tab.java
+++ vaadin-tabs-flow-parent/vaadin-tabs-flow/src/main/java/com/vaadin/flow/component/tabs/Tab.java
@@ -33,10 +33,10 @@ import com.vaadin.flow.component.shared.HasTooltip;
  * @author Vaadin Ltd.
  */
 @Tag("vaadin-tab")
-@NpmPackage(value = "@vaadin/polymer-legacy-adapter", version = "24.0.0-rc1")
+@NpmPackage(value = "@vaadin/polymer-legacy-adapter", version = "24.1.0-alpha1")
 @JsModule("@vaadin/polymer-legacy-adapter/style-modules.js")
 @JsModule("@vaadin/tabs/src/vaadin-tab.js")
-@NpmPackage(value = "@vaadin/tabs", version = "24.0.0-rc1")
+@NpmPackage(value = "@vaadin/tabs", version = "24.1.0-alpha1")
 public class Tab extends Component implements HasComponents, HasLabel, HasStyle,
         HasThemeVariant<TabVariant>, HasTooltip {
 
