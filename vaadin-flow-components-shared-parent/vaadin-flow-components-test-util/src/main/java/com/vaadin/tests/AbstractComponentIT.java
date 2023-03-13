e5fa1ca71a8a77398c98f11f09ae24a0ab01af62
Ugur Saglam <106508695+ugur-vaadin@users.noreply.github.com>
2023-03-13 10:22:40 +0200

    test: merge it fix back-ports in a single pr (#4799)
    * fix: merge 2 it fix PRs in a single commit
    
    * fix: remove ParallelTest related code

--- vaadin-flow-components-shared-parent/vaadin-flow-components-test-util/src/main/java/com/vaadin/tests/AbstractComponentIT.java
+++ vaadin-flow-components-shared-parent/vaadin-flow-components-test-util/src/main/java/com/vaadin/tests/AbstractComponentIT.java
@@ -1,14 +1,7 @@
 package com.vaadin.tests;
 
-import org.junit.BeforeClass;
-
 public abstract class AbstractComponentIT
         extends com.vaadin.flow.testutil.AbstractComponentIT {
-
-    @BeforeClass
-    public static void setupClass() {
-        ParallelTest.setupClass();
-    }
 
     protected int getDeploymentPort() {
         return 8080;
