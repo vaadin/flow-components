2bb50e524f6ebe8b51c0cf1e8288d48d69cdad56
ugur-vaadin <ugur@vaadin.com>
2023-03-13 09:24:11 +0200

    fix: remove ParallelTest related code

--- vaadin-flow-components-shared-parent/vaadin-flow-components-test-util/src/main/java/com/vaadin/tests/AbstractValidationTest.java
+++ vaadin-flow-components-shared-parent/vaadin-flow-components-test-util/src/main/java/com/vaadin/tests/AbstractValidationTest.java
@@ -1,14 +1,7 @@
 package com.vaadin.tests;
 
-import org.junit.BeforeClass;
-
 public abstract class AbstractValidationTest
         extends com.vaadin.flow.testutil.AbstractValidationTest {
-
-    @BeforeClass
-    public static void setupClass() {
-        ParallelTest.setupClass();
-    }
 
     protected int getDeploymentPort() {
         return 8080;
