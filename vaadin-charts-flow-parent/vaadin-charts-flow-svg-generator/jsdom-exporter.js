76e061495103d5c8423d754eac8c040e9851fbb8
Zhe Sun <zhe@vaadin.com>
2023-03-20 14:32:16 +0200

    chore: update header in code files

--- vaadin-charts-flow-parent/vaadin-charts-flow-svg-generator/jsdom-exporter.js
+++ vaadin-charts-flow-parent/vaadin-charts-flow-svg-generator/jsdom-exporter.js
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2000-2023 Vaadin Ltd
+ *
+ * This program is available under Vaadin Commercial License and Service Terms.
+ *
+ * See <https://vaadin.com/commercial-license-and-service-terms> for the full
+ * license.
+ */
 /**
  * Sample of serverside generation of Highcharts using an extension to jsdom in
  * node.js.
--- vaadin-grid-flow-parent/vaadin-grid-flow-integration-tests/benchmark/run-benchmark.js
+++ vaadin-grid-flow-parent/vaadin-grid-flow-integration-tests/benchmark/run-benchmark.js
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2000-2023 Vaadin Ltd
+ *
+ * This program is available under Vaadin Commercial License and Service Terms.
+ *
+ * See <https://vaadin.com/commercial-license-and-service-terms> for the full
+ * license.
+ */
 const { spawn, execSync } = require('child_process');
 const fs = require('fs');
 const path = require('path');
