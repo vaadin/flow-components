537de28fe7e1bc1b43646a6ab0b6ab0359b85f7c
Sergey Vinogradov <mr.vursen@gmail.com>
2023-03-27 17:25:46 +0300

    test: force legacy Chrome headless mode

--- vaadin-flow-components-shared-parent/vaadin-flow-components-test-util/src/main/java/com/vaadin/tests/AbstractComponentIT.java
+++ vaadin-flow-components-shared-parent/vaadin-flow-components-test-util/src/main/java/com/vaadin/tests/AbstractComponentIT.java
@@ -1,10 +1,22 @@
 package com.vaadin.tests;
 
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public abstract class AbstractComponentIT
         extends com.vaadin.flow.testutil.AbstractComponentIT {
 
     protected int getDeploymentPort() {
         return 8080;
+    }
+
+    @Override
+    protected void updateHeadlessChromeOptions(ChromeOptions options) {
+        // Force legacy Chrome headless mode for the time being,
+        // as `--headless=new` has an issue that doesn't allow
+        // tests to adjust the browser window size with
+        // `getDriver().manage().window().setSize(...)`.
+        // See more https://github.com/SeleniumHQ/selenium/issues/11706
+        options.addArguments("--headless");
     }
 
 }
