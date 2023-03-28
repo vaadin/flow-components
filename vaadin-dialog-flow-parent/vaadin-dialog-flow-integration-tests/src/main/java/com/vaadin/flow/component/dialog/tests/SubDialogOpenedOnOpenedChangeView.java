077e716c16351ac059967b569c192bbb094d15e7
Ugur Saglam <106508695+ugur-vaadin@users.noreply.github.com>
2023-03-28 14:20:41 +0300

    fix: use addOpenedChangeListener instead of subscribing to opened-changed (#4778)
    * fix: ignore parent being set inert while closing a dialog
    
    * refactor: add new line at the end of the view
    
    * refactor: run formatter
    
    * fix: add output span to the page
    
    * refactor: remove unused id assignments from view
    
    * fix: use addOpenedChangeListener instead of subscribing to opened-changed event
    
    * fix: revert addOpenedChangeListener change
    
    * fix: do not set ignore parent inert if close was called on a closed dialog
    
    * test: refactor addDialogCloseActionListener and onCloseConfigured related tests
    
    * fix: revert set ignore parent inert workaround
    
    * fix: use addOpenedChangeListener instead listening to opened-changed
    
    * fix: use addOpenedChangeListener instead listening opened property change
    
    * test: wait until the shortcut event is displayed
    
    * refactor: run formatter
    
    * refactor: revert spacing changes
    
    * refactor: revert close action listener test refactoring changes

---
+++ vaadin-dialog-flow-parent/vaadin-dialog-flow-integration-tests/src/main/java/com/vaadin/flow/component/dialog/tests/SubDialogOpenedOnOpenedChangeView.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2000-2023 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.dialog.tests;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.router.Route;
+
+/**
+ * Page created for testing purposes. Not suitable for demos.
+ *
+ * @author Vaadin Ltd
+ */
+@Route("vaadin-dialog/sub-dialog-opened-on-opened-change")
+public class SubDialogOpenedOnOpenedChangeView extends Div {
+
+    public SubDialogOpenedOnOpenedChangeView() {
+        Span output = new Span();
+        output.setId("output");
+
+        Dialog mainDialog = new Dialog();
+        mainDialog.setHeaderTitle("Main Dialog");
+        mainDialog.addOpenedChangeListener(e -> {
+            if (!e.isOpened()) {
+                Dialog subDialog = new Dialog();
+                subDialog.setHeaderTitle("Sub-Dialog");
+                subDialog.open();
+            }
+        });
+        mainDialog.addDetachListener(ev -> output.setText("Detached"));
+
+        Button closeMainDialogAndOpenSubDialogButton = new Button(
+                "Close main dialog and open sub-dialog",
+                ev -> mainDialog.close());
+        closeMainDialogAndOpenSubDialogButton
+                .setId("close-main-dialog-and-open-sub-dialog");
+        mainDialog.add(closeMainDialogAndOpenSubDialogButton);
+
+        Button openMainDialogButton = new Button("Open main dialog");
+        openMainDialogButton.setId("open-main-dialog");
+        openMainDialogButton.addClickListener(e -> mainDialog.open());
+
+        add(openMainDialogButton, output);
+    }
+}
