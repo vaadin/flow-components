32c8d12a4fa31934733348edd08da0f8408c83eb
Zhe Sun <zhe@vaadin.com>
2023-04-06 15:42:53 +0300

    chore: license further fix

--- vaadin-details-flow-parent/vaadin-details-flow/src/main/java/com/vaadin/flow/component/details/DetailsVariant.java
+++ vaadin-details-flow-parent/vaadin-details-flow/src/main/java/com/vaadin/flow/component/details/DetailsVariant.java
@@ -8,26 +8,6 @@
  */
 package com.vaadin.flow.component.details;
 
-/*
- * #%L
- * Details for Vaadin Flow
- * %%
- * Copyright (C) 2017 - 2019 Vaadin Ltd
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
-
 /**
  * The set of theme variants applicable to the {@code vaadin-details} component.
  */
