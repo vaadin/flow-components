91a7d645b2a4c17df71d1c78d84cc336a4f59e86
Sascha Ißbrücker <sissbruecker@vaadin.com>
2023-04-20 13:51:48 +0200

    add integration test

--- vaadin-grid-flow-parent/vaadin-grid-flow-integration-tests/src/main/java/com/vaadin/flow/component/grid/it/ColumnAutoWidthPage.java
+++ vaadin-grid-flow-parent/vaadin-grid-flow-integration-tests/src/main/java/com/vaadin/flow/component/grid/it/ColumnAutoWidthPage.java
@@ -23,20 +23,15 @@ import com.vaadin.flow.router.Route;
 
 @Route("vaadin-grid/column-auto-width")
 public class ColumnAutoWidthPage extends Div {
-
-    public static final String GRID_ID = "auto-width-grid";
-
     public ColumnAutoWidthPage() {
         Grid<Person> grid = new Grid<>();
-        grid.setId(GRID_ID);
         grid.getStyle().set("--lumo-font-family",
                 "Arial, Helvetica, sans-serif");
         grid.setItems(new Person("Jorma", 2018));
 
-        grid.addComponentColumn(person -> {
-            NativeButton button = new NativeButton("F");
-            return button;
-        }).setAutoWidth(true).setHeader("A").setFlexGrow(0);
+        grid.addComponentColumn(
+                person -> new NativeButton(person.getFirstName()))
+                .setAutoWidth(true).setHeader("A").setFlexGrow(0);
         grid.addColumn(Person::getId).setHeader("B");
         grid.addColumn(
                 person -> "This is some length of text to check auto column width.")
@@ -45,6 +40,13 @@ public class ColumnAutoWidthPage extends Div {
                 .setHeader("Some long text for column header").setFlexGrow(0);
 
         add(grid);
+
+        NativeButton updateItems = new NativeButton("Update items", event -> {
+            grid.setItems(new Person("A longer firstname", 2018));
+            grid.recalculateColumnWidths();
+        });
+        updateItems.setId("update-items");
+        add(updateItems);
     }
 
 }
