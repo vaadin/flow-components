1654094827a7ebb7926b6179a8a4cc99cf5c2d3e
Sascha Ißbrücker <sissbruecker@vaadin.com>
2023-05-10 17:38:05 +0200

    fix: make empty grid clear loading state (CP: 24.0) (#5042)
    * fix: make empty grid clear loading state (#5040)
    
    * test: add test for empty grid stuck in a loading state
    
    * fix: propose an idea how empty grid loading use case could be fixed (still having issues though!, tests are not passing!)
    
    * skip data provider request when size is zero
    
    * mock setRequestedRange in connector tests
    
    * run formatter
    
    * replace IT with connector test
    
    * add comment
    
    ---------
    
    Co-authored-by: Bretislav Wajtr <bretislav@vaadin.com>
    
    * test: add test for empty grid stuck in a loading state
    
    * polish test
    
    * use testbench api
    
    ---------
    
    Co-authored-by: Bretislav Wajtr <bretislav@vaadin.com>

---
+++ vaadin-grid-flow-parent/vaadin-grid-flow-integration-tests/src/test/java/com/vaadin/flow/component/grid/it/GridEmptyIT.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2000-2023 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.grid.it;
+
+import com.vaadin.flow.component.grid.testbench.GridElement;
+import org.junit.Test;
+
+import com.vaadin.flow.component.button.testbench.ButtonElement;
+import com.vaadin.flow.testutil.TestPath;
+import com.vaadin.tests.AbstractComponentIT;
+
+@TestPath("vaadin-grid/grid-empty")
+public class GridEmptyIT extends AbstractComponentIT {
+
+    @Test
+    public void emptyGrid_clearCache_loadingStateCleared() {
+        open();
+
+        // Force data provider request by clearing the grid's cache
+        $(ButtonElement.class).id("clear-cache-button").click();
+
+        GridElement grid = $(GridElement.class).id("empty-grid");
+        waitUntil(driver -> "false".equals(grid.getAttribute("loading")));
+    }
+
+}
