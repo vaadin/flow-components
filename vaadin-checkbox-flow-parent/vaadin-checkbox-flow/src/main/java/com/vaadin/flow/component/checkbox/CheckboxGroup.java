55960f60388851863da208059c7c73f1d9828d4d
Diego Cardoso <diego@vaadin.com>
2023-05-15 18:21:02 +0300

    feat: make CheckBoxGroup implement HasAriaLabel (#5029)
    * feat: make CheckBoxGroup implement HasAriaLabel
    
    * test: add unit tests for HasAriaLabel methods
    
    ---------
    
    Co-authored-by: Serhii Kulykov <iamkulykov@gmail.com>

--- vaadin-checkbox-flow-parent/vaadin-checkbox-flow/src/main/java/com/vaadin/flow/component/checkbox/CheckboxGroup.java
+++ vaadin-checkbox-flow-parent/vaadin-checkbox-flow/src/main/java/com/vaadin/flow/component/checkbox/CheckboxGroup.java
@@ -30,6 +30,7 @@ import com.vaadin.flow.component.AbstractSinglePropertyField;
 import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.ComponentUtil;
+import com.vaadin.flow.component.HasAriaLabel;
 import com.vaadin.flow.component.HasHelper;
 import com.vaadin.flow.component.HasLabel;
 import com.vaadin.flow.component.HasSize;
@@ -89,9 +90,10 @@ import elemental.json.JsonArray;
 @NpmPackage(value = "@vaadin/checkbox-group", version = "24.1.0-alpha10")
 @JsModule("@vaadin/checkbox-group/src/vaadin-checkbox-group.js")
 public class CheckboxGroup<T>
-        extends AbstractSinglePropertyField<CheckboxGroup<T>, Set<T>> implements
-        HasClientValidation, HasDataView<T, Void, CheckboxGroupDataView<T>>,
-        HasHelper, HasItemComponents<T>, HasLabel, HasSize, HasStyle,
+        extends AbstractSinglePropertyField<CheckboxGroup<T>, Set<T>>
+        implements HasAriaLabel, HasClientValidation,
+        HasDataView<T, Void, CheckboxGroupDataView<T>>, HasHelper,
+        HasItemComponents<T>, HasLabel, HasSize, HasStyle,
         HasListDataView<T, CheckboxGroupListDataView<T>>, HasTooltip,
         HasThemeVariant<CheckboxGroupVariant>, HasValidationProperties,
         HasValidator<Set<T>>, MultiSelect<CheckboxGroup<T>, T> {
@@ -495,6 +497,27 @@ public class CheckboxGroup<T>
      */
     public String getLabel() {
         return getElement().getProperty("label");
+    }
+
+    @Override
+    public void setAriaLabel(String ariaLabel) {
+        getElement().setProperty("accessibleName", ariaLabel);
+    }
+
+    @Override
+    public Optional<String> getAriaLabel() {
+        return Optional.ofNullable(getElement().getProperty("accessibleName"));
+    }
+
+    @Override
+    public void setAriaLabelledBy(String labelledBy) {
+        getElement().setProperty("accessibleNameRef", labelledBy);
+    }
+
+    @Override
+    public Optional<String> getAriaLabelledBy() {
+        return Optional
+                .ofNullable(getElement().getProperty("accessibleNameRef"));
     }
 
     /**
