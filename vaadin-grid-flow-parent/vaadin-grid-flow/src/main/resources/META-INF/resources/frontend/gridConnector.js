33fa3addce6df6f36706e6fd4263ad9e3ac2ca70
Diego Cardoso <diego@vaadin.com>
2023-06-05 17:11:14 +0300

    refator: use animationFrame instead of microTask

--- vaadin-grid-flow-parent/vaadin-grid-flow/src/main/resources/META-INF/resources/frontend/gridConnector.js
+++ vaadin-grid-flow-parent/vaadin-grid-flow/src/main/resources/META-INF/resources/frontend/gridConnector.js
@@ -1,5 +1,5 @@
 import { Debouncer } from '@polymer/polymer/lib/utils/debounce.js';
-import { timeOut, animationFrame, microTask } from '@polymer/polymer/lib/utils/async.js';
+import { timeOut, animationFrame } from '@polymer/polymer/lib/utils/async.js';
 import { Grid } from '@vaadin/grid/src/vaadin-grid.js';
 import { ItemCache } from '@vaadin/grid/src/vaadin-grid-data-provider-mixin.js';
 import { isFocusable } from '@vaadin/grid/src/vaadin-grid-active-item-mixin.js';
@@ -893,7 +893,7 @@ import { isFocusable } from '@vaadin/grid/src/vaadin-grid-active-item-mixin.js';
           if (!grid.loading) {
             grid.__confirmParentUpdateDebouncer = Debouncer.debounce(
               grid.__confirmParentUpdateDebouncer,
-              microTask,
+              animationFrame,
               () => grid.__updateVisibleRows()
             );
           }
