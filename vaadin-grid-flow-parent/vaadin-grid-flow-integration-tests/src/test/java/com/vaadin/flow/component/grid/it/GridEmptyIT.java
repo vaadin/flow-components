073deae2202b2d70e9097f283cd9777a781aafb1
Sergey Vinogradov <mr.vursen@gmail.com>
2023-06-19 15:32:33 +0300

    add integration tests

---
+++ vaadin-grid-flow-parent/vaadin-grid-flow-integration-tests/src/test/java/com/vaadin/flow/component/grid/it/GridEmptyIT.java
@@ -0,0 +1,26 @@
+package com.vaadin.flow.component.grid.it;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.vaadin.flow.component.grid.testbench.GridElement;
+import com.vaadin.flow.testutil.TestPath;
+import com.vaadin.tests.AbstractComponentIT;
+
+@TestPath("vaadin-grid/empty")
+public class GridEmptyIT extends AbstractComponentIT {
+    private GridElement grid;
+
+    @Before
+    public void init() {
+        open();
+        grid = $(GridElement.class).first();
+    }
+
+    @Test
+    public void getFirstAndLastVisibleRowIndex_doesNotThrow() {
+        Assert.assertEquals("Should return -1 when getting the first visible row index", -1, grid.getFirstVisibleRowIndex());
+        Assert.assertEquals("Should return -1 when getting the last visible row index", -1, grid.getLastVisibleRowIndex());
+    }
+}
