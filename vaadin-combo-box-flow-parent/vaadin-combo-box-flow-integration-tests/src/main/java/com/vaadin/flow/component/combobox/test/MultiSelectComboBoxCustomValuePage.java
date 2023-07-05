5dc78448f793b25c6a1b9684865898905d97a818
Sascha Ißbrücker <sissbruecker@vaadin.com>
2023-07-05 10:53:01 +0200

    fix: properly update client side filtering flag (#5217)
    * fix: properly update client side filtering flag
    
    * add integration test
    
    * revert ignored test
    
    * cleanup

---
+++ vaadin-combo-box-flow-parent/vaadin-combo-box-flow-integration-tests/src/main/java/com/vaadin/flow/component/combobox/test/MultiSelectComboBoxCustomValuePage.java
@@ -0,0 +1,29 @@
+package com.vaadin.flow.component.combobox.test;
+
+import com.vaadin.flow.component.combobox.MultiSelectComboBox;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.router.Route;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+@Route("vaadin-multi-select-combo-box/custom-value")
+public class MultiSelectComboBoxCustomValuePage extends Div {
+    public MultiSelectComboBoxCustomValuePage() {
+        MultiSelectComboBox<String> comboBox = new MultiSelectComboBox<>();
+
+        List<String> items = new ArrayList<>(
+                IntStream.range(0, 100).mapToObj(i -> "item " + i).toList());
+        comboBox.setItems(items);
+
+        // Allow custom values, automatically add them to the data provider
+        comboBox.setAllowCustomValue(true);
+        comboBox.addCustomValueSetListener(e -> {
+            items.add(e.getDetail());
+            comboBox.getDataProvider().refreshAll();
+        });
+
+        add(comboBox);
+    }
+}
