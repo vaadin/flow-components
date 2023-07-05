5fd39c66d572701801df490077004876c6d0baf7
Sascha Ißbrücker <sissbruecker@vaadin.com>
2023-07-05 11:32:09 +0200

    replace toList API usage

--- vaadin-combo-box-flow-parent/vaadin-combo-box-flow-integration-tests/src/main/java/com/vaadin/flow/component/combobox/test/MultiSelectComboBoxCustomValuePage.java
+++ vaadin-combo-box-flow-parent/vaadin-combo-box-flow-integration-tests/src/main/java/com/vaadin/flow/component/combobox/test/MultiSelectComboBoxCustomValuePage.java
@@ -6,6 +6,7 @@ import com.vaadin.flow.router.Route;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @Route("vaadin-multi-select-combo-box/custom-value")
@@ -13,8 +14,8 @@ public class MultiSelectComboBoxCustomValuePage extends Div {
     public MultiSelectComboBoxCustomValuePage() {
         MultiSelectComboBox<String> comboBox = new MultiSelectComboBox<>();
 
-        List<String> items = new ArrayList<>(
-                IntStream.range(0, 100).mapToObj(i -> "item " + i).toList());
+        List<String> items = new ArrayList<>(IntStream.range(0, 100)
+                .mapToObj(i -> "item " + (i + 1)).collect(Collectors.toList()));
         comboBox.setItems(items);
 
         // Allow custom values, automatically add them to the data provider
