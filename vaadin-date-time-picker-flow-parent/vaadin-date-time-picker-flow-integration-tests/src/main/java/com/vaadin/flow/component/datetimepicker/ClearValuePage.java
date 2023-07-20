86bc0c7ed2df53affe1a40aa22b9dc5a7807dd1d
Sergey Vinogradov <mr.vursen@gmail.com>
2023-07-20 11:43:45 +0300

    Update vaadin-date-time-picker-flow-parent/vaadin-date-time-picker-flow-integration-tests/src/main/java/com/vaadin/flow/component/datetimepicker/ClearValuePage.java

--- vaadin-date-time-picker-flow-parent/vaadin-date-time-picker-flow-integration-tests/src/main/java/com/vaadin/flow/component/datetimepicker/ClearValuePage.java
+++ vaadin-date-time-picker-flow-parent/vaadin-date-time-picker-flow-integration-tests/src/main/java/com/vaadin/flow/component/datetimepicker/ClearValuePage.java
@@ -9,12 +9,12 @@ public class ClearValuePage extends Div {
     public static final String CLEAR_BUTTON = "clear-button";
 
     public ClearValuePage() {
-        DateTimePicker datePicker = new DateTimePicker();
+        DateTimePicker dateTimePicker = new DateTimePicker();
 
         NativeButton clearButton = new NativeButton("Clear value");
         clearButton.setId(CLEAR_BUTTON);
-        clearButton.addClickListener(event -> datePicker.clear());
+        clearButton.addClickListener(event -> dateTimePicker.clear());
 
-        add(datePicker, clearButton);
+        add(dateTimePicker, clearButton);
     }
 }
