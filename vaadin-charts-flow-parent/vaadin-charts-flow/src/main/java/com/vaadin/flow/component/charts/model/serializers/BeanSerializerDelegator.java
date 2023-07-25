af3cb628819dc2efc4f9d978df3a8dcc99534018
Zhe Sun <zhe@vaadin.com>
2023-07-25 16:01:35 +0300

    chore: fix format

--- vaadin-charts-flow-parent/vaadin-charts-flow/src/main/java/com/vaadin/flow/component/charts/model/serializers/BeanSerializerDelegator.java
+++ vaadin-charts-flow-parent/vaadin-charts-flow/src/main/java/com/vaadin/flow/component/charts/model/serializers/BeanSerializerDelegator.java
@@ -76,7 +76,8 @@ public class BeanSerializerDelegator<T> extends BeanSerializerBase {
     }
 
     @Override
-    protected BeanSerializerBase withByNameInclusion(Set<String> set, Set<String> set1) {
+    protected BeanSerializerBase withByNameInclusion(Set<String> set,
+            Set<String> set1) {
         return null;
     }
 
@@ -86,7 +87,9 @@ public class BeanSerializerDelegator<T> extends BeanSerializerBase {
     }
 
     @Override
-    protected BeanSerializerBase withProperties(BeanPropertyWriter[] beanPropertyWriters, BeanPropertyWriter[] beanPropertyWriters1) {
+    protected BeanSerializerBase withProperties(
+            BeanPropertyWriter[] beanPropertyWriters,
+            BeanPropertyWriter[] beanPropertyWriters1) {
         return null;
     }
 
