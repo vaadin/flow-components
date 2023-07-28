698923674fecd9f7498d3321832d90cda28cec8e
Sergey Vinogradov <mr.vursen@gmail.com>
2023-07-28 12:09:11 +0300

    Update vaadin-select-flow-parent/vaadin-select-flow-integration-tests/src/test/java/com/vaadin/flow/component/select/test/validation/BasicValidationIT.java

--- vaadin-select-flow-parent/vaadin-select-flow-integration-tests/src/test/java/com/vaadin/flow/component/select/test/validation/BasicValidationIT.java
+++ vaadin-select-flow-parent/vaadin-select-flow-integration-tests/src/test/java/com/vaadin/flow/component/select/test/validation/BasicValidationIT.java
@@ -31,7 +31,6 @@ public class BasicValidationIT extends AbstractValidationIT<SelectElement> {
         testField.sendKeys(Keys.TAB);
         assertClientInvalid();
         assertServerInvalid();
-
     }
 
     @Test
