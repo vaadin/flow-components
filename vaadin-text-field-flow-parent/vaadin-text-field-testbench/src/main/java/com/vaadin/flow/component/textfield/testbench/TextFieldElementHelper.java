9914767d52476caf82e998e9ff4807c81cb62d92
mukherjeesudebi <sudebi@vaadin.com>
2023-08-01 15:06:04 +0300

    formatting

--- vaadin-text-field-flow-parent/vaadin-text-field-testbench/src/main/java/com/vaadin/flow/component/textfield/testbench/TextFieldElementHelper.java
+++ vaadin-text-field-flow-parent/vaadin-text-field-testbench/src/main/java/com/vaadin/flow/component/textfield/testbench/TextFieldElementHelper.java
@@ -4,8 +4,14 @@ import com.vaadin.testbench.TestBenchElement;
 
 class TextFieldElementHelper {
     static void setValue(TestBenchElement element, String value) {
-        element.getCommandExecutor().executeScript("arguments[0].inputElement.value = arguments[1]", element, value);
-        element.getCommandExecutor().executeScript("arguments[0].inputElement.dispatchEvent(new CustomEvent('input', { bubbles: true, composed: true}))", element);
-        element.getCommandExecutor().executeScript("arguments[0].inputElement.dispatchEvent(new CustomEvent('change', { bubbles: true }))", element);
+        element.getCommandExecutor().executeScript(
+                "arguments[0].inputElement.value = arguments[1]", element,
+                value);
+        element.getCommandExecutor().executeScript(
+                "arguments[0].inputElement.dispatchEvent(new CustomEvent('input', { bubbles: true, composed: true}))",
+                element);
+        element.getCommandExecutor().executeScript(
+                "arguments[0].inputElement.dispatchEvent(new CustomEvent('change', { bubbles: true }))",
+                element);
     }
 }
