21d0695ecc12b023954034c705302c75bd839769
Sergey Vinogradov <mr.vursen@gmail.com>
2023-08-02 16:39:00 +0300

    enhance test custom field

--- vaadin-custom-field-flow-parent/vaadin-custom-field-flow-integration-tests/src/main/java/com/vaadin/flow/component/customfield/tests/DirtyStatePage.java
+++ vaadin-custom-field-flow-parent/vaadin-custom-field-flow-integration-tests/src/main/java/com/vaadin/flow/component/customfield/tests/DirtyStatePage.java
@@ -1,25 +1,36 @@
 package com.vaadin.flow.component.customfield.tests;
 
 import com.vaadin.flow.component.customfield.CustomField;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import com.vaadin.tests.dirtystate.AbstractDirtyStatePage;
 
 @Route("vaadin-custom-field/dirty-state")
 public class DirtyStatePage
         extends AbstractDirtyStatePage<CustomField<String>> {
-    private class NameField extends CustomField<String> {
+    private class TestCustomField extends CustomField<String> {
+        private TextField field1 = new TextField();
+        private TextField field2 = new TextField();
+
+        TestCustomField() {
+            add(field1, field2);
+        }
+
         @Override
         protected String generateModelValue() {
-            return "";
+            return field1.getValue() + "," + field2.getValue();
         }
 
         @Override
         protected void setPresentationValue(String newPresentationValue) {
+            String[] parts = newPresentationValue.split(",");
+            field1.setValue(parts[1]);
+            field2.setValue(parts[2]);
         }
     };
 
     @Override
     protected CustomField<String> createTestField() {
-        return new NameField();
+        return new TestCustomField();
     }
 }
