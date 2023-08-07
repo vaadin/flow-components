d9322cf9e848fa03ed82605e0ed3129e6da26810
Sudebi Mukherjee <122896925+mukherjeesudebi@users.noreply.github.com>
2023-08-07 11:58:47 +0300

    feat: integrate client-side validation into BigDecimalField (#5322)

---
+++ vaadin-text-field-flow-parent/vaadin-text-field-flow-integration-tests/src/main/java/com/vaadin/flow/component/textfield/tests/BigDecimalFieldClearValuePage.java
@@ -0,0 +1,31 @@
+package com.vaadin.flow.component.textfield.tests;
+
+import java.math.BigDecimal;
+
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.NativeButton;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.component.textfield.BigDecimalField;
+
+@Route("vaadin-big-decimal-field/clear-value")
+public class BigDecimalFieldClearValuePage extends Div {
+    public static final String CLEAR_BUTTON = "clear-button";
+    public static final String CLEAR_AND_SET_VALUE_BUTTON = "clear-and-set-value-button";
+
+    public BigDecimalFieldClearValuePage() {
+        BigDecimalField bigDecimalField = new BigDecimalField();
+
+        NativeButton clearButton = new NativeButton("Clear value");
+        clearButton.setId(CLEAR_BUTTON);
+        clearButton.addClickListener(event -> bigDecimalField.clear());
+
+        NativeButton clearAndSetValueButton = new NativeButton(
+                "Clear and set value", event -> {
+                    bigDecimalField.clear();
+                    bigDecimalField.setValue(BigDecimal.valueOf(12.34));
+                });
+        clearAndSetValueButton.setId(CLEAR_AND_SET_VALUE_BUTTON);
+
+        add(bigDecimalField, clearButton, clearAndSetValueButton);
+    }
+}
---
+++ vaadin-text-field-flow-parent/vaadin-text-field-flow-integration-tests/src/test/java/com/vaadin/flow/component/textfield/tests/BigDecimalFieldClearValueIT.java
@@ -0,0 +1,53 @@
+package com.vaadin.flow.component.textfield.tests;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Keys;
+
+import com.vaadin.flow.component.textfield.testbench.BigDecimalFieldElement;
+import com.vaadin.flow.testutil.TestPath;
+import com.vaadin.testbench.TestBenchElement;
+import com.vaadin.tests.AbstractComponentIT;
+
+import static com.vaadin.flow.component.textfield.tests.BigDecimalFieldClearValuePage.CLEAR_BUTTON;
+import static com.vaadin.flow.component.textfield.tests.BigDecimalFieldClearValuePage.CLEAR_AND_SET_VALUE_BUTTON;
+
+@TestPath("vaadin-big-decimal-field/clear-value")
+public class BigDecimalFieldClearValueIT extends AbstractComponentIT {
+    private BigDecimalFieldElement bigDecimalField;
+
+    private TestBenchElement input;
+
+    @Before
+    public void init() {
+        open();
+        bigDecimalField = $(BigDecimalFieldElement.class).first();
+        input = bigDecimalField.$("input").first();
+    }
+
+    @Test
+    public void setInputValue_clearValue_inputValueIsEmpty() {
+        bigDecimalField.sendKeys("1234", Keys.ENTER);
+        Assert.assertEquals("1234", input.getPropertyString("value"));
+
+        $("button").id(CLEAR_BUTTON).click();
+        Assert.assertEquals("", input.getPropertyString("value"));
+    }
+
+    @Test
+    public void badInput_setInputValue_clearValue_inputValueIsEmpty() {
+        bigDecimalField.sendKeys("--2", Keys.ENTER);
+        Assert.assertEquals("--2", input.getPropertyString("value"));
+
+        $("button").id(CLEAR_BUTTON).click();
+        Assert.assertEquals("", input.getPropertyString("value"));
+    }
+
+    @Test
+    public void badInput_setInputValue_clearAndSetValue_inputValueIsPresent() {
+        bigDecimalField.sendKeys("--2", Keys.ENTER);
+        $("button").id(CLEAR_AND_SET_VALUE_BUTTON).click();
+        Assert.assertEquals("12.34", input.getPropertyString("value"));
+    }
+}
