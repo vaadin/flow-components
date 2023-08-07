203f130536b4f85d97a29b97a7ad2fadbf719900
Sergey Vinogradov <mr.vursen@gmail.com>
2023-08-07 15:51:02 +0300

    fix: reject emails with double dot between domain and tld (#5327)
    * fix: Email pattern accepts double dot between domain and tld (vaadin/flow#17333)
    
    * add unit tests
    
    * run formatter
    
    ---------
    
    Co-authored-by: Arnaud Brunet <gronono@users.noreply.github.com>
    Co-authored-by: Sascha Ißbrücker <sissbruecker@vaadin.com>

--- vaadin-text-field-flow-parent/vaadin-text-field-flow/src/test/java/com/vaadin/flow/component/textfield/validation/EmailFieldBasicValidationTest.java
+++ vaadin-text-field-flow-parent/vaadin-text-field-flow/src/test/java/com/vaadin/flow/component/textfield/validation/EmailFieldBasicValidationTest.java
@@ -15,11 +15,45 @@
  */
 package com.vaadin.flow.component.textfield.validation;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.tests.validation.AbstractBasicValidationTest;
 
 public class EmailFieldBasicValidationTest
         extends AbstractBasicValidationTest<EmailField, String> {
+    private static String[] VALID_EMAILS = { "email@example.com",
+            "firstname.lastname@example.com", "email@subdomain.example.com",
+            "firstname+lastname@example.com", "email@123.123.123.123",
+            "1234567890@example.com", "email@example-one.com",
+            "_______@example.com", "email@example.name", "email@example.museum",
+            "email@example.co.jp", "firstname-lastname@example.com", };
+
+    private static String[] INVALID_EMAILS = { "plainaddress",
+            "#@%^%#$@#$@#.com", "@example.com", "Joe Smith <email@example.com>",
+            "email.example.com", "email@example@example.com",
+            "あいうえお@example.com", "email@example.com (Joe Smith)",
+            "email@example..com", "email@example", };
+
+    @Test
+    public void setInvalidEmail_fieldIsInvalid() {
+        for (String email : INVALID_EMAILS) {
+            testField.setValue(email);
+            Assert.assertTrue("Should be invalid when setting " + email,
+                    testField.isInvalid());
+        }
+    }
+
+    @Test
+    public void setValidEmail_fieldIsValid() {
+        for (String email : VALID_EMAILS) {
+            testField.setValue(email);
+            Assert.assertFalse("Should be valid when setting " + email,
+                    testField.isInvalid());
+        }
+    }
+
     protected EmailField createTestField() {
         return new EmailField();
     }
