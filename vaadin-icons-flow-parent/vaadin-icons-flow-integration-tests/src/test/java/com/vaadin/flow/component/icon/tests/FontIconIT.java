5690ee9b9ccb24569456747e16f2848ecc2202ce
Tomi Virkki <virkki@vaadin.com>
2023-08-29 17:50:13 +0300

    test: add an IT

---
+++ vaadin-icons-flow-parent/vaadin-icons-flow-integration-tests/src/test/java/com/vaadin/flow/component/icon/tests/FontIconIT.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2000-2023 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.icon.tests;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import com.vaadin.flow.testutil.TestPath;
+import com.vaadin.tests.AbstractComponentIT;
+
+@TestPath("vaadin-icons/font-icons")
+public class FontIconIT extends AbstractComponentIT {
+
+    @Before
+    public void init() {
+        open();
+    }
+
+    @Test
+    public void fontAwesomeIcon() {
+        var icon = findElement(By.tagName("vaadin-icon"));
+        // Get the pseudo element's width and height separated by a space
+        var beforeBounds = executeScript(
+                """
+                        const { width, height } = window.getComputedStyle(arguments[0], '::before');
+                        return `${width} ${height}`;
+                        """,
+                icon);
+
+        Assert.assertEquals("21px 24px", beforeBounds);
+    }
+}
