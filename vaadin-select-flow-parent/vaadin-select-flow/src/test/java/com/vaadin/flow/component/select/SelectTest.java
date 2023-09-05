c133ebb7923aa9394b9ea86acbfc7daab535091a
sujoykd <106066145+sujoykd@users.noreply.github.com>
2023-09-05 13:24:42 +0300

    fix: return the item position of an item in Select if it exists (#5369) (#5430)

--- vaadin-select-flow-parent/vaadin-select-flow/src/test/java/com/vaadin/flow/component/select/SelectTest.java
+++ vaadin-select-flow-parent/vaadin-select-flow/src/test/java/com/vaadin/flow/component/select/SelectTest.java
@@ -846,6 +846,15 @@ public class SelectTest {
         Assert.assertEquals(1, listenerInvokedCount.get());
     }
 
+    @Test
+    public void getItemPosition_shouldReturnItemIndexIfItemExists() {
+        select.setItems("foo", "bar", "buzz");
+        Assert.assertEquals(0, select.getItemPosition("foo"));
+        Assert.assertEquals(1, select.getItemPosition("bar"));
+        Assert.assertEquals(2, select.getItemPosition("buzz"));
+        Assert.assertEquals(-1, select.getItemPosition("does not exist"));
+    }
+
     private void validateItem(int index, String textContent, String label,
             boolean enabled) {
         Element item = getListBoxChild(index);
