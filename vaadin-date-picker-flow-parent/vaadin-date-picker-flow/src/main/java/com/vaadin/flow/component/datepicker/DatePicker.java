638823a6a08ffab0b754308ffbb862e6f17b5eff
sujoykd <106066145+sujoykd@users.noreply.github.com>
2023-09-05 09:49:09 +0300

    fix: restore incorrectly removed Synchronize for opened-changed (#5413)

--- vaadin-date-picker-flow-parent/vaadin-date-picker-flow/src/main/java/com/vaadin/flow/component/datepicker/DatePicker.java
+++ vaadin-date-picker-flow-parent/vaadin-date-picker-flow/src/main/java/com/vaadin/flow/component/datepicker/DatePicker.java
@@ -748,9 +748,13 @@ public class DatePicker
 
     /**
      * Gets the states of the drop-down for the datepicker
+     * <p>
+     * This property is synchronized automatically from client side when an
+     * {@code opened-changed} event happens.
      *
      * @return {@code true} if the drop-down is opened, {@code false} otherwise
      */
+    @Synchronize(property = "opened", value = "opened-changed")
     public boolean isOpened() {
         return getElement().getProperty("opened", false);
     }
