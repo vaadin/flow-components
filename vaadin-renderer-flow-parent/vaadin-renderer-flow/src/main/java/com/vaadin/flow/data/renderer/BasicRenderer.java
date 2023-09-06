4019bea31c5d8990b7a3c97877416875cac3cb83
sujoykd <106066145+sujoykd@users.noreply.github.com>
2023-09-06 16:54:18 +0300

    fix: align basic renderers component type with element type (#5438)
    * fix: create component of the same type as the element
    
    Cannot instantiate an instance of the component class.
    Instead create component should use the component type that aligns with the element and type of renderer.
    
    * test: add integration tests
    
    * fix: formatting
    
    * fix: remove unused imports
    
    * fix: test case naming
    
    * fix: test case naming
    
    * fix: test case naming

--- vaadin-renderer-flow-parent/vaadin-renderer-flow/src/main/java/com/vaadin/flow/data/renderer/BasicRenderer.java
+++ vaadin-renderer-flow-parent/vaadin-renderer-flow/src/main/java/com/vaadin/flow/data/renderer/BasicRenderer.java
@@ -17,6 +17,7 @@ package com.vaadin.flow.data.renderer;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.ComponentUtil;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.data.provider.DataKeyMapper;
 import com.vaadin.flow.dom.Element;
 import com.vaadin.flow.dom.ElementFactory;
@@ -98,7 +99,7 @@ public abstract class BasicRenderer<SOURCE, TARGET>
     public Component createComponent(SOURCE item) {
         Element span = ElementFactory
                 .createSpan(getFormattedValue(valueProvider.apply(item)));
-        return ComponentUtil.componentFromElement(span, Component.class, true);
+        return ComponentUtil.componentFromElement(span, Span.class, true);
     }
 
     /**
