850df71084811cc53df005a349ff143f14ada597
web-padawan <iamkulykov@gmail.com>
2023-09-22 12:34:17 +0300

    chore: update web components to 24.2.0-beta1

--- vaadin-charts-flow-parent/vaadin-charts-flow/src/main/java/com/vaadin/flow/component/charts/Chart.java
+++ vaadin-charts-flow-parent/vaadin-charts-flow/src/main/java/com/vaadin/flow/component/charts/Chart.java
@@ -86,9 +86,9 @@ import elemental.json.impl.JreJsonFactory;
  * @author Vaadin Ltd
  */
 @Tag("vaadin-chart")
-@NpmPackage(value = "@vaadin/polymer-legacy-adapter", version = "24.2.0-alpha16")
+@NpmPackage(value = "@vaadin/polymer-legacy-adapter", version = "24.2.0-beta1")
 @JsModule("@vaadin/polymer-legacy-adapter/style-modules.js")
-@NpmPackage(value = "@vaadin/charts", version = "24.2.0-alpha16")
+@NpmPackage(value = "@vaadin/charts", version = "24.2.0-beta1")
 @JsModule("@vaadin/charts/src/vaadin-chart.js")
 public class Chart extends Component implements HasStyle, HasSize, HasTheme {
 
