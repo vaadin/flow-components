2f78468bf362be808b30c5ffcb992caa7800f108
Sergey Vinogradov <mr.vursen@gmail.com>
2023-09-25 17:42:15 +0300

    chore: Update NpmPackages Webjars versions (main) (#5504)
    * chore: Update NpmPackages Webjars versions (main)
    
    * refactor: make Grid connector work with DataProviderController

--- vaadin-side-nav-flow-parent/vaadin-side-nav-flow/src/main/java/com/vaadin/flow/component/sidenav/SideNavItem.java
+++ vaadin-side-nav-flow-parent/vaadin-side-nav-flow/src/main/java/com/vaadin/flow/component/sidenav/SideNavItem.java
@@ -53,7 +53,7 @@ import java.util.stream.Collectors;
  * @author Vaadin Ltd
  */
 @Tag("vaadin-side-nav-item")
-@NpmPackage(value = "@vaadin/side-nav", version = "24.2.0-beta1")
+@NpmPackage(value = "@vaadin/side-nav", version = "24.3.0-alpha1")
 @JsModule("@vaadin/side-nav/src/vaadin-side-nav-item.js")
 public class SideNavItem extends SideNavItemContainer
         implements HasPrefix, HasSuffix {
