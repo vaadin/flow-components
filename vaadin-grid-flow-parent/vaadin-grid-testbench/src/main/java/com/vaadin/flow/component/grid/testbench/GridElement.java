de5c895d8b36077fc84cf441537239444f5d4990
Rodolfo Felipe <31481067+rodolforfq@users.noreply.github.com>
2023-10-12 04:06:48 -0400

    feat: add util methods for rows and cells to GridElement (#5508)

--- vaadin-grid-flow-parent/vaadin-grid-testbench/src/main/java/com/vaadin/flow/component/grid/testbench/GridElement.java
+++ vaadin-grid-flow-parent/vaadin-grid-testbench/src/main/java/com/vaadin/flow/component/grid/testbench/GridElement.java
@@ -15,10 +15,10 @@
  */
 package com.vaadin.flow.component.grid.testbench;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import com.vaadin.flow.component.checkbox.testbench.CheckboxElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
@@ -196,6 +196,44 @@ public class GridElement extends TestBenchElement {
     }
 
     /**
+     * Gets the rows (present in the DOM) specified by the lower and upper row
+     * indexes.
+     *
+     * @param firstRowIndex
+     *            the lower row index to be retrieved (inclusive)
+     * @param lastRowIndex
+     *            the upper row index to be retrieved (inclusive)
+     * @return a {@link GridTRElement} list with the rows contained between the
+     *         given coordinates.
+     * @throws IndexOutOfBoundsException
+     *             if either of the provided row indexes do not exist
+     */
+    public List<GridTRElement> getRows(int firstRowIndex, int lastRowIndex)
+            throws IndexOutOfBoundsException {
+        int rowCount = getRowCount();
+        if (firstRowIndex < 0 || lastRowIndex < 0 || firstRowIndex >= rowCount
+                || lastRowIndex >= rowCount) {
+            throw new IndexOutOfBoundsException(
+                    "firstRowIndex and lastRowIndex: expected to be 0.."
+                            + (rowCount - 1) + " but were " + firstRowIndex
+                            + " and " + lastRowIndex);
+        }
+        String script = "var grid = arguments[0];"
+                + "var firstRowIndex = arguments[1];"
+                + "var lastRowIndex = arguments[2];"
+                + "var rowsInDom = grid._getRenderedRows();"
+                + "return Array.from(rowsInDom).filter((row) => { return row.index >= firstRowIndex && row.index <= lastRowIndex;});";
+        Object rows = executeScript(script, this, firstRowIndex, lastRowIndex);
+        if (rows != null) {
+            return ((ArrayList<?>) rows).stream().map(
+                    elem -> ((TestBenchElement) elem).wrap(GridTRElement.class))
+                    .toList();
+        } else {
+            return new ArrayList<>();
+        }
+    }
+
+    /**
      * Gets the <code>tr</code> element for the given row index.
      *
      * @param rowIndex
@@ -205,18 +243,8 @@ public class GridElement extends TestBenchElement {
      *             if no row with given index exists
      */
     public GridTRElement getRow(int rowIndex) throws IndexOutOfBoundsException {
-        int rowCount = getRowCount();
-        if (rowIndex < 0 || rowIndex >= rowCount) {
-            throw new IndexOutOfBoundsException("rowIndex: expected to be 0.."
-                    + (rowCount - 1) + " but was " + rowIndex);
-        }
-        String script = "var grid = arguments[0];"
-                + "var rowIndex = arguments[1];"
-                + "var rowsInDom = grid.$.items.children;"
-                + "var rowInDom = Array.from(rowsInDom).filter(function(row) { return !row.hidden && row.index == rowIndex;})[0];"
-                + "return rowInDom;";
-        return ((TestBenchElement) executeScript(script, this, rowIndex))
-                .wrap(GridTRElement.class);
+        var rows = getRows(rowIndex, rowIndex);
+        return rows.size() == 1 ? rows.get(0) : null;
     }
 
     /**
@@ -414,6 +442,7 @@ public class GridElement extends TestBenchElement {
      * @return the multi-select column, or null
      */
     private GridColumnElement getMultiSelectColumn() {
+        generatedColumnIdsIfNeeded();
         List<Long> columnIds = (List<Long>) executeScript(
                 "return arguments[0]._getColumns().filter(function(col) { return typeof col.selectAll != 'undefined';}).map(function(column) { return column.__generatedTbId;});",
                 this);
@@ -430,4 +459,44 @@ public class GridElement extends TestBenchElement {
                 .id("selectAllCheckbox");
         selectAllCheckbox.click();
     }
+
+    /**
+     * Gets all the currently visible rows.
+     *
+     * @return a {@link GridTRElement} list representing the currently visible
+     *         rows.
+     */
+    public List<GridTRElement> getVisibleRows() {
+        return getRows(getFirstVisibleRowIndex(), getLastVisibleRowIndex());
+    }
+
+    /**
+     * Gets the grid cells for the given row and column elements.
+     *
+     * @param rowIndex
+     *            the row index
+     * @param columnElements
+     *            the column elements
+     * @return a {@link GridTHTDElement} list with the cells for the given
+     *         coordinates.
+     */
+    public List<GridTHTDElement> getCells(int rowIndex,
+            GridColumnElement... columnElements) {
+        GridTRElement row = getRow(rowIndex);
+        return row != null ? row.getCells(columnElements) : new ArrayList<>();
+    }
+
+    /**
+     * Gets the grid cells for the given row.
+     *
+     * @param rowIndex
+     *            the row index
+     * @return a {@link GridTHTDElement} list with the cells for the given
+     *         coordinates.
+     */
+    public List<GridTHTDElement> getCells(int rowIndex) {
+        return getCells(rowIndex,
+                getAllColumns().toArray(new GridColumnElement[0]));
+    }
+
 }
