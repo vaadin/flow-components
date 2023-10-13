7b0bdab1f9926de4ffdcc2ede58545299e0e7e00
Sergey Vinogradov <mr.vursen@gmail.com>
2023-10-13 13:33:34 +0300

    revert vite.config.ts

--- vaadin-date-picker-flow-parent/vaadin-date-picker-flow-integration-tests/vite.config.ts
+++ vaadin-date-picker-flow-parent/vaadin-date-picker-flow-integration-tests/vite.config.ts
@@ -2,7 +2,7 @@
 import { UserConfigFn } from 'vite';
 // @ts-ignore can not be resolved until Flow generates base Vite config
 import { overrideVaadinConfig } from './vite.generated';
-import { useLocalWebComponents } from '../../shared/web-components-vite-plugin';
+// import { useLocalWebComponents } from '../../shared/web-components-vite-plugin';
 
 const customConfig: UserConfigFn = (env) => ({
   // Here you can add custom Vite parameters
@@ -15,11 +15,15 @@ const customConfig: UserConfigFn = (env) => ({
   },
 
   // Use local version of web-components, disabled by default
-  // To use this un-comment the lines below and change the path to
-  // the absolute path of your web-components repo's node_modules
+  // To use this un-comment the lines below and change the path to 
+  // the absolute path of your web-components repo's node_modules 
   // folder
   // DO NOT COMMIT THESE CHANGES!
-  plugins: [useLocalWebComponents('/Users/vursen/dev/vaadin/web-components/node_modules')]
+  /*
+  plugins: [
+    useLocalWebComponents('/path/to/web-components/node_modules')
+  ]
+   */
 });
 
 export default overrideVaadinConfig(customConfig);
