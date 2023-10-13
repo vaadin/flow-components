f8b7f239f5f6e4ef0013b0330ec8838911541116
Sergey Vinogradov <mr.vursen@gmail.com>
2023-10-13 17:14:24 +0300

    trigger focusout

--- vaadin-text-field-flow-parent/vaadin-text-field-testbench/src/main/java/com/vaadin/flow/component/textfield/testbench/TextFieldElementHelper.java
+++ vaadin-text-field-flow-parent/vaadin-text-field-testbench/src/main/java/com/vaadin/flow/component/textfield/testbench/TextFieldElementHelper.java
@@ -6,12 +6,12 @@ import com.vaadin.testbench.TestBenchElement;
 
 class TextFieldElementHelper {
     static void setValue(TestBenchElement element, String value) {
-        element.focus();
         element.setProperty("value", value);
         element.dispatchEvent("input",
                 Collections.singletonMap("bubbles", true));
         element.dispatchEvent("change",
                 Collections.singletonMap("bubbles", true));
-        element.getCommandExecutor().executeScript("arguments[0].blur()", element);
+        element.dispatchEvent("focusout",
+                Collections.singletonMap("bubbles", true));
     }
 }
