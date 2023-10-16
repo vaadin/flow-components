6602bbd8a394ddebe0ad9df6f6ea3d79b8377148
Sergey Vinogradov <mr.vursen@gmail.com>
2023-10-16 15:30:59 +0300

    add eager validation tests

---
+++ vaadin-text-field-flow-parent/vaadin-text-field-flow-integration-tests/src/test/java/com/vaadin/flow/component/textfield/tests/validation/BigDecimalFieldEagerValidationIT.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2000-2023 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.textfield.tests.validation;
+
+import org.junit.Test;
+import org.openqa.selenium.Keys;
+
+import com.vaadin.flow.component.textfield.testbench.BigDecimalFieldElement;
+import com.vaadin.flow.testutil.TestPath;
+import com.vaadin.tests.validation.AbstractValidationIT;
+
+@TestPath("vaadin-big-decimal-field/validation/eager")
+public class BigDecimalFieldEagerValidationIT
+        extends AbstractValidationIT<BigDecimalFieldElement> {
+    @Test
+    public void enterChars_fieldValidatesOnEveryChar() {
+        // Entered: -
+        testField.sendKeys("-");
+        assertValidationCount(1);
+        assertServerInvalid();
+        assertClientInvalid();
+
+        resetValidationCount();
+
+        // Entered: -2
+        testField.sendKeys("2");
+        assertValidationCount(1);
+        assertServerValid();
+        assertClientValid();
+
+        resetValidationCount();
+
+        // Entered: -
+        testField.sendKeys(Keys.BACK_SPACE);
+        assertValidationCount(1);
+        assertServerInvalid();
+        assertClientInvalid();
+
+        resetValidationCount();
+
+        // Entered:
+        testField.sendKeys(Keys.BACK_SPACE);
+        assertValidationCount(1);
+        assertServerValid();
+        assertClientValid();
+    }
+
+    protected BigDecimalFieldElement getTestField() {
+        return $(BigDecimalFieldElement.class).first();
+    }
+}
