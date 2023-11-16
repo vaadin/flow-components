f9462090c5e25063ceab923e2c42e23a503f1017
Serhii Kulykov <iamkulykov@gmail.com>
2023-11-16 15:50:25 +0200

    chore: update web components to 23.4.0-alpha1 (#5734)

--- vaadin-app-layout-flow-parent/vaadin-app-layout-flow/src/main/java/com/vaadin/flow/component/applayout/DrawerToggle.java
+++ vaadin-app-layout-flow-parent/vaadin-app-layout-flow/src/main/java/com/vaadin/flow/component/applayout/DrawerToggle.java
@@ -35,10 +35,10 @@ import com.vaadin.flow.component.dependency.NpmPackage;
  * </code>
  */
 @Tag("vaadin-drawer-toggle")
-@NpmPackage(value = "@vaadin/polymer-legacy-adapter", version = "23.3.26")
+@NpmPackage(value = "@vaadin/polymer-legacy-adapter", version = "23.4.0-alpha1")
 @JsModule("@vaadin/polymer-legacy-adapter/style-modules.js")
-@NpmPackage(value = "@vaadin/app-layout", version = "23.3.26")
-@NpmPackage(value = "@vaadin/vaadin-app-layout", version = "23.3.26")
+@NpmPackage(value = "@vaadin/app-layout", version = "23.4.0-alpha1")
+@NpmPackage(value = "@vaadin/vaadin-app-layout", version = "23.4.0-alpha1")
 @JsModule("@vaadin/app-layout/src/vaadin-drawer-toggle.js")
 public class DrawerToggle extends Button {
 
