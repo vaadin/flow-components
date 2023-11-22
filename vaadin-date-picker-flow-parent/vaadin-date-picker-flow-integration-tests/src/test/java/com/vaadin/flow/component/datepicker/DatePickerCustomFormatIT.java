a4c54eda6e23c7ff21c8be4bdea000b81684cf1a
ugur-vaadin <ugur@vaadin.com>
2023-11-22 02:41:00 +0200

    chore: run formatter

--- vaadin-date-picker-flow-parent/vaadin-date-picker-flow-integration-tests/src/test/java/com/vaadin/flow/component/datepicker/DatePickerCustomFormatIT.java
+++ vaadin-date-picker-flow-parent/vaadin-date-picker-flow-integration-tests/src/test/java/com/vaadin/flow/component/datepicker/DatePickerCustomFormatIT.java
@@ -277,10 +277,11 @@ public class DatePickerCustomFormatIT extends AbstractComponentIT {
     @Test
     public void renameTest() {
         String id = DatePickerCustomFormatPage.RENAME_DATE_PICKER;
-        TestBenchElement output = $("span").id(
-                DatePickerCustomFormatPage.RENAME_OUTPUT);
+        TestBenchElement output = $("span")
+                .id(DatePickerCustomFormatPage.RENAME_OUTPUT);
 
-        String todayString = LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE);
+        String todayString = LocalDate.now()
+                .format(DateTimeFormatter.ISO_LOCAL_DATE);
         Assert.assertEquals(todayString, output.getText());
 
         $(DatePickerElement.class).id(id).click();
