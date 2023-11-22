4a324e3a537f9807a7af5a4dd170b3f4535c6556
Břetislav Wajtr <bretislav@vaadin.com>
2023-11-22 14:38:39 +0100

    fix: avoid using formatRawCellContents() for number cell formatting because it ignores conditional formatting (#5725)

--- vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow/src/test/java/com/vaadin/flow/component/spreadsheet/tests/CustomDataFormatterTest.java
+++ vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow/src/test/java/com/vaadin/flow/component/spreadsheet/tests/CustomDataFormatterTest.java
@@ -1,19 +1,20 @@
 package com.vaadin.flow.component.spreadsheet.tests;
 
-import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.spreadsheet.Spreadsheet;
+import java.util.Locale;
+
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellStyle;
-import org.apache.poi.ss.usermodel.DataFormat;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Locale;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.spreadsheet.Spreadsheet;
 
 public class CustomDataFormatterTest {
     Spreadsheet spreadsheet;
-    Cell cell;
+    Cell fourPartDataFormatCell;
+    Cell threePartDataFormatCell;
+    Cell generalFormatCell;
 
     @Before
     public void init() {
@@ -22,36 +23,99 @@ public class CustomDataFormatterTest {
         UI.setCurrent(ui);
 
         spreadsheet = new Spreadsheet();
-        cell = spreadsheet.createCell(1, 1, null);
+        this.fourPartDataFormatCell = createFourPartDataFormatCell(spreadsheet);
+        this.threePartDataFormatCell = createThreePartDataFormatCell(
+                spreadsheet);
+        this.generalFormatCell = createGeneralFormatCell(spreadsheet);
+    }
 
+    private Cell createFourPartDataFormatCell(Spreadsheet spreadsheet) {
+        var cell = spreadsheet.createCell(1, 1, null);
         var cellStyle = spreadsheet.getWorkbook().createCellStyle();
         var dataFormat = spreadsheet.getWorkbook().createDataFormat();
         cell.setCellStyle(cellStyle);
         cellStyle.setDataFormat(dataFormat
                 .getFormat("_(* #,##0_);_(* (#,##0);_(* \\\"-\\\"_);_(@_)"));
+        return cell;
+    }
+
+    private Cell createThreePartDataFormatCell(Spreadsheet spreadsheet) {
+        var cell = spreadsheet.createCell(2, 1, null);
+        var cellStyle = spreadsheet.getWorkbook().createCellStyle();
+        var dataFormat = spreadsheet.getWorkbook().createDataFormat();
+        cell.setCellStyle(cellStyle);
+        cellStyle.setDataFormat(
+                dataFormat.getFormat("#,###.#;-#,###.#;\\\"-\\\""));
+        return cell;
+    }
+
+    private Cell createGeneralFormatCell(Spreadsheet spreadsheet) {
+        var cell = spreadsheet.createCell(3, 1, null);
+        var cellStyle = spreadsheet.getWorkbook().createCellStyle();
+        cell.setCellStyle(cellStyle);
+        return cell;
     }
 
     @Test
-    public void cellWithPositiveNumber_getCellValue_formatIsCorrect() {
-        cell.setCellValue(12345);
-        Assert.assertEquals("12,345", spreadsheet.getCellValue(cell));
+    public void fourPartDataFormatCellWithPositiveNumber_getCellValue_formatIsCorrect() {
+        fourPartDataFormatCell.setCellValue(12345);
+        Assert.assertEquals("12,345",
+                spreadsheet.getCellValue(fourPartDataFormatCell));
     }
 
     @Test
-    public void cellWithNegativeNumber_getCellValue_formatIsCorrect() {
-        cell.setCellValue(-12345);
-        Assert.assertEquals("(12,345)", spreadsheet.getCellValue(cell));
+    public void fourPartDataFormatCellWithNegativeNumber_getCellValue_formatIsCorrect() {
+        fourPartDataFormatCell.setCellValue(-12345);
+        Assert.assertEquals("(12,345)",
+                spreadsheet.getCellValue(fourPartDataFormatCell));
     }
 
     @Test
-    public void cellWithNumberZero_getCellValue_formatIsCorrect() {
-        cell.setCellValue(0);
-        Assert.assertEquals("\"-\"", spreadsheet.getCellValue(cell).trim());
+    public void fourPartDataFormatCellWithNumberZero_getCellValue_formatIsCorrect() {
+        fourPartDataFormatCell.setCellValue(0);
+        Assert.assertEquals("\"-\"",
+                spreadsheet.getCellValue(fourPartDataFormatCell).trim());
     }
 
     @Test
-    public void cellTextValue_getCellValue_sameTextIsReturned() {
-        cell.setCellValue("text");
-        Assert.assertEquals("text", spreadsheet.getCellValue(cell).trim());
+    public void fourPartDataFormatCellTextValue_getCellValue_sameTextIsReturned() {
+        fourPartDataFormatCell.setCellValue("text");
+        Assert.assertEquals("text",
+                spreadsheet.getCellValue(fourPartDataFormatCell).trim());
+    }
+
+    @Test
+    public void threePartDataFormatCellWithPositiveNumber_getCellValue_formatIsCorrect() {
+        threePartDataFormatCell.setCellValue(12345.6789);
+        Assert.assertEquals("12,345.7",
+                spreadsheet.getCellValue(threePartDataFormatCell));
+    }
+
+    @Test
+    public void threePartDataFormatCellWithNegativeNumber_getCellValue_formatIsCorrect() {
+        threePartDataFormatCell.setCellValue(-12345.6789);
+        Assert.assertEquals("-12,345.7",
+                spreadsheet.getCellValue(threePartDataFormatCell));
+    }
+
+    @Test
+    public void threePartDataFormatCellWithNumberZero_getCellValue_formatIsCorrect() {
+        threePartDataFormatCell.setCellValue(0);
+        Assert.assertEquals("\"-\"",
+                spreadsheet.getCellValue(threePartDataFormatCell).trim());
+    }
+
+    @Test
+    public void threePartDataFormatCellTextValue_getCellValue_sameTextIsReturned() {
+        threePartDataFormatCell.setCellValue("text");
+        Assert.assertEquals("text",
+                spreadsheet.getCellValue(threePartDataFormatCell).trim());
+    }
+
+    @Test
+    public void generalFormatCellTextValue_getCellValue_textIsReturned() {
+        generalFormatCell.setCellValue("text");
+        Assert.assertEquals("text",
+                spreadsheet.getCellValue(generalFormatCell).trim());
     }
 }
