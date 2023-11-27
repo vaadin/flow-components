e4b9f00f76db270efdb8c93837e54515bcac9aa4
Diego Cardoso <diego@vaadin.com>
2023-11-27 13:56:59 -0300

    fix: call updateSelectionModeOnClient on attach (#5768) (CP: 23.4) (#5782)

--- vaadin-grid-flow-parent/vaadin-grid-flow-integration-tests/src/test/java/com/vaadin/flow/component/grid/it/DetachReattachIt.java
+++ vaadin-grid-flow-parent/vaadin-grid-flow-integration-tests/src/test/java/com/vaadin/flow/component/grid/it/DetachReattachIt.java
@@ -125,6 +125,22 @@ public class DetachReattachIt extends AbstractComponentIT {
         checkLogsForErrors();
     }
 
+    @Test
+    public void hideGridAndChangeMode_detachAndReattach_noErrorIsThrown() {
+        open();
+
+        $("button").id("hide-grid-button").click();
+        $("button").id("selection-mode-none-button").click();
+        $("button").id("detach-and-reattach-button").click();
+        $("button").id("show-grid-button").click();
+
+        GridElement grid = $(GridElement.class).first();
+        // Click on the first cell on the first row
+        grid.getCell(0, 0).click();
+
+        checkLogsForErrors();
+    }
+
     private WebElement getRow(GridElement grid, int row) {
         return grid.$("*").id("items").findElements(By.cssSelector("tr"))
                 .get(row);
