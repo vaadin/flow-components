62a6e46e8ef20255a252a47cfc7f4a920d98c4ac
Bretislav Wajtr <bretislav@vaadin.com>
2024-01-01 09:57:14 +0100

    fix: process the last registered grid edit request

---
+++ vaadin-grid-flow-parent/vaadin-grid-flow-integration-tests/src/main/java/com/vaadin/flow/component/grid/it/GridEditorPage.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2000-2023 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.grid.it;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.editor.Editor;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.NativeButton;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.bean.Person;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.provider.ListDataProvider;
+import com.vaadin.flow.router.Route;
+
+@Route("vaadin-grid/editor")
+public class GridEditorPage extends Div {
+
+    public GridEditorPage() {
+        Grid<Person> grid = new Grid<>();
+
+        final List<Person> items = createGridData();
+
+        ListDataProvider<Person> dataProvider = new ListDataProvider<>(items);
+        grid.setItems(dataProvider);
+
+        Grid.Column<Person> nameColumn = grid.addColumn(Person::getFirstName)
+                .setHeader("Name");
+
+        Binder<Person> binder = new Binder<>(Person.class);
+        Editor<Person> editor = grid.getEditor();
+        editor.setBinder(binder);
+
+        TextField field = new TextField();
+        binder.bind(field, "firstName");
+        nameColumn.setEditorComponent(field);
+        editor.addOpenListener(event -> field.focus());
+        grid.addItemDoubleClickListener(
+                event -> grid.getEditor().editItem(event.getItem()));
+
+        NativeButton subsequentEditRequests = new NativeButton("Subsequent edits",
+                event -> {
+                    editor.editItem(items.get(0));
+                    editor.editItem(items.get(1));
+                });
+        subsequentEditRequests.setId("subsequent-edit-requests");
+
+        add(grid, subsequentEditRequests);
+    }
+
+    private List<Person> createGridData() {
+        List<Person> items = new ArrayList<>();
+        items.add(new Person("foo", 10));
+        items.add(new Person("bar", 11));
+        items.add(new Person("yxz", 12));
+        return items;
+    }
+}
