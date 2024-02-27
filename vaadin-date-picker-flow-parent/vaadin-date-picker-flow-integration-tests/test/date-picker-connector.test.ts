91837af3dbec0a988b647804d0fa76cd7e1be57c
web-padawan <iamkulykov@gmail.com>
2024-02-27 11:18:31 +0200

    test: refactor to use date-fns format explicitly

--- vaadin-date-picker-flow-parent/vaadin-date-picker-flow-integration-tests/test/date-picker-connector.test.ts
+++ vaadin-date-picker-flow-parent/vaadin-date-picker-flow-integration-tests/test/date-picker-connector.test.ts
@@ -1,4 +1,5 @@
 import { expect, fixtureSync } from '@open-wc/testing';
+import dateFnsFormat from 'date-fns/format';
 import { init, extractDateParts, datepickerConnector, type FlowDatePicker } from './shared.js';
 
 describe('date-picker connector', () => {
@@ -18,41 +19,37 @@ describe('date-picker connector', () => {
   const DATE = new Date();
   const DATE_OBJ = extractDateParts(DATE);
 
-  const D = DATE_OBJ.day;
-  const DD = `${D}`.length == 1 ? `0${D}` : `${D}`;
-
-  const M = DATE_OBJ.month + 1;
-  const MM = `${M}`.length == 1 ? `0${M}` : `${M}`;
-
-  const YYYY = DATE_OBJ.year;
-  const YY = `${YYYY}`.slice(2);
-
   [
     // Day, month, year
-    ['dd.MM.yyyy', `${DD}.${MM}.${YYYY}`],
-    ['ddMMyyyy', `${DD}${MM}${YYYY}`],
-    ['yyyy-MM-dd', `${YYYY}-${MM}-${DD}`],
-    ['MM/dd/yyyy', `${MM}/${DD}/${YYYY}`],
-    ['ddMMyy', `${DD}${MM}${YY}`],
+    'dd.MM.yyyy',
+    'ddMMyyyy',
+    'yyyy-MM-dd',
+    'MM/dd/yyyy',
+    'ddMMyy',
     // Day and month only
-    ['dd.MM', `${DD}.${MM}`],
-    ['ddMM', `${DD}${MM}`],
-    ['dd-MM', `${DD}-${MM}`],
-    ['MM/dd', `${MM}/${DD}`],
-    ['M/dd', `${M}/${DD}`],
+    'dd.MM',
+    'ddMM',
+    'dd-MM',
+    'MM/dd',
+    'M/dd',
     // Day only
-    ['dd', `${DD}`]
-  ].forEach(([format, date]) => {
-    it(`should format date using ${format} format`, () => {
-      datePicker.$connector.updateI18n('en-US', { dateFormats: [format] });
+    'dd'
+  ].forEach((format) => {
+    describe(`${format} format`, () => {
+      let date;
 
-      expect(datePicker.i18n.formatDate(DATE_OBJ)).to.equal(date);
-    });
+      beforeEach(() => {
+        date = dateFnsFormat(DATE, format);
+        datePicker.$connector.updateI18n('en-US', { dateFormats: [format] });
+      });
 
-    it(`should parse date using ${format} format`, () => {
-      datePicker.$connector.updateI18n('en-US', { dateFormats: [format] });
+      it(`should format date using ${format} format`, () => {
+        expect(datePicker.i18n.formatDate(DATE_OBJ)).to.equal(date);
+      });
 
-      expect(datePicker.i18n.parseDate(date)).to.eql(DATE_OBJ);
+      it(`should parse date using ${format} format`, () => {
+        expect(datePicker.i18n.parseDate(date)).to.eql(DATE_OBJ);
+      });
     });
   });
 });
