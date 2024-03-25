cf82e707bc72e32505edb6b4510315f52cab850a
Yuriy Yevstihnyeyev <yuriy@vaadin.com>
2024-03-25 09:31:32 +0200

    Update license headers in files

--- vaadin-charts-flow-parent/vaadin-charts-flow/src/main/java/com/vaadin/flow/component/charts/model/style/PlotOptionsStyle.java
+++ vaadin-charts-flow-parent/vaadin-charts-flow/src/main/java/com/vaadin/flow/component/charts/model/style/PlotOptionsStyle.java
@@ -1,14 +1,12 @@
-package com.vaadin.flow.component.charts.model.style;
-
 /**
- * Copyright (C) 2000-2022 Vaadin Ltd
+ * Copyright (C) 2000-2024 Vaadin Ltd
  *
  * This program is available under Vaadin Commercial License and Service Terms.
- *
  *
  * See <https://vaadin.com/commercial-license-and-service-terms> for the full
  * license.
  */
+package com.vaadin.flow.component.charts.model.style;
 import com.vaadin.flow.component.charts.model.AbstractConfigurationObject;
 import com.vaadin.flow.component.charts.model.ChartType;
 import com.vaadin.flow.component.charts.model.PlotOptionsArea;
