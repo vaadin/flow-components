545b6f8ff9d9ea45673456f7d026251be641ef34
Sergey Vinogradov <mr.vursen@gmail.com>
2024-05-15 09:28:58 +0300

    remove unused import

--- vaadin-list-box-flow-parent/vaadin-list-box-testbench/src/main/java/com/vaadin/flow/component/listbox/testbench/ListBoxElement.java
+++ vaadin-list-box-flow-parent/vaadin-list-box-testbench/src/main/java/com/vaadin/flow/component/listbox/testbench/ListBoxElement.java
@@ -15,8 +15,6 @@
  */
 package com.vaadin.flow.component.listbox.testbench;
 
-import com.vaadin.testbench.HasLabel;
-
 import com.vaadin.testbench.HasSelectByText;
 import com.vaadin.testbench.TestBenchElement;
 import com.vaadin.testbench.elementsbase.Element;
