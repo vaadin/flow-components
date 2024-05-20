48768194ae52a471728a7ccdfdabd5e3aaa57999
Sergey Vinogradov <mr.vursen@gmail.com>
2024-05-20 11:42:04 +0300

    fix: set maxFiles to Infinity when setting a MultiFileReceiver (#6253)

--- vaadin-upload-flow-parent/vaadin-upload-flow/src/test/java/com/vaadin/flow/component/upload/tests/UploadTest.java
+++ vaadin-upload-flow-parent/vaadin-upload-flow/src/test/java/com/vaadin/flow/component/upload/tests/UploadTest.java
@@ -3,6 +3,10 @@ package com.vaadin.flow.component.upload.tests;
 import com.vaadin.flow.component.upload.Upload;
 
 import org.junit.Test;
+import org.junit.Assert;
+
+import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
+import com.vaadin.flow.component.upload.receivers.MultiFileMemoryBuffer;
 
 public class UploadTest {
 
@@ -10,5 +14,19 @@ public class UploadTest {
     public void uploadNewUpload() {
         // Test no NPE due missing UI when setAttribute is called.
         Upload upload = new Upload();
+    }
+
+    @Test
+    public void switchBetweenSingleAndMultiFileReceiver_assertMaxFilesProperty() {
+        Upload upload = new Upload();
+
+        upload.setReceiver(new MemoryBuffer());
+        Assert.assertEquals(1, upload.getElement().getProperty("maxFiles", 0));
+
+        upload.setReceiver(new MultiFileMemoryBuffer());
+        Assert.assertNull(upload.getElement().getProperty("maxFiles"));
+
+        upload.setReceiver(new MemoryBuffer());
+        Assert.assertEquals(1, upload.getElement().getProperty("maxFiles", 0));
     }
 }
