be7cae13c36717241a070fc39c3dfe2f6ec41cb7
Ugur Saglam <106508695+ugur-vaadin@users.noreply.github.com>
2024-08-15 15:14:59 +0300

    feat: add dashboard packages (#6548)
    * feat: add dashboard packages
    
    * chore: use correct license
    
    * refactor: comment out npm package temporarily
    
    * refactor: add polymer legacy adapter
    
    * chore: remove incorrect npm package
    
    * fix: correct the inverted module names
    
    * chore: add license to pom
    
    * fix: add module to root level pom
    
    * chore: run formatter
    
    * feat: add packages for testbench and integration tests
    
    * fix: update polymer legacy adapter version
    
    * chore: remove unused dependency

---
+++ vaadin-dashboard-flow-parent/vaadin-dashboard-testbench/src/main/java/com/vaadin/flow/component/dashboard/testbench/DashboardElement.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright 2000-2024 Vaadin Ltd.
+ *
+ * This program is available under Vaadin Commercial License and Service Terms.
+ *
+ * See {@literal <https://vaadin.com/commercial-license-and-service-terms>} for the full
+ * license.
+ */
+package com.vaadin.flow.component.dashboard.testbench;
+
+import com.vaadin.testbench.TestBenchElement;
+import com.vaadin.testbench.elementsbase.Element;
+
+/**
+ * @author Vaadin Ltd
+ */
+@Element("vaadin-dashboard")
+public class DashboardElement extends TestBenchElement {
+}
