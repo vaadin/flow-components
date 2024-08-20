c9706f48fdd7ff9249574b417c2d97b36c3be5ae
ugur-vaadin <ugur@vaadin.com>
2024-08-20 19:30:12 +0300

    chore: run formatter

--- vaadin-dashboard-flow-parent/vaadin-dashboard-flow-integration-tests/src/test/java/com/vaadin/flow/component/dashboard/tests/DashboardWidgetIT.java
+++ vaadin-dashboard-flow-parent/vaadin-dashboard-flow-integration-tests/src/test/java/com/vaadin/flow/component/dashboard/tests/DashboardWidgetIT.java
@@ -8,12 +8,13 @@
  */
 package com.vaadin.flow.component.dashboard.tests;
 
-import com.vaadin.flow.component.dashboard.testbench.DashboardWidgetElement;
-import com.vaadin.flow.testutil.TestPath;
-import com.vaadin.tests.AbstractComponentIT;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import com.vaadin.flow.component.dashboard.testbench.DashboardWidgetElement;
+import com.vaadin.flow.testutil.TestPath;
+import com.vaadin.tests.AbstractComponentIT;
 
 /**
  * @author Vaadin Ltd
