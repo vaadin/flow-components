7eb91e1874c99f8e978a47e1e39c7903e134e3e5
Sascha Ißbrücker <sissbruecker@vaadin.com>
2024-08-23 12:01:52 +0200

    fix: make radio button group with size request serializable

--- vaadin-radio-button-flow-parent/vaadin-radio-button-flow/src/test/java/com/vaadin/flow/component/radiobutton/tests/RadioButtonSerializableTest.java
+++ vaadin-radio-button-flow-parent/vaadin-radio-button-flow/src/test/java/com/vaadin/flow/component/radiobutton/tests/RadioButtonSerializableTest.java
@@ -15,7 +15,23 @@
  */
 package com.vaadin.flow.component.radiobutton.tests;
 
+import org.junit.Test;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
 import com.vaadin.flow.testutil.ClassesSerializableTest;
 
 public class RadioButtonSerializableTest extends ClassesSerializableTest {
+    @Test
+    public void setItems_addToUI_radioButtonGroupIsSerializable()
+            throws Throwable {
+        var group = new RadioButtonGroup<>();
+        group.setItems("Item 1", "Item 2");
+
+        var ui = new UI();
+        UI.setCurrent(ui);
+        ui.add(group);
+
+        serializeAndDeserialize(ui);
+    }
 }
