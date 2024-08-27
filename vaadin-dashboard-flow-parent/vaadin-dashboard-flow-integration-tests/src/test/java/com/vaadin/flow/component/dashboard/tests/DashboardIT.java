ece91bc61fac36fa12425e421f5931ed92e66baf
Ugur Saglam <106508695+ugur-vaadin@users.noreply.github.com>
2024-08-27 17:05:13 +0300

    feat: introduce add and remove widget functionality to dashboard (#6572)
    * feat: introduce add and remove widget functionality to dashboard
    
    * fix: add missing lines and run formatter
    
    * fix: fix compilation error
    
    * chore: run formatter
    
    * test: remove first and last widgets and use clickelementswithjs
    
    * refactor: make remove from parent use removevirtualchild for widgets
    
    * refactor: use append and remove with virtual children and remove from parent before add
    
    * chore: remove unused code
    
    * refactor: merge attach handlers
    
    * refactor: do not try to remove virtual child on detach
    
    * refactor: use widgets list to update detach listeners
    
    * test: add more unit tests for testing node ids on detach
    
    * refactor: use dashboard remove when removing widget from parent
    
    * test: use clickelementwithjs without locating element
    
    * refactor: remove detach listeners
    
    * test: add tests for moving widgets between parents
    
    * test: add unit tests for moving widgets between components
    
    * test: cleanup dashboard widget related unit tests
    
    * refactor: use add remove directly instead of using virtual children
    
    * refactor: simplify removing all widgets by reusing single remove logic
    
    * refactor: override getchildren

--- vaadin-dashboard-flow-parent/vaadin-dashboard-flow-integration-tests/src/test/java/com/vaadin/flow/component/dashboard/tests/DashboardIT.java
+++ vaadin-dashboard-flow-parent/vaadin-dashboard-flow-integration-tests/src/test/java/com/vaadin/flow/component/dashboard/tests/DashboardIT.java
@@ -8,6 +8,15 @@
  */
 package com.vaadin.flow.component.dashboard.tests;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.vaadin.flow.component.dashboard.testbench.DashboardElement;
+import com.vaadin.flow.component.dashboard.testbench.DashboardWidgetElement;
 import com.vaadin.flow.testutil.TestPath;
 import com.vaadin.tests.AbstractComponentIT;
 
@@ -16,4 +25,43 @@ import com.vaadin.tests.AbstractComponentIT;
  */
 @TestPath("vaadin-dashboard")
 public class DashboardIT extends AbstractComponentIT {
+
+    private DashboardElement dashboardElement;
+
+    @Before
+    public void init() {
+        open();
+        dashboardElement = $(DashboardElement.class).waitForFirst();
+    }
+
+    @Test
+    public void addWidgets_widgetsAreCorrectlyAdded() {
+        assertWidgetsByTitle("Widget 1", "Widget 2", "Widget 3");
+    }
+
+    @Test
+    public void addWidgetsAtIndex1_widgetIsAddedIntoTheCorrectPlace() {
+        clickElementWithJs("add-widget-at-index-1");
+        assertWidgetsByTitle("Widget 1", "Widget at index 1", "Widget 2",
+                "Widget 3");
+    }
+
+    @Test
+    public void removeFirstAndLastWidgets_widgetsAreCorrectlyRemoved() {
+        clickElementWithJs("remove-first-and-last-widgets");
+        assertWidgetsByTitle("Widget 2");
+    }
+
+    @Test
+    public void removeAllWidgets_widgetsAreCorrectlyRemoved() {
+        clickElementWithJs("remove-all-widgets");
+        assertWidgetsByTitle();
+    }
+
+    private void assertWidgetsByTitle(String... expectedWidgetTitles) {
+        List<DashboardWidgetElement> widgets = dashboardElement.getWidgets();
+        List<String> widgetTitles = widgets.stream()
+                .map(DashboardWidgetElement::getTitle).toList();
+        Assert.assertEquals(Arrays.asList(expectedWidgetTitles), widgetTitles);
+    }
 }
--- vaadin-dashboard-flow-parent/vaadin-dashboard-flow/src/test/java/com/vaadin/flow/component/dashboard/tests/DashboardTest.java
+++ vaadin-dashboard-flow-parent/vaadin-dashboard-flow/src/test/java/com/vaadin/flow/component/dashboard/tests/DashboardTest.java
@@ -8,5 +8,238 @@
  */
 package com.vaadin.flow.component.dashboard.tests;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.dashboard.Dashboard;
+import com.vaadin.flow.component.dashboard.DashboardWidget;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.internal.JsonUtils;
+import com.vaadin.flow.server.VaadinSession;
+
+import elemental.json.JsonArray;
+
 public class DashboardTest {
+
+    private final UI ui = new UI();
+    private Dashboard dashboard;
+
+    @Before
+    public void setup() {
+        UI.setCurrent(ui);
+        VaadinSession session = Mockito.mock(VaadinSession.class);
+        Mockito.when(session.hasLock()).thenReturn(true);
+        ui.getInternals().setSession(session);
+        dashboard = new Dashboard();
+        ui.add(dashboard);
+        fakeClientCommunication();
+    }
+
+    @After
+    public void tearDown() {
+        UI.setCurrent(null);
+    }
+
+    @Test
+    public void addWidget_widgetIsAdded() {
+        DashboardWidget widget1 = new DashboardWidget();
+        DashboardWidget widget2 = new DashboardWidget();
+        dashboard.add(widget1, widget2);
+        fakeClientCommunication();
+        assertWidgets(dashboard, widget1, widget2);
+    }
+
+    @Test
+    public void addNullWidget_exceptionIsThrown() {
+        Assert.assertThrows(NullPointerException.class,
+                () -> dashboard.add((DashboardWidget) null));
+    }
+
+    @Test
+    public void addNullWidgetInArray_noWidgetIsAdded() {
+        DashboardWidget widget = new DashboardWidget();
+        try {
+            dashboard.add(widget, null);
+        } catch (NullPointerException e) {
+            // Do nothing
+        }
+        fakeClientCommunication();
+        assertWidgets(dashboard);
+    }
+
+    @Test
+    public void addWidgetAtIndex_widgetIsCorrectlyAdded() {
+        DashboardWidget widget1 = new DashboardWidget();
+        DashboardWidget widget2 = new DashboardWidget();
+        DashboardWidget widget3 = new DashboardWidget();
+        dashboard.add(widget1, widget2);
+        fakeClientCommunication();
+        dashboard.addWidgetAtIndex(1, widget3);
+        fakeClientCommunication();
+        assertWidgets(dashboard, widget1, widget3, widget2);
+    }
+
+    @Test
+    public void addNullWidgetAtIndex_exceptionIsThrown() {
+        Assert.assertThrows(NullPointerException.class,
+                () -> dashboard.addWidgetAtIndex(0, null));
+    }
+
+    @Test
+    public void removeWidget_widgetIsRemoved() {
+        DashboardWidget widget1 = new DashboardWidget();
+        DashboardWidget widget2 = new DashboardWidget();
+        dashboard.add(widget1, widget2);
+        fakeClientCommunication();
+        dashboard.remove(widget1);
+        fakeClientCommunication();
+        assertWidgets(dashboard, widget2);
+    }
+
+    @Test
+    public void removeNullWidget_exceptionIsThrown() {
+        Assert.assertThrows(NullPointerException.class,
+                () -> dashboard.remove((DashboardWidget) null));
+    }
+
+    @Test
+    public void removeAllWidgets_widgetsAreRemoved() {
+        DashboardWidget widget1 = new DashboardWidget();
+        DashboardWidget widget2 = new DashboardWidget();
+        dashboard.add(widget1, widget2);
+        fakeClientCommunication();
+        dashboard.removeAll();
+        fakeClientCommunication();
+        assertWidgets(dashboard);
+    }
+
+    @Test
+    public void removeWidgetFromParent_widgetIsRemoved() {
+        DashboardWidget widget1 = new DashboardWidget();
+        dashboard.add(widget1);
+        fakeClientCommunication();
+        widget1.removeFromParent();
+        fakeClientCommunication();
+        assertWidgets(dashboard);
+    }
+
+    @Test
+    public void addWidget_virtualNodeIdsInSync() {
+        DashboardWidget widget1 = new DashboardWidget();
+        dashboard.add(widget1);
+        fakeClientCommunication();
+        assertWidgets(dashboard, widget1);
+    }
+
+    @Test
+    public void removeWidget_virtualNodeIdsInSync() {
+        DashboardWidget widget1 = new DashboardWidget();
+        dashboard.add(widget1);
+        fakeClientCommunication();
+        widget1.removeFromParent();
+        fakeClientCommunication();
+        assertWidgets(dashboard);
+    }
+
+    @Test
+    public void selfRemoveChild_virtualNodeIdsInSync() {
+        DashboardWidget widget1 = new DashboardWidget();
+        DashboardWidget widget2 = new DashboardWidget();
+        dashboard.add(widget1, widget2);
+        fakeClientCommunication();
+        widget1.removeFromParent();
+        fakeClientCommunication();
+        assertWidgets(dashboard, widget2);
+    }
+
+    @Test
+    public void addSeparately_selfRemoveChild_doesNotThrow() {
+        DashboardWidget widget1 = new DashboardWidget();
+        DashboardWidget widget2 = new DashboardWidget();
+        dashboard.add(widget1);
+        dashboard.add(widget2);
+        fakeClientCommunication();
+        widget1.removeFromParent();
+        fakeClientCommunication();
+        assertWidgets(dashboard, widget2);
+    }
+
+    @Test
+    public void addWidgetFromLayoutToDashboard_widgetIsMoved() {
+        Div parent = new Div();
+        ui.add(parent);
+        DashboardWidget widget = new DashboardWidget();
+        parent.add(widget);
+        fakeClientCommunication();
+        dashboard.add(widget);
+        fakeClientCommunication();
+        Assert.assertTrue(parent.getChildren().noneMatch(widget::equals));
+        assertWidgets(dashboard, widget);
+    }
+
+    @Test
+    public void addWidgetFromDashboardToLayout_widgetIsMoved() {
+        DashboardWidget widget = new DashboardWidget();
+        dashboard.add(widget);
+        fakeClientCommunication();
+        Div parent = new Div();
+        ui.add(parent);
+        parent.add(widget);
+        fakeClientCommunication();
+        assertWidgets(dashboard);
+        Assert.assertTrue(parent.getChildren().anyMatch(widget::equals));
+    }
+
+    @Test
+    public void addWidgetToAnotherDashboard_widgetIsMoved() {
+        DashboardWidget widget = new DashboardWidget();
+        dashboard.add(widget);
+        fakeClientCommunication();
+        Dashboard newDashboard = new Dashboard();
+        ui.add(newDashboard);
+        newDashboard.add(widget);
+        fakeClientCommunication();
+        assertWidgets(dashboard);
+        assertWidgets(newDashboard, widget);
+    }
+
+    private void fakeClientCommunication() {
+        ui.getInternals().getStateTree().runExecutionsBeforeClientResponse();
+        ui.getInternals().getStateTree().collectChanges(ignore -> {
+        });
+    }
+
+    private static void assertWidgets(Dashboard dashboard,
+            DashboardWidget... expectedWidgets) {
+        assertVirtualChildren(dashboard, expectedWidgets);
+        Assert.assertEquals(Arrays.asList(expectedWidgets),
+                dashboard.getWidgets());
+    }
+
+    private static void assertVirtualChildren(Dashboard dashboard,
+            Component... components) {
+        // Get a List of the node ids
+        List<Integer> expectedChildNodeIds = Arrays.stream(components)
+                .map(component -> component.getElement().getNode().getId())
+                .toList();
+        // Get the node ids from the items property of the dashboard
+        List<Integer> actualChildNodeIds = getChildNodeIds(dashboard);
+        Assert.assertEquals(expectedChildNodeIds, actualChildNodeIds);
+    }
+
+    private static List<Integer> getChildNodeIds(Dashboard dashboard) {
+        JsonArray jsonArrayOfIds = (JsonArray) dashboard.getElement()
+                .getPropertyRaw("items");
+        return JsonUtils.objectStream(jsonArrayOfIds)
+                .mapToInt(obj -> (int) obj.getNumber("nodeid")).boxed()
+                .toList();
+    }
 }
