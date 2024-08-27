4b76531f2a1614603d55a96d2462b6f93e7e228b
Ugur Saglam <106508695+ugur-vaadin@users.noreply.github.com>
2024-08-27 17:05:13 +0300

    feat: introduce add and remove widget functionality to dashboard (#6572)
    * feat: introduce add and remove widget functionality to dashboard
    
    * fix: add missing lines and run formatter
    
    * fix: fix compilation error
    
    * chore: run formatter
    
    * test: remove first and last widgets and use clickelementswithjs
    
    * refactor: make remove from parent use removevirtualchild for widgets
    
    * refactor: use append and remove with virtual children and remove from parent before add
    
    * chore: remove unused code
    
    * refactor: merge attach handlers
    
    * refactor: do not try to remove virtual child on detach
    
    * refactor: use widgets list to update detach listeners
    
    * test: add more unit tests for testing node ids on detach
    
    * refactor: use dashboard remove when removing widget from parent
    
    * test: use clickelementwithjs without locating element
    
    * refactor: remove detach listeners
    
    * test: add tests for moving widgets between parents
    
    * test: add unit tests for moving widgets between components
    
    * test: cleanup dashboard widget related unit tests
    
    * refactor: use add remove directly instead of using virtual children
    
    * refactor: simplify removing all widgets by reusing single remove logic
    
    * refactor: override getchildren

--- vaadin-dashboard-flow-parent/vaadin-dashboard-flow-integration-tests/src/main/java/com/vaadin/flow/component/dashboard/tests/DashboardPage.java
+++ vaadin-dashboard-flow-parent/vaadin-dashboard-flow-integration-tests/src/main/java/com/vaadin/flow/component/dashboard/tests/DashboardPage.java
@@ -8,7 +8,12 @@
  */
 package com.vaadin.flow.component.dashboard.tests;
 
+import java.util.List;
+
+import com.vaadin.flow.component.dashboard.Dashboard;
+import com.vaadin.flow.component.dashboard.DashboardWidget;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.NativeButton;
 import com.vaadin.flow.router.Route;
 
 /**
@@ -16,4 +21,55 @@ import com.vaadin.flow.router.Route;
  */
 @Route("vaadin-dashboard")
 public class DashboardPage extends Div {
+
+    public DashboardPage() {
+        DashboardWidget widget1 = new DashboardWidget();
+        widget1.setTitle("Widget 1");
+        widget1.setId("widget-1");
+
+        DashboardWidget widget2 = new DashboardWidget();
+        widget2.setTitle("Widget 2");
+        widget2.setId("widget-2");
+
+        DashboardWidget widget3 = new DashboardWidget();
+        widget3.setTitle("Widget 3");
+        widget3.setId("widget-3");
+
+        Dashboard dashboard = new Dashboard();
+        dashboard.add(widget1, widget2, widget3);
+
+        NativeButton addWidgetAtIndex1 = new NativeButton(
+                "Add widget at index 1");
+        addWidgetAtIndex1.addClickListener(click -> {
+            DashboardWidget widgetAtIndex1 = new DashboardWidget();
+            widgetAtIndex1.setTitle("Widget at index 1");
+            widgetAtIndex1.setId("widget-at-index-1");
+            dashboard.addWidgetAtIndex(1, widgetAtIndex1);
+        });
+        addWidgetAtIndex1.setId("add-widget-at-index-1");
+
+        NativeButton removeFirstAndLastWidgets = new NativeButton(
+                "Remove first and last widgets");
+        removeFirstAndLastWidgets.addClickListener(click -> {
+            List<DashboardWidget> currentWidgets = dashboard.getWidgets();
+            if (currentWidgets.isEmpty()) {
+                return;
+            }
+            int currentWidgetCount = currentWidgets.size();
+            if (currentWidgetCount == 1) {
+                dashboard.remove(dashboard.getWidgets().get(0));
+            } else {
+                dashboard.remove(dashboard.getWidgets().get(0),
+                        dashboard.getWidgets().get(currentWidgetCount - 1));
+            }
+        });
+        removeFirstAndLastWidgets.setId("remove-first-and-last-widgets");
+
+        NativeButton removeAllWidgets = new NativeButton("Remove all widgets");
+        removeAllWidgets.addClickListener(click -> dashboard.removeAll());
+        removeAllWidgets.setId("remove-all-widgets");
+
+        add(addWidgetAtIndex1, removeFirstAndLastWidgets, removeAllWidgets,
+                dashboard);
+    }
 }
