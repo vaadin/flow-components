9c8e6fc1828e547a158674efa45038e450fd86d8
Ugur Saglam <106508695+ugur-vaadin@users.noreply.github.com>
2024-08-27 17:05:13 +0300

    feat: introduce add and remove widget functionality to dashboard (#6572)
    * feat: introduce add and remove widget functionality to dashboard
    
    * fix: add missing lines and run formatter
    
    * fix: fix compilation error
    
    * chore: run formatter
    
    * test: remove first and last widgets and use clickelementswithjs
    
    * refactor: make remove from parent use removevirtualchild for widgets
    
    * refactor: use append and remove with virtual children and remove from parent before add
    
    * chore: remove unused code
    
    * refactor: merge attach handlers
    
    * refactor: do not try to remove virtual child on detach
    
    * refactor: use widgets list to update detach listeners
    
    * test: add more unit tests for testing node ids on detach
    
    * refactor: use dashboard remove when removing widget from parent
    
    * test: use clickelementwithjs without locating element
    
    * refactor: remove detach listeners
    
    * test: add tests for moving widgets between parents
    
    * test: add unit tests for moving widgets between components
    
    * test: cleanup dashboard widget related unit tests
    
    * refactor: use add remove directly instead of using virtual children
    
    * refactor: simplify removing all widgets by reusing single remove logic
    
    * refactor: override getchildren

--- vaadin-dashboard-flow-parent/vaadin-dashboard-flow/src/main/java/com/vaadin/flow/component/dashboard/Dashboard.java
+++ vaadin-dashboard-flow-parent/vaadin-dashboard-flow/src/main/java/com/vaadin/flow/component/dashboard/Dashboard.java
@@ -8,10 +8,30 @@
  */
 package com.vaadin.flow.component.dashboard;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import org.slf4j.LoggerFactory;
+
+import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.dom.Element;
+import com.vaadin.flow.dom.ElementDetachEvent;
+import com.vaadin.flow.dom.ElementDetachListener;
+import com.vaadin.flow.shared.Registration;
+
+import elemental.json.Json;
+import elemental.json.JsonArray;
+import elemental.json.JsonObject;
 
 /**
  * @author Vaadin Ltd
@@ -20,6 +40,210 @@ import com.vaadin.flow.component.dependency.NpmPackage;
 @NpmPackage(value = "@vaadin/polymer-legacy-adapter", version = "24.5.0-alpha8")
 @JsModule("@vaadin/polymer-legacy-adapter/style-modules.js")
 @JsModule("@vaadin/dashboard/src/vaadin-dashboard.js")
+@JsModule("./flow-component-renderer.js")
 // @NpmPackage(value = "@vaadin/dashboard", version = "24.6.0-alpha0")
 public class Dashboard extends Component {
+
+    private final List<DashboardWidget> widgets = new ArrayList<>();
+
+    private boolean pendingUpdate = false;
+
+    /**
+     * Creates an empty dashboard.
+     */
+    public Dashboard() {
+    }
+
+    /**
+     * Returns the widgets in the dashboard.
+     *
+     * @return The widgets in the dashboard
+     */
+    public List<DashboardWidget> getWidgets() {
+        return Collections.unmodifiableList(widgets);
+    }
+
+    /**
+     * Adds the given widgets to the dashboard.
+     *
+     * @param widgets
+     *            the widgets to add, not {@code null}
+     */
+    public void add(DashboardWidget... widgets) {
+        Objects.requireNonNull(widgets, "Widgets to add cannot be null.");
+        List<DashboardWidget> toAdd = new ArrayList<>(widgets.length);
+        for (DashboardWidget widget : widgets) {
+            Objects.requireNonNull(widget, "Widget to add cannot be null.");
+            toAdd.add(widget);
+        }
+        toAdd.forEach(this::doAddWidget);
+        updateClient();
+    }
+
+    /**
+     * Adds the given widget as child of this dashboard at the specific index.
+     * <p>
+     * In case the specified widget has already been added to another parent, it
+     * will be removed from there and added to this one.
+     *
+     * @param index
+     *            the index, where the widget will be added. The index must be
+     *            non-negative and may not exceed the children count
+     * @param widget
+     *            the widget to add, not {@code null}
+     */
+    public void addWidgetAtIndex(int index, DashboardWidget widget) {
+        Objects.requireNonNull(widget, "Widget to add cannot be null.");
+        if (index < 0) {
+            throw new IllegalArgumentException(
+                    "Cannot add a widget with a negative index.");
+        }
+        // The case when the index is bigger than the children count is handled
+        // inside the method below
+        doAddWidget(index, widget);
+        updateClient();
+    }
+
+    /**
+     * Removes the given widgets from this dashboard.
+     *
+     * @param widgets
+     *            the widgets to remove, not {@code null}
+     * @throws IllegalArgumentException
+     *             if there is a widget whose non {@code null} parent is not
+     *             this dashboard
+     */
+    public void remove(DashboardWidget... widgets) {
+        Objects.requireNonNull(widgets, "Widgets to remove cannot be null.");
+        List<DashboardWidget> toRemove = new ArrayList<>(widgets.length);
+        for (DashboardWidget widget : widgets) {
+            Objects.requireNonNull(widget, "Widget to remove cannot be null.");
+            Element parent = widget.getElement().getParent();
+            if (parent == null) {
+                LoggerFactory.getLogger(getClass()).debug(
+                        "Removal of a widget with no parent does nothing.");
+                continue;
+            }
+            if (getElement().equals(parent)) {
+                toRemove.add(widget);
+            } else {
+                throw new IllegalArgumentException("The given widget (" + widget
+                        + ") is not a child of this dashboard");
+            }
+        }
+        toRemove.forEach(this::doRemoveWidget);
+        updateClient();
+    }
+
+    /**
+     * Removes all widgets from this dashboard.
+     */
+    public void removeAll() {
+        doRemoveAllWidgets();
+        updateClient();
+    }
+
+    @Override
+    public Stream<Component> getChildren() {
+        return getWidgets().stream().map(Component.class::cast);
+    }
+
+    @Override
+    protected void onAttach(AttachEvent attachEvent) {
+        super.onAttach(attachEvent);
+        attachRenderer();
+        doUpdateClient();
+    }
+
+    private final Map<Element, Registration> childDetachListenerMap = new HashMap<>();
+
+    // Must not use lambda here as that would break serialization. See
+    // https://github.com/vaadin/flow-components/issues/5597
+    private final ElementDetachListener childDetachListener = new ElementDetachListener() {
+        @Override
+        public void onDetach(ElementDetachEvent e) {
+            var detachedElement = e.getSource();
+            getWidgets().stream()
+                    .filter(widget -> Objects.equals(detachedElement,
+                            widget.getElement()))
+                    .findAny().ifPresent(detachedWidget -> {
+                        // The child was removed from the dashboard
+
+                        // Remove the registration for the child detach listener
+                        childDetachListenerMap.get(detachedWidget.getElement())
+                                .remove();
+                        childDetachListenerMap
+                                .remove(detachedWidget.getElement());
+
+                        widgets.remove(detachedWidget);
+                        updateClient();
+                    });
+        }
+    };
+
+    private void updateClient() {
+        if (pendingUpdate) {
+            return;
+        }
+        pendingUpdate = true;
+        getElement().getNode()
+                .runWhenAttached(ui -> ui.beforeClientResponse(this, ctx -> {
+                    doUpdateClient();
+                    pendingUpdate = false;
+                }));
+    }
+
+    private void doUpdateClient() {
+        widgets.forEach(widget -> {
+            Element childWidgetElement = widget.getElement();
+            if (!childDetachListenerMap.containsKey(childWidgetElement)) {
+                childDetachListenerMap.put(childWidgetElement,
+                        childWidgetElement
+                                .addDetachListener(childDetachListener));
+            }
+        });
+        getElement().setPropertyJson("items", createItemsJsonArray());
+    }
+
+    private void attachRenderer() {
+        getElement().executeJs(
+                "Vaadin.FlowComponentHost.patchVirtualContainer(this);");
+        String appId = UI.getCurrent().getInternals().getAppId();
+        getElement().executeJs(
+                "this.renderer = (root, _, model) => Vaadin.FlowComponentHost.setChildNodes($0, [model.item.nodeid], root);",
+                appId);
+    }
+
+    private JsonArray createItemsJsonArray() {
+        JsonArray jsonItems = Json.createArray();
+        for (DashboardWidget widget : widgets) {
+            JsonObject jsonItem = Json.createObject();
+            jsonItem.put("nodeid", getWidgetNodeId(widget));
+            jsonItems.set(jsonItems.length(), jsonItem);
+        }
+        return jsonItems;
+    }
+
+    private int getWidgetNodeId(DashboardWidget widget) {
+        return widget.getElement().getNode().getId();
+    }
+
+    private void doRemoveAllWidgets() {
+        new ArrayList<>(widgets).forEach(this::doRemoveWidget);
+    }
+
+    private void doRemoveWidget(DashboardWidget widget) {
+        getElement().removeChild(widget.getElement());
+        widgets.remove(widget);
+    }
+
+    private void doAddWidget(int index, DashboardWidget widget) {
+        getElement().appendChild(widget.getElement());
+        widgets.add(index, widget);
+    }
+
+    private void doAddWidget(DashboardWidget widget) {
+        getElement().appendChild(widget.getElement());
+        widgets.add(widget);
+    }
 }
--- vaadin-dashboard-flow-parent/vaadin-dashboard-flow/src/test/java/com/vaadin/flow/component/dashboard/tests/DashboardWidgetTest.java
+++ vaadin-dashboard-flow-parent/vaadin-dashboard-flow/src/test/java/com/vaadin/flow/component/dashboard/tests/DashboardWidgetTest.java
@@ -8,5 +8,86 @@
  */
 package com.vaadin.flow.component.dashboard.tests;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.dashboard.DashboardWidget;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.server.VaadinSession;
+
 public class DashboardWidgetTest {
+
+    private final UI ui = new UI();
+
+    @Before
+    public void setup() {
+        UI.setCurrent(ui);
+        VaadinSession session = Mockito.mock(VaadinSession.class);
+        Mockito.when(session.hasLock()).thenReturn(true);
+        ui.getInternals().setSession(session);
+    }
+
+    @After
+    public void tearDown() {
+        UI.setCurrent(null);
+    }
+
+    @Test
+    public void addWidgetToLayout_widgetIsAdded() {
+        Div layout = new Div();
+        ui.add(layout);
+        DashboardWidget widget = new DashboardWidget();
+        layout.add(widget);
+        fakeClientCommunication();
+        Assert.assertTrue(layout.getChildren().anyMatch(widget::equals));
+    }
+
+    @Test
+    public void removeWidgetFromLayout_widgetIsRemoved() {
+        Div layout = new Div();
+        ui.add(layout);
+        DashboardWidget widget = new DashboardWidget();
+        layout.add(widget);
+        fakeClientCommunication();
+        layout.remove(widget);
+        fakeClientCommunication();
+        Assert.assertTrue(layout.getChildren().noneMatch(widget::equals));
+    }
+
+    @Test
+    public void addWidgetToLayout_removeFromParent_widgetIsRemoved() {
+        Div layout = new Div();
+        ui.add(layout);
+        DashboardWidget widget = new DashboardWidget();
+        layout.add(widget);
+        fakeClientCommunication();
+        widget.removeFromParent();
+        fakeClientCommunication();
+        Assert.assertTrue(layout.getChildren().noneMatch(widget::equals));
+    }
+
+    @Test
+    public void addWidgetFromLayoutToAnotherLayout_widgetIsMoved() {
+        Div parent = new Div();
+        ui.add(parent);
+        DashboardWidget widget = new DashboardWidget();
+        parent.add(widget);
+        fakeClientCommunication();
+        Div newParent = new Div();
+        ui.add(newParent);
+        newParent.add(widget);
+        fakeClientCommunication();
+        Assert.assertTrue(parent.getChildren().noneMatch(widget::equals));
+        Assert.assertTrue(newParent.getChildren().anyMatch(widget::equals));
+    }
+
+    private void fakeClientCommunication() {
+        ui.getInternals().getStateTree().runExecutionsBeforeClientResponse();
+        ui.getInternals().getStateTree().collectChanges(ignore -> {
+        });
+    }
 }
