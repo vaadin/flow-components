66643e9873a699e8ae88badfc185231e5068b882
Ugur Saglam <106508695+ugur-vaadin@users.noreply.github.com>
2024-09-12 15:09:21 +0300

    feat: support drag drop for dashboard (#6633)
    * feat: support drag drop for dashboard
    
    * chore: run formatter
    
    * test: fix unit test assertion
    
    * test: update test route names to align with others
    
    * test: add its that test the server client communication
    
    * refactor: simplify js event workaround
    
    * fix: do not remove widgets on detach
    
    * refactor: remove item sanitization on drag reorder
    
    * refactor: check children of element and remove workaround
    
    * refactor: make dashboard not editable by default
    
    * test: use selenium for drag drop and add tests for editable

--- vaadin-dashboard-flow-parent/vaadin-dashboard-flow-integration-tests/src/main/java/com/vaadin/flow/component/dashboard/tests/DashboardWidgetPage.java
+++ vaadin-dashboard-flow-parent/vaadin-dashboard-flow-integration-tests/src/main/java/com/vaadin/flow/component/dashboard/tests/DashboardWidgetPage.java
@@ -20,7 +20,7 @@ import com.vaadin.flow.router.Route;
 /**
  * @author Vaadin Ltd
  */
-@Route("vaadin-dashboard-widget")
+@Route("vaadin-dashboard/widget")
 public class DashboardWidgetPage extends Div {
 
     public DashboardWidgetPage() {
