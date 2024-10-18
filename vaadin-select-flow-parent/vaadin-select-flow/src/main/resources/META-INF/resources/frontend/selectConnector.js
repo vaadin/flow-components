8ca601a55c5fcb6358ed4b0a7407d9cf2eb15e41
Sergey Vinogradov <mr.vursen@gmail.com>
2024-10-18 12:19:46 +0400

    refactor: eliminate unnecessary self-invoking function

--- vaadin-select-flow-parent/vaadin-select-flow/src/main/resources/META-INF/resources/frontend/selectConnector.js
+++ vaadin-select-flow-parent/vaadin-select-flow/src/main/resources/META-INF/resources/frontend/selectConnector.js
@@ -1,27 +1,25 @@
-(function () {
-  const tryCatchWrapper = function (callback) {
-    return window.Vaadin.Flow.tryCatchWrapper(callback, 'Vaadin Select');
-  };
+const tryCatchWrapper = function (callback) {
+  return window.Vaadin.Flow.tryCatchWrapper(callback, 'Vaadin Select');
+};
 
-  window.Vaadin.Flow.selectConnector = {
-    initLazy: (select) =>
-      tryCatchWrapper(function (select) {
-        // do not init this connector twice for the given select
-        if (select.$connector) {
-          return;
-        }
+window.Vaadin.Flow.selectConnector = {
+  initLazy: (select) =>
+    tryCatchWrapper(function (select) {
+      // do not init this connector twice for the given select
+      if (select.$connector) {
+        return;
+      }
 
-        select.$connector = {};
+      select.$connector = {};
 
-        select.renderer = tryCatchWrapper(function (root) {
-          const listBox = select.querySelector('vaadin-select-list-box');
-          if (listBox) {
-            if (root.firstChild) {
-              root.removeChild(root.firstChild);
-            }
-            root.appendChild(listBox);
+      select.renderer = tryCatchWrapper(function (root) {
+        const listBox = select.querySelector('vaadin-select-list-box');
+        if (listBox) {
+          if (root.firstChild) {
+            root.removeChild(root.firstChild);
           }
-        });
-      })(select)
-  };
-})();
+          root.appendChild(listBox);
+        }
+      });
+    })(select)
+};
