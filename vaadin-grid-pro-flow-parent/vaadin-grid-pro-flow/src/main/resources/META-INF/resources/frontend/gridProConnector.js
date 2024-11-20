e925ef44a518689851aeeeb7669f79e7a90d2c7b
Tomi Virkki <tomivirkki@users.noreply.github.com>
2024-11-20 13:55:52 +0200

    fix: hide the grid-pro editor component until updated (#6820)

--- vaadin-grid-pro-flow-parent/vaadin-grid-pro-flow/src/main/resources/META-INF/resources/frontend/gridProConnector.js
+++ vaadin-grid-pro-flow-parent/vaadin-grid-pro-flow/src/main/resources/META-INF/resources/frontend/gridProConnector.js
@@ -7,6 +7,8 @@
  * See <https://vaadin.com/commercial-license-and-service-terms> for the full
  * license.
  */
+const LOADING_EDITOR_CELL_ATTRIBUTE = 'loading-editor';
+
 (function () {
   const tryCatchWrapper = function (callback) {
     return window.Vaadin.Flow.tryCatchWrapper(callback, 'Vaadin Grid Pro');
@@ -17,7 +19,16 @@
   }
 
   window.Vaadin.Flow.gridProConnector = {
-    selectAll: (editor) => {
+    selectAll: (editor, itemKey, grid) => {
+      if (editor.__itemKey !== itemKey) {
+        // This is an outdated call that can occur if the user starts editing a cell,
+        // and quickly starts editing another cell on the same column before the editor
+        // is unhidden for the first cell.
+        return;
+      }
+
+      grid.toggleAttribute(LOADING_EDITOR_CELL_ATTRIBUTE, false);
+
       if (editor instanceof HTMLInputElement) {
         editor.select();
       } else if (editor.focusElement && editor.focusElement instanceof HTMLInputElement) {
@@ -39,11 +50,20 @@
           root.appendChild(component);
           this._grid._cancelStopEdit();
           component.focus();
+
+          component.__itemKey = rowData.item.key;
+          this._grid.toggleAttribute(LOADING_EDITOR_CELL_ATTRIBUTE, true);
         });
 
         // Not needed in case of custom editor as value is set on server-side.
         // Overridden in order to avoid blinking of the cell content.
         column._setEditorValue = function (editor, value) {};
+
+        const stopCellEdit = column._stopCellEdit;
+        column._stopCellEdit = function () {
+          stopCellEdit.apply(this, arguments);
+          this._grid.toggleAttribute(LOADING_EDITOR_CELL_ATTRIBUTE, false);
+        };
       })(column, component),
 
     patchEditModeRenderer: (column) =>
