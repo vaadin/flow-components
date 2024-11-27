36842be4121732958ffaa2860f6ff105ed0bcfb6
Tatu Lund <tatu@vaadin.com>
2024-11-27 11:24:58 +0200

    refactor: remove unnecessary data communicator reset in Grid (#5304)
    * fix: DataChangeEvent should be fired upon filter change
    
    * Update GridListDataViewTest.java
    
    Add unit test
    
    * Formatting
    
    * remove reset
    
    ---------
    
    Co-authored-by: Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>
    Co-authored-by: Sascha Ißbrücker <sissbruecker@vaadin.com>

--- vaadin-grid-flow-parent/vaadin-grid-flow/src/main/java/com/vaadin/flow/component/grid/Grid.java
+++ vaadin-grid-flow-parent/vaadin-grid-flow/src/main/java/com/vaadin/flow/component/grid/Grid.java
@@ -5100,8 +5100,6 @@ public class Grid<T> extends Component implements HasStyle, HasSize,
             SerializableComparator<T> sortComparator) {
         updateInMemorySorting(sortComparator);
         updateInMemoryFiltering(filter);
-
-        dataCommunicator.reset();
     }
 
     private void updateInMemoryFiltering(
