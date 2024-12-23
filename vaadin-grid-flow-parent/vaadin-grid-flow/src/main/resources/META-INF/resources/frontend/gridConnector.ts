79644f06f76376eac1cb442c0cac366764113876
Serhii Kulykov <iamkulykov@gmail.com>
2024-12-23 10:28:46 +0200

    chore: format JS in connectors and integration tests with Prettier (#6983)

--- vaadin-grid-flow-parent/vaadin-grid-flow/src/main/resources/META-INF/resources/frontend/gridConnector.ts
+++ vaadin-grid-flow-parent/vaadin-grid-flow/src/main/resources/META-INF/resources/frontend/gridConnector.ts
@@ -3,7 +3,7 @@ import { Debouncer } from '@polymer/polymer/lib/utils/debounce.js';
 import { timeOut, animationFrame } from '@polymer/polymer/lib/utils/async.js';
 import { Grid } from '@vaadin/grid/src/vaadin-grid.js';
 import { isFocusable } from '@vaadin/grid/src/vaadin-grid-active-item-mixin.js';
-import { GridFlowSelectionColumn } from "./vaadin-grid-flow-selection-column.js";
+import { GridFlowSelectionColumn } from './vaadin-grid-flow-selection-column.js';
 
 window.Vaadin.Flow.gridConnector = {};
 window.Vaadin.Flow.gridConnector.initLazy = (grid) => {
@@ -904,11 +904,11 @@ window.Vaadin.Flow.gridConnector.initLazy = (grid) => {
   };
 
   /*
-    * Manage aria-multiselectable attribute depending on the selection mode.
-    * see more: https://github.com/vaadin/web-components/issues/1536
-    * or: https://www.w3.org/TR/wai-aria-1.1/#aria-multiselectable
-    * For selection mode SINGLE, set the aria-multiselectable attribute to false
-    */
+   * Manage aria-multiselectable attribute depending on the selection mode.
+   * see more: https://github.com/vaadin/web-components/issues/1536
+   * or: https://www.w3.org/TR/wai-aria-1.1/#aria-multiselectable
+   * For selection mode SINGLE, set the aria-multiselectable attribute to false
+   */
   grid.$connector.updateMultiSelectable = function () {
     if (!grid.$) {
       return;
@@ -975,9 +975,9 @@ window.Vaadin.Flow.gridConnector.initLazy = (grid) => {
   // automatically excluding columns from sorting when they get hidden.
   // In Flow, it's the developer's responsibility to remove the column
   // from the backend sort order when the column gets hidden.
-  grid._getActiveSorters = function() {
+  grid._getActiveSorters = function () {
     return this._sorters.filter((sorter) => sorter.direction);
-  }
+  };
 
   grid.__applySorters = () => {
     const sorters = grid._mapSorters();
@@ -1025,13 +1025,10 @@ window.Vaadin.Flow.gridConnector.initLazy = (grid) => {
     });
   };
 
-  grid.addEventListener(
-    'vaadin-context-menu-before-open',
-    function (e) {
-      const { key, columnId } = e.detail;
-      grid.$server.updateContextMenuTargetItem(key, columnId);
-    }
-  );
+  grid.addEventListener('vaadin-context-menu-before-open', function (e) {
+    const { key, columnId } = e.detail;
+    grid.$server.updateContextMenuTargetItem(key, columnId);
+  });
 
   grid.getContextMenuBeforeOpenDetail = function (event) {
     // For `contextmenu` events, we need to access the source event,
@@ -1044,81 +1041,66 @@ window.Vaadin.Flow.gridConnector.initLazy = (grid) => {
   };
 
   grid.preventContextMenu = function (event) {
-      const isLeftClick = event.type === 'click';
-      const { column } = grid.getEventContext(event);
+    const isLeftClick = event.type === 'click';
+    const { column } = grid.getEventContext(event);
 
-      return isLeftClick && column instanceof GridFlowSelectionColumn;
+    return isLeftClick && column instanceof GridFlowSelectionColumn;
   };
 
-  grid.addEventListener(
-    'click',
-    (e) => _fireClickEvent(e, 'item-click')
-  );
-  grid.addEventListener(
-    'dblclick',
-    (e) => _fireClickEvent(e, 'item-double-click')
-  );
+  grid.addEventListener('click', (e) => _fireClickEvent(e, 'item-click'));
+  grid.addEventListener('dblclick', (e) => _fireClickEvent(e, 'item-double-click'));
 
-  grid.addEventListener(
-    'column-resize',
-    (e) => {
-      const cols = grid._getColumnsInOrder().filter((col) => !col.hidden);
+  grid.addEventListener('column-resize', (e) => {
+    const cols = grid._getColumnsInOrder().filter((col) => !col.hidden);
 
-      cols.forEach((col) => {
-        col.dispatchEvent(new CustomEvent('column-drag-resize'));
-      });
+    cols.forEach((col) => {
+      col.dispatchEvent(new CustomEvent('column-drag-resize'));
+    });
 
-      grid.dispatchEvent(
-        new CustomEvent('column-drag-resize', {
-          detail: {
-            resizedColumnKey: e.detail.resizedColumn._flowId
-          }
-        })
-      );
+    grid.dispatchEvent(
+      new CustomEvent('column-drag-resize', {
+        detail: {
+          resizedColumnKey: e.detail.resizedColumn._flowId
+        }
+      })
+    );
+  });
+
+  grid.addEventListener('column-reorder', (e) => {
+    const columns = grid._columnTree
+      .slice(0)
+      .pop()
+      .filter((c) => c._flowId)
+      .sort((b, a) => b._order - a._order)
+      .map((c) => c._flowId);
+
+    grid.dispatchEvent(
+      new CustomEvent('column-reorder-all-columns', {
+        detail: { columns }
+      })
+    );
+  });
+
+  grid.addEventListener('cell-focus', (e) => {
+    const eventContext = grid.getEventContext(e);
+    const expectedSectionValues = ['header', 'body', 'footer'];
+
+    if (expectedSectionValues.indexOf(eventContext.section) === -1) {
+      return;
     }
-  );
 
-  grid.addEventListener(
-    'column-reorder',
-    (e) => {
-      const columns = grid._columnTree
-        .slice(0)
-        .pop()
-        .filter((c) => c._flowId)
-        .sort((b, a) => b._order - a._order)
-        .map((c) => c._flowId);
+    grid.dispatchEvent(
+      new CustomEvent('grid-cell-focus', {
+        detail: {
+          itemKey: eventContext.item ? eventContext.item.key : null,
 
-      grid.dispatchEvent(
-        new CustomEvent('column-reorder-all-columns', {
-          detail: { columns }
-        })
-      );
-    }
-  );
+          internalColumnId: eventContext.column ? eventContext.column._flowId : null,
 
-  grid.addEventListener(
-    'cell-focus',
-    (e) => {
-      const eventContext = grid.getEventContext(e);
-      const expectedSectionValues = ['header', 'body', 'footer'];
-
-      if (expectedSectionValues.indexOf(eventContext.section) === -1) {
-        return;
-      }
-
-      grid.dispatchEvent(
-        new CustomEvent('grid-cell-focus', {
-          detail: {
-            itemKey: eventContext.item ? eventContext.item.key : null,
-
-            internalColumnId: eventContext.column ? eventContext.column._flowId : null,
-
-            section: eventContext.section
-          }
-        })
-      );
-    }
-  );
+          section: eventContext.section
+        }
+      })
+    );
+  });
 
   function _fireClickEvent(event, eventName) {
     // Click event was handled by the component inside grid, do nothing.
@@ -1170,32 +1152,29 @@ window.Vaadin.Flow.gridConnector.initLazy = (grid) => {
 
   grid.dragFilter = (rowData) => rowData.item && !rowData.item.dragDisabled;
 
-  grid.addEventListener(
-    'grid-dragstart',
-    (e) => {
-      if (grid._isSelected(e.detail.draggedItems[0])) {
-        // Dragging selected (possibly multiple) items
-        if (grid.__selectionDragData) {
-          Object.keys(grid.__selectionDragData).forEach((type) => {
-            e.detail.setDragData(type, grid.__selectionDragData[type]);
-          });
-        } else {
-          (grid.__dragDataTypes || []).forEach((type) => {
-            e.detail.setDragData(type, e.detail.draggedItems.map((item) => item.dragData[type]).join('\n'));
-          });
-        }
-
-        if (grid.__selectionDraggedItemsCount > 1) {
-          e.detail.setDraggedItemsCount(grid.__selectionDraggedItemsCount);
-        }
+  grid.addEventListener('grid-dragstart', (e) => {
+    if (grid._isSelected(e.detail.draggedItems[0])) {
+      // Dragging selected (possibly multiple) items
+      if (grid.__selectionDragData) {
+        Object.keys(grid.__selectionDragData).forEach((type) => {
+          e.detail.setDragData(type, grid.__selectionDragData[type]);
+        });
       } else {
-        // Dragging just one (non-selected) item
         (grid.__dragDataTypes || []).forEach((type) => {
-          e.detail.setDragData(type, e.detail.draggedItems[0].dragData[type]);
+          e.detail.setDragData(type, e.detail.draggedItems.map((item) => item.dragData[type]).join('\n'));
         });
       }
+
+      if (grid.__selectionDraggedItemsCount > 1) {
+        e.detail.setDraggedItemsCount(grid.__selectionDraggedItemsCount);
+      }
+    } else {
+      // Dragging just one (non-selected) item
+      (grid.__dragDataTypes || []).forEach((type) => {
+        e.detail.setDragData(type, e.detail.draggedItems[0].dragData[type]);
+      });
     }
-  );
+  });
 
   grid.isItemSelectable = (item) => {
     // If there is no selectable data, assume the item is selectable
