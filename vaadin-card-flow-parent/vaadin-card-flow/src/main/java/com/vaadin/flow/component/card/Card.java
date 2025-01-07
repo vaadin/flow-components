7f39ab59162adc3c72e97db57db737ce66ae0ea6
Sascha Ißbrücker <sissbruecker@vaadin.com>
2025-01-07 10:22:32 +0100

    cleanup component package

--- vaadin-card-flow-parent/vaadin-card-flow/src/main/java/com/vaadin/flow/component/card/Card.java
+++ vaadin-card-flow-parent/vaadin-card-flow/src/main/java/com/vaadin/flow/component/card/Card.java
@@ -15,11 +15,6 @@
  */
 package com.vaadin.flow.component.card;
 
-import java.util.Set;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.HasSize;
 import com.vaadin.flow.component.HasStyle;
@@ -27,16 +22,15 @@ import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.shared.HasThemeVariant;
-import com.vaadin.flow.dom.Element;
 
 /**
- *
+ * Card is a visual content container for creating a card-based layout.
  *
  * @author Vaadin Ltd
  */
 @Tag("vaadin-card")
-// @NpmPackage(value = "@vaadin/polymer-legacy-adapter", version = "24.7.0-alpha2")
-// @JsModule("@vaadin/polymer-legacy-adapter/style-modules.js")
+@NpmPackage(value = "@vaadin/polymer-legacy-adapter", version = "24.7.0-alpha2")
+@JsModule("@vaadin/polymer-legacy-adapter/style-modules.js")
 @NpmPackage(value = "@vaadin/card", version = "24.7.0-alpha2")
 @JsModule("@vaadin/card/src/vaadin-card.js")
 public class Card extends Component
@@ -44,60 +38,8 @@ public class Card extends Component
         HasThemeVariant<CardVariant> {
 
     /**
-     * Default constructor. Creates an empty card.
+     * Creates an empty card.
      */
     public Card() {
-    }
-
-    /**
-     * Adds the given components as children of this component.
-     *
-     * @param components
-     *            the components to add
-     */
-    private void add(Component... components) {
-        assert components != null;
-        for (Component component : components) {
-            assert component != null;
-            getElement().appendChild(component.getElement());
-        }
-    }
-
-    /**
-     * Removes the given child components from this component.
-     *
-     * @param components
-     *            The components to remove.
-     * @throws IllegalArgumentException
-     *             if any of the components is not a child of this component.
-     */
-    protected void remove(Component... components) {
-        for (Component component : components) {
-            if (getElement().equals(component.getElement().getParent())) {
-                component.getElement().removeAttribute("slot");
-                getElement().removeChild(component.getElement());
-            } else {
-                throw new IllegalArgumentException("The given component ("
-                        + component + ") is not a child of this component");
-            }
-        }
-    }
-
-    /**
-     * Removes all contents from this component except elements in
-     * {@code exclusion} array. This includes child components, text content as
-     * well as child elements that have been added directly to this component
-     * using the {@link Element} API.
-     *
-     * @see Card#removeAll()
-     */
-    private void removeAll(Element... exclusion) {
-        Set<Element> toExclude = Stream.of(exclusion)
-                .collect(Collectors.toSet());
-        Predicate<Element> filter = toExclude::contains;
-        getElement().getChildren().filter(filter.negate())
-                .forEach(child -> child.removeAttribute("slot"));
-        getElement().removeAllChildren();
-        getElement().appendChild(exclusion);
     }
 }
--- vaadin-card-flow-parent/vaadin-card-flow/src/test/java/com/vaadin/flow/component/card/CardSerializableTest.java
+++ vaadin-card-flow-parent/vaadin-card-flow/src/test/java/com/vaadin/flow/component/card/CardSerializableTest.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.vaadin.flow.component.card.tests;
+package com.vaadin.flow.component.card;
 
 import com.vaadin.flow.testutil.ClassesSerializableTest;
 
