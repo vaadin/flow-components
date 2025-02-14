bfd581ad7ade14eb6b06127f82316670941da510
Tatu Lund <tatu@vaadin.com>
2025-02-14 13:46:38 +0200

    Create GridSingleSelectionUpdateAndDeselectIT.java

---
+++ vaadin-grid-flow-parent/vaadin-grid-flow-integration-tests/src/test/java/com/vaadin/flow/component/grid/it/GridSingleSelectionUpdateAndDeselectIT.java
@@ -0,0 +1,30 @@
+package com.vaadin.flow.component.grid.it;
+
+import com.vaadin.flow.component.grid.testbench.GridElement;
+import com.vaadin.flow.testutil.TestPath;
+import com.vaadin.tests.AbstractComponentIT;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+@TestPath("vaadin-grid/grid-single-selection-update-and-deselect")
+public class GridSingleSelectionUpdateAndDeselectIT
+        extends AbstractComponentIT {
+
+    @Before
+    public void init() {
+        open();
+    }
+
+    @Test
+    public void shouldHaveCorrectValueAfterUpdateAndDeselect() {
+        // Click the update button
+        clickElementWithJs("update-name");
+        GridElement grid = $(GridElement.class).first();
+        String name = grid.getCell(0, 1).getText();
+
+        // Expect the name to be updated
+        Assert.assertEquals("Bar", name);
+    }
+
+}
