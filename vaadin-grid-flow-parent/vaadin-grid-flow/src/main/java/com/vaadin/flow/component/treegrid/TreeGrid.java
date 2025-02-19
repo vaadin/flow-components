80c5dc100029facbf05a926869300ec42e385fe2
Sascha Ißbrücker <sissbruecker@vaadin.com>
2025-02-19 10:16:14 +0100

    remove duplicate withProperty call

--- vaadin-grid-flow-parent/vaadin-grid-flow/src/main/java/com/vaadin/flow/component/treegrid/TreeGrid.java
+++ vaadin-grid-flow-parent/vaadin-grid-flow/src/main/java/com/vaadin/flow/component/treegrid/TreeGrid.java
@@ -681,9 +681,8 @@ public class TreeGrid<T> extends Grid<T>
      */
     public <V extends Component> Column<T> addComponentHierarchyColumn(
             ValueProvider<T, V> componentProvider) {
-        return addColumn(new HierarchyColumnComponentRenderer<V, T>(
-                componentProvider, this).withProperty("children",
-                        item -> getDataCommunicator().hasChildren(item)));
+        return addColumn(new HierarchyColumnComponentRenderer<>(
+                componentProvider, this));
     }
 
     /**
