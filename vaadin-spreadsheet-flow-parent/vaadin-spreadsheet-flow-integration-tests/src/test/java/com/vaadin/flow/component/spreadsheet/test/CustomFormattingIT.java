8cbec3218b0af97750a021cac514a0ac79ac24b8
thevaadinman <patrik@vaadin.com>
2025-02-25 15:16:02 +0200

    attempt to fix some of the formatting

--- vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow-integration-tests/src/test/java/com/vaadin/flow/component/spreadsheet/test/CustomFormattingIT.java
+++ vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow-integration-tests/src/test/java/com/vaadin/flow/component/spreadsheet/test/CustomFormattingIT.java
@@ -25,7 +25,8 @@ public class CustomFormattingIT extends AbstractSpreadsheetIT {
     private static final int COL_COUNT = 8;
 
     // The 8 basic named colors, in order in the document, as CSS strings
-    private static final String[] NAMED_COLOR_CSS = { "rgba(0, 0, 0, 1)", // black
+    private static final String[] NAMED_COLOR_CSS = { // new line here
+            "rgba(0, 0, 0, 1)", // black
             "rgba(0, 0, 255, 1)", // blue
             "rgba(0, 255, 255, 1)", // cyan
             "rgba(0, 255, 0, 1)", // green
@@ -40,22 +41,24 @@ public class CustomFormattingIT extends AbstractSpreadsheetIT {
     static {
         // Color values in hex, taken from the modified CellFormatPart.java
         // in vaadin-spreadsheet-flow. These are the 56 indexed Excel colors
-        final int[] rgb_hex = { 0x000000, 0xFFFFFF, 0xFF0000, 0x00FF00,
-                0x0000FF, 0xFFFF00, 0xFF00FF, 0x00FFFF, 0x800000, 0x008000,
-                0x000080, 0x808000, 0x800080, 0x008080, 0xC0C0C0, 0x808080,
-                0x9999FF, 0x993366, 0xFFFFCC, 0xCCFFFF, 0x660066, 0xFF8080,
-                0x0066CC, 0xCCCCFF, 0x000080, 0xFF00FF, 0xFFFF00, 0x00FFFF,
-                0x800080, 0x800000, 0x008080, 0x0000FF, 0x00CCFF, 0xCCFFFF,
-                0xCCFFCC, 0xFFFF99, 0x99CCFF, 0xFF99CC, 0xCC99FF, 0xFFCC99,
-                0x3366FF, 0x33CCCC, 0x99CC00, 0xFFCC00, 0xFF9900, 0xFF6600,
-                0x666699, 0x969696, 0x003366, 0x339966, 0x003300, 0x333300,
-                0x993300, 0x993366, 0x333399, 0x333333 };
+        final int[] rgb_hex = { // --- line change here ---
+                0x000000, 0xFFFFFF, 0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00,
+                0xFF00FF, 0x00FFFF, 0x800000, 0x008000, 0x000080, 0x808000,
+                0x800080, 0x008080, 0xC0C0C0, 0x808080, 0x9999FF, 0x993366,
+                0xFFFFCC, 0xCCFFFF, 0x660066, 0xFF8080, 0x0066CC, 0xCCCCFF,
+                0x000080, 0xFF00FF, 0xFFFF00, 0x00FFFF, 0x800080, 0x800000,
+                0x008080, 0x0000FF, 0x00CCFF, 0xCCFFFF, 0xCCFFCC, 0xFFFF99,
+                0x99CCFF, 0xFF99CC, 0xCC99FF, 0xFFCC99, 0x3366FF, 0x33CCCC,
+                0x99CC00, 0xFFCC00, 0xFF9900, 0xFF6600, 0x666699, 0x969696,
+                0x003366, 0x339966, 0x003300, 0x333300, 0x993300, 0x993366,
+                0x333399, 0x333333 };
 
         // Convert table to CSS strings
         for (int i = 0; i < 56; ++i) {
-            INDEXED_COLOR_CSS[i] = "rgba(" + ((rgb_hex[i] >>> 16) & 0xff) + ", "
-                    + ((rgb_hex[i] >>> 8) & 0xff) + ", " + (rgb_hex[i] & 0xff)
-                    + ", 1)";
+            INDEXED_COLOR_CSS[i] = "rgba(" + // results returnd as rgba
+                ((rgb_hex[i] >>> 16) & 0xff) + ", " + // red component
+                ((rgb_hex[i] >>> 8) & 0xff) + ", " + // green component
+                (rgb_hex[i] & 0xff) + ", 1)"; // blue component and solid alpha
         }
     }
 
--- vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow/src/main/java/org/apache/poi/ss/format/CellFormatPart.java
+++ vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow/src/main/java/org/apache/poi/ss/format/CellFormatPart.java
@@ -8,6 +8,27 @@
  */
 package org.apache.poi.ss.format;
 
+/* 
+ * The Vaadin CI pipeline requires Vaadin's license header to be first in the file.
+ * While this file is modified, most of it was part of Apache POI, which is licensed
+ * under the Apache License, Version 2.0.
+ */
+
+/* ====================================================================
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to You under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   ==================================================================== */
+
 import static org.apache.poi.ss.format.CellFormatter.quote;
 
 import java.awt.*;
@@ -99,7 +120,8 @@ public class CellFormatPart {
     static {
         // Build indexed color list based on this table
         // https://www.excelsupersite.com/what-are-the-56-colorindex-colors-in-excel/
-        INDEXED_COLORS = List.of(new Color(0x000000), // Color 1 / black
+        INDEXED_COLORS = List.of( // new line
+                new Color(0x000000), // Color 1 / black
                 new Color(0xFFFFFF), // Color 2 / white
                 new Color(0xFF0000), // Color 3 / red
                 new Color(0x00FF00), // Color 4 / green
