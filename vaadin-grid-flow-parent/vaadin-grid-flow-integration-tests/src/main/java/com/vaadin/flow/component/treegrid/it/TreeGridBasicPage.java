a0c9076d1dd318a1335159ae778e0d97915a79fc
Sergey Vinogradov <mr.vursen@gmail.com>
2025-03-07 20:20:59 +0400

    wip

--- vaadin-grid-flow-parent/vaadin-grid-flow-integration-tests/src/main/java/com/vaadin/flow/component/treegrid/it/TreeGridBasicPage.java
+++ vaadin-grid-flow-parent/vaadin-grid-flow-integration-tests/src/main/java/com/vaadin/flow/component/treegrid/it/TreeGridBasicPage.java
@@ -15,51 +15,106 @@
  */
 package com.vaadin.flow.component.treegrid.it;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.NativeButton;
 import com.vaadin.flow.component.treegrid.TreeGrid;
+import com.vaadin.flow.data.bean.HierarchicalTestBean;
 import com.vaadin.flow.data.bean.PeopleGenerator;
 import com.vaadin.flow.data.bean.PersonWithLevel;
+import com.vaadin.flow.data.provider.hierarchy.AbstractBackEndHierarchicalDataProvider;
+import com.vaadin.flow.data.provider.hierarchy.AbstractHierarchicalDataProvider;
+import com.vaadin.flow.data.provider.hierarchy.FlatHierarchyDataProvider;
+import com.vaadin.flow.data.provider.hierarchy.HierarchicalQuery;
 import com.vaadin.flow.data.provider.hierarchy.TreeData;
 import com.vaadin.flow.data.provider.hierarchy.TreeDataProvider;
 import com.vaadin.flow.router.Route;
 
 @Route("vaadin-grid/tree-grid-basic")
 public class TreeGridBasicPage extends Div {
+    private List<Item> items = buildItemsHierarchy();
+
+    private class DataProvider extends AbstractBackEndHierarchicalDataProvider<Item, String> {
+        @Override
+        public int getChildCount(HierarchicalQuery<Item, String> query) {
+            return query.getParent() == null ? items.size() : query.getParent().getChildItems().size();
+        }
+
+        @Override
+        public boolean hasChildren(Item item) {
+            return item.getChildItems() != null && !item.getChildItems().isEmpty();
+        }
+
+        @Override
+        public Stream<Item> fetchChildrenFromBackEnd(HierarchicalQuery<Item, String> query) {
+            return query.getParent() == null ? items.stream() : query.getParent().getChildItems().stream();
+        }
+    }
+
+    private class Item {
+        private String name;
+
+        private List<Item> childItems;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public List<Item> getChildItems() {
+            return childItems;
+        }
+
+        public void setChildItems(List<Item> childItems) {
+            this.childItems = childItems;
+        }
+    }
 
     public TreeGridBasicPage() {
-        PeopleGenerator peopleGenerator = new PeopleGenerator();
-        List<PersonWithLevel> people = peopleGenerator
-                .generatePeopleWithLevels(1000, 0);
-
-        TreeGrid<PersonWithLevel> grid = new TreeGrid<>();
+        TreeGrid<Item> grid = new TreeGrid<>();
         grid.addHierarchyColumn(person -> person.getFirstName())
                 .setHeader("First name");
+        grid.setDataProvider(new DataProvider());
 
-        TreeData<PersonWithLevel> data = new TreeData<>();
-        data.addItems(people, person -> {
-            if (person.getLevel() == 0) {
-                return peopleGenerator.generatePeopleWithLevels(5, 1);
-            }
+        // NativeButton expandAll = new NativeButton("Expand all",
+        //         e -> grid.expandRecursively(data.getRootItems(), 3));
+        // expandAll.setId("expand-all");
 
-            return Collections.emptyList();
-        });
-        grid.setDataProvider(new TreeDataProvider<>(data));
+        // NativeButton refreshItem = new NativeButton("Refresh item", e -> {
+        //     PersonWithLevel person = data.getChildren(people.get(0)).get(1);
+        //     person.setFirstName("Updated");
+        //     grid.getDataProvider().refreshItem(person);
+        // });
 
-        NativeButton expandAll = new NativeButton("Expand all",
-                e -> grid.expandRecursively(data.getRootItems(), 3));
-        expandAll.setId("expand-all");
+        add(grid);
+    }
 
-        NativeButton refreshItem = new NativeButton("Refresh item", e -> {
-            PersonWithLevel person = data.getChildren(people.get(0)).get(1);
-            person.setFirstName("Updated");
-            grid.getDataProvider().refreshItem(person);
-        });
+    private List<Item> getFlatItems() {
+        return items.stream()
+            .flatMap(item -> expandedItems.contains(item)
+                ? Stream.concat(Stream.of(item), item.childItems().stream())
+                : Stream.of(item))
+            .toList();
+    }
 
-        add(grid, expandAll, refreshItem);
+    private List<Item> buildItemsHierarchy() {
+        return IntStream.range(0, 100)
+            .mapToObj(i -> new Item("Item " + i,
+                IntStream.range(0, 50)
+                    .mapToObj(j -> new Item("Child Item " + i + "-" + j, null))
+                    .toList()))
+            .toList();
     }
 
 }
