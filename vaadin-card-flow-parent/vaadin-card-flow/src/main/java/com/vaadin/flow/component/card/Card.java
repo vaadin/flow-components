789d5bd6b891b456afc6be9fc4e93abe10b72b98
ugur-vaadin <ugur@vaadin.com>
2025-03-14 17:49:24 +0200

    chore: run formatter

--- vaadin-card-flow-parent/vaadin-card-flow/src/main/java/com/vaadin/flow/component/card/Card.java
+++ vaadin-card-flow-parent/vaadin-card-flow/src/main/java/com/vaadin/flow/component/card/Card.java
@@ -87,8 +87,9 @@ public class Card extends Component implements HasSize,
     }
 
     /**
-     * Sets the component used as the card's title. If a {@link #setHeader(Component) header component} is
-     * set, the title will not be displayed.
+     * Sets the component used as the card's title. If a
+     * {@link #setHeader(Component) header component} is set, the title will not
+     * be displayed.
      * <p>
      * Passing {@code null} removes the current title from the card.
      *
@@ -109,8 +110,9 @@ public class Card extends Component implements HasSize,
     }
 
     /**
-     * Sets the component used as the card's subtitle. If a {@link #setHeader(Component) header component} is
-     * set, the subtitle will not be displayed.
+     * Sets the component used as the card's subtitle. If a
+     * {@link #setHeader(Component) header component} is set, the subtitle will
+     * not be displayed.
      * <p>
      * Passing {@code null} removes the current subtitle from the card.
      *
@@ -132,7 +134,9 @@ public class Card extends Component implements HasSize,
 
     /**
      * Sets the component used as the card's header. The header is prioritized
-     * over the {@link #setTitle(Component) title} and {@link #setSubtitle(Component) subtitle} components, and will be displayed instead.
+     * over the {@link #setTitle(Component) title} and
+     * {@link #setSubtitle(Component) subtitle} components, and will be
+     * displayed instead.
      * <p>
      * Passing {@code null} removes the current header component from the card.
      *
