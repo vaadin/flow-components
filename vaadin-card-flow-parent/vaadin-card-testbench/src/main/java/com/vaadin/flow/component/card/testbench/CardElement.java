01f46833e516586beb814e474748cd0e414425ff
ugur-vaadin <ugur@vaadin.com>
2025-03-19 17:50:55 +0200

    feat: add card testbench methods

--- vaadin-card-flow-parent/vaadin-card-testbench/src/main/java/com/vaadin/flow/component/card/testbench/CardElement.java
+++ vaadin-card-flow-parent/vaadin-card-testbench/src/main/java/com/vaadin/flow/component/card/testbench/CardElement.java
@@ -15,6 +15,11 @@
  */
 package com.vaadin.flow.component.card.testbench;
 
+import java.util.Collections;
+import java.util.List;
+
+import org.openqa.selenium.By;
+
 import com.vaadin.testbench.TestBenchElement;
 import com.vaadin.testbench.elementsbase.Element;
 
@@ -23,4 +28,96 @@ import com.vaadin.testbench.elementsbase.Element;
  */
 @Element("vaadin-card")
 public class CardElement extends TestBenchElement {
+
+    /**
+     * Gets the content elements.
+     *
+     * @return the content elements
+     */
+    public List<TestBenchElement> getContentElements() {
+        var contentWrapper = findElement(By.cssSelector("div:not([slot])"));
+        if (contentWrapper == null) {
+            return Collections.emptyList();
+        }
+        return contentWrapper.getPropertyElements("children");
+    }
+
+    /**
+     * Gets the element in the title slot.
+     *
+     * @return the title element, or {@code null} if not found
+     */
+    public TestBenchElement getTitle() {
+        return getElementInSlot("title");
+    }
+
+    /**
+     * Gets the title property value.
+     *
+     * @return title property value
+     */
+    public String getStringTitle() {
+        return getPropertyString("title");
+    }
+
+    /**
+     * Gets the element in the subtitle slot.
+     *
+     * @return the subtitle element, or {@code null} if not found
+     */
+    public TestBenchElement getSubtitle() {
+        return getElementInSlot("subtitle");
+    }
+
+    /**
+     * Gets the element in the media slot.
+     *
+     * @return the media element, or {@code null} if not found
+     */
+    public TestBenchElement getMedia() {
+        return getElementInSlot("media");
+    }
+
+    /**
+     * Gets the element in the header slot.
+     *
+     * @return the title element, or {@code null} if not found
+     */
+    public TestBenchElement getHeader() {
+        return getElementInSlot("header");
+    }
+
+    /**
+     * Gets the element in the header prefix slot.
+     *
+     * @return the header prefix element, or {@code null} if not found
+     */
+    public TestBenchElement getHeaderPrefix() {
+        return getElementInSlot("header-prefix");
+    }
+
+    /**
+     * Gets the element in the header suffix slot.
+     *
+     * @return the header suffix element, or {@code null} if not found
+     */
+    public TestBenchElement getHeaderSuffix() {
+        return getElementInSlot("header-suffix");
+    }
+
+    /**
+     * Gets the elements in the footer slot.
+     *
+     * @return the footer elements
+     */
+    public List<TestBenchElement> getFooterElements() {
+        return wrapElements(
+                getWrappedElement()
+                        .findElements(By.cssSelector("[slot='footer']")),
+                getCommandExecutor());
+    }
+
+    private TestBenchElement getElementInSlot(String slotName) {
+        return findElement(By.cssSelector("[slot='%s']".formatted(slotName)));
+    }
 }
