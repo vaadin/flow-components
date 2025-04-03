2bca9f766ce3f545e28675cc693ba2ff5cfb33ba
Tatu Lund <tatu@vaadin.com>
2025-04-03 10:40:41 +0300

    Use hasAttribute for opened instead

--- vaadin-context-menu-flow-parent/vaadin-context-menu-testbench/src/main/java/com/vaadin/flow/component/contextmenu/testbench/ContextMenuElement.java
+++ vaadin-context-menu-flow-parent/vaadin-context-menu-testbench/src/main/java/com/vaadin/flow/component/contextmenu/testbench/ContextMenuElement.java
@@ -52,7 +52,7 @@ public class ContextMenuElement extends TestBenchElement {
      */
     public boolean isOpen() {
         try {
-            return getAttribute("opened").equals("true");
+            return hasAttribute("opened");
         } catch (StaleElementReferenceException e) {
             return false;
         }
