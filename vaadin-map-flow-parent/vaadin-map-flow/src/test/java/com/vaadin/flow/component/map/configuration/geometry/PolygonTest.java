a6713ab98f066d8474a879ff18056c4e867ff348
Sascha Ißbrücker <sissbruecker@vaadin.com>
2025-04-04 11:36:56 +0200

    update PolygonTest

--- vaadin-map-flow-parent/vaadin-map-flow/src/test/java/com/vaadin/flow/component/map/configuration/geometry/PolygonTest.java
+++ vaadin-map-flow-parent/vaadin-map-flow/src/test/java/com/vaadin/flow/component/map/configuration/geometry/PolygonTest.java
@@ -32,21 +32,10 @@ public class PolygonTest {
         final Polygon polygon = new Polygon(List.of(new Coordinate(1, 1),
                 new Coordinate(2, 2), new Coordinate(1, 1)));
 
-        final Coordinate[][] coordinates = polygon.getCoordinates();
-        Assert.assertNotNull(coordinates);
-        Assert.assertEquals(1, coordinates.length);
+        Coordinate[][] expected = new Coordinate[][] { { new Coordinate(1, 1),
+                new Coordinate(2, 2), new Coordinate(1, 1) } };
 
-        final Coordinate[] linearRing = coordinates[0];
-        Assert.assertEquals(3, linearRing.length);
-
-        Assert.assertEquals(1, linearRing[0].getX(), 0);
-        Assert.assertEquals(1, linearRing[0].getY(), 0);
-
-        Assert.assertEquals(2, linearRing[1].getX(), 0);
-        Assert.assertEquals(2, linearRing[1].getY(), 0);
-
-        Assert.assertEquals(1, linearRing[2].getX(), 0);
-        Assert.assertEquals(1, linearRing[2].getY(), 0);
+        assertCoordinates(expected, polygon);
     }
 
     @Test
@@ -72,21 +61,32 @@ public class PolygonTest {
         polygon.setCoordinates(List.of(new Coordinate(10, 10),
                 new Coordinate(20, 20), new Coordinate(10, 10)));
 
-        final Coordinate[][] coordinates = polygon.getCoordinates();
-        Assert.assertNotNull(coordinates);
-        Assert.assertEquals(1, coordinates.length);
+        Coordinate[][] expected = new Coordinate[][] { { new Coordinate(10, 10),
+                new Coordinate(20, 20), new Coordinate(10, 10) } };
 
-        final Coordinate[] linearRing = coordinates[0];
-        Assert.assertEquals(3, linearRing.length);
+        assertCoordinates(expected, polygon);
 
-        Assert.assertEquals(10, linearRing[0].getX(), 0);
-        Assert.assertEquals(10, linearRing[0].getY(), 0);
+        Mockito.verify(propertyChangeListenerMock, Mockito.times(1))
+                .propertyChange(Mockito.any());
+    }
 
-        Assert.assertEquals(20, linearRing[1].getX(), 0);
-        Assert.assertEquals(20, linearRing[1].getY(), 0);
+    @Test
+    public void setCoordinatesArray() {
+        final TestPolygon polygon = new TestPolygon(
+                List.of(new Coordinate(1, 1), new Coordinate(2, 2),
+                        new Coordinate(1, 1)));
 
-        Assert.assertEquals(10, linearRing[2].getX(), 0);
-        Assert.assertEquals(10, linearRing[2].getY(), 0);
+        polygon.addPropertyChangeListener(propertyChangeListenerMock);
+
+        Coordinate[][] newCoordinates = new Coordinate[][] {
+                { new Coordinate(10, 10), new Coordinate(20, 20),
+                        new Coordinate(20, 10), new Coordinate(10, 10) },
+                { new Coordinate(12, 12), new Coordinate(15, 15),
+                        new Coordinate(15, 12), new Coordinate(12, 12) } };
+
+        polygon.setCoordinates(newCoordinates);
+
+        assertCoordinates(newCoordinates, polygon);
 
         Mockito.verify(propertyChangeListenerMock, Mockito.times(1))
                 .propertyChange(Mockito.any());
@@ -114,21 +114,27 @@ public class PolygonTest {
 
         polygon.translate(10, 10);
 
-        final Coordinate[][] coordinates = polygon.getCoordinates();
-        Assert.assertNotNull(coordinates);
-        Assert.assertEquals(1, coordinates.length);
+        Coordinate[][] expected = new Coordinate[][] { { new Coordinate(11, 11),
+                new Coordinate(12, 12), new Coordinate(11, 11) } };
 
-        final Coordinate[] linearRing = coordinates[0];
-        Assert.assertEquals(3, linearRing.length);
+        assertCoordinates(expected, polygon);
+    }
 
-        Assert.assertEquals(11, linearRing[0].getX(), 0);
-        Assert.assertEquals(11, linearRing[0].getY(), 0);
+    private void assertCoordinates(Coordinate[][] expected, Polygon polygon) {
+        Coordinate[][] actual = polygon.getCoordinates();
 
-        Assert.assertEquals(12, linearRing[1].getX(), 0);
-        Assert.assertEquals(12, linearRing[1].getY(), 0);
+        Assert.assertNotNull(actual);
+        Assert.assertEquals(expected.length, actual.length);
 
-        Assert.assertEquals(11, linearRing[2].getX(), 0);
-        Assert.assertEquals(11, linearRing[2].getY(), 0);
+        for (int i = 0; i < expected.length; i++) {
+            Assert.assertEquals(expected[i].length, actual[i].length);
+            for (int j = 0; j < expected[i].length; j++) {
+                Assert.assertEquals(expected[i][j].getX(), actual[i][j].getX(),
+                        0);
+                Assert.assertEquals(expected[i][j].getY(), actual[i][j].getY(),
+                        0);
+            }
+        }
     }
 
     private static class TestPolygon extends Polygon {
