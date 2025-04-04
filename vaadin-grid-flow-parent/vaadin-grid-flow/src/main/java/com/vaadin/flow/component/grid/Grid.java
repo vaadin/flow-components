d987914586d6e5c2a251d1a8f959f5f5c47e885a
Yuriy Yevstihnyeyev <yuriy@vaadin.com>
2025-04-04 14:02:29 +0300

    chore: Update Grid version (14.12)

--- vaadin-grid-flow-parent/vaadin-grid-flow/src/main/java/com/vaadin/flow/component/grid/Grid.java
+++ vaadin-grid-flow-parent/vaadin-grid-flow/src/main/java/com/vaadin/flow/component/grid/Grid.java
@@ -121,7 +121,7 @@ import elemental.json.JsonValue;
  *
  */
 @Tag("vaadin-grid")
-@NpmPackage(value = "@vaadin/vaadin-grid", version = "5.10.1")
+@NpmPackage(value = "@vaadin/vaadin-grid", version = "5.10.2")
 @JsModule("@vaadin/vaadin-grid/src/vaadin-grid.js")
 @JsModule("@vaadin/vaadin-grid/src/vaadin-grid-column.js")
 @JsModule("@vaadin/vaadin-grid/src/vaadin-grid-sorter.js")
