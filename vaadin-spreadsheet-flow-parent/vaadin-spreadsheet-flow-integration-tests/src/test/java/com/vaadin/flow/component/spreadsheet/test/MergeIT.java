8086f48dcd6ca50c1fb05390c0a035a1dd9c525a
Ugur Saglam <106508695+ugur-vaadin@users.noreply.github.com>
2025-04-08 16:42:25 +0300

    fix: do not set z-index for sub-cells with text (CP 23.5) (#7288)
    * fix: do not set z-index for sub-cells with text (#7239)
    
    * fix: make sure subcells are hidden
    
    * fix: do not set zindex for subcells with text and revert initial fix
    
    * test: replace the test to match the new fix
    
    * test: fix getting styles in the it

--- vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow-integration-tests/src/test/java/com/vaadin/flow/component/spreadsheet/test/MergeIT.java
+++ vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow-integration-tests/src/test/java/com/vaadin/flow/component/spreadsheet/test/MergeIT.java
@@ -11,6 +11,7 @@ package com.vaadin.flow.component.spreadsheet.test;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 
 import com.vaadin.flow.component.spreadsheet.tests.fixtures.TestFixtures;
 import com.vaadin.flow.testutil.TestPath;
@@ -83,5 +84,17 @@ public class MergeIT extends AbstractSpreadsheetIT {
         loadTestFixture(TestFixtures.MergeCells);
 
         Assert.assertEquals("A1 text", getMergedCellContent("A1"));
+    }
+
+    @Test
+    public void mergeCellsWithText_firstSubCellShouldNotHaveZIndex() {
+        setCellValue("A1", "A1 text");
+        selectRegion("A1", "B1");
+        loadTestFixture(TestFixtures.MergeCells);
+        var cellSelector = ".cell.row1.col1:not(merged-cell)";
+        var element = findElementInShadowRoot(By.cssSelector(cellSelector));
+        var style = element.getAttribute("style");
+        var hasZIndex = style != null && style.contains("z-index");
+        Assert.assertFalse(hasZIndex);
     }
 }
