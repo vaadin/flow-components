f22e26e63e53af51000dcab413ac2049a85e4d9c
Tatu Lund <tatu@vaadin.com>
2025-04-23 15:15:32 +0300

    Create MenuBarItemElement.java

---
+++ vaadin-menu-bar-flow-parent/vaadin-menu-bar-testbench/src/main/java/com/vaadin/flow/component/menubar/testbench/MenuBarItemElement.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2000-2025 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.menubar.testbench;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import com.vaadin.testbench.TestBenchElement;
+import com.vaadin.testbench.elementsbase.Element;
+
+@Element("vaadin-menu-bar-item")
+public class MenuBarItemElement extends TestBenchElement {
+
+    /**
+     * Get the sub menu overlay element linked to this menu item.
+     *
+     * @return TestBenchElement for the open sub menu.
+     */
+    public TestBenchElement getSubMenu() {
+        waitForSubMenu();
+        return (TestBenchElement) executeScript(
+                "return arguments[0].__overlay;", this);
+    }
+
+    /**
+     * Check if the item has open sub menu.
+     *
+     * @return True if there is sub menu open
+     */
+    public boolean isExpended() {
+        return hasAttribute("expanded");
+    }
+
+    private void waitForSubMenu() {
+        waitUntil(ExpectedConditions.attributeToBe(this, "aria-expanded",
+                "true"));
+    }
+
+}
