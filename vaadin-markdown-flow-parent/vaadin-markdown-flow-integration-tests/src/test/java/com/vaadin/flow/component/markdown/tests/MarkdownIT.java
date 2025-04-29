7c471b3dec5e10bb88a4f18ce545303de284abbe
Tomi Virkki <virkki@vaadin.com>
2025-04-29 14:50:43 +0300

    feat: add Markdown component

---
+++ vaadin-markdown-flow-parent/vaadin-markdown-flow-integration-tests/src/test/java/com/vaadin/flow/component/markdown/tests/MarkdownIT.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2000-2025 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.markdown.tests;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.vaadin.flow.component.markdown.testbench.MarkdownElement;
+import com.vaadin.flow.testutil.TestPath;
+import com.vaadin.tests.AbstractComponentIT;
+
+@TestPath("markdown")
+public class MarkdownIT extends AbstractComponentIT {
+
+    private MarkdownElement markdownElement;
+
+    @Before
+    public void init() {
+        open();
+        markdownElement = $(MarkdownElement.class).waitForFirst();
+    }
+
+    @Test
+    public void sizeMatches() {
+        Assert.assertEquals(300, markdownElement.getSize().getHeight());
+        Assert.assertEquals(400, markdownElement.getSize().getWidth());
+    }
+
+    @Test
+    public void markdownMatches() {
+        String markdown = markdownElement.getMarkdown();
+        Assert.assertEquals("**Hello** _World_", markdown);
+    }
+
+    @Test
+    public void appendMarkdown_markdownMatches() {
+        clickElementWithJs("append-button");
+        String markdown = markdownElement.getMarkdown();
+        Assert.assertEquals("**Hello** _World_!", markdown);
+    }
+
+    @Test
+    public void setMarkdown_markdownMatches() {
+        clickElementWithJs("set-button");
+        String markdown = markdownElement.getMarkdown();
+        Assert.assertEquals("**Updated** _Markdown_", markdown);
+    }
+
+}
---
+++ vaadin-markdown-flow-parent/vaadin-markdown-flow/src/main/java/com/vaadin/flow/component/markdown/Markdown.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2000-2025 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.markdown;
+
+import java.util.Objects;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.HasSize;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.dependency.NpmPackage;
+
+/**
+ * Markdown is a component for rendering Markdown content. It takes Markdown
+ * source as input and renders the corresponding HTML.
+ * 
+ * @author Vaadin Ltd
+ */
+@Tag("vaadin-markdown")
+@NpmPackage(value = "@vaadin/polymer-legacy-adapter", version = "24.8.0-alpha15")
+@JsModule("@vaadin/polymer-legacy-adapter/style-modules.js")
+// @NpmPackage(value = "@vaadin/markdown", version = "24.8.0-alpha15")
+@JsModule("@vaadin/markdown/src/vaadin-markdown.js")
+public class Markdown extends Component implements HasSize {
+
+    private String serverMarkdown;
+    private String clientMarkdown;
+
+    /**
+     * Default constructor. Creates an empty Markdown.
+     */
+    public Markdown() {
+    }
+
+    /**
+     * Creates a Markdown with content.
+     *
+     * @param markdown
+     *            the markdown content
+     * @see #setMarkdown(String)
+     */
+    public Markdown(String markdown) {
+        setMarkdown(markdown);
+    }
+
+    /**
+     * Sets the markdown content.
+     * 
+     * @param markdown
+     *            the markdown content
+     */
+    public void setMarkdown(String markdown) {
+        serverMarkdown = markdown;
+        scheduleMarkdownUpdate();
+    }
+
+    /**
+     * Appends the markdown content.
+     * 
+     * @param markdown
+     *            the markdown content to append
+     */
+    public void appendMarkdown(String markdown) {
+        if (serverMarkdown == null) {
+            serverMarkdown = "";
+        }
+        setMarkdown(serverMarkdown + markdown);
+    }
+
+    /**
+     * Gets the markdown content.
+     * 
+     * @return the markdown content
+     */
+    public String getMarkdown() {
+        return serverMarkdown;
+    }
+
+    private void scheduleMarkdownUpdate() {
+        getElement().getNode()
+                .runWhenAttached(ui -> ui.beforeClientResponse(this, ctx -> {
+                    if (Objects.equals(clientMarkdown, serverMarkdown)) {
+                        return;
+                    }
+
+                    if (serverMarkdown != null && clientMarkdown != null
+                            && serverMarkdown.startsWith(clientMarkdown)) {
+                        // This is a simple optimization to a common case where
+                        // new content is streamed at the end of the existing
+                        // content.
+                        // If the updated serverMarkdown starts with the
+                        // clientMarkdown, we can just send the difference to
+                        // the client.
+                        getElement().executeJs("this.markdown += $0",
+                                serverMarkdown
+                                        .substring(clientMarkdown.length()));
+                    } else {
+                        // In all other cases, we need to send the whole updated
+                        // content.
+                        getElement().executeJs("this.markdown = $0",
+                                serverMarkdown);
+                    }
+
+                    clientMarkdown = serverMarkdown;
+                }));
+    }
+}
