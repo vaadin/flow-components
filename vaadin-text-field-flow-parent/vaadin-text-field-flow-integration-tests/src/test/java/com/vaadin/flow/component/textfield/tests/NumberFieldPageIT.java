ec7755a4dd19fdff5fc41713aaeb6fc63e5b170e
web-padawan <iamkulykov@gmail.com>
2025-05-03 11:25:36 +0300

    test: remove ITs for prefix and suffix, update unit tests

--- vaadin-text-field-flow-parent/vaadin-text-field-flow-integration-tests/src/test/java/com/vaadin/flow/component/textfield/tests/NumberFieldPageIT.java
+++ vaadin-text-field-flow-parent/vaadin-text-field-flow-integration-tests/src/test/java/com/vaadin/flow/component/textfield/tests/NumberFieldPageIT.java
@@ -138,36 +138,4 @@ public class NumberFieldPageIT extends AbstractComponentIT {
         String message = $("div").id("clear-message").getText();
         Assert.assertEquals("Old value: 'null'. New value: '123.0'.", message);
     }
-
-    @Test
-    public void dollarFieldHasDollarPrefix() {
-        WebElement dollarField = findElement(By.id("dollar-field"));
-        WebElement span = dollarField.findElement(By.tagName("span"));
-
-        Assert.assertEquals("$", span.getText());
-
-        int spanX = span.getLocation().getX();
-        int middleX = dollarField.getLocation().getX()
-                + dollarField.getSize().getWidth() / 2;
-
-        Assert.assertTrue(
-                "The dollar sign should be located on the left side of the text field",
-                spanX < middleX);
-    }
-
-    @Test
-    public void euroFieldHasEuroSuffix() {
-        WebElement euroField = findElement(By.id("euro-field"));
-        WebElement span = euroField.findElement(By.tagName("span"));
-
-        Assert.assertEquals("€", span.getText());
-
-        int spanX = span.getLocation().getX();
-        int middleX = euroField.getLocation().getX()
-                + euroField.getSize().getWidth() / 2;
-
-        Assert.assertTrue(
-                "The euro sign should be located on the right side of the text field",
-                spanX > middleX);
-    }
 }
