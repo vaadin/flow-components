986d2da53c44228c5f6f10ee1234e63eee47e700
Diego Cardoso <diego@vaadin.com>
2025-05-14 12:20:16 +0200

    fix: try to set value manually only for Firefox

--- vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow-client/src/main/java/com/vaadin/addon/spreadsheet/client/SpreadsheetWidget.java
+++ vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow-client/src/main/java/com/vaadin/addon/spreadsheet/client/SpreadsheetWidget.java
@@ -34,6 +34,7 @@ import com.vaadin.addon.spreadsheet.client.MergedRegionUtil.MergedRegionContaine
 import com.vaadin.addon.spreadsheet.client.SheetTabSheet.SheetTabSheetHandler;
 import com.vaadin.addon.spreadsheet.client.SpreadsheetConnector.CommsTrigger;
 import com.vaadin.addon.spreadsheet.shared.GroupingData;
+import com.vaadin.client.BrowserInfo;
 import com.vaadin.client.Focusable;
 import com.vaadin.client.ServerConnector;
 import com.vaadin.client.Util;
@@ -1229,7 +1230,15 @@ public class SpreadsheetWidget extends Composite implements SheetHandler,
                         var assignedElement = ((Slot) customEditor)
                                 .getAssignedElement();
                         assignedElement.focus();
-                        assignedElement.setNodeValue(enteredCharacter);
+
+                        if (BrowserInfo.get().isFirefox()) {
+                            // Chrome and Safari pass down the keypress event to
+                            // the focused input while Firefox doesn't, so we do
+                            // our best effort to try to set the 'value'
+                            // property of the custom editor
+                            assignedElement.setPropertyString("value",
+                                    enteredCharacter);
+                        }
                     }
                 }
             }
