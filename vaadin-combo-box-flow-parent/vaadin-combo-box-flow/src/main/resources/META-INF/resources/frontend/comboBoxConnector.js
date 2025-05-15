8cfc31dd0ae3dc214172830ff595e66c97e1d4b9
Sascha Ißbrücker <sissbruecker@vaadin.com>
2025-05-15 20:17:13 +0200

    refactor: replace Polymer dependencies in connectors

--- vaadin-combo-box-flow-parent/vaadin-combo-box-flow/src/main/resources/META-INF/resources/frontend/comboBoxConnector.js
+++ vaadin-combo-box-flow-parent/vaadin-combo-box-flow/src/main/resources/META-INF/resources/frontend/comboBoxConnector.js
@@ -1,5 +1,5 @@
-import { Debouncer } from '@polymer/polymer/lib/utils/debounce.js';
-import { timeOut } from '@polymer/polymer/lib/utils/async.js';
+import { Debouncer } from '@vaadin/component-base/src/debounce.js';
+import { timeOut } from '@vaadin/component-base/src/async.js';
 import { ComboBoxPlaceholder } from '@vaadin/combo-box/src/vaadin-combo-box-placeholder.js';
 
 window.Vaadin.Flow.comboBoxConnector = {};
