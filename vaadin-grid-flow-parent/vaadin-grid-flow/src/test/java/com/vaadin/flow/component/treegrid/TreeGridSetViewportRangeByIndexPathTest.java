d26a6026c9babcbd349113834eec97f02879b777
Sergey Vinogradov <mr.vursen@gmail.com>
2025-08-07 13:47:22 +0400

    add more tests, fix rebase error

--- vaadin-grid-flow-parent/vaadin-grid-flow/src/test/java/com/vaadin/flow/component/treegrid/TreeGridSetViewportRangeByIndexPathTest.java
+++ vaadin-grid-flow-parent/vaadin-grid-flow/src/test/java/com/vaadin/flow/component/treegrid/TreeGridSetViewportRangeByIndexPathTest.java
@@ -15,10 +15,6 @@
  */
 package com.vaadin.flow.component.treegrid;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.verify;
-
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -44,7 +40,10 @@ import elemental.json.JsonValue;
 public class TreeGridSetViewportRangeByIndexPathTest {
     private MockUI ui;
 
-    private Update updateMock = mock(Update.class);
+    private GridArrayUpdater arrayUpdater = Mockito
+            .mock(GridArrayUpdater.class);
+
+    private Update arrayUpdate = Mockito.mock(Update.class);
 
     @SuppressWarnings("unchecked")
     private ArgumentCaptor<List<JsonValue>> updateItemsCaptor = ArgumentCaptor
@@ -53,39 +52,33 @@ public class TreeGridSetViewportRangeByIndexPathTest {
     private TreeGrid<String> treeGrid = new TreeGrid<>() {
         @Override
         protected GridArrayUpdater createDefaultArrayUpdater() {
-            return new GridArrayUpdater() {
-                @Override
-                public void initialize() {
-                }
-
-                @Override
-                public Update startUpdate(int sizeChange) {
-                    return updateMock;
-                }
-            };
+            return arrayUpdater;
         }
     };
 
     @Before
     public void init() {
+        Mockito.when(arrayUpdater.startUpdate(Mockito.anyInt()))
+                .thenReturn(arrayUpdate);
+
         TreeData<String> treeData = new TreeData<>();
         treeData.addItems(generateItems(null, 100), (parentItem) -> {
             int depth = parentItem.split("-").length - 1;
-            if (depth < 3) {
+            if (depth <= 2) {
                 return generateItems(parentItem, 3);
             }
             return Collections.emptyList();
         });
+        treeGrid.setTreeData(treeData);
         treeGrid.addDataGenerator(
                 (item, jsonObject) -> jsonObject.put("name", item));
-        treeGrid.setTreeData(treeData);
         treeGrid.setPageSize(5);
 
         ui = new MockUI();
         ui.add(treeGrid);
 
         fakeClientCommunication();
-        reset(updateMock);
+        Mockito.reset(arrayUpdate);
     }
 
     @After
@@ -94,13 +87,13 @@ public class TreeGridSetViewportRangeByIndexPathTest {
     }
 
     @Test
-    public void setViewportRangeByIndexPath_assertFlatIndex() {
+    public void setViewportRangeByIndexPath_correctFlatIndexReturned() {
         treeGrid.expand("Item 50", "Item 50-0", "Item 50-1", "Item 50-2");
 
-        int ITEM_50_SIZE = 3;
-        int ITEM_50_0_SIZE = 3;
-        int ITEM_50_1_SIZE = 3;
-        int ITEM_50_2_SIZE = 3;
+        int SIZE_50 = 3;
+        int SIZE_50_0 = 3;
+        int SIZE_50_1 = 3;
+        int SIZE_50_2 = 3;
 
         Assert.assertEquals(0,
                 treeGrid.setViewportRangeByIndexPath(new int[] { 0 }, 5));
@@ -112,17 +105,17 @@ public class TreeGridSetViewportRangeByIndexPathTest {
 
         treeGrid.getDataCommunicator().reset();
 
-        Assert.assertEquals(53 + ITEM_50_0_SIZE, treeGrid
+        Assert.assertEquals(53 + SIZE_50_0, treeGrid
                 .setViewportRangeByIndexPath(new int[] { 50, 1, 0 }, 5));
 
         treeGrid.getDataCommunicator().reset();
 
-        Assert.assertEquals(54 + ITEM_50_0_SIZE + ITEM_50_1_SIZE, treeGrid
+        Assert.assertEquals(54 + SIZE_50_0 + SIZE_50_1, treeGrid
                 .setViewportRangeByIndexPath(new int[] { 50, 2, 0 }, 5));
 
         treeGrid.getDataCommunicator().reset();
 
-        Assert.assertEquals(59 + ITEM_50_SIZE + ITEM_50_2_SIZE,
+        Assert.assertEquals(59 + SIZE_50 + SIZE_50_2,
                 treeGrid.setViewportRangeByIndexPath(new int[] { 59 }, 5));
 
         treeGrid.getDataCommunicator().reset();
@@ -132,7 +125,41 @@ public class TreeGridSetViewportRangeByIndexPathTest {
     }
 
     @Test
-    public void setViewportRangeByIndexPath_assertViewportRange() {
+    public void pathWithTooLargeIndexes_setViewportRangeByIndexPath_correctFlatIndexReturned() {
+        treeGrid.expand("Item 99", "Item 99-2");
+
+        int SIZE_99 = 3;
+        int SIZE_99_2 = 3;
+
+        Assert.assertEquals(99 + SIZE_99 + SIZE_99_2,
+                treeGrid.setViewportRangeByIndexPath(
+                        new int[] { 1000, 1000, 1000 }, 5));
+    }
+
+    @Test
+    public void pathWithNegativeIndexes_setViewportRangeByIndexPath_correctFlatIndexReturned() {
+        treeGrid.expand("Item 98", "Item 98-2", "Item 99", "Item 99-1");
+
+        int SIZE_98 = 3;
+        int SIZE_98_2 = 3;
+        int SIZE_99_1 = 3;
+
+        Assert.assertEquals(99 + SIZE_98 + SIZE_98_2,
+                treeGrid.setViewportRangeByIndexPath(new int[] { -1 }, 5));
+
+        treeGrid.getDataCommunicator().reset();
+
+        Assert.assertEquals(99 + SIZE_98 + SIZE_98_2 + 2,
+                treeGrid.setViewportRangeByIndexPath(new int[] { -1, -2 }, 5));
+
+        treeGrid.getDataCommunicator().reset();
+
+        Assert.assertEquals(99 + SIZE_98 + SIZE_98_2 + 2 + SIZE_99_1, treeGrid
+                .setViewportRangeByIndexPath(new int[] { -1, -2, -1 }, 5));
+    }
+
+    @Test
+    public void setViewportRangeByIndexPath_viewportRangeReturned() {
         treeGrid.expandRecursively(treeGrid.getTreeData().getRootItems(), 1);
 
         treeGrid.setViewportRangeByIndexPath(new int[] { 0 }, 5);
@@ -153,7 +180,7 @@ public class TreeGridSetViewportRangeByIndexPathTest {
     }
 
     @Test
-    public void setSmallPageSize_setViewportRangeByIndexPath_throwsWhenBufferExceedsLimit() {
+    public void setSmallPageSize_setViewportRangeByIndexPath_throwsWhenPaddingExceedsLimit() {
         treeGrid.setPageSize(20);
 
         treeGrid.setViewportRangeByIndexPath(new int[] { 0 }, 500);
@@ -164,7 +191,7 @@ public class TreeGridSetViewportRangeByIndexPathTest {
     }
 
     @Test
-    public void setLargePageSize_setViewportRangeByIndexPath_throwsWhenBufferExceedsLimit() {
+    public void setLargePageSize_setViewportRangeByIndexPath_throwsWhenPaddingExceedsLimit() {
         treeGrid.setPageSize(80);
 
         treeGrid.setViewportRangeByIndexPath(new int[] { 0 }, 800);
@@ -175,7 +202,8 @@ public class TreeGridSetViewportRangeByIndexPathTest {
     }
 
     private LinkedList<String> captureViewportRange() {
-        verify(updateMock).set(Mockito.anyInt(), updateItemsCaptor.capture());
+        Mockito.verify(arrayUpdate).set(Mockito.anyInt(),
+                updateItemsCaptor.capture());
 
         return updateItemsCaptor.getValue().stream().map(
                 (jsonObject) -> ((JsonObject) jsonObject).getString("name"))
@@ -188,8 +216,7 @@ public class TreeGridSetViewportRangeByIndexPathTest {
         Assert.assertEquals(size, viewportRange.size());
         Assert.assertEquals(firstItem, viewportRange.getFirst());
         Assert.assertEquals(lastItem, viewportRange.getLast());
-
-        reset(updateMock);
+        Mockito.reset(arrayUpdate);
     }
 
     private void fakeClientCommunication() {
