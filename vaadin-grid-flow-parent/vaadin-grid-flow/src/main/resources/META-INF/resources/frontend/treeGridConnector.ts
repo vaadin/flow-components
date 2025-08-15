dca3bd736bf760fff471b67bdd999416881e0a50
Sergey Vinogradov <mr.vursen@gmail.com>
2025-08-15 15:04:00 +0400

    explain __loadCachePage override

--- vaadin-grid-flow-parent/vaadin-grid-flow/src/main/resources/META-INF/resources/frontend/treeGridConnector.ts
+++ vaadin-grid-flow-parent/vaadin-grid-flow/src/main/resources/META-INF/resources/frontend/treeGridConnector.ts
@@ -14,7 +14,14 @@ window.Vaadin.Flow.treeGridConnector.initLazy = function (grid) {
     return [renderedRows[0]?.index ?? 0, renderedRows[renderedRows.length - 1]?.index ?? 0];
   }
 
+  // TODO: Use _shouldLoadCachePage instead when released
   grid._dataProviderController.__loadCachePage = function (cache, page) {
+    // `$server.setViewportRangeByIndexPath` sets a preloaded viewport range based on
+    // the provided index path and `padding` parameter. Setting the new range clears
+    // the old range, which is still visible because the actual scroll happens only
+    // after all connector calls in that update are processed. This check prevents
+    // the old range from being unnecessarily re-requested while the new range is
+    // still being processed, which could cause flickering.
     if (grid.__pendingScrollToIndexes) {
       return;
     }
