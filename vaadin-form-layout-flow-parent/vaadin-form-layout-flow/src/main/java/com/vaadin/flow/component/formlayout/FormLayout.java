672886008e68133b89f12eec801832a4615fa630
Sascha Ißbrücker <sissbruecker@vaadin.com>
2025-08-19 08:43:04 +0200

    chore: bump web components to 24.9.0-alpha1

--- vaadin-form-layout-flow-parent/vaadin-form-layout-flow/src/main/java/com/vaadin/flow/component/formlayout/FormLayout.java
+++ vaadin-form-layout-flow-parent/vaadin-form-layout-flow/src/main/java/com/vaadin/flow/component/formlayout/FormLayout.java
@@ -172,9 +172,9 @@ import elemental.json.JsonValue;
  * @author Vaadin Ltd
  */
 @Tag("vaadin-form-layout")
-@NpmPackage(value = "@vaadin/polymer-legacy-adapter", version = "24.8.5")
+@NpmPackage(value = "@vaadin/polymer-legacy-adapter", version = "24.9.0-alpha1")
 @JsModule("@vaadin/polymer-legacy-adapter/style-modules.js")
-@NpmPackage(value = "@vaadin/form-layout", version = "24.8.5")
+@NpmPackage(value = "@vaadin/form-layout", version = "24.9.0-alpha1")
 @JsModule("@vaadin/form-layout/src/vaadin-form-layout.js")
 public class FormLayout extends Component
         implements HasSize, HasStyle, HasComponents, ClickNotifier<FormLayout> {
@@ -303,9 +303,9 @@ public class FormLayout extends Component
      * @author Vaadin Ltd
      */
     @Tag("vaadin-form-item")
-    @NpmPackage(value = "@vaadin/polymer-legacy-adapter", version = "24.8.5")
+    @NpmPackage(value = "@vaadin/polymer-legacy-adapter", version = "24.9.0-alpha1")
     @JsModule("@vaadin/polymer-legacy-adapter/style-modules.js")
-    @NpmPackage(value = "@vaadin/form-layout", version = "24.8.5")
+    @NpmPackage(value = "@vaadin/form-layout", version = "24.9.0-alpha1")
     @JsModule("@vaadin/form-layout/src/vaadin-form-item.js")
     public static class FormItem extends Component
             implements HasComponents, HasStyle, ClickNotifier<FormItem> {
@@ -411,9 +411,9 @@ public class FormLayout extends Component
      * @author Vaadin Ltd
      */
     @Tag("vaadin-form-row")
-    @NpmPackage(value = "@vaadin/polymer-legacy-adapter", version = "24.8.5")
+    @NpmPackage(value = "@vaadin/polymer-legacy-adapter", version = "24.9.0-alpha1")
     @JsModule("@vaadin/polymer-legacy-adapter/style-modules.js")
-    @NpmPackage(value = "@vaadin/form-layout", version = "24.8.5")
+    @NpmPackage(value = "@vaadin/form-layout", version = "24.9.0-alpha1")
     @JsModule("@vaadin/form-layout/src/vaadin-form-row.js")
     public static class FormRow extends Component implements HasComponents {
 
