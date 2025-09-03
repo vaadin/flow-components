0055ca06f652f1d0259b1428a390124c4d6fc427
Herberts Markuns <herberts@vaadin.com>
2025-09-03 11:32:44 +0300

    Add tests to verify override of spreadsheet event handlers

---
+++ vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow/src/test/java/com/vaadin/flow/component/spreadsheet/tests/CustomHandlerTest.java
@@ -0,0 +1,76 @@
+package com.vaadin.flow.component.spreadsheet.tests;
+
+import com.vaadin.flow.component.ComponentEventBus;
+import com.vaadin.flow.component.ComponentUtil;
+import com.vaadin.flow.component.spreadsheet.Spreadsheet;
+import com.vaadin.flow.component.spreadsheet.SpreadsheetHandlerImpl;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+
+public class CustomHandlerTest {
+
+    @Test
+    public void singleOriginalSpreadsheetEventIsRegistered() {
+        var originalHandler = mock(SpreadsheetHandlerImpl.class);
+
+        var spreadsheet = new Spreadsheet() {
+            @Override
+            public ComponentEventBus getEventBus() {
+                return super.getEventBus();
+            }
+
+            @Override
+            protected SpreadsheetHandlerImpl createDefaultHandler() {
+                return originalHandler;
+            }
+        };
+
+        var activeListeners = spreadsheet.getEventBus().getListeners(Spreadsheet.SpreadsheetEvent.class);
+        Assert.assertEquals(1, activeListeners.size());
+
+        // Mock spreadsheet event to trigger the original handler
+        var spreadsheetEvent = mock(Spreadsheet.SpreadsheetEvent.class);
+        when(spreadsheetEvent.getType()).thenReturn("onConnectorInit");
+        
+        verify(originalHandler, times(0)).onConnectorInit();
+        ComponentUtil.fireEvent(spreadsheet, spreadsheetEvent); // Fire spreadsheet event
+        verify(originalHandler).onConnectorInit();
+    }
+
+
+    @Test
+    public void newCustomHandlerOverridesOriginal() {
+        var originalHandler = mock(SpreadsheetHandlerImpl.class);
+        var newHandler = mock(SpreadsheetHandlerImpl.class);
+
+        var spreadsheet = new Spreadsheet() {
+            @Override
+            public ComponentEventBus getEventBus() {
+                return super.getEventBus();
+            }
+
+            @Override
+            protected SpreadsheetHandlerImpl createDefaultHandler() {
+                return originalHandler;
+            }
+        };
+
+        spreadsheet.setSpreadsheetHandler(newHandler);
+
+        var activeListeners = spreadsheet.getEventBus().getListeners(Spreadsheet.SpreadsheetEvent.class);
+        Assert.assertEquals(1, activeListeners.size());
+
+        // Mock spreadsheet event to trigger the new handler
+        var spreadsheetEvent = mock(Spreadsheet.SpreadsheetEvent.class);
+        when(spreadsheetEvent.getType()).thenReturn("onConnectorInit");
+
+        verify(newHandler, times(0)).onConnectorInit();
+        ComponentUtil.fireEvent(spreadsheet, spreadsheetEvent); // Fire spreadsheet event
+        verify(originalHandler, times(0)).onConnectorInit();
+        verify(newHandler).onConnectorInit();
+    }
+
+
+}
