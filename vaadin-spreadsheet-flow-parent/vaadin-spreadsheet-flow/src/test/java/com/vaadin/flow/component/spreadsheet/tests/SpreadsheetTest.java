d3ed464066f0c4bf085452923c8010cd607e80cf
Herberts Markuns <herberts@vaadin.com>
2025-09-09 13:02:04 +0300

    Cleanup

--- vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow/src/test/java/com/vaadin/flow/component/spreadsheet/tests/SpreadsheetTest.java
+++ vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow/src/test/java/com/vaadin/flow/component/spreadsheet/tests/SpreadsheetTest.java
@@ -1,9 +1,18 @@
+/**
+ * Copyright 2000-2025 Vaadin Ltd.
+ *
+ * This program is available under Vaadin Commercial License and Service Terms.
+ *
+ * See {@literal <https://vaadin.com/commercial-license-and-service-terms>} for the full
+ * license.
+ */
 package com.vaadin.flow.component.spreadsheet.tests;
 
-import com.vaadin.flow.component.spreadsheet.Spreadsheet;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import com.vaadin.flow.component.spreadsheet.Spreadsheet;
 
 public class SpreadsheetTest {
     @Test
