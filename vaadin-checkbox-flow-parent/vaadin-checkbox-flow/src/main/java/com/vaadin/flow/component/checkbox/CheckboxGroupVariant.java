9ae424ce6edeba1df9b89c11e5dcb42b353548e3
ugur-vaadin <ugur@vaadin.com>
2025-10-14 17:39:52 +0300

    feat: use both horizontal and vertical in group variants

--- vaadin-checkbox-flow-parent/vaadin-checkbox-flow/src/main/java/com/vaadin/flow/component/checkbox/CheckboxGroupVariant.java
+++ vaadin-checkbox-flow-parent/vaadin-checkbox-flow/src/main/java/com/vaadin/flow/component/checkbox/CheckboxGroupVariant.java
@@ -21,14 +21,19 @@ import com.vaadin.flow.component.shared.ThemeVariant;
  * Set of theme variants applicable for {@code vaadin-checkbox-group} component.
  */
 public enum CheckboxGroupVariant implements ThemeVariant {
+    /**
+     * @deprecated Use {@link #VERTICAL} instead.
+     */
+    @Deprecated(since = "25.0", forRemoval = true)
     LUMO_VERTICAL("vertical"),
     /**
      * @deprecated Use {@link #HELPER_ABOVE_FIELD} instead.
      */
     @Deprecated(since = "25.0", forRemoval = true)
     LUMO_HELPER_ABOVE_FIELD("helper-above-field"),
-    HELPER_ABOVE_FIELD("helper-above-field"),
-    HORIZONTAL("horizontal");
+    HORIZONTAL("horizontal"),
+    VERTICAL("vertical"),
+    HELPER_ABOVE_FIELD("helper-above-field");
 
     private final String variant;
 
