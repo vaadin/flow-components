275f72767eea7d31d923a7d1644577caf69a3a14
Sascha Ißbrücker <sissbruecker@vaadin.com>
2025-11-11 18:54:58 +0100

    fix invalid getProperty usage

--- vaadin-combo-box-flow-parent/vaadin-combo-box-flow-integration-tests/src/test/java/com/vaadin/flow/component/combobox/test/ClearValueIT.java
+++ vaadin-combo-box-flow-parent/vaadin-combo-box-flow-integration-tests/src/test/java/com/vaadin/flow/component/combobox/test/ClearValueIT.java
@@ -179,8 +179,8 @@ public class ClearValueIT extends AbstractComponentIT {
                 comboBox.getSelectedText());
         Assert.assertEquals(String.format(
                 "Unexpected 'allowCustomValue' property name for combo box with id '%s'",
-                comboBoxId), Boolean.toString(allowCustomValue),
-                comboBox.getPropertyString("allowCustomValue"));
+                comboBoxId), allowCustomValue,
+                comboBox.getPropertyBoolean("allowCustomValue"));
 
         findElement(By.id(buttonId)).click();
 
