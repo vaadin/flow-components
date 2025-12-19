30122149f52e44628727ce1576f3819c24aef431
Sergey Vinogradov <mr.vursen@gmail.com>
2025-12-19 16:14:34 +0400

    refactor: add GridDataCommunicator for Grid-specific logic (#8387)

---
+++ vaadin-grid-flow-parent/vaadin-grid-flow/src/main/java/com/vaadin/flow/component/grid/GridDataCommunicator.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2000-2025 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.grid;
+
+import com.vaadin.flow.data.provider.ArrayUpdater;
+import com.vaadin.flow.data.provider.CompositeDataGenerator;
+import com.vaadin.flow.data.provider.DataCommunicator;
+import com.vaadin.flow.dom.Element;
+
+/**
+ * WARNING: This class is for internal use only.
+ */
+class GridDataCommunicator<T> extends DataCommunicator<T> {
+    public GridDataCommunicator(Element element,
+            CompositeDataGenerator<T> dataGenerator,
+            ArrayUpdater arrayUpdater) {
+        super(dataGenerator, arrayUpdater, data -> element
+                .callJsFunction("$connector.updateFlatData", data),
+                element.getNode());
+    }
+}
