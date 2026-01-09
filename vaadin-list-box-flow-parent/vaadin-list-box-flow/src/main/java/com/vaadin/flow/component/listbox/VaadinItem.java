226d8bd5dff0a5f8fe7dc39dcd327e372e7d1b68
Sascha Ißbrücker <sissbruecker@vaadin.com>
2026-01-09 11:31:03 +0100

    chore: bump components to 25.1.0-alpha1 (#8433)
    Co-authored-by: Zhe Sun <31067185+ZheSun88@users.noreply.github.com>

--- vaadin-list-box-flow-parent/vaadin-list-box-flow/src/main/java/com/vaadin/flow/component/listbox/VaadinItem.java
+++ vaadin-list-box-flow-parent/vaadin-list-box-flow/src/main/java/com/vaadin/flow/component/listbox/VaadinItem.java
@@ -32,7 +32,7 @@ import com.vaadin.flow.data.binder.HasItemComponents;
  *            type of the item represented by this component
  */
 @Tag("vaadin-item")
-@NpmPackage(value = "@vaadin/item", version = "25.0.1")
+@NpmPackage(value = "@vaadin/item", version = "25.1.0-alpha1")
 @JsModule("@vaadin/item/src/vaadin-item.js")
 class VaadinItem<T> extends Component
         implements HasItemComponents.ItemComponent<T>, HasComponents {
