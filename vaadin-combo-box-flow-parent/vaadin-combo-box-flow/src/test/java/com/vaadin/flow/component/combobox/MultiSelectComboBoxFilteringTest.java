dd718e8c7f9803bd61571d6eeb9aa8eb59949925
Anton Platonov <platosha@gmail.com>
2026-01-20 15:57:44 +0200

    refactor: move MockUI to root package (#8507)

--- vaadin-combo-box-flow-parent/vaadin-combo-box-flow/src/test/java/com/vaadin/flow/component/combobox/MultiSelectComboBoxFilteringTest.java
+++ vaadin-combo-box-flow-parent/vaadin-combo-box-flow/src/test/java/com/vaadin/flow/component/combobox/MultiSelectComboBoxFilteringTest.java
@@ -23,7 +23,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.vaadin.tests.dataprovider.MockUI;
+import com.vaadin.tests.MockUI;
 
 public class MultiSelectComboBoxFilteringTest {
     private MockUI ui;
