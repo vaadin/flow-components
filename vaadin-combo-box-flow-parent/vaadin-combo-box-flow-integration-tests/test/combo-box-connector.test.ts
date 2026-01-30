2d8ca3c3bc7753cf538104d42b070c07688034f5
Sascha Ißbrücker <sissbruecker@vaadin.com>
2026-01-30 16:55:59 +0100

    fix: clear filter debouncer on connector reset (#8563) (CP: 25.0) (#8579)

---
+++ vaadin-combo-box-flow-parent/vaadin-combo-box-flow-integration-tests/test/combo-box-connector.test.ts
@@ -0,0 +1,62 @@
+import { expect, fixtureSync } from '@open-wc/testing';
+import { comboBoxConnector, FlowComboBox, init } from './shared.ts';
+import '@vaadin/combo-box';
+import * as sinon from 'sinon';
+
+describe('combo-box connector', () => {
+  let comboBox: FlowComboBox;
+
+  beforeEach(() => {
+    comboBox = fixtureSync('<vaadin-combo-box></vaadin-combo-box>');
+    init(comboBox);
+  });
+
+  it('should not reinitialize the connector', () => {
+    const connector = comboBox.$connector;
+    comboBoxConnector.initLazy(comboBox);
+    expect(comboBox.$connector).to.equal(connector);
+  });
+
+  describe('filter debouncing', () => {
+    let clock: sinon.SinonFakeTimers;
+
+    beforeEach(async () => {
+      clock = sinon.useFakeTimers({
+        toFake: ['setTimeout', 'clearTimeout']
+      });
+    });
+
+    afterEach(() => {
+      clock.restore();
+    });
+
+    it('should debounce filter requests with default timeout', () => {
+      comboBox.dataProvider!({ page: 0, pageSize: comboBox.pageSize, filter: 'a' }, () => {});
+      expect(comboBox.$server.setViewportRange).to.be.not.called;
+      clock.tick(500);
+      expect(comboBox.$server.setViewportRange).to.be.calledOnce;
+
+      comboBox.$server.setViewportRange.resetHistory();
+
+      comboBox.dataProvider!({ page: 0, pageSize: comboBox.pageSize, filter: 'ab' }, () => {});
+      clock.tick(250);
+      comboBox.dataProvider!({ page: 0, pageSize: comboBox.pageSize, filter: 'abc' }, () => {});
+      clock.tick(250);
+      expect(comboBox.$server.setViewportRange).to.be.not.called;
+      clock.tick(250);
+      expect(comboBox.$server.setViewportRange).to.be.calledOnce;
+    });
+
+    it('should cancel filter request when the connector is reset', () => {
+      comboBox.dataProvider!({ page: 0, pageSize: comboBox.pageSize, filter: 'test' }, () => {});
+      expect(comboBox._filterDebouncer).to.exist;
+
+      comboBox.$connector.reset();
+      expect(comboBox._filterDebouncer).to.not.exist;
+
+      clock.tick(600);
+
+      expect(comboBox.$server.setViewportRange).to.not.be.called;
+    });
+  });
+});
