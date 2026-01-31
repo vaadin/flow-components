9f053bbc56f11ae6b5425d54956ed20e187039be
Sascha Ißbrücker <sissbruecker@vaadin.com>
2026-01-31 10:13:57 +0100

    refactor UploadFileList

--- vaadin-upload-flow-parent/vaadin-upload-flow/src/main/java/com/vaadin/flow/component/upload/UploadFileList.java
+++ vaadin-upload-flow-parent/vaadin-upload-flow/src/main/java/com/vaadin/flow/component/upload/UploadFileList.java
@@ -17,16 +17,11 @@ package com.vaadin.flow.component.upload;
 
 import java.util.Objects;
 
-import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Tag;
-import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dependency.NpmPackage;
-import com.vaadin.flow.function.SerializableConsumer;
 import com.vaadin.flow.internal.JacksonUtils;
-
-import tools.jackson.databind.node.ObjectNode;
 
 /**
  * A component that displays the list of files being uploaded. When linked to an
@@ -83,12 +78,7 @@ public class UploadFileList extends Component implements HasUploadManager {
     public void setI18n(UploadFileListI18N i18n) {
         this.i18n = Objects.requireNonNull(i18n,
                 "The i18n properties object should not be null");
-
-        runBeforeClientResponse(ui -> {
-            if (i18n == this.i18n) {
-                setI18nWithJS();
-            }
-        });
+        getElement().setPropertyJson("i18n", JacksonUtils.beanToJson(i18n));
     }
 
     /**
@@ -102,41 +92,5 @@ public class UploadFileList extends Component implements HasUploadManager {
      */
     public UploadFileListI18N getI18n() {
         return i18n;
-    }
-
-    @Override
-    protected void onAttach(AttachEvent attachEvent) {
-        super.onAttach(attachEvent);
-
-        // Element state is not persisted across attach/detach
-        if (this.i18n != null) {
-            setI18nWithJS();
-        }
-    }
-
-    private void setI18nWithJS() {
-        ObjectNode i18nJson = JacksonUtils.beanToJson(this.i18n);
-
-        // Assign new I18N object to WC, by deeply merging the existing
-        // WC I18N, and the values from the new UploadFileListI18N instance,
-        // into an empty object
-        getElement().executeJs(
-                "const file = Object.assign({}, this.i18n.file, $0.file);"
-                        + "const error = Object.assign({}, this.i18n.error, $0.error);"
-                        + "const uploadingStatus = Object.assign({}, this.i18n.uploading.status, $0.uploading && $0.uploading.status);"
-                        + "const uploadingRemainingTime = Object.assign({}, this.i18n.uploading.remainingTime, $0.uploading && $0.uploading.remainingTime);"
-                        + "const uploadingError = Object.assign({}, this.i18n.uploading.error, $0.uploading && $0.uploading.error);"
-                        + "const uploading = {status: uploadingStatus,"
-                        + "  remainingTime: uploadingRemainingTime,"
-                        + "  error: uploadingError};"
-                        + "const units = $0.units || this.i18n.units;"
-                        + "this.i18n = Object.assign({}, this.i18n, $0, {"
-                        + "  file: file, error: error, uploading: uploading, units: units});",
-                i18nJson);
-    }
-
-    private void runBeforeClientResponse(SerializableConsumer<UI> command) {
-        getElement().getNode().runWhenAttached(ui -> ui
-                .beforeClientResponse(this, context -> command.accept(ui)));
     }
 }
