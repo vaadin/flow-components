5854e23f3a4e63e37c0a1ff9ce6bb65f6641e431
Sascha Ißbrücker <sissbruecker@vaadin.com>
2026-01-31 09:37:16 +0100

    make dashboard i18n ITs verify DOM

--- vaadin-dashboard-flow-parent/vaadin-dashboard-flow-integration-tests/src/main/java/com/vaadin/flow/component/dashboard/tests/DashboardI18nPage.java
+++ vaadin-dashboard-flow-parent/vaadin-dashboard-flow-integration-tests/src/main/java/com/vaadin/flow/component/dashboard/tests/DashboardI18nPage.java
@@ -18,48 +18,6 @@ import com.vaadin.flow.router.Route;
 @Route("vaadin-dashboard/i18n")
 public class DashboardI18nPage extends Div {
 
-    enum I18nEntry {
-        SELECT_SECTION("selectSection"),
-
-        SELECT_WIDGET("selectWidget"),
-
-        REMOVE("remove"),
-
-        RESIZE("resize"),
-
-        RESIZE_APPLY("resizeApply"),
-
-        RESIZE_SHRINK_WIDTH("resizeShrinkWidth"),
-
-        RESIZE_GROW_WIDTH("resizeGrowWidth"),
-
-        RESIZE_SHRINK_HEIGHT("resizeShrinkHeight"),
-
-        RESIZE_GROW_HEIGHT("resizeGrowHeight"),
-
-        MOVE("move"),
-
-        MOVE_APPLY("moveApply"),
-
-        MOVE_FORWARD("moveForward"),
-
-        MOVE_BACKWARD("moveBackward");
-
-        private final String key;
-
-        I18nEntry(String key) {
-            this.key = key;
-        }
-
-        public String getKey() {
-            return key;
-        }
-
-        public String getCustomValue() {
-            return "Custom " + key;
-        }
-    }
-
     public DashboardI18nPage() {
         Dashboard dashboard = new Dashboard();
         dashboard.setEditable(true);
@@ -72,29 +30,15 @@ public class DashboardI18nPage extends Div {
                 e -> dashboard.setI18n(getCustomI18n()));
         setCustomI18n.setId("set-custom-i18n");
 
-        add(setCustomI18n, dashboard);
+        NativeButton setEmptyI18n = new NativeButton("Set empty i18n",
+                e -> dashboard.setI18n(new Dashboard.DashboardI18n()));
+        setEmptyI18n.setId("set-empty-i18n");
+
+        add(setCustomI18n, setEmptyI18n, dashboard);
     }
 
     private static Dashboard.DashboardI18n getCustomI18n() {
-        Dashboard.DashboardI18n dashboardI18n = new Dashboard.DashboardI18n();
-        dashboardI18n
-                .setSelectSection(I18nEntry.SELECT_SECTION.getCustomValue());
-        dashboardI18n.setSelectWidget(I18nEntry.SELECT_WIDGET.getCustomValue());
-        dashboardI18n.setRemove(I18nEntry.REMOVE.getCustomValue());
-        dashboardI18n.setResize(I18nEntry.RESIZE.getCustomValue());
-        dashboardI18n.setResizeApply(I18nEntry.RESIZE_APPLY.getCustomValue());
-        dashboardI18n.setResizeShrinkWidth(
-                I18nEntry.RESIZE_SHRINK_WIDTH.getCustomValue());
-        dashboardI18n.setResizeGrowWidth(
-                I18nEntry.RESIZE_GROW_WIDTH.getCustomValue());
-        dashboardI18n.setResizeShrinkHeight(
-                I18nEntry.RESIZE_SHRINK_HEIGHT.getCustomValue());
-        dashboardI18n.setResizeGrowHeight(
-                I18nEntry.RESIZE_GROW_HEIGHT.getCustomValue());
-        dashboardI18n.setMove(I18nEntry.MOVE.getCustomValue());
-        dashboardI18n.setMoveApply(I18nEntry.MOVE_APPLY.getCustomValue());
-        dashboardI18n.setMoveForward(I18nEntry.MOVE_FORWARD.getCustomValue());
-        dashboardI18n.setMoveBackward(I18nEntry.MOVE_BACKWARD.getCustomValue());
-        return dashboardI18n;
+        return new Dashboard.DashboardI18n().setRemove("Custom remove")
+                .setMove("Custom move").setResize("Custom resize");
     }
 }
