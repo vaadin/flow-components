379b7bb64158cb301181052650a896a77813bd41
Sascha Ißbrücker <sissbruecker@vaadin.com>
2026-02-10 09:48:53 +0100

    chore: bump web components to 24.10.0-alpha1 (#8643)

--- vaadin-flow-components-shared-parent/vaadin-flow-components-base/src/main/java/com/vaadin/flow/component/shared/TooltipConfiguration.java
+++ vaadin-flow-components-shared-parent/vaadin-flow-components-base/src/main/java/com/vaadin/flow/component/shared/TooltipConfiguration.java
@@ -27,7 +27,7 @@ import com.vaadin.flow.server.VaadinService;
  *
  * @author Vaadin Ltd
  */
-@NpmPackage(value = "@vaadin/tooltip", version = "24.9.10")
+@NpmPackage(value = "@vaadin/tooltip", version = "24.10.0-alpha1")
 @JsModule("./tooltip.ts")
 public class TooltipConfiguration implements Serializable {
 
