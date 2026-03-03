d2951f9c90f14763af753332cb5a6f3c83ac13aa
Sascha Ißbrücker <sissbruecker@vaadin.com>
2026-03-03 09:52:33 +0100

    Revert "feat: add signal-based text binding to Notification (#8502)" (#8814)
    This reverts commit b009aae0

--- vaadin-notification-flow-parent/vaadin-notification-flow/src/test/java/com/vaadin/flow/component/notification/NotificationTest.java
+++ vaadin-notification-flow-parent/vaadin-notification-flow/src/test/java/com/vaadin/flow/component/notification/NotificationTest.java
@@ -31,7 +31,6 @@ import com.vaadin.flow.component.HasStyle;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.notification.Notification.Position;
-import com.vaadin.flow.signals.local.ValueSignal;
 import com.vaadin.tests.MockUIRule;
 
 import net.jcip.annotations.NotThreadSafe;
@@ -101,7 +100,7 @@ public class NotificationTest {
 
     @Test
     public void constructorsWithNoDurationCreateNotCloseableNotifications() {
-        final long constructorsWithoutDurationParameter = 4L;
+        final long constructorsWithoutDurationParameter = 3L;
 
         long constructorsWithNoIntParameter = Stream
                 .of(Notification.class.getConstructors())
@@ -116,8 +115,7 @@ public class NotificationTest {
 
         Collection<Notification> notificationsToCheck = Arrays.asList(
                 new Notification(), new Notification("test"),
-                new Notification(new Span("one"), new Span("two")),
-                new Notification(new ValueSignal<>("signal")));
+                new Notification(new Span("one"), new Span("two")));
 
         Assert.assertEquals(
                 "Not all of the Notification constructors without duration parameter are tested",
