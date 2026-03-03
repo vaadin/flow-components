c8a18cfa12cf65f031a68bbb102db26db066bc18
Sascha Ißbrücker <sissbruecker@vaadin.com>
2026-03-03 11:31:36 +0100

    fix: mark AbstractLogin.bindEnabled as not supported

---
+++ vaadin-login-flow-parent/vaadin-login-flow/src/test/java/com/vaadin/flow/component/login/LoginFormSignalTest.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2000-2026 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.login;
+
+import org.junit.Test;
+
+import com.vaadin.flow.signals.local.ValueSignal;
+import com.vaadin.tests.AbstractSignalsUnitTest;
+
+public class LoginFormSignalTest extends AbstractSignalsUnitTest {
+    @Test(expected = UnsupportedOperationException.class)
+    public void bindEnabled_throwsException() {
+        var login = new LoginForm();
+        login.bindEnabled(new ValueSignal<>(false));
+    }
+}
