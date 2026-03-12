37a851f5c94e78b13d2ad6e5f3b21eb5bb1c2ebb
Ugur Saglam <106508695+ugur-vaadin@users.noreply.github.com>
2026-03-12 10:21:58 +0200

    fix: sync presentation value on data change (#8891)
    * fix: sync presentation value on data change
    
    * test: move button click to server value retrieve method

---
+++ vaadin-list-box-flow-parent/vaadin-list-box-flow-integration-tests/src/main/java/com/vaadin/flow/component/listbox/test/SelectionPreservationPage.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2000-2026 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.listbox.test;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.NativeButton;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.listbox.MultiSelectListBox;
+import com.vaadin.flow.component.shared.SelectionPreservationMode;
+import com.vaadin.flow.data.provider.ListDataProvider;
+import com.vaadin.flow.router.Route;
+
+@Route("vaadin-list-box/selection-preservation")
+public class SelectionPreservationPage extends Div {
+
+    public SelectionPreservationPage() {
+        var items = new ArrayList<>(IntStream.range(0, 10).boxed().toList());
+        var dataProvider = new ListDataProvider<>(items);
+
+        var listBox = new MultiSelectListBox<Integer>();
+        listBox.setItems(dataProvider);
+        listBox.setItemLabelGenerator(String::valueOf);
+
+        var modePreserveAll = new NativeButton("Preserve All",
+                e -> listBox.setSelectionPreservationMode(
+                        SelectionPreservationMode.PRESERVE_ALL));
+        modePreserveAll.setId("mode-preserve-all");
+
+        var modePreserveExisting = new NativeButton("Preserve Existing",
+                e -> listBox.setSelectionPreservationMode(
+                        SelectionPreservationMode.PRESERVE_EXISTING));
+        modePreserveExisting.setId("mode-preserve-existing");
+
+        var modeDiscard = new NativeButton("Discard",
+                e -> listBox.setSelectionPreservationMode(
+                        SelectionPreservationMode.DISCARD));
+        modeDiscard.setId("mode-discard");
+
+        var refreshAll = new NativeButton("Refresh all",
+                e -> dataProvider.refreshAll());
+        refreshAll.setId("refresh-all");
+
+        var removeItem5 = new NativeButton("Remove item 5",
+                e -> items.remove(Integer.valueOf(5)));
+        removeItem5.setId("remove-item-5");
+
+        var serverValue = new Span();
+        serverValue.setId("server-value");
+
+        var showServerValue = new NativeButton("Show server value",
+                e -> serverValue.setText(listBox.getSelectedItems().stream()
+                        .sorted().map(String::valueOf)
+                        .collect(Collectors.joining(","))));
+        showServerValue.setId("show-server-value");
+
+        var modeButtons = new Div(modePreserveAll, modePreserveExisting,
+                modeDiscard);
+        var actionButtons = new Div(refreshAll, removeItem5, showServerValue);
+
+        add(listBox, modeButtons, actionButtons, serverValue);
+    }
+}
