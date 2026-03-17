c55af60f1299368789bce4418af121ded0412165
Sascha Ißbrücker <sissbruecker@vaadin.com>
2026-03-17 14:38:51 +0100

    chore: convert board unit tests to JUnit 6 (#8933)

--- vaadin-board-flow-parent/vaadin-board-flow/src/test/java/com/vaadin/flow/component/board/BoardSerializableTest.java
+++ vaadin-board-flow-parent/vaadin-board-flow/src/test/java/com/vaadin/flow/component/board/BoardSerializableTest.java
@@ -10,5 +10,5 @@ package com.vaadin.flow.component.board;
 
 import com.vaadin.flow.testutil.ClassesSerializableTest;
 
-public class BoardSerializableTest extends ClassesSerializableTest {
+class BoardSerializableTest extends ClassesSerializableTest {
 }
