01a0a26b2cbd192ce3090e4983554755720a1b59
Diego Cardoso <diego@vaadin.com>
2026-03-24 13:37:02 +0100

    chore: migrate menu-bar unit tests from JUnit 4 to JUnit 6

--- vaadin-menu-bar-flow-parent/vaadin-menu-bar-flow/src/test/java/com/vaadin/flow/component/menubar/tests/MenuBarSerializableTest.java
+++ vaadin-menu-bar-flow-parent/vaadin-menu-bar-flow/src/test/java/com/vaadin/flow/component/menubar/tests/MenuBarSerializableTest.java
@@ -17,5 +17,5 @@ package com.vaadin.flow.component.menubar.tests;
 
 import com.vaadin.flow.testutil.ClassesSerializableTest;
 
-public class MenuBarSerializableTest extends ClassesSerializableTest {
+class MenuBarSerializableTest extends ClassesSerializableTest {
 }
