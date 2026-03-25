6dda067aeef9e981860c5def78c2041a6b97ab2c
Sascha Ißbrücker <sissbruecker@vaadin.com>
2026-03-25 14:07:01 +0100

    chore: convert accordion unit tests to JUnit 6 (#9010)

--- vaadin-accordion-flow-parent/vaadin-accordion-flow/src/test/java/com/vaadin/flow/component/accordion/AccordionSerializableTest.java
+++ vaadin-accordion-flow-parent/vaadin-accordion-flow/src/test/java/com/vaadin/flow/component/accordion/AccordionSerializableTest.java
@@ -17,5 +17,5 @@ package com.vaadin.flow.component.accordion;
 
 import com.vaadin.flow.testutil.ClassesSerializableTest;
 
-public class AccordionSerializableTest extends ClassesSerializableTest {
+class AccordionSerializableTest extends ClassesSerializableTest {
 }
