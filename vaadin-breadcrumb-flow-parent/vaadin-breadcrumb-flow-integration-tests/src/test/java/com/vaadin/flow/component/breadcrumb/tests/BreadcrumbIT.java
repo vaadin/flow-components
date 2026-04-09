60a19669a3921a7f9a8754a124187d207485e82b
Artur Signell <artur@vaadin.com>
2026-04-09 08:15:48 +0000

    feat: add Breadcrumb Flow component module scaffolding

---
+++ vaadin-breadcrumb-flow-parent/vaadin-breadcrumb-flow-integration-tests/src/test/java/com/vaadin/flow/component/breadcrumb/tests/BreadcrumbIT.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2000-2026 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.breadcrumb.tests;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.vaadin.flow.component.breadcrumb.testbench.BreadcrumbElement;
+import com.vaadin.flow.testutil.TestPath;
+import com.vaadin.tests.AbstractComponentIT;
+
+@TestPath("vaadin-breadcrumb")
+public class BreadcrumbIT extends AbstractComponentIT {
+
+    private BreadcrumbElement component;
+
+    @Before
+    public void init() {
+        open();
+        component = $(BreadcrumbElement.class).waitForFirst();
+    }
+
+    @Test
+    public void componentIsPresent() {
+        Assert.assertNotNull(component);
+    }
+}
---
+++ vaadin-breadcrumb-flow-parent/vaadin-breadcrumb-flow/src/main/java/com/vaadin/flow/component/breadcrumb/Breadcrumb.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2000-2026 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.breadcrumb;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.HasSize;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.dependency.NpmPackage;
+
+/**
+ * Breadcrumb is a component for ...
+ *
+ * @author Vaadin Ltd
+ */
+@Tag("vaadin-breadcrumb")
+@NpmPackage(value = "@vaadin/breadcrumb", version = "25.2.0-alpha6")
+@JsModule("@vaadin/breadcrumb/src/vaadin-breadcrumb.js")
+public class Breadcrumb extends Component implements HasSize {
+
+    /**
+     * Creates a new {@link Breadcrumb}.
+     */
+    public Breadcrumb() {
+    }
+}
