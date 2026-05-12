d1c6c0c144e603d0a11288cedf6bfd946810420f
Tatu Lund <tatu@vaadin.com>
2026-05-12 11:43:01 +0300

    fix: Make SpreadsheetOverlay native popover (#9241)
    Fixes #9240 
    
    Note1: Spreadsheet tabs initial alignment is wrong if the first tab is
    not the selected one when Spreadsheet is placed on Dialog. This is not
    regression from this PR, but a different bug that needs to be addressed
    separately.
    
    Note2: When Spreadsheet is in Dialog, inserting comment does not work.
    This is not regression from this PR, as inserting comment after this PR
    still works, when Spreadsheet is not in Dialog. This is different bug
    that needs to be address separately.
    
    - Actually these bugs happen also with Vaadin 24.10, so they are not
    regressions from Dialog's native popover refactor.

--- vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow-client/src/main/java/com/vaadin/addon/spreadsheet/client/SpreadsheetOverlay.java
+++ vaadin-spreadsheet-flow-parent/vaadin-spreadsheet-flow-client/src/main/java/com/vaadin/addon/spreadsheet/client/SpreadsheetOverlay.java
@@ -18,34 +18,65 @@ import com.vaadin.client.ui.VOverlay;
  * A VOverlay Implementation that attaches the overlay to the container added by
  * vaadin-spreadsheet webcomponent
  */
-@SuppressWarnings("deprecation")
+@SuppressWarnings({ "deprecation", "java:S1699" })
 public class SpreadsheetOverlay extends VOverlay {
+
+    private static final String POPOVER_ATTRIBUTE = "popover";
 
     /**
      * A VContextMenu Implementation that attaches the overlay to the container
      * added by vaadin-spreadsheet webcomponent
      */
     public static class SpreadsheetContextMenu extends VContextMenu {
+
         public SpreadsheetContextMenu() {
             DOM.setElementProperty(getElement(), "id", "PID_VAADIN_CM");
+            setPopover(getElement());
         }
 
         @Override
         public Element getOverlayContainer() {
             return getOverlayContainerElement();
         }
+
+        @Override
+        public void show() {
+            super.show();
+            showPopover(getElement());
+        }
+
+        @Override
+        public void showAt(int x, int y) {
+            super.showAt(x, y);
+            showPopover(getElement());
+        }
     }
 
     public SpreadsheetOverlay() {
         super();
+        setPopover(getElement());
     }
 
     public SpreadsheetOverlay(boolean autoHide, boolean modal) {
         super(autoHide, modal);
+        setPopover(getElement());
     }
 
     public SpreadsheetOverlay(boolean autoHide) {
         super(autoHide);
+        setPopover(getElement());
+    }
+
+    private static void setPopover(Element el) {
+        if (el != null) {
+            el.setAttribute(POPOVER_ATTRIBUTE, "manual");
+        }
+    }
+
+    @Override
+    public void show() {
+        super.show();
+        showPopover(getElement());
     }
 
     @Override
@@ -57,4 +88,14 @@ public class SpreadsheetOverlay extends VOverlay {
         Element overlays = DOM.getElementById("spreadsheet-overlays");
         return overlays == null ? RootPanel.getBodyElement() : overlays;
     }
+
+    // @formatter:off
+    private static native void showPopover(Element el) 
+    /*-{
+        var fn = el && el.showPopover;
+        if (typeof fn === "function") {
+            fn.call(el);
+        }
+    }-*/;
+    // @formatter:on
 }
