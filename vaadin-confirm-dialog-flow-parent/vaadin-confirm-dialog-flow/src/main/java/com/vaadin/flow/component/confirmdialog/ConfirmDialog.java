8d2d1cfb210ea78afe4909fc68a768d42f07f23d
Artur Signell <artur@vaadin.com>
2026-07-25 19:59:51 +0300

    Revert "chore: use dev-disable-dates web components snapshot"
    This reverts commit 2f5e32db4e818d96659d0782c40c8c3b38a8e87c.

--- vaadin-confirm-dialog-flow-parent/vaadin-confirm-dialog-flow/src/main/java/com/vaadin/flow/component/confirmdialog/ConfirmDialog.java
+++ vaadin-confirm-dialog-flow-parent/vaadin-confirm-dialog-flow/src/main/java/com/vaadin/flow/component/confirmdialog/ConfirmDialog.java
@@ -64,7 +64,7 @@ import com.vaadin.flow.signals.Signal;
  * @since 1.0
  */
 @Tag("vaadin-confirm-dialog")
-@NpmPackage(value = "@vaadin/confirm-dialog", version = "dev-disable-dates")
+@NpmPackage(value = "@vaadin/confirm-dialog", version = "25.3.0-alpha2")
 @JsModule("@vaadin/confirm-dialog/src/vaadin-confirm-dialog.js")
 @ModalRoot
 public class ConfirmDialog extends Component
