87d4f83cef2f13020b964df431903813e146973d
Serhii Kulykov <iamkulykov@gmail.com>
2026-09-26 15:09:08 +0300

    test: shorten comment

--- vaadin-grid-flow-parent/vaadin-grid-flow-integration-tests/src/test/java/com/vaadin/flow/component/grid/it/GridTooltipIT.java
+++ vaadin-grid-flow-parent/vaadin-grid-flow-integration-tests/src/test/java/com/vaadin/flow/component/grid/it/GridTooltipIT.java
@@ -45,8 +45,7 @@ public class GridTooltipIT extends AbstractComponentIT {
     @Test
     public void hoverOverColumnHeader_shouldNotProduceErrors() {
         scrollToElement(grid);
-        // Header cells have no item in their event context, which used to make
-        // the tooltip generator throw (see issue #10247)
+        // Grid event context does not provide item for header cells
         showTooltip(grid.getHeaderCell(0));
         checkLogsForErrors();
     }
